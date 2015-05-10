406824efe04437684a7d2e6ddd116887e282cb32
sqlboy <mvchambers@me.com>
2015-05-10 14:59:09 -0700

    Created an abstract test class which will serve as the base class for all unit tests.

--- Archivist/src/test/java/com/zorroa/archivist/ArchivistApplicationTests.java
+++ Archivist/src/test/java/com/zorroa/archivist/ArchivistApplicationTests.java
@@ -1,18 +1,24 @@
 package com.zorroa.archivist;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = ArchivistApplication.class)
 @WebAppConfiguration
-public class ArchivistApplicationTests {
+public abstract class ArchivistApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    public static final Logger logger = LoggerFactory.getLogger(ArchivistConfiguration.class);
 
+    public String getStaticImagePath() {
+        FileSystemResource resource = new FileSystemResource("src/test/resources/static/images");
+        String path = resource.getFile().getAbsolutePath();
+        logger.info("test image path: {}", path);
+        return path;
+    }
 }
