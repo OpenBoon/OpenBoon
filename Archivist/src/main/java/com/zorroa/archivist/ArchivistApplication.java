119b0a0b4f1144a5bc210fc0861206d50f9c7182
sqlboy <mvchambers@me.com>
2015-05-10 09:00:31 -0700

    Initial commit of a standard barebones maven based project.

---
+++ Archivist/src/main/java/com/zorroa/archivist/ArchivistApplication.java
@@ -0,0 +1,12 @@
+package com.zorroa.archivist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchivistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchivistApplication.class, args);
+    }
+}
