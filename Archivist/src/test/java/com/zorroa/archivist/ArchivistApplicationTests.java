774cb9560de33a7bb9110561788e5b134016909c
sqlboy <mvchambers@me.com>
2015-05-10 09:08:54 -0700

    Fixed a broken refactor/rename.

--- Archivist/src/test/java/com/zorroa/archivist/ArchivistApplicationTests.java
+++ Archivist/src/test/java/com/zorroa/archivist/ArchivistApplicationTests.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = ArchivistApplication.class)
 @WebAppConfiguration
-public class ZorroaArchivistApplicationTests {
+public class ArchivistApplicationTests {
 
 	@Test
 	public void contextLoads() {
