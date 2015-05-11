d8d2f4795c3df4e02e8b50fd1d9029d09ea9a3fe
sqlboy <mvchambers@me.com>
2015-05-11 07:30:41 -0700

    Added @Before function to all tests that deletes elasticsearch

--- Archivist/src/test/java/com/zorroa/archivist/ArchivistApplicationTests.java
+++ Archivist/src/test/java/com/zorroa/archivist/ArchivistApplicationTests.java
@@ -1,8 +1,13 @@
 package com.zorroa.archivist;
 
+import org.elasticsearch.client.Client;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -15,10 +20,26 @@ public abstract class ArchivistApplicationTests {
 
     public static final Logger logger = LoggerFactory.getLogger(ArchivistConfiguration.class);
 
+    @Autowired
+    protected Client client;
+
+    @Value("${archivist.index.alias}")
+    protected String alias;
+
     public String getStaticImagePath() {
         FileSystemResource resource = new FileSystemResource("src/test/resources/static/images");
         String path = resource.getFile().getAbsolutePath();
         logger.info("test image path: {}", path);
         return path;
+    }
+
+    public void refreshIndex() {
+        client.admin().indices().prepareRefresh(alias).get();
+    }
+
+    @Before
+    public void clearIndex() {
+        client.prepareDeleteByQuery(alias).setQuery(QueryBuilders.matchAllQuery()).get();
+        refreshIndex();
     }
 }
