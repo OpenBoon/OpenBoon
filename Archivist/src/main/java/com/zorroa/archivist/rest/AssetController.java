f24239619c6a54c483113832f664b8bd3a730cae
sqlboy <mvchambers@me.com>
2015-05-21 10:19:16 -0400

    Added REST endpoint for doing asset searchingAdded REST endpoint for doing asset searching. Added REST endpoint for doing asset searches.  This just passes the request directly to elastic, but eventually we'll add security filters to the query.

---
+++ Archivist/src/main/java/com/zorroa/archivist/rest/AssetController.java
@@ -0,0 +1,45 @@
+package com.zorroa.archivist.rest;
+
+import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.search.SearchRequestBuilder;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.Client;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
+
+@RestController
+public class AssetController {
+
+    @Value("${archivist.index.alias}")
+    private String alias;
+
+    @Autowired
+    Client client;
+
+
+    @RequestMapping(value="/assets/_search", method=RequestMethod.GET)
+    public DeferredResult<String> search(@RequestBody String query) {
+        SearchRequestBuilder builder = client.prepareSearch(alias)
+                .setTypes("asset")
+                .setSource(query);
+
+        DeferredResult<String> result = new DeferredResult<String>();
+        builder.execute(new ActionListener<SearchResponse>() {
+            @Override
+            public void onResponse(SearchResponse response) {
+                result.setResult(response.toString());
+            }
+            @Override
+            public void onFailure(Throwable e) {
+                result.setErrorResult(e);
+            }
+        });
+
+        return result;
+    }
+}
