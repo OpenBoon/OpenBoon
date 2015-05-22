9d0abfedd33f4d48ea82c8b01776c4b55e004e7d
sqlboy <mvchambers@me.com>
2015-05-22 15:55:25 -0400

    Removed spring cloud as a dependency, not giving us anything.

--- Archivist/src/main/java/com/zorroa/archivist/ArchivistApplication.java
+++ Archivist/src/main/java/com/zorroa/archivist/ArchivistApplication.java
@@ -3,11 +3,13 @@ package com.zorroa.archivist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan
 @EnableAutoConfiguration
 @SpringBootApplication
+@EnableConfigurationProperties
 public class ArchivistApplication {
 
     public static void main(String[] args) {
