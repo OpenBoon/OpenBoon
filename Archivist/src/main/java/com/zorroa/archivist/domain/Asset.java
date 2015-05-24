5c2d629a97dfc31d58a883474887ac79aa1fda83
sqlboy <mvchambers@me.com>
2015-05-24 15:37:58 -0400

    Added a convinience method to Asset for pulling values of out the elastic document.

--- Archivist/src/main/java/com/zorroa/archivist/domain/Asset.java
+++ Archivist/src/main/java/com/zorroa/archivist/domain/Asset.java
@@ -2,15 +2,15 @@ package com.zorroa.archivist.domain;
 
 import java.util.Map;
 
+import com.google.common.base.Splitter;
+
 public class Asset {
 
     private String id;
     private long version;
-    private Map<String,Object> data;
+    private Map<String,Object> document;
 
-    public Asset() {
-
-    }
+    public Asset() { }
 
     public String getId() {
         return id;
@@ -28,11 +28,25 @@ public class Asset {
         this.version = version;
     }
 
-    public Map<String, Object> getData() {
-        return data;
+    public Map<String, Object> getDocument() {
+        return document;
     }
 
-    public void setData(Map<String, Object> data) {
-        this.data = data;
+    public void setDocument(Map<String, Object> data) {
+        this.document = data;
+    }
+
+    public String getString(String key) {
+        Map<String, Object> map = getMap(key.substring(0, key.lastIndexOf('.')));
+        return (String) map.get(key.substring(key.lastIndexOf('.')+1));
+    }
+
+    private Map<String,Object> getMap(String key) {
+        // TODO: this might not always be a map.
+        Map<String,Object> current = null;
+        for (String e: Splitter.on('.').split(key)) {
+            current = (Map<String,Object>) document.get(e);
+        }
+        return current;
     }
 }
---
+++ Archivist/src/test/java/com/zorroa/archivist/repository/AssetDaoTests.java
@@ -0,0 +1,40 @@
+package com.zorroa.archivist.repository;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.zorroa.archivist.ArchivistApplicationTests;
+import com.zorroa.archivist.domain.Asset;
+import com.zorroa.archivist.domain.AssetBuilder;
+
+public class AssetDaoTests extends ArchivistApplicationTests {
+
+    @Autowired
+    AssetDao assetDao;
+
+    @Test
+    public void testCreateAndGet() {
+        AssetBuilder builder = new AssetBuilder(getTestImage("beer_kettle_01.jpg"));
+        String id = assetDao.create(builder);
+        refreshIndex(100);
+
+        Asset asset = assetDao.get(id);
+        assertEquals("jpg", asset.getString("source.extension"));
+        assertEquals(getStaticImagePath(), asset.getString("source.directory"));
+        assertEquals(getStaticImagePath() + "/beer_kettle_01.jpg", asset.getString("source.path"));
+        assertEquals("beer_kettle_01.jpg", asset.getString("source.filename"));
+
+    }
+
+    @Test
+    public void testGetAll() {
+        AssetBuilder builder = new AssetBuilder(getTestImage("beer_kettle_01.jpg"));
+        assetDao.create(builder);
+        refreshIndex(100);
+
+        assertEquals(1, assetDao.getAll().size());
+    }
+
+}
