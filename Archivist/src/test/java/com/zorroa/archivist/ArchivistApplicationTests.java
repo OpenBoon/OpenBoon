ec4a74fb0e1a299616b7ce336dccaad4b27b6499
sqlboy <mvchambers@me.com>
2015-05-24 09:14:18 -0400

    Added a function for sleep+refresh elastic.

--- Archivist/src/test/java/com/zorroa/archivist/ArchivistApplicationTests.java
+++ Archivist/src/test/java/com/zorroa/archivist/ArchivistApplicationTests.java
@@ -50,4 +50,20 @@ public abstract class ArchivistApplicationTests {
     public void refreshIndex() {
         client.admin().indices().prepareRefresh(alias).get();
     }
+
+    public void refreshIndex(long sleep) {
+        try {
+            Thread.sleep(sleep/2);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        client.admin().indices().prepareRefresh(alias).get();
+        try {
+            Thread.sleep(sleep/2);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 }
