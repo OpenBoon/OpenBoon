855c1347d8299812309c720ecf19e4a4d100fb42
sqlboy <mvchambers@me.com>
2015-05-29 13:16:05 -0400

    Added HTTP endpoint for proxies.  This changes how an asset stores the proxies to not expose the full path, just the file name.
    To access a proxy, hit the proxy/image endpoint, with the name of the proxt file which
    is included in the asset record.
    http://localhost:8066/proxy/image/0003eb3b-bc17-4e9c-b96f-4aead3289fee.png

---
+++ Archivist/src/main/java/com/zorroa/archivist/rest/ProxyController.java
@@ -0,0 +1,92 @@
+package com.zorroa.archivist.rest;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.common.io.ByteStreams;
+import com.zorroa.archivist.service.ImageService;
+
+/**
+ * Provides endpoints for downloading proxy images.
+ *
+ * @author chambers
+ *
+ */
+@Controller
+public class ProxyController {
+
+    private static final int CACHE_MAX_SIZE = 1000;
+    private static final int CACHE_TIMEOUT_MINUTES = 60;
+
+    @Autowired
+    ImageService imageService;
+
+    private final LoadingCache<String, ProxyImage> proxyCache = CacheBuilder.newBuilder()
+        .maximumSize(CACHE_MAX_SIZE)
+        .expireAfterWrite(CACHE_TIMEOUT_MINUTES, TimeUnit.MINUTES)
+        .build(new CacheLoader<String, ProxyImage>() {
+         public ProxyImage load(String key) throws Exception {
+             return loadProxyImage(key);
+         }
+    });
+
+    @RequestMapping(value = "proxy/image/{id:.+}", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<byte[]> getProxy(@PathVariable String id) throws ExecutionException {
+        ProxyImage image = proxyCache.get(id);
+        return ResponseEntity.ok()
+                .contentLength(image.size)
+                .contentType(image.type)
+                .body(image.content);
+    }
+
+    @ExceptionHandler(ExecutionException.class)
+    public ResponseEntity<byte[]> brokenImage() throws IOException {
+        byte[] bytes = ByteStreams.toByteArray(
+                getClass().getResourceAsStream( "/broken128.png"));
+        return ResponseEntity.ok()
+                .contentLength(bytes.length)
+                .contentType(MediaType.IMAGE_PNG)
+                .body(bytes);
+    }
+
+    private ProxyImage loadProxyImage(String id) throws IOException {
+        String[] e = id.split("\\.");
+        return ProxyImage.load(imageService.generateProxyPath(e[0], e[1]));
+    }
+
+    private static class ProxyImage {
+        public byte[] content;
+        public final MediaType type = MediaType.IMAGE_PNG;
+        public long size;
+
+        public static final ProxyImage load(File file) throws IOException {
+            RandomAccessFile f = new RandomAccessFile(file, "r");
+            try {
+                ProxyImage result = new ProxyImage();
+                result.content = new byte[(int)f.length()];
+                f.readFully(result.content);
+                result.size = f.length();
+                return result;
+            } finally {
+                f.close();
+            }
+        }
+    }
+}
--- Archivist/src/test/java/com/zorroa/archivist/service/ImageServiceTests.java
+++ Archivist/src/test/java/com/zorroa/archivist/service/ImageServiceTests.java
@@ -5,25 +5,35 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.google.common.base.Splitter;
 import com.zorroa.archivist.ArchivistApplicationTests;
 import com.zorroa.archivist.domain.Proxy;
 import com.zorroa.archivist.domain.ProxyOutput;
 
-public class ProxyServiceTests extends ArchivistApplicationTests {
+public class ImageServiceTests extends ArchivistApplicationTests {
 
     @Autowired
-    ImageService proxyService;
+    ImageService imageService;
 
     @Test
     public void testMakeProxy() throws IOException {
         File original = this.getTestImage("beer_kettle_01.jpg");
         ProxyOutput output = new ProxyOutput("png", 128, 8);
-        Proxy proxy = proxyService.makeProxy(original, output);
+        Proxy proxy = imageService.makeProxy(original, output);
         assertEquals(128, proxy.getHeight());
-        assertTrue(new File(proxy.getPath()).exists());
+        List<String> e = Splitter.on('.').limit(2).splitToList(proxy.getFile());
+        assertTrue(imageService.generateProxyPath(e.get(0), e.get(1)).exists());
+    }
+
+    @Test
+    public void testGenerateProxyPath() throws IOException {
+        String id = "ABCD1234-ABCD1234-ABCD1234-ABCD1234";
+        File path = imageService.generateProxyPath(id, "png");
+        logger.info(path.getAbsolutePath());
     }
 }
