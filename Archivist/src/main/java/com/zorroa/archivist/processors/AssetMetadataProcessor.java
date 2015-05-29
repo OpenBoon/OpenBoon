a15f7d1de215ef5c55482c25e758fa171e07f0a4
sqlboy <mvchambers@me.com>
2015-05-29 10:37:21 -0400

    Broke apart some functions in the AssetMetadataProcessor.

--- Archivist/src/main/java/com/zorroa/archivist/processors/AssetMetadataProcessor.java
+++ Archivist/src/main/java/com/zorroa/archivist/processors/AssetMetadataProcessor.java
@@ -23,15 +23,17 @@ public class AssetMetadataProcessor extends IngestProcessor {
 
     private static final Logger logger = LoggerFactory.getLogger(AssetMetadataProcessor.class);
 
-    public AssetMetadataProcessor() {
-        // TODO Auto-generated constructor stub
-    }
+    public AssetMetadataProcessor() { }
 
     @Override
     public void process(AssetBuilder asset) {
         if (isImageType(asset)) {
+            /*
+             * Depending on how configurable we want this to be, we might end up having
+             * to split these into separate classes.
+             */
             extractDimensions(asset);
-            extractExifData(asset);
+            extractImageData(asset);
         }
     }
 
@@ -45,42 +47,68 @@ public class AssetMetadataProcessor extends IngestProcessor {
         }
     }
 
-    public void extractExifData(AssetBuilder asset) {
+    /**
+     * Handles pulling metadata out of the image itself, either by
+     * EXIF, EXR header, DPX header, etc.  Currently only supports
+     * EXIF.
+     *
+     * @param asset
+     */
+    public void extractImageData(AssetBuilder asset) {
 
-         try {
-             Metadata metadata = ImageMetadataReader.readMetadata(asset.getFile());
-             ExifSubIFDDirectory d = metadata.getFirstDirectoryOfType(ExifSubIFDDirectory.class);
-             if (d != null) {
-                 asset.put("exif", "compression", d.getString(ExifSubIFDDirectory.TAG_COMPRESSION));
-                 asset.put("exif", "aperture", d.getString(ExifSubIFDDirectory.TAG_APERTURE));
-             }
-             else {
-                 logger.warn("Exif metdata not found: {}", asset.getAbsolutePath());
-             }
-
-             IptcDirectory i = metadata.getFirstDirectoryOfType(IptcDirectory.class);
-             if (i != null) {
-                 asset.put("iptc", "keywords", i.getString(IptcDirectory.TAG_KEYWORDS));
-             }
-             else {
-                 logger.warn("Iptc metdata not found: {}", asset.getAbsolutePath());
-             }
-
-             /*
-             for (Directory dir: metadata.getDirectories()) {
-                 for (Tag tag: dir.getTags()) {
-                     logger.info("{}", tag);
-                 }
-             }
+        /*
+         * Attempt to process EXIF data.
+         */
+        try {
+            /*
+             * Reuse the metadata object.
              */
-         } catch (Exception e) {
-             logger.error("Failed to process EXIF data:", e);
+            Metadata metadata = ImageMetadataReader.readMetadata(asset.getFile());
+            extractExifData(asset, metadata);
+            extractIptcData(asset, metadata);
+        }
+        catch (Exception e) {
+            logger.error("Failed to load EXIF data, unexpected " + e, e);
+        }
+    }
+
+    /*
+     * Eventually refactor these metadata processors to be configuration based.
+     */
+
+    private void extractExifData(AssetBuilder asset, Metadata metadata) {
+
+         ExifSubIFDDirectory d = metadata.getFirstDirectoryOfType(ExifSubIFDDirectory.class);
+         if (d != null) {
+             asset.put("exif", "compression", d.getString(ExifSubIFDDirectory.TAG_COMPRESSION));
+             asset.put("exif", "aperture", d.getString(ExifSubIFDDirectory.TAG_APERTURE));
          }
+         else {
+             logger.warn("Exif metdata not found: {}", asset.getAbsolutePath());
+         }
+
+         /*
+          * Example of how to just dump all data.
+          *
+         for (Directory dir: metadata.getDirectories()) {
+             for (Tag tag: dir.getTags()) {
+                 logger.info("{}", tag);
+             }
+         }
+         */
+    }
+
+    private void extractIptcData(AssetBuilder asset, Metadata metadata) {
+        IptcDirectory i = metadata.getFirstDirectoryOfType(IptcDirectory.class);
+        if (i != null) {
+            asset.put("iptc", "keywords", i.getString(IptcDirectory.TAG_KEYWORDS));
+        }
+        else {
+            logger.warn("Iptc metdata not found: {}", asset.getAbsolutePath());
+        }
     }
 
     public boolean isImageType(AssetBuilder asset) {
         return imageService.getSupportedFormats().contains(asset.getExtension());
     }
-
-
 }
