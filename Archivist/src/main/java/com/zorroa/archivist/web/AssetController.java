5c340f83d7cc88d99cbac3c0cfe962a538df0d39
sqlboy <mvchambers@me.com>
2015-06-01 11:05:52 -0400

    Added _count endpoint to AssetController

--- Archivist/src/main/java/com/zorroa/archivist/web/AssetController.java
+++ Archivist/src/main/java/com/zorroa/archivist/web/AssetController.java
@@ -1,6 +1,10 @@
 package com.zorroa.archivist.web;
 
+import java.security.Principal;
+
 import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.count.CountRequestBuilder;
+import org.elasticsearch.action.count.CountResponse;
 import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.Client;
@@ -22,7 +26,7 @@ public class AssetController {
     Client client;
 
     @RequestMapping(value="/assets/_search", method=RequestMethod.GET)
-    public DeferredResult<String> search(@RequestBody String query) {
+    public DeferredResult<String> search(@RequestBody String query, Principal principle) {
         SearchRequestBuilder builder = client.prepareSearch(alias)
                 .setTypes("asset")
                 .setSource(query);
@@ -31,6 +35,27 @@ public class AssetController {
         builder.execute(new ActionListener<SearchResponse>() {
             @Override
             public void onResponse(SearchResponse response) {
+                result.setResult(response.toString());
+            }
+            @Override
+            public void onFailure(Throwable e) {
+                result.setErrorResult(e);
+            }
+        });
+
+        return result;
+    }
+
+    @RequestMapping(value="/assets/_count", method=RequestMethod.GET)
+    public DeferredResult<String> count(@RequestBody String query, Principal principle) {
+        CountRequestBuilder builder = client.prepareCount(alias)
+                .setTypes("asset")
+                .setSource(query.getBytes());
+
+        DeferredResult<String> result = new DeferredResult<String>();
+        builder.execute(new ActionListener<CountResponse>() {
+            @Override
+            public void onResponse(CountResponse response) {
                 result.setResult(response.toString());
             }
             @Override
