4d158aed63fbfaad8db715eae6a6141b69194e24
sqlboy <mvchambers@me.com>
2015-06-01 06:44:07 -0400

    Converted tabs to spaces in a couple files.

--- Archivist/src/main/java/com/zorroa/archivist/ArchivistClusterStateException.java
+++ Archivist/src/main/java/com/zorroa/archivist/ArchivistClusterStateException.java
@@ -2,29 +2,29 @@ package com.zorroa.archivist;
 
 public class ArchivistClusterStateException extends ArchivistException {
 
-	public ArchivistClusterStateException() {
-		// TODO Auto-generated constructor stub
-	}
+    public ArchivistClusterStateException() {
+        // TODO Auto-generated constructor stub
+    }
 
-	public ArchivistClusterStateException(String message) {
-		super(message);
-		// TODO Auto-generated constructor stub
-	}
+    public ArchivistClusterStateException(String message) {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
 
-	public ArchivistClusterStateException(Throwable cause) {
-		super(cause);
-		// TODO Auto-generated constructor stub
-	}
+    public ArchivistClusterStateException(Throwable cause) {
+        super(cause);
+        // TODO Auto-generated constructor stub
+    }
 
-	public ArchivistClusterStateException(String message, Throwable cause) {
-		super(message, cause);
-		// TODO Auto-generated constructor stub
-	}
+    public ArchivistClusterStateException(String message, Throwable cause) {
+        super(message, cause);
+        // TODO Auto-generated constructor stub
+    }
 
-	public ArchivistClusterStateException(String message, Throwable cause,
-			boolean enableSuppression, boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
-	}
+    public ArchivistClusterStateException(String message, Throwable cause,
+            boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+        // TODO Auto-generated constructor stub
+    }
 
 }
