e9d9c59bd2092038a05810ec29f2dcac928e4c1b
sqlboy <mvchambers@me.com>
2015-06-06 11:41:55 -0400

    Added a utility class with some standard role constants.

---
+++ Archivist/src/main/java/com/zorroa/archivist/domain/StandardRoles.java
@@ -0,0 +1,10 @@
+package com.zorroa.archivist.domain;
+
+public class StandardRoles {
+
+    public static final String ADMIN_ROLE = "ADMIN_ROLE";
+    public static final String USER_ROLE = "USER_ROLE";
+
+
+
+}
