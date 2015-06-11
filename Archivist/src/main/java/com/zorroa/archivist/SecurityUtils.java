25a8cfcaaadd0c5640a52b3ddbf21676c322350a
Matt Chambers <mvchambers@me.com>
2015-06-11 18:33:41 -0400

    Added util function for getting the current authenticated user's session id.

--- Archivist/src/main/java/com/zorroa/archivist/SecurityUtils.java
+++ Archivist/src/main/java/com/zorroa/archivist/SecurityUtils.java
@@ -1,6 +1,7 @@
 package com.zorroa.archivist;
 
 import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.web.context.request.RequestContextHolder;
 
 public class SecurityUtils {
 
@@ -8,4 +9,7 @@ public class SecurityUtils {
         return BCrypt.hashpw(plainPassword, BCrypt.gensalt());
     }
 
+    public static String getSessionId() {
+        return RequestContextHolder.currentRequestAttributes().getSessionId();
+    }
 }
