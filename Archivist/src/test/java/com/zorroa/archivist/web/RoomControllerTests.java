0beaf366f62ce5d150828da09f2e596eb30e7bf4
Matt Chambers <mvchambers@me.com>
2015-06-11 22:35:00 -0400

    Follow up commit to rooms which adds REST endpoints for geting all rooms, getting a room, joining a room, etc.  Still need to add the members of the room as part of the room Object.

---
+++ Archivist/src/test/java/com/zorroa/archivist/web/RoomControllerTests.java
@@ -0,0 +1,113 @@
+package com.zorroa.archivist.web;
+
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.web.servlet.MvcResult;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.google.common.collect.Sets;
+import com.zorroa.archivist.Json;
+import com.zorroa.archivist.domain.Room;
+import com.zorroa.archivist.domain.RoomBuilder;
+import com.zorroa.archivist.service.RoomService;
+
+public class RoomControllerTests extends MockMvcTest {
+
+    @Autowired
+    RoomController roomController;
+
+    @Autowired
+    RoomService roomService;
+
+    @Test
+    public void testCreate() throws Exception {
+        RoomBuilder bld = new RoomBuilder();
+        bld.setName("A Room");
+        bld.setVisible(true);
+        bld.setInviteList(Sets.newHashSet("mvchambers@me.com"));
+
+        MvcResult result = mvc.perform(post("/rooms")
+                 .session(admin())
+                 .contentType(MediaType.APPLICATION_JSON_VALUE)
+                 .content(Json.serialize(bld)))
+                 .andExpect(status().isOk())
+                 .andReturn();
+
+        Room room = Json.deserialize(result.getResponse().getContentAsByteArray(), Room.class);
+        assertEquals(bld.getName(), room.getName());
+        assertEquals(bld.getInviteList(), room.getInviteList());
+    }
+
+    @Test
+    public void testGetAll() throws Exception {
+
+        for (int i=0; i<10; i++) {
+            RoomBuilder bld = new RoomBuilder();
+            bld.setName("Room #" + i);
+            bld.setVisible(true);
+            roomService.create(bld);
+        }
+
+        MvcResult result = mvc.perform(get("/rooms")
+                 .session(admin())
+                 .contentType(MediaType.APPLICATION_JSON_VALUE))
+                 .andExpect(status().isOk())
+                 .andReturn();
+
+        List<Room> rooms = Json.Mapper.readValue(
+                result.getResponse().getContentAsByteArray(), new TypeReference<List<Room>>(){});
+        assertEquals(10, rooms.size());
+    }
+
+    @Test
+    public void testJoin() throws Exception {
+
+        MockHttpSession session = admin();
+
+        for (int i=0; i<10; i++) {
+            RoomBuilder bld = new RoomBuilder();
+            bld.setName("Room #" + i);
+            bld.setVisible(true);
+            Room room = roomService.create(bld);
+
+            mvc.perform(put("/rooms/" + room.getId() + "/_join")
+                    .session(session)
+                    .contentType(MediaType.APPLICATION_JSON_VALUE))
+                    .andExpect(status().isOk())
+                    .andReturn();
+
+            Room room2 = roomService.getActiveRoom(session.getId());
+            assertEquals(room.getId(), room2.getId());
+        }
+    }
+
+    @Test
+    public void testGet() throws Exception {
+
+        RoomBuilder bld = new RoomBuilder();
+        bld.setName("A Room");
+        bld.setVisible(true);
+        bld.setInviteList(Sets.newHashSet("mvchambers@me.com"));
+        Room room1 = roomService.create(bld);
+
+        MvcResult result = mvc.perform(get("/rooms/" + room1.getId())
+                .session(admin())
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        Room room2 = Json.deserialize(result.getResponse().getContentAsByteArray(), Room.class);
+        assertEquals(room1.getName(), room2.getName());
+        assertEquals(room1.getInviteList(), room2.getInviteList());
+    }
+}
