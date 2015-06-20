c83d5241c4e27847e1cbae43b42dc2ffb1a16a79
Matt Chambers <mvchambers@me.com>
2015-06-20 07:11:51 -0400

    Unused imports.

--- Archivist/src/main/java/com/zorroa/archivist/processors/ProxyProcessor.java
+++ Archivist/src/main/java/com/zorroa/archivist/processors/ProxyProcessor.java
@@ -4,7 +4,6 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.Comparator;
--- Archivist/src/test/java/com/zorroa/archivist/processor/ProxyProcessorTests.java
+++ Archivist/src/test/java/com/zorroa/archivist/processor/ProxyProcessorTests.java
@@ -1,6 +1,7 @@
 package com.zorroa.archivist.processor;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 import java.util.Map;
