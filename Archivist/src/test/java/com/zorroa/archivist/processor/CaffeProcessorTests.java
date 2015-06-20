200cb0fbcc35e67fb9a22c07d15b719e9203daca
Dan Wexler <wex@zorroa.com>
2015-06-20 10:50:13 -0700

    Fix CaffeProcessor test.

--- Archivist/src/test/java/com/zorroa/archivist/processor/CaffeProcessorTests.java
+++ Archivist/src/test/java/com/zorroa/archivist/processor/CaffeProcessorTests.java
@@ -1,14 +1,21 @@
 package com.zorroa.archivist.processor;
 
+import static org.junit.Assert.*;
+
 import com.zorroa.archivist.ArchivistApplicationTests;
-import com.zorroa.archivist.domain.IngestBuilder;
-import com.zorroa.archivist.domain.IngestPipeline;
-import com.zorroa.archivist.domain.IngestPipelineBuilder;
-import com.zorroa.archivist.domain.IngestProcessorFactory;
+import com.zorroa.archivist.domain.*;
+import com.zorroa.archivist.repository.AssetDao;
 import com.zorroa.archivist.repository.IngestPipelineDao;
+import com.zorroa.archivist.service.IngestSchedulerService;
 import com.zorroa.archivist.service.IngestService;
+import org.elasticsearch.common.collect.Maps;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 public class CaffeProcessorTests extends ArchivistApplicationTests {
 
@@ -18,20 +25,30 @@ public class CaffeProcessorTests extends ArchivistApplicationTests {
     @Autowired
     IngestService ingestService;
 
+    @Autowired
+    IngestSchedulerService ingestSchedulerService;
+
+    @Autowired
+    AssetDao assetDao;
+
     @Test
     public void testProcess() throws InterruptedException {
 
+
+        Map<String, Object> args = Maps.newHashMap();
+
         IngestPipelineBuilder builder = new IngestPipelineBuilder();
-        builder.setName("caffe");
-        builder.addToProcessors(new IngestProcessorFactory("com.zorroa.archivist.processors.CaffeProcessor", null));
+        builder.setName("test");
+        builder.addToProcessors(
+                new IngestProcessorFactory("com.zorroa.archivist.processors.CaffeProcessor", args));
         ingestPipelineDao.create(builder);
 
-        IngestPipeline pipeline = ingestService.createIngestPipeline(builder);
-        ingestService.createIngest(new IngestBuilder(getStaticImagePath()));
-
-        Thread.sleep(1000);
+        Ingest ingest = ingestService.createIngest(new IngestBuilder(getStaticImagePath()).setPipeline("test"));
+        ingestSchedulerService.executeIngest(ingest);
         refreshIndex();
 
-        // TODO need callback or API in place to check the ingest works
+        List<Asset> assets = assetDao.getAll();
+        assertTrue(((String)((ArrayList)((LinkedHashMap) assets.get(0).getDocument().get("caffe")).get("keywords")).get(0)).equals("lakeside"));
+        assertTrue(((String)((ArrayList)((LinkedHashMap) assets.get(1).getDocument().get("caffe")).get("keywords")).get(0)).equals("barber chair"));
     }
 }
