d2d710e7d102e5180260677aaec1d79864e7d842
Matt Chambers <mvchambers@me.com>
2015-07-03 19:54:26 -0400

    Added a murmur3 128bit checksum processor, which adds some checksum data.  The next step is to build an ingest pipeline that just checks for files that have changed.

---
+++ Archivist/src/main/java/com/zorroa/archivist/processors/ChecksumProcessor.java
@@ -0,0 +1,25 @@
+package com.zorroa.archivist.processors;
+
+import com.google.common.hash.Hashing;
+import com.google.common.io.Files;
+import com.zorroa.archivist.domain.AssetBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class ChecksumProcessor extends IngestProcessor {
+
+    private static final Logger logger = LoggerFactory.getLogger(AssetMetadataProcessor.class);
+
+    @Override
+    public void process(AssetBuilder asset) {
+        try {
+            byte[] bytes = Files.toByteArray(asset.getFile());
+            asset.put("source", "hash", Hashing.murmur3_128().hashBytes(bytes).toString());
+        } catch (IOException e) {
+            logger.warn("Failed to calculate CRC for file '{}'", asset.getAbsolutePath(), e);
+        }
+    }
+
+}
--- Archivist/src/test/java/com/zorroa/archivist/processors/ChecksumProcessorTest.java
+++ Archivist/src/test/java/com/zorroa/archivist/processors/ChecksumProcessorTest.java
@@ -1,10 +1,68 @@
 package com.zorroa.archivist.processors;
 
+import com.zorroa.archivist.ArchivistApplicationTests;
+import com.zorroa.archivist.domain.*;
+import com.zorroa.archivist.repository.AssetDao;
+import com.zorroa.archivist.repository.IngestPipelineDao;
+import com.zorroa.archivist.service.IngestSchedulerService;
+import com.zorroa.archivist.service.IngestService;
 import junit.framework.TestCase;
+import org.elasticsearch.common.collect.Maps;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by chambers on 7/3/15.
  */
-public class ChecksumProcessorTest extends TestCase {
+public class ChecksumProcessorTest extends ArchivistApplicationTests {
 
+    @Autowired
+    IngestPipelineDao ingestPipelineDao;
+
+    @Autowired
+    IngestService ingestService;
+
+    @Autowired
+    IngestSchedulerService ingestSchedulerService;
+
+    @Autowired
+    AssetDao assetDao;
+
+    @Test
+    public void testProcess() throws InterruptedException {
+
+        Map<String, Object> args = Maps.newHashMap();
+
+        IngestPipelineBuilder builder = new IngestPipelineBuilder();
+        builder.setName("test");
+        builder.addToProcessors(
+                new IngestProcessorFactory("com.zorroa.archivist.processors.ChecksumProcessor", args));
+        ingestPipelineDao.create(builder);
+
+        Ingest ingest = ingestService.createIngest(new IngestBuilder(getStaticImagePath()).setPipeline("test"));
+        ingestSchedulerService.executeIngest(ingest);
+        refreshIndex();
+
+        List<Asset> assets = assetDao.getAll();
+        assertEquals(2, assets.size());
+
+        for (Asset asset: assets) {
+            String path = asset.getValue("source.path");
+            String crc32 = asset.getValue("source.hash");
+
+            if (path.contains("beer_kettle_01.jpg")) {
+                assertEquals(crc32, "9faa728d41dfb2c9416fb7c7fc6ad77d");
+            }
+
+            if (path.contains("new_zealand_wellington_harbour.jpg")) {
+                assertEquals(crc32, "306a7de6f3d2da7bb4cd540e2cbfba8e");
+            }
+        }
+    }
 }
