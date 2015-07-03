837e560a0b0d80d64627c0d843bf720c954b2a68
Matt Chambers <mvchambers@me.com>
2015-07-03 19:52:09 -0400

    Small re-org. Removed cluster service and moved ProxyProcessorTests to the correct package.

---
+++ Archivist/src/test/java/com/zorroa/archivist/processors/ChecksumProcessorTest.java
@@ -0,0 +1,10 @@
+package com.zorroa.archivist.processors;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by chambers on 7/3/15.
+ */
+public class ChecksumProcessorTest extends TestCase {
+
+}
--- Archivist/src/test/java/com/zorroa/archivist/processors/ProxyProcessorTests.java
+++ Archivist/src/test/java/com/zorroa/archivist/processors/ProxyProcessorTests.java
@@ -1,4 +1,4 @@
-package com.zorroa.archivist.processor;
+package com.zorroa.archivist.processors;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
