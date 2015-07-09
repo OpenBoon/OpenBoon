f0af432a276735b0f66a4e7d26fb32c0ffea314b
Dan Wexler <wex@zorroa.com>
2015-07-09 09:50:54 -0700

    Move class loader code from SDK to Archivist.

--- Archivist/src/main/java/com/zorroa/archivist/domain/IngestProcessorFactory.java
+++ Archivist/src/main/java/com/zorroa/archivist/domain/IngestProcessorFactory.java
@@ -1,10 +1,15 @@
 package com.zorroa.archivist.domain;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
 import java.lang.reflect.InvocationTargetException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
 import java.util.Map;
 
-import com.zorroa.archivist.service.IngestProcessorServiceImpl;
+import com.zorroa.archivist.sdk.AssetBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,8 +27,6 @@ public class IngestProcessorFactory implements Serializable {
 
     private transient IngestProcessor processor = null;
 
-    public static ClassLoader classLoader;
-
     public IngestProcessorFactory() { }
 
     public IngestProcessorFactory(Class<?> klass) {
@@ -31,7 +34,56 @@ public class IngestProcessorFactory implements Serializable {
         this.args = Maps.newHashMap();
     }
 
+    private static ClassLoader classLoader = null;
+
+    public ClassLoader getSiteClassLoader() {
+        if (classLoader == null) {
+            classLoader = AssetBuilder.class.getClassLoader();
+            // Create an array of URLs to search as a classpath when loading processors
+            URL[] zorroaJarURLs = null;
+            Map<String, String> env = System.getenv();
+            String sitePath = env.get("ZORROA_SITE_PATH");
+            if (sitePath == null) {
+                logger.warn("ZORROA_SITE_PATH is not set.");
+                classLoader = AssetBuilder.class.getClassLoader();
+            } else {
+                File folder = new File(sitePath);
+                if (!folder.exists()) {
+                    logger.warn("Invalid ZORROA_SITE_PATH: " + sitePath);
+                    classLoader = AssetBuilder.class.getClassLoader();
+                } else {
+                    File[] listOfFiles = folder.listFiles();
+                    ArrayList<URL> urls = new ArrayList<URL>();
+                    for (int i = 0; i < listOfFiles.length; i++) {
+                        if (listOfFiles[i].isFile()) {
+                            String path = listOfFiles[i].getAbsolutePath();
+                            try {
+                                String ext = path.substring(path.lastIndexOf('.') + 1).toLowerCase();
+                                if (ext.equals("jar")) {
+                                    try {
+                                        URL url = new File(path).toURI().toURL();
+                                        urls.add(url);
+                                    } catch (IOException e) {
+                                        logger.error("Cannot create URL to Zorroa jar file " + path);
+                                        e.printStackTrace();
+                                    }
+                                }
+                            } catch (java.lang.StringIndexOutOfBoundsException e) {
+                            }
+                        }
+                    }
+                    zorroaJarURLs = urls.toArray(new URL[urls.size()]);
+                    classLoader = new URLClassLoader(zorroaJarURLs, AssetBuilder.class.getClassLoader());
+                }
+            }
+        }
+        return classLoader;
+    }
+
     public IngestProcessor init() {
+        if (classLoader == null) {
+            classLoader = getSiteClassLoader();
+        }
         if (processor == null) {
             try {
                 Class<?> pclass = classLoader.loadClass(klass);
