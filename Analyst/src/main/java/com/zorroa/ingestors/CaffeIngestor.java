f78f39167664bd71c9c343df9a7a041375e5e3a5
Dan Wexler <wex@zorroa.com>
2015-08-03 13:55:05 -0700

    Use GPU in Caffe and better failure checks.

--- Analyst/src/main/java/com/zorroa/ingestors/CaffeIngestor.java
+++ Analyst/src/main/java/com/zorroa/ingestors/CaffeIngestor.java
@@ -1,16 +1,15 @@
 package com.zorroa.ingestors;
 
-import java.io.File;
-import java.util.List;
-import java.util.Arrays;
-import java.util.Map;
-
+import com.zorroa.archivist.sdk.AssetBuilder;
 import com.zorroa.archivist.sdk.IngestProcessor;
+import com.zorroa.archivist.sdk.Proxy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.zorroa.archivist.sdk.AssetBuilder;
-import com.zorroa.archivist.sdk.Proxy;
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -58,28 +57,30 @@ public class CaffeIngestor extends IngestProcessor {
 
     @Override
     public void process(AssetBuilder asset) {
-        if (ingestProcessorService.isImage(asset)) {
-            // Start with the original image, but try to find a proxy to use for classification
-            String classifyPath = asset.getFile().getPath();
-            List<Proxy> proxyList = (List<Proxy>) asset.document.get("proxies");
-            if (proxyList == null) {
-                logger.error("Cannot find proxy list");
-            } else {
-                for (Proxy proxy : proxyList) {
-                    if (proxy.getWidth() >= 256 || proxy.getHeight() >= 256) {
-                        String proxyName = proxy.getFile();
-                        proxyName = proxyName.substring(0, proxyName.lastIndexOf('.'));
-                        classifyPath = ingestProcessorService.getProxyFile(proxyName, "png").getPath();
-                        break;
-                    }
-                }
-            }
-            String keywords = classify(nativeCaffeClassifier, classifyPath);
-            logger.info("CaffeIngestor: " + keywords);
-            List<String> keywordList = Arrays.asList(keywords.split(","));
-            asset.map("caffe", "keywords", "type", "string");
-            asset.map("caffe", "keywords", "copy_to", null);
-            asset.put("caffe", "keywords", keywordList);
+        if (!ingestProcessorService.isImage(asset)) {
+            return;     // Only process images
         }
+
+        List<Proxy> proxyList = (List<Proxy>) asset.document.get("proxies");
+        if (proxyList == null) {
+            logger.error("Cannot find proxy list for " + asset.getFilename() + ", skipping Caffe analysis.");
+            return;     // No proxies implies bad image file, which crashes Caffe
+        }
+
+        String classifyPath = asset.getFile().getPath();
+        for (Proxy proxy : proxyList) {
+            if (proxy.getWidth() >= 256 || proxy.getHeight() >= 256) {
+                String proxyName = proxy.getFile();
+                proxyName = proxyName.substring(0, proxyName.lastIndexOf('.'));
+                classifyPath = ingestProcessorService.getProxyFile(proxyName, "png").getPath();
+                break;
+            }
+        }
+        String keywords = classify(nativeCaffeClassifier, classifyPath);
+        logger.info("CaffeIngestor: " + keywords);
+        List<String> keywordList = Arrays.asList(keywords.split(","));
+        asset.map("caffe", "keywords", "type", "string");
+        asset.map("caffe", "keywords", "copy_to", null);
+        asset.put("caffe", "keywords", keywordList);
     }
 }
