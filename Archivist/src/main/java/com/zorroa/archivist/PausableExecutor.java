2ae24b6f35a02db64f453f6f2bf65aba160f732b
Dan Wexler <wex@zorroa.com>
2015-08-06 11:02:40 -0700

    IngestProcessor.teardown to cleanup JNI perthread data

--- Archivist/src/main/java/com/zorroa/archivist/PausableExecutor.java
+++ Archivist/src/main/java/com/zorroa/archivist/PausableExecutor.java
@@ -3,10 +3,18 @@ package com.zorroa.archivist;
 /**
  * Created by chambers on 7/31/15.
  */
-import java.util.concurrent.*;
+
+import com.zorroa.archivist.domain.IngestProcessorFactory;
+
+import java.util.List;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class PausableExecutor extends ThreadPoolExecutor {
     private final Continue cont = new Continue();
+
+    private List<IngestProcessorFactory> processors;
 
     public PausableExecutor(int threads) {
         super(threads, threads, 10, TimeUnit.SECONDS, new LinkedBlockingQueue<>());
@@ -20,6 +28,14 @@ public class PausableExecutor extends ThreadPoolExecutor {
         cont.resume();
     }
 
+    public List<IngestProcessorFactory> getProcessors() {
+        return processors;
+    }
+
+    public void setProcessors(List<IngestProcessorFactory> processors) {
+        this.processors = processors;
+    }
+
     protected void beforeExecute(Thread t, Runnable r) {
         try {
             cont.checkIn();
@@ -27,5 +43,17 @@ public class PausableExecutor extends ThreadPoolExecutor {
             return;
         }
         super.beforeExecute(t, r);
+    }
+
+    protected void afterExecute(Runnable r, Throwable t) {
+        super.afterExecute(r, t);
+        if (t != null) {
+            // Teardown processors if we have an exception during execution
+            for (IngestProcessorFactory processorFactory : processors) {
+                if (processorFactory.getProcessor() != null) {
+                    processorFactory.getProcessor().teardown();
+                }
+            }
+        }
     }
 }
