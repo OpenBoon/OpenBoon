c68b4c02a9799c3d7eb1601b6bd27806aff44ffa
Matt Chambers <mvchambers@me.com>
2015-09-04 14:39:52 -0400

    Added /health check URL

--- Archivist/src/main/java/com/zorroa/archivist/web/WebSecurityConfig.java
+++ Archivist/src/main/java/com/zorroa/archivist/web/WebSecurityConfig.java
@@ -38,6 +38,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http
             .authorizeRequests()
                 .antMatchers("/").permitAll()
+                .antMatchers("/health/**").permitAll()
                 .anyRequest().authenticated()
             .and()
                 .httpBasic()
