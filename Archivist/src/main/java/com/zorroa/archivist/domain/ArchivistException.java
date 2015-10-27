928d4ce336788778b7c9261ff2e802117dbadcbb
Matt Chambers <chambers@Matts-MBP.home>
2015-10-27 14:39:45 -0400

    Adding a few common exceptions we can use to send down errors.

--- Archivist/src/main/java/com/zorroa/archivist/domain/ArchivistException.java
+++ Archivist/src/main/java/com/zorroa/archivist/domain/ArchivistException.java
@@ -3,5 +3,21 @@ package com.zorroa.archivist.domain;
 /**
  * Created by chambers on 10/27/15.
  */
-public class ArchivistException {
+public class ArchivistException extends RuntimeException {
+
+    public ArchivistException() {
+        super();
+    }
+
+    public ArchivistException(String message) {
+        super(message);
+    }
+
+    public ArchivistException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ArchivistException(Throwable cause) {
+        super(cause);
+    }
 }
