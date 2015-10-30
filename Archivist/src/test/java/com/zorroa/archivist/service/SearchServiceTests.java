92ae8984665d09a6fbcb91ecf98415b24de5b23e
Matt Chambers <chambers@Matts-MBP.home>
2015-10-30 15:53:38 -0400

    Added SearchService tests for checking if permission filtering works.

---
+++ Archivist/src/test/java/com/zorroa/archivist/service/SearchServiceTests.java
@@ -0,0 +1,71 @@
+package com.zorroa.archivist.service;
+
+import com.google.common.io.Files;
+import com.zorroa.archivist.ArchivistApplicationTests;
+import com.zorroa.archivist.domain.AssetSearchBuilder;
+import com.zorroa.archivist.repository.AssetDao;
+import com.zorroa.archivist.repository.UserDao;
+import com.zorroa.archivist.sdk.domain.AssetBuilder;
+import com.zorroa.archivist.sdk.domain.Asset;
+import com.zorroa.archivist.sdk.domain.Permission;
+import com.zorroa.archivist.sdk.domain.PermissionBuilder;
+import com.zorroa.archivist.sdk.service.UserService;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by chambers on 10/30/15.
+ */
+public class SearchServiceTests extends ArchivistApplicationTests {
+
+
+    @Autowired
+    AssetDao assetDao;
+
+    @Autowired
+    SearchService searchService;
+
+    @Autowired
+    UserService userService;
+
+    @Test
+    public void testSearchPermissionsMiss() throws IOException {
+
+        String filename = "captain_america.jpg";
+        String filepath = "/tmp/" + filename;
+        Files.touch(new File(filepath));
+
+        Permission perm = userService.createPermission(new PermissionBuilder().setName("test").setDescription("test"));
+
+        AssetBuilder builder = new AssetBuilder(filepath);
+        builder.setSearchPermissions(perm);
+        Asset asset1 = assetDao.create(builder);
+        refreshIndex(100);
+
+        assertEquals(0, searchService.search(
+                new AssetSearchBuilder().setQuery("captain")).getHits().getTotalHits());
+
+    }
+
+    @Test
+    public void testSearchPermissionsHit() throws IOException {
+
+        String filename = "captain_america.jpg";
+        String filepath = "/tmp/" + filename;
+        Files.touch(new File(filepath));
+
+        AssetBuilder builder = new AssetBuilder(filepath);
+        builder.setSearchPermissions(userService.getPermissions().get(0));
+        Asset asset1 = assetDao.create(builder);
+        refreshIndex(100);
+
+        assertEquals(1, searchService.search(
+                new AssetSearchBuilder().setQuery("captain")).getHits().getTotalHits());
+
+    }
+}
