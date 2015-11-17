e747bada1bc822e8f61f2e36449a53a8c16ae0bf
Matt Chambers <mvchambers@me.com>
2015-11-17 12:07:05 -0500

    Added ExportService function to get all outputs.

--- ArchivistSDK/src/main/java/com/zorroa/archivist/sdk/service/ExportService.java
+++ ArchivistSDK/src/main/java/com/zorroa/archivist/sdk/service/ExportService.java
@@ -2,6 +2,9 @@ package com.zorroa.archivist.sdk.service;
 
 import com.zorroa.archivist.sdk.domain.Export;
 import com.zorroa.archivist.sdk.domain.ExportBuilder;
+import com.zorroa.archivist.sdk.domain.ExportOutput;
+
+import java.util.List;
 
 /**
  * Created by chambers on 11/1/15.
@@ -11,4 +14,6 @@ public interface ExportService {
     Export create(ExportBuilder builder);
 
     Export get(int id);
+
+    List<ExportOutput> getAllOutputs(Export export);
 }
