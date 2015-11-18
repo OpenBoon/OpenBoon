5148ec33495bf6f8c778e3cc350c4dc760957184
Matt Chambers <mvchambers@me.com>
2015-11-18 10:26:12 -0500

    Added some export REST endpoints.

---
+++ Archivist/src/main/java/com/zorroa/archivist/web/ExportController.java
@@ -0,0 +1,48 @@
+package com.zorroa.archivist.web;
+
+import com.zorroa.archivist.sdk.domain.Export;
+import com.zorroa.archivist.sdk.domain.ExportBuilder;
+import com.zorroa.archivist.sdk.domain.ExportFilter;
+import com.zorroa.archivist.sdk.domain.ExportOutput;
+import com.zorroa.archivist.sdk.service.ExportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * Created by chambers on 11/13/15.
+ */
+@RestController
+public class ExportController {
+
+    @Autowired
+    ExportService exportService;
+
+    @PreAuthorize("hasRole('export')")
+    @RequestMapping(value="/api/v1/exports", method= RequestMethod.POST)
+    public Export create(@RequestBody ExportBuilder builder) {
+        return exportService.create(builder);
+    }
+
+    @PreAuthorize("hasRole('export')")
+    @RequestMapping(value="/api/v1/exports/{id}", method=RequestMethod.GET)
+    public Export get(@PathVariable int id) {
+        return exportService.get(id);
+    }
+
+
+    @PreAuthorize("hasRole('export')")
+    @RequestMapping(value="/api/v1/exports/_search", method=RequestMethod.POST)
+    public List<Export> getAll(@RequestBody ExportFilter filter) {
+        return exportService.getAll(filter);
+    }
+
+    @PreAuthorize("hasRole('export')")
+    @RequestMapping(value="/api/v1/exports/{id}/outputs", method=RequestMethod.GET)
+    public List<ExportOutput> getAllOutputs(@PathVariable int id) {
+        Export export = exportService.get(id);
+        return exportService.getAllOutputs(export);
+    }
+}
---
+++ Archivist/src/test/java/com/zorroa/archivist/web/ExportControllerTests.java
@@ -0,0 +1,137 @@
+package com.zorroa.archivist.web;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.google.common.collect.Lists;
+import com.zorroa.archivist.sdk.domain.*;
+import com.zorroa.archivist.sdk.processor.ProcessorFactory;
+import com.zorroa.archivist.sdk.processor.export.ExportProcessor;
+import com.zorroa.archivist.sdk.service.ExportService;
+import com.zorroa.archivist.sdk.service.IngestService;
+import com.zorroa.archivist.sdk.util.Json;
+import com.zorroa.archivist.service.IngestExecutorService;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Created by chambers on 11/17/15.
+ */
+public class ExportControllerTests  extends MockMvcTest {
+
+    @Autowired
+    ExportService exportService;
+
+    @Autowired
+    IngestService ingestService;
+
+    @Autowired
+    IngestExecutorService ingestExecutorService;
+
+    Export export;
+
+    ExportBuilder builder;
+
+    @Before
+    public void init() {
+
+        Ingest ingest = ingestService.createIngest(new IngestBuilder(getStaticImagePath()));
+        ingestExecutorService.executeIngest(ingest);
+        refreshIndex(1000);
+
+        ExportOptions options = new ExportOptions();
+        options.getImages().setFormat("jpg");
+        options.getImages().setScale(.5);
+
+        AssetSearchBuilder search = new AssetSearchBuilder();
+        search.setQuery("beer");
+
+        ProcessorFactory<ExportProcessor> outputFactory = new ProcessorFactory<>();
+        outputFactory.setKlass("com.zorroa.archivist.sdk.processor.export.ZipFileExport");
+
+        builder = new ExportBuilder();
+        builder.setNote("An export for Bob");
+        builder.setOptions(options);
+        builder.setSearch(search);
+        builder.setOutputs(Lists.newArrayList(outputFactory));
+
+        export = exportService.create(builder);
+    }
+
+    @Test
+    public void testGetAllByFilter() throws Exception {
+        MockHttpSession session = admin();
+        ExportFilter filter = new ExportFilter();
+
+        MvcResult result = mvc.perform(post("/api/v1/exports/_search")
+                .session(session)
+                .content(Json.serialize(filter))
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        List<Export> exports = Json.Mapper.readValue(result.getResponse().getContentAsString(),
+                new TypeReference<List<Export>>() {});
+        assertEquals(1, exports.size());
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+        MockHttpSession session = admin();
+
+        MvcResult result = mvc.perform(post("/api/v1/exports")
+                .session(session)
+                .content(Json.serialize(builder))
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        Export export2 = Json.Mapper.readValue(result.getResponse().getContentAsString(), Export.class);
+        assertNotEquals(export.getId(), export2.getId());
+        assertEquals("beer", export2.getSearch().getQuery());
+        assertEquals(export2.getNote(), export.getNote());
+    }
+
+    @Test
+    public void testGet() throws Exception {
+        MockHttpSession session = admin();
+
+        MvcResult result = mvc.perform(get("/api/v1/exports/" + export.getId())
+                .session(session)
+                .content(Json.serialize(builder))
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        Export export2 = Json.Mapper.readValue(result.getResponse().getContentAsString(), Export.class);
+        assertEquals(export.getId(), export2.getId());
+        assertEquals("beer", export2.getSearch().getQuery());
+        assertEquals(export2.getNote(), export.getNote());
+    }
+
+    @Test
+    public void testGetAllOutputs() throws Exception {
+        MockHttpSession session = admin();
+
+        MvcResult result = mvc.perform(get("/api/v1/exports/" + export.getId() + "/outputs")
+                .session(session)
+                .content(Json.serialize(builder))
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        List<ExportOutput> outputs = Json.Mapper.readValue(result.getResponse().getContentAsString(),
+                new TypeReference<List<ExportOutput>>() { });
+        assertEquals(1, outputs.size());
+    }
+}
