d6d51695dfd0c2a9d197fdc79a60dce6174d445d
Matt Chambers <mvchambers@me.com>
2015-11-25 23:36:42 -0500

    Added Schema based versions of AssetMetadataProcessor and ProxyProcessor.  The orignals I left untouched.  Eventually these classes would replace them.

---
+++ Archivist/src/main/java/com/zorroa/archivist/processors/SchemaAssetMetadataProcessor.java
@@ -0,0 +1,226 @@
+package com.zorroa.archivist.processors;
+
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.lang.Rational;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.Tag;
+import com.drew.metadata.exif.GpsDirectory;
+import com.google.common.collect.*;
+import com.zorroa.archivist.sdk.domain.AssetBuilder;
+import com.zorroa.archivist.sdk.processor.ingest.IngestProcessor;
+import com.zorroa.archivist.sdk.schema.ImageSchema;
+import org.elasticsearch.common.joda.time.format.DateTimeFormat;
+import org.elasticsearch.common.joda.time.format.DateTimeFormatter;
+import org.elasticsearch.common.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.awt.geom.Point2D;
+import java.lang.reflect.Array;
+import java.util.*;
+
+/**
+ *
+ * Extracts metadata from all types of supported files.
+ * Base data that we create is added to the "source" namespace.
+ *
+ * @author chambers
+ *
+ */
+public class SchemaAssetMetadataProcessor extends IngestProcessor {
+
+    private static final Logger logger = LoggerFactory.getLogger(SchemaAssetMetadataProcessor.class);
+
+    public SchemaAssetMetadataProcessor() { }
+
+    /**
+     * Extract asset metadata. Copy some fields to the default search
+     * and suggestion field, and create a "source" namespace with dimensions,
+     * location, and date information if available.
+     *
+     * Information extracted from the file headers are added to namespaces
+     * such as "Exif", "IPTC" and others. Field and namespaces consist of
+     * [A-Za-z0-9] characters, with no spaces, dashes or other characters.
+     *
+     * Arguments to this processor allow you to specify an ordered list
+     * of fields to search for a master date field (dateTags=[String]) and
+     * a list of fields to copy to the default search and suggestion field
+     * (keywordTags=[String]). The components of these fields are specified
+     * as, e.g., ["Exif.UserComment", "IPTC.Keywords"].
+     *
+     * @param asset
+     */
+    @Override
+    public void process(AssetBuilder asset) {
+        switch(asset.getSource().getType()) {
+            case Image:
+                asset.addSchema(new ImageSchema());
+                extractImageData(asset);
+                break;
+            default:
+                return;
+        }
+    }
+
+    /**
+     * Handles pulling metadata out of the image itself, either by
+     * EXIF, EXR header, DPX header, etc.  Currently only supports
+     * EXIF.
+     *
+     * @param asset
+     */
+    public void extractImageData(AssetBuilder asset) {
+        /*
+         * Extract all metadata fields into the format <directory>:<tag>=<value>,
+         * and store two versions of the value, the original value, and an optional
+         * .description variant which is more human-readable. Tag names are stored
+         * using a string containing only [A-Za-a0-9] characters, with spaces, dashes
+         * and other characters removed. This results in the EXIF-standard names,
+         * though some formats (e.g. IPTC) contain tags with '/' or '-', (ugh).
+         * Some tags have multiple names, which we'll handle later.
+         */
+        try {
+            Metadata metadata = ImageMetadataReader.readMetadata(asset.getFile());
+            extractExifMetadata(asset, metadata);   // Extract all useful metadata fields in raw & descriptive format
+            extractExifLocation(asset, metadata);   // Find the best location value and promote to top-level
+        } catch (Exception e) {
+            logger.error("Failed to load metadata: " + e.getMessage());
+            asset.put("errors", "SchemaAssetMetadataProcessor", e.getMessage());
+        }
+    }
+
+    /**
+     * The default set of tags for building the keyword list.
+     */
+    private static final Set<String> defaultKeywordTags = ImmutableSet.<String>builder()
+            .add("Exif.UserComment")
+            .add("Exif.ColorSpace")
+            .add("Exif.Make")
+            .add("Exif.Model")
+            .add("IPTC.Keywords")
+            .add("IPTC.CopyrightNotice")
+            .add("IPTC.Source")
+            .add("IPTC.City")
+            .add("IPTC.ProvinceState")
+            .add("IPTC.CountryPrimaryLocationName")
+            .add("File.Filename")
+            .add("Xmp.Lens")
+            .build();
+
+    private static final List<String> defaultDateArgs = ImmutableList.<String>builder()
+            .add("Exif.DateTimeOriginal")
+            .add("Exif.DateTimeDigitized")
+            .add("Exif.DateTime")
+            .add("IPTC.DateCreated")
+            .add("IPTC.TimeCreated")
+            .add("File.FileModifiedDate")
+            .build();
+
+    private static final DateTimeFormatter extraDateFormatter = DateTimeFormat.forPattern("yyyy:MM:dd HH:mm:ss");
+
+    private void extractExifMetadata(AssetBuilder asset, Metadata metadata) {
+
+        Set<String> keywordArgs = getArgs().containsKey("keywordTags") ?
+            ImmutableSet.copyOf((List<String>) getArgs().get("keywordTags")) : defaultKeywordTags;
+
+        List<String> dateArgs = getArgs().containsKey("dateTags") ?
+                (List<String>)getArgs().get("dateTags") : defaultDateArgs;
+
+        /**
+         * Set our mostValidDateField to a value outside the possible range.
+         * If we find a date field less than this value we use it.  If we then
+         * find one even further up the list, we use that.
+         */
+        int mostValidDateField = defaultDateArgs.size() + 1;
+
+        for (Directory directory : metadata.getDirectories()) {
+            String namespace = directory.getName().split(" ", 2)[0];
+            for (Tag tag : directory.getTags()) {
+                Object value = directory.getObject(tag.getTagType());
+                if (value == null) {
+                    continue;
+                }
+                String key = tag.getTagName().replaceAll("[^A-Za-z0-9]", "");
+                String id = namespace + "." + key;
+
+                /*
+                 * Handle string formatted dates
+                 */
+                Date date = directory.getDate(tag.getTagType());
+                if (date == null && value instanceof String) {
+                    try {
+                        DateTime dt = extraDateFormatter.parseDateTime((String)value);
+                        date = dt.toDate();
+                    } catch (IllegalArgumentException e) {
+                        /*
+                         * It wasn't a date, just ignore
+                         */
+                    }
+                }
+
+                /*
+                 * Check for the most valid date.
+                 */
+                if (date != null) {
+                    int dateFieldPriority = dateArgs.indexOf(id);
+                    if (dateFieldPriority < mostValidDateField) {
+                        mostValidDateField = dateFieldPriority;
+                        asset.getSource().setDate(date);
+                        continue;
+                    }
+                }
+
+                /*
+                 * Check for special data types that need to be handled, otherwise
+                 * just add the data to the object
+                 */
+                if (value instanceof String) {
+                    asset.setAttr(namespace, key, (String)value, keywordArgs.contains(id) ? 5 : 0);
+                } else if (value instanceof Rational) {
+                    Rational rational = (Rational)value;
+                    asset.setAttr(namespace, key, rational.doubleValue());
+                } else if (value.getClass().isArray()) {
+                    String componentName = value.getClass().getComponentType().getName();
+                    if (componentName.equals("java.lang.String")) {
+                        String[] strList = (String[])value;
+                        asset.setAttr(namespace, key, strList, keywordArgs.contains(id) ? 5 : 0);
+                    } else if (componentName.equals("com.drew.lang.Rational")) {
+                        Rational[] rationals = (Rational[]) value;
+                        Double[] doubles = new Double[rationals.length];
+                        for (int i = 0; i < rationals.length; i++) {
+                            doubles[i] = rationals[i].doubleValue();
+                        }
+                        asset.setAttr(namespace, key, doubles);
+                    } else if (componentName.equals("byte") && Array.getLength(value) <= 16) {
+                        asset.setAttr(namespace, key, value);
+                    } else {
+                        asset.setAttr(namespace, key, value);
+                    }
+                } else {
+                    asset.setAttr(namespace, key, value);
+                }
+            }
+        }
+    }
+
+    private static double dmsToDegrees(int d, int m, int s) {
+        return Math.signum(d) * (Math.abs(d) + (m / 60.0) + (s / 3600.0));
+    }
+
+    private void extractExifLocation(AssetBuilder asset, Metadata metadata) {
+        Directory exifDirectory = metadata.getFirstDirectoryOfType(GpsDirectory.class);
+        if (exifDirectory != null) {
+            int[] latitude = exifDirectory.getIntArray(GpsDirectory.TAG_LATITUDE);
+            int[] longitude = exifDirectory.getIntArray(GpsDirectory.TAG_LONGITUDE);
+            if (latitude != null && longitude != null) {
+                String latitudeRef = exifDirectory.getString(GpsDirectory.TAG_LATITUDE_REF);
+                String longitudeRef = exifDirectory.getString(GpsDirectory.TAG_LONGITUDE_REF);
+                double lat = dmsToDegrees(latitude[0], latitude[1], latitude[2]);
+                double lon = dmsToDegrees(longitude[0], longitude[1], longitude[2]);
+                Point2D.Double location = new Point2D.Double(lat, lon);
+                asset.getSchema("image", ImageSchema.class).setLocation(location);
+            }
+        }
+    }
+}
---
+++ Archivist/src/main/java/com/zorroa/archivist/processors/SchemaProxyProcessor.java
@@ -0,0 +1,145 @@
+package com.zorroa.archivist.processors;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zorroa.archivist.sdk.domain.AssetBuilder;
+import com.zorroa.archivist.sdk.domain.Proxy;
+import com.zorroa.archivist.sdk.domain.ProxyOutput;
+import com.zorroa.archivist.sdk.processor.ingest.IngestProcessor;
+import com.zorroa.archivist.sdk.schema.ImageSchema;
+import com.zorroa.archivist.sdk.service.ImageService;
+import org.elasticsearch.common.collect.ImmutableList;
+import org.elasticsearch.common.collect.Lists;
+import org.elasticsearch.common.primitives.Ints;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class SchemaProxyProcessor extends IngestProcessor {
+
+    private static final Logger logger = LoggerFactory.getLogger(SchemaProxyProcessor.class);
+
+    @Autowired
+    protected ImageService imageService;
+
+    public SchemaProxyProcessor() { }
+
+    protected  List<ProxyOutput> parseProxyOutput(String key) {
+        try {   // Re-parse from generic args Map to List<ProxyOutput>
+            ObjectMapper mapper = new ObjectMapper();
+            Object proxyList = getArgs().get("proxies");
+            return mapper.readValue(mapper.writeValueAsString(proxyList), new TypeReference<List<ProxyOutput>>(){});
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public void process(AssetBuilder asset) {
+        List<ProxyOutput> outputs = parseProxyOutput("proxies");
+        if (outputs == null) {
+            String format = imageService.getDefaultProxyFormat();
+            outputs = Lists.newArrayList(
+                    new ProxyOutput(format, 128, 8, 0.5f),
+                    new ProxyOutput(format, 256, 8, 0.7f),
+                    new ProxyOutput(format, 1024, 8, 0.9f)
+            );
+        }
+        if (asset.isImage()) {
+            extractDimensions(asset);
+            int width = asset.getAttr("image", "width");
+            List<Proxy> result = Lists.newArrayList();
+            for (ProxyOutput output : outputs) {
+                if (output.getSize() < width) {
+                    addResult(asset, output, result);
+                } else {
+                    if (result.size() == 0) {
+                        // No proxies generated, copy the source file as a proxy
+                        // but use a lower quality and the standard proxy format
+                        String format = imageService.getDefaultProxyFormat();
+                        ProxyOutput sourceProxy = new ProxyOutput(format, width, 8, 0.5f);
+                        addResult(asset, sourceProxy, result);
+                    }
+                    break;
+                }
+            }
+
+            if (!result.isEmpty()) {
+                Collections.sort(result, (o1, o2) ->
+                        Ints.compare(o1.getWidth() * o1.getHeight(), o2.getWidth() * o2.getHeight()));
+
+                asset.getDocument().put("tinyProxy", makeTinyProxy(result.get(0)));
+                asset.getDocument().put("proxies", result);
+            }
+        }
+    }
+
+    private void addResult(AssetBuilder asset, ProxyOutput output, List<Proxy> result) {
+        try {
+            result.add(imageService.makeProxy(asset.getFile(), output));
+        } catch (IOException e) {
+            logger.warn("Failed to create proxy {}: " + e.getMessage(), output);
+            asset.put("errors", "SchemaProxyProcessor", e.getMessage());
+        }
+    }
+
+    private void extractDimensions(AssetBuilder asset) {
+        try {
+            Dimension size = imageService.getImageDimensions(asset.getFile());
+            ImageSchema schema = asset.getSchema("image", ImageSchema.class);
+            schema.setWidth(size.width);
+            schema.setHeight(size.height);
+        } catch (IOException e) {
+            logger.warn("Unable to determine image dimensions: {}: " + e.getMessage(), asset);
+            asset.put("source", "error", "ProxyDimensions");
+        }
+    }
+
+    private static final List<String> NO_TINY_PROXY = ImmutableList.of(
+            "#FF0000", "#FFFFFF", "#FF0000",
+            "#FFFFFF", "#FF0000", "#FFFFFF",
+            "#FF0000", "#FFFFFF", "#FF0000");
+
+    private List<String> makeTinyProxy(Proxy smallest) {
+        try {
+            // Create a 3x3 proxy, avoid borders and blurring by downsampling
+            // to an 11x11 image, ignoring the outer frame, and taking the
+            // center pixel of each 3x3 block.
+            File proxyFile = new File(smallest.getPath());
+            BufferedImage source = ImageIO.read(proxyFile);
+            BufferedImage tinyImage = new BufferedImage(11, 11, BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g2 = tinyImage.createGraphics();
+            g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+            g2.drawImage(source, 0, 0, 11, 11, null);
+            g2.dispose();
+
+            List<String> colors = Lists.newArrayListWithCapacity(9);
+            for (int y = 0; y < 3; y++) {
+                for (int x = 0; x < 3; x++) {
+                    Color c = new Color(tinyImage.getRGB(x * 3 + 2, y * 3 + 2));
+                    colors.add(String.format("#%02x%02x%02x", c.getRed(),c.getGreen(),c.getBlue()));
+                }
+            }
+
+            return colors;
+
+        } catch (IOException e) {
+            logger.warn("Failed to create tiny proxy of " + smallest.getPath() + "," + e, e);
+        }
+
+        return NO_TINY_PROXY;
+    }
+}
