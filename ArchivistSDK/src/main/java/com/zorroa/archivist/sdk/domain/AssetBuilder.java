80cd084badf6aeb31ce513d9a6e26995433909b5
Matt Chambers <mvchambers@me.com>
2015-11-28 20:02:33 -0500

    Ignore keywords with less than 1 confidence.

--- ArchivistSDK/src/main/java/com/zorroa/archivist/sdk/domain/AssetBuilder.java
+++ ArchivistSDK/src/main/java/com/zorroa/archivist/sdk/domain/AssetBuilder.java
@@ -114,11 +114,17 @@ public class AssetBuilder {
 
     }
     public AssetBuilder addKeywords(int confidence, boolean suggest, String ... words) {
+        if (confidence < 1) {
+            return this;
+        }
         keywords.addKeywords(confidence, suggest, words);
         return this;
     }
 
     public AssetBuilder addKeywords(int confidence, boolean suggest, Collection<String> words) {
+        if (confidence < 1) {
+            return this;
+        }
         keywords.addKeywords(confidence, suggest, words.toArray(new String[] {}));
         return this;
     }
