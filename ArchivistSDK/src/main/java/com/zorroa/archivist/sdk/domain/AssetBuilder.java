24fb0cc2cd3e3fc62f67321ae1a9803231c51fd8
Matt Chambers <mvchambers@me.com>
2015-11-28 20:10:23 -0500

    Just use the non-varg version of setAttr.

--- ArchivistSDK/src/main/java/com/zorroa/archivist/sdk/domain/AssetBuilder.java
+++ ArchivistSDK/src/main/java/com/zorroa/archivist/sdk/domain/AssetBuilder.java
@@ -137,13 +137,13 @@ public class AssetBuilder {
         return keywords;
     }
 
-    public void setAttr(String namespace, String key, Object ... value) {
+    public void setAttr(String namespace, String key, Object[] values) {
         AttrSchema schema = (AttrSchema) this.schemas.get(namespace);
         if (schema == null) {
             schema = new AttrSchema(namespace);
             addSchema(schema);
         }
-        schema.setAttr(key, value);
+        schema.setAttr(key, values);
     }
 
 
