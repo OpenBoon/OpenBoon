0cb1d8491c56b984528ac13301f9782024333f26
Matt Chambers <mvchambers@me.com>
2015-11-30 10:51:21 -0500

    Bump to 0.10.3 on a change to the FolderService API.

--- ArchivistSDK/src/main/java/com/zorroa/archivist/sdk/service/FolderService.java
+++ ArchivistSDK/src/main/java/com/zorroa/archivist/sdk/service/FolderService.java
@@ -3,6 +3,7 @@ package com.zorroa.archivist.sdk.service;
 import com.zorroa.archivist.sdk.domain.Folder;
 import com.zorroa.archivist.sdk.domain.FolderBuilder;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
@@ -16,7 +17,15 @@ public interface FolderService {
 
     List<Folder> getAllDecendents(Folder folder);
 
-    Set<String> getAllDecendentIds(List<String> folderIds);
+    /**
+     * Returns a Set of all downstream folder Ids starting from the given startFolderIds.  If
+     * includeStartFolder is set to true, the starting folder IDs are included in the result.
+     *
+     * @param startFolderIds
+     * @param includeStartFolders
+     * @return
+     */
+    Set<String> getAllDescendantIds(Collection<String> startFolderIds, boolean includeStartFolders);
 
     Folder create(FolderBuilder builder);
 
