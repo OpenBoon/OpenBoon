e1cdb252e7d97243b244e2e9794e13354283cede
Matt Chambers <mvchambers@me.com>
2015-12-01 11:50:54 -0500

    FolderService: changes to folder service needed to accomdate smart queries.

--- ArchivistSDK/src/main/java/com/zorroa/archivist/sdk/service/FolderService.java
+++ ArchivistSDK/src/main/java/com/zorroa/archivist/sdk/service/FolderService.java
@@ -12,26 +12,34 @@ public interface FolderService {
     Folder get(String id);
 
     List<Folder> getAll();
-    
+
+    /**
+     * Return all folders with the given unique IDs.
+     *
+     * @param ids
+     * @return
+     */
+    List<Folder> getAll(Collection<String> ids);
+
     List<Folder> getChildren(Folder folder);
 
     /**
-     * Return a recursive list of all descendant folders.
+     * Return a Set of all descendant folders starting from the given start folders.  If
+     * includeStartFolders is set to true, the starting folders are included in the result.
+     *
+     * @param startFolders
+     * @param includeStartFolders
+     * @return
+     */
+    Set<Folder> getAllDescendants(Collection<Folder> startFolders, boolean includeStartFolders);
+
+    /**
+     * Return a recursive list of all descendant folders from the current folder.
      *
      * @param folder
      * @return
      */
-    List<Folder> getAllDescendants(Folder folder);
-
-    /**
-     * Returns a Set of all downstream folder Ids starting from the given startFolderIds.  If
-     * includeStartFolder is set to true, the starting folder IDs are included in the result.
-     *
-     * @param startFolderIds
-     * @param includeStartFolders
-     * @return
-     */
-    Set<String> getAllDescendantIds(Collection<String> startFolderIds, boolean includeStartFolders);
+    Set<Folder> getAllDescendants(Folder folder);
 
     Folder create(FolderBuilder builder);
 
