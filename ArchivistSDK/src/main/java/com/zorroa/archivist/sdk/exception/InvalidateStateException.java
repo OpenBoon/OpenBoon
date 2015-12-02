2141e072a9d37aab20a976cf276b656f7ef3ca9e
Matt Chambers <mvchambers@me.com>
2015-12-02 15:31:21 -0500

    Added new package for exceptions

---
+++ ArchivistSDK/src/main/java/com/zorroa/archivist/sdk/exception/InvalidateStateException.java
@@ -0,0 +1,25 @@
+package com.zorroa.archivist.sdk.exception;
+
+import com.zorroa.archivist.sdk.domain.ArchivistException;
+
+/**
+ * Created by chambers on 12/2/15.
+ */
+public class InvalidateStateException extends ArchivistException {
+
+    public InvalidateStateException() {
+        super();
+    }
+
+    public InvalidateStateException(String message) {
+        super(message);
+    }
+
+    public InvalidateStateException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InvalidateStateException(Throwable cause) {
+        super(cause);
+    }
+}
