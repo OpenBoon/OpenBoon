779b42987428d388d70129f9b6d2e23c6b6352fb
Matt Chambers <mvchambers@me.com>
2015-12-03 10:13:46 -0500

    sql V1_9: added some new file size and asset count related fields to outputs.

--- Archivist/src/main/resources/db/migration/V1_9__export_phase2.sql
+++ Archivist/src/main/resources/db/migration/V1_9__export_phase2.sql
@@ -1,1 +1,6 @@
 ALTER TABLE export_output ADD str_file_ext VARCHAR(16) NOT NULL;
+
+ALTER TABLE export ADD int_asset_count INTEGER NOT NULL DEFAULT 0;
+ALTER TABLE export ADD int_total_file_size BIGINT NOT NULL DEFAULT 0;
+
+ALTER TABLE export_output ADD int_file_size BIGINT NOT NULL DEFAULT 0;
