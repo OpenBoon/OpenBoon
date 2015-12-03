71590e5d059eccdb10012cb63ba042629b6e69f8
Matt Chambers <mvchambers@me.com>
2015-12-03 16:33:40 -0500

    sql: small reorganization of V1_9

--- Archivist/src/main/resources/db/migration/V1_9__export_phase2.sql
+++ Archivist/src/main/resources/db/migration/V1_9__export_phase2.sql
@@ -1,6 +1,5 @@
 ALTER TABLE export_output ADD str_file_ext VARCHAR(16) NOT NULL;
+ALTER TABLE export_output ADD int_file_size BIGINT NOT NULL DEFAULT 0;
 
 ALTER TABLE export ADD int_asset_count INTEGER NOT NULL DEFAULT 0;
 ALTER TABLE export ADD int_total_file_size BIGINT NOT NULL DEFAULT 0;
-
-ALTER TABLE export_output ADD int_file_size BIGINT NOT NULL DEFAULT 0;
