a5569a48c4069532a62570325f15ad95af66d7a7
Matt Chambers <mvchambers@me.com>
2015-12-15 10:48:03 -0500

    Message: allow string based message types.

--- ArchivistSDK/src/main/java/com/zorroa/archivist/sdk/domain/Message.java
+++ ArchivistSDK/src/main/java/com/zorroa/archivist/sdk/domain/Message.java
@@ -4,26 +4,41 @@ import com.zorroa.archivist.sdk.util.Json;
 
 public class Message {
 
-    private MessageType type;
+    private String type;
     private String payload;
 
     public Message(MessageType type, String payload) {
-        this.type = type;
+        this.type = type.toString();
         this.payload = payload;
     }
 
     public Message(MessageType type, Object payload) {
+        this.type = type.toString();
+        this.payload = Json.serializeToString(payload);
+    }
+
+    public Message(String type, String payload) {
+        this.type = type;
+        this.payload = payload;
+    }
+
+    public Message(String type, Object payload) {
         this.type = type;
         this.payload = Json.serializeToString(payload);
     }
 
     public Message() { }
 
-    public MessageType getType() {
+    public String getType() {
         return type;
     }
 
     public Message setType(MessageType type) {
+        this.type = type.toString();
+        return this;
+    }
+
+    public Message setType(String type) {
         this.type = type;
         return this;
     }
@@ -46,5 +61,15 @@ public class Message {
         return String.format("%s\t%s", type.toString(), payload.replace('\n', ' '));
     }
 
+    public String serialize(String endMessage) {
+        StringBuilder sb = new StringBuilder(payload.length() + 64);
+        sb.append(type.toString());
+        sb.append("\t");
+        sb.append(payload.replace('\n', ' '));
+        if (endMessage != null) {
+            sb.append(endMessage);
+        }
+        return sb.toString();
+    }
 
 }
