1bb9c9e4fe4f60db6eda71b8e487cd3e7fe544af
Matt Chambers <mvchambers@me.com>
2015-12-15 11:19:53 -0500

    Messaging: refactored messaging into the MessagingService and updated EventServerHandler.java to handle new capabilities.

--- Archivist/src/main/java/com/zorroa/archivist/event/EventServerHandler.java
+++ Archivist/src/main/java/com/zorroa/archivist/event/EventServerHandler.java
@@ -1,7 +1,10 @@
 package com.zorroa.archivist.event;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import com.zorroa.archivist.sdk.domain.Message;
 import com.zorroa.archivist.sdk.domain.MessageType;
+import com.zorroa.archivist.sdk.domain.Session;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
@@ -12,13 +15,25 @@ import io.netty.util.concurrent.GlobalEventExecutor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Set;
+import java.util.Collection;
 
 public class EventServerHandler extends SimpleChannelInboundHandler<String> {
 
     private static final Logger logger = LoggerFactory.getLogger(EventServerHandler.class);
 
     static final ChannelGroup channels = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+
+    private static final String END_MESSAGE = "\r\n";
+
+    /**
+     * Stores a session ID to channel mapping.
+     */
+    private final Cache<String, Channel> channelMap = CacheBuilder.newBuilder()
+            .weakValues()
+            .removalListener((c) -> {
+                logger.info("Channel {} was removed", c);
+            })
+            .build();
 
     @Override
     public boolean isSharable() {
@@ -37,19 +52,39 @@ public class EventServerHandler extends SimpleChannelInboundHandler<String> {
         channels.remove(incoming);
     }
 
-    public void send(Set<String> sessions, String payload) {
-        //TODO: remove string concat, push up to Message class
-        channels.writeAndFlush(payload + "\r\n", (c)-> {
-            String session = (String) c.attr(AttributeKey.valueOf("session")).get();
-            return sessions.contains(session);
-        });
+    public void send(Collection<Session> sessions, Message message) {
+        String text = message.serialize(END_MESSAGE);
+
+        for (Session session: sessions) {
+            Channel channel = channelMap.asMap().get(session.getCookieId());
+            if (channel == null) {
+                continue;
+            }
+            try {
+                channel.writeAndFlush(text).sync();
+            } catch (InterruptedException e) {
+                logger.warn("Failed to send '{}' to channel '{}',", text, channel, e);
+            }
+        }
     }
 
     public void broadcast(Message message) {
-        //TODO: remove string concat, push up to Message class
-        logger.trace("broadcasting message: {}", message);
-        String text = message.toString() + "\r\n";
+        String text = message.serialize(END_MESSAGE);
         channels.writeAndFlush(text);
+    }
+
+    public void send(Session session, Message message) {
+        Channel channel = channelMap.asMap().get(session.getCookieId());
+        if (channel == null) {
+            return;
+        }
+
+        String text = message.serialize(END_MESSAGE);
+        try {
+            channel.writeAndFlush(text).sync();
+        } catch (InterruptedException e) {
+            logger.warn("Failed to send '{}' to channel '{}',", text, channel, e);
+        }
     }
 
     @Override
@@ -57,13 +92,15 @@ public class EventServerHandler extends SimpleChannelInboundHandler<String> {
             throws Exception {
 
         MessageType type = MessageType.valueOf(msg.substring(0, msg.indexOf(' ')));
-        String sessionId = msg.substring(msg.indexOf(' ') + 1);
+        String cookieId = msg.substring(msg.indexOf(' ') + 1);
 
         switch(type) {
         case SESSION:
-            logger.trace("Setting session attr on channel: {}", sessionId);
-            ctx.channel().attr(AttributeKey.valueOf("session")).set(sessionId);
-            ctx.channel().writeAndFlush("OK\n");
+            logger.trace("Setting cookie attr on channel: {}", cookieId);
+            channelMap.put(cookieId, ctx.channel());
+            ctx.channel().attr(AttributeKey.valueOf("cookie")).set(cookieId);
+            ctx.channel().writeAndFlush("OK" + END_MESSAGE);
+            break;
         default:
             // ignore
             return;
---
+++ Archivist/src/main/java/com/zorroa/archivist/service/MessagingServiceImpl.java
@@ -0,0 +1,109 @@
+package com.zorroa.archivist.service;
+
+import com.zorroa.archivist.event.EventServerHandler;
+import com.zorroa.archivist.repository.SessionDao;
+import com.zorroa.archivist.sdk.domain.Message;
+import com.zorroa.archivist.sdk.domain.Room;
+import com.zorroa.archivist.sdk.domain.Session;
+import com.zorroa.archivist.sdk.domain.User;
+import com.zorroa.archivist.sdk.service.MessagingService;
+import com.zorroa.archivist.sdk.service.RoomService;
+import com.zorroa.archivist.sdk.service.UserService;
+import com.zorroa.archivist.security.SecurityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * Created by chambers on 12/14/15.
+ */
+@Component
+public class MessagingServiceImpl implements MessagingService {
+
+    private static final Logger logger = LoggerFactory.getLogger(MessagingServiceImpl.class);
+
+    @Autowired
+    RoomService roomService;
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    EventServerHandler eventServerHandler;
+
+    @Autowired
+    SessionDao sessionDao;
+
+    @Override
+    public void broadcast(Message message) {
+        eventServerHandler.broadcast(message);
+    }
+
+    @Override
+    public void sendToActiveRoom(Message message) {
+        sendToRoom(roomService.getActiveRoom(), message);
+    }
+
+    @Override
+    public void sendToRoom(Room room, Message message) {
+        /*
+         * If the room is null just log it and move on.
+         */
+        if (room == null) {
+            logger.warn("The current session {} is not in a room.", SecurityUtils.getSessionId());
+            return;
+        }
+
+        if (message.getPayload() == null) {
+            logger.warn("The current session {} has a null message payload", SecurityUtils.getSessionId());
+            return;
+        }
+
+        logger.info("Sending: {} to active room", message);
+        eventServerHandler.send(sessionDao.getAll(room), message);
+    }
+
+    @Override
+    public void sendToActiveSession(Message message) {
+        sendToRoom(roomService.getActiveRoom(), message);
+    }
+
+    @Override
+    public void sendToSession(Session session, Message message) {
+        /*
+         * If the room is null just log it and move on.
+         */
+        if (session == null) {
+            logger.warn("The current session {} is null.", SecurityUtils.getSessionId());
+            return;
+        }
+
+        if (message.getPayload() == null) {
+            logger.warn("The not sending null message payload to {}", SecurityUtils.getSessionId());
+            return;
+        }
+
+        logger.debug("Sending: {} to active room", message);
+        eventServerHandler.send(session, message);
+    }
+
+    @Override
+    public void sendToActiveUser(Message message) {
+
+        User user = SecurityUtils.getUser();
+        if (user == null) {
+            return;
+        }
+
+        sendToUser(user, message);
+    }
+
+    @Override
+    public void sendToUser(User user, Message message) {
+        List<Session> sessions = sessionDao.getAll(user);
+        eventServerHandler.send(sessions, message);
+    }
+}
