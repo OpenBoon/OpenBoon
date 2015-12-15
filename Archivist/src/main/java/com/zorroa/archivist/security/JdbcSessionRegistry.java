be044ad51982a0916a9c803d3e6f5c43a9c4bb30
Matt Chambers <mvchambers@me.com>
2015-12-15 14:27:16 -0500

    Security: moved the session manager into the correct package.

--- Archivist/src/main/java/com/zorroa/archivist/security/JdbcSessionRegistry.java
+++ Archivist/src/main/java/com/zorroa/archivist/security/JdbcSessionRegistry.java
@@ -1,4 +1,4 @@
-package com.zorroa.archivist.web;
+package com.zorroa.archivist.security;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Maps;
--- Archivist/src/main/java/com/zorroa/archivist/security/WebSecurityConfig.java
+++ Archivist/src/main/java/com/zorroa/archivist/security/WebSecurityConfig.java
@@ -2,7 +2,6 @@ package com.zorroa.archivist.security;
 
 import com.zorroa.archivist.sdk.service.RoomService;
 import com.zorroa.archivist.sdk.service.UserService;
-import com.zorroa.archivist.web.JdbcSessionRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
