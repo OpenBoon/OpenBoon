7df3f8bb14fd295c4b01f8dc3661ddd9bab7bd06
Matt Chambers <mvchambers@me.com>
2015-12-15 11:46:07 -0500

    Messaging: added unit tests.

---
+++ Archivist/src/test/java/com/zorroa/archivist/TestMessagingClient.java
@@ -0,0 +1,96 @@
+package com.zorroa.archivist;
+
+import com.google.common.collect.Queues;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.Delimiters;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Queue;
+
+/**
+ * Created by chambers on 12/14/15.
+ */
+public class TestMessagingClient {
+
+    private static final Logger logger = LoggerFactory.getLogger(TestMessagingClient.class);
+
+    static final String HOST = System.getProperty("host", "127.0.0.1");
+    static final int PORT = Integer.parseInt(System.getProperty("port", "8087"));
+
+    Channel channel;
+    EventLoopGroup group;
+
+    private static final Queue<String> queue = Queues.newLinkedBlockingQueue();
+
+    public TestMessagingClient() throws Exception {
+        connect();
+    }
+    public void sendSession(String id) {
+        channel.writeAndFlush("SESSION " + id + "\r\n");
+    }
+
+    public void shutdown() {
+        group.shutdownGracefully();
+    }
+
+    public String pop() {
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return queue.poll();
+    }
+
+    public void connect () throws Exception {
+
+        group = new NioEventLoopGroup();
+
+        Bootstrap b = new Bootstrap();
+        b.group(group)
+                .channel(NioSocketChannel.class)
+                .handler(new TestMesssagingClientInitializer());
+
+        // Start the connection attempt.
+        channel = b.connect(HOST, PORT).sync().channel();
+    }
+
+    public static class TestMesssagingClientHandler extends SimpleChannelInboundHandler<String> {
+
+        @Override
+        public void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+            logger.info(msg);
+            queue.add(msg);
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+            cause.printStackTrace();
+            ctx.close();
+        }
+    }
+
+    public static class TestMesssagingClientInitializer extends ChannelInitializer<SocketChannel> {
+
+        @Override
+        public void initChannel(SocketChannel ch) throws Exception {
+            ChannelPipeline pipeline = ch.pipeline();
+
+            // On top of the SSL handler, add the text line codec.
+            pipeline.addLast(new DelimiterBasedFrameDecoder(8192, Delimiters.lineDelimiter()));
+            pipeline.addLast(new StringDecoder());
+            pipeline.addLast(new StringEncoder());
+
+            // and then business logic.
+            pipeline.addLast(new TestMesssagingClientHandler());
+        }
+    }
+}
---
+++ Archivist/src/test/java/com/zorroa/archivist/service/MessageServiceTests.java
@@ -0,0 +1,103 @@
+package com.zorroa.archivist.service;
+
+import com.zorroa.archivist.ArchivistApplicationTests;
+import com.zorroa.archivist.TestMessagingClient;
+import com.zorroa.archivist.sdk.domain.*;
+import com.zorroa.archivist.sdk.service.MessagingService;
+import com.zorroa.archivist.sdk.service.RoomService;
+import com.zorroa.archivist.sdk.service.UserService;
+import com.zorroa.archivist.security.SecurityUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by chambers on 12/14/15.
+ */
+public class MessageServiceTests extends ArchivistApplicationTests {
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    RoomService roomService;
+
+    @Autowired
+    MessagingService messagingService;
+
+    @Autowired
+    SessionRegistry sessionRegistry;
+
+    TestMessagingClient client;
+    Room room;
+
+    @Before
+    public void init() throws Exception {
+        client = new TestMessagingClient();
+        room = roomService.create(new RoomBuilder("test"));
+
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+        sessionRegistry.registerNewSession(request.getSession().getId(), SecurityUtils.getUser());
+
+        roomService.join(room, userService.getSession(SecurityUtils.getSessionId()));
+        client.sendSession(SecurityUtils.getSessionId());
+        assertEquals("OK", client.pop());
+    }
+
+    @After
+    public void shutdown() {
+        client.shutdown();
+    }
+
+    @Test
+    public void broadcast() throws Exception {
+        messagingService.broadcast(new Message("TEST", "foo"));
+        assertEquals("TEST\tfoo", client.pop());
+    }
+
+    @Test
+    public void testSendToActiveRoom() throws Exception {
+        messagingService.sendToActiveRoom(new Message("TEST", "foo"));
+        assertEquals("TEST\tfoo", client.pop());
+    }
+
+    @Test
+    public void testSendToRoom() throws Exception {
+        messagingService.sendToRoom(room, new Message("TEST", "foo"));
+        assertEquals("TEST\tfoo", client.pop());
+    }
+
+    @Test
+    public void testSendToActiveSession() throws Exception {
+        messagingService.sendToActiveSession(new Message("TEST", "foo"));
+        assertEquals("TEST\tfoo", client.pop());
+    }
+
+    @Test
+    public void testSendSession() throws Exception {
+        Session session = userService.getSession(SecurityUtils.getSessionId());
+        messagingService.sendToSession(session, new Message("TEST", "foo"));
+        assertEquals("TEST\tfoo", client.pop());
+    }
+
+    @Test
+    public void testSendToUser() throws Exception {
+        User user = SecurityUtils.getUser();
+        messagingService.sendToUser(user, new Message("TEST", "foo"));
+        assertEquals("TEST\tfoo", client.pop());
+    }
+
+    @Test
+    public void testSendToAciveUser() throws Exception {
+        messagingService.sendToActiveUser(new Message("TEST", "foo"));
+        assertEquals("TEST\tfoo", client.pop());
+    }
+}
