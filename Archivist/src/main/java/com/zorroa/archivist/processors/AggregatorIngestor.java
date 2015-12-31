de856fedd1cd6f3b1cc3119cca8ac0fe536f3331
Matt Chambers <mvchambers@me.com>
2015-12-31 17:04:19 -0500

    AggregatorIngestor: thread safe fixes, also fixes issue where aggegators would be executed multiple times.

--- Archivist/src/main/java/com/zorroa/archivist/processors/AggregatorIngestor.java
+++ Archivist/src/main/java/com/zorroa/archivist/processors/AggregatorIngestor.java
@@ -3,33 +3,36 @@ package com.zorroa.archivist.processors;
 import com.zorroa.archivist.sdk.domain.AssetBuilder;
 import com.zorroa.archivist.sdk.domain.Ingest;
 import com.zorroa.archivist.sdk.processor.ingest.IngestProcessor;
-import org.elasticsearch.client.Client;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.ApplicationContext;
 
 import java.util.ArrayList;
+import java.util.concurrent.atomic.LongAdder;
 
 public class AggregatorIngestor extends IngestProcessor {
-    private int assetsSinceLastAgg = 0;
+    /**
+     * Using LongAdder here, its not safe to have multiple
+     * threads incrementing a primitive integer.
+     */
+    private final LongAdder assetsSinceLastAgg = new LongAdder();
+
+    /**
+     * The number of assets that must be processed before aggregation occurs.
+     */
     private final int kAggCount = 128;
-    private ArrayList<IngestProcessor> aggregators = new ArrayList<>();
+
+    /**
+     * The aggregators to run.
+     */
+    private final ArrayList<IngestProcessor> aggregators = new ArrayList<>();
 
     @Autowired
     ApplicationContext applicationContext;
 
-    @Autowired
-    protected Client client;
-
-    @Value("${archivist.index.alias}")
-    protected String alias;
-
     @Override
     public void init(Ingest ingest) {
-        aggregators.clear();                            // Not sure this is needed?
         aggregators.add(new DateAggregator());
-        aggregators.add(new RatingAggregator());
         aggregators.add(new IngestPathAggregator());
         AutowireCapableBeanFactory autowire = applicationContext.getAutowireCapableBeanFactory();
         for (IngestProcessor aggregator : aggregators) {
@@ -40,36 +43,34 @@ public class AggregatorIngestor extends IngestProcessor {
 
     @Override
     public void process(AssetBuilder assetBuilder) {
-        if (++assetsSinceLastAgg > kAggCount) {
-            assetsSinceLastAgg = 0;
-            aggregate();
+        assetsSinceLastAgg.increment();
+        if (assetsSinceLastAgg.longValue() > kAggCount) {
+            /*
+             * Synchronize around the aggregators and double check
+             * the condition to avoid double execution.
+             */
+            synchronized(aggregators) {
+                if (assetsSinceLastAgg.longValue() > kAggCount) {
+                    aggregate();
+                    assetsSinceLastAgg.reset();
+                }
+            }
         }
     }
 
     @Override
     public void teardown() {
-        if (assetsSinceLastAgg > 0) {
+        if (assetsSinceLastAgg.longValue() > 0) {
             aggregate();
         }
     }
 
     private void aggregate() {
-        refreshIndex(1);    // Wait for ES to settle after ingestion
+        /*
+         * Refresh is run from the ingest executor, so the data should be available.
+         */
         for (IngestProcessor agg: aggregators) {
             agg.process(null);
         }
     }
-
-    private void refreshIndex(long sleep) {
-        try {
-            Thread.sleep(sleep/2);
-        } catch (InterruptedException e) {
-        }
-        client.admin().indices().prepareRefresh(alias).get();
-        try {
-            Thread.sleep(sleep/2);
-        } catch (InterruptedException e) {
-        }
-    }
-
 }
