5393b6d6d0f19d94ffdce463e940706277626257
Matt Chambers <mvchambers@me.com>
2016-01-01 16:59:53 -0500

    removed handlesAssetType

--- ArchivistSDK/src/main/java/com/zorroa/archivist/sdk/processor/ingest/IngestProcessor.java
+++ ArchivistSDK/src/main/java/com/zorroa/archivist/sdk/processor/ingest/IngestProcessor.java
@@ -1,7 +1,6 @@
 package com.zorroa.archivist.sdk.processor.ingest;
 
 import com.zorroa.archivist.sdk.domain.AssetBuilder;
-import com.zorroa.archivist.sdk.domain.AssetType;
 import com.zorroa.archivist.sdk.domain.Ingest;
 import com.zorroa.archivist.sdk.processor.Processor;
 
@@ -12,15 +11,4 @@ public abstract class IngestProcessor extends Processor {
     public abstract void process(AssetBuilder asset);
 
     public void init(Ingest ingest) { };
-
-    /**
-     * Return true if the processor handles a given AssetType.  The default implementation
-     * returns true.
-     *
-     * @param type
-     * @return
-     */
-    public boolean handlesAssetType(AssetType type) {
-        return true;
-    }
 }
