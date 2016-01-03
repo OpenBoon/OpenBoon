489b27deaa3ebb48c6d8da4577fc517cce60392e
Matt Chambers <mvchambers@me.com>
2016-01-03 08:24:50 -0500

    IngestBuilder: come clean ups, removed old methods

--- ArchivistSDK/src/main/java/com/zorroa/archivist/sdk/domain/IngestBuilder.java
+++ ArchivistSDK/src/main/java/com/zorroa/archivist/sdk/domain/IngestBuilder.java
@@ -1,15 +1,13 @@
 package com.zorroa.archivist.sdk.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.google.common.collect.ImmutableSet;
-
-import java.util.Set;
-
 public class IngestBuilder {
 
     private String path;
-    private Set<String> fileTypes = ImmutableSet.<String>of();
-    private int pipelineId = 1;     // "standard" pipeline is always 1
+
+    /**
+     * The ingest pipeline to run.  A -1 ID is the "standard" or default pipeline.
+     */
+    private int pipelineId = -1;
     private boolean updateOnExist = true;
     private int assetWorkerThreads = 4;
 
@@ -17,29 +15,6 @@ public class IngestBuilder {
 
     public IngestBuilder(String path) {
         this.path = path;
-    }
-
-    public IngestBuilder(String path, Set<String> fileTypes) {
-        this.path = path;
-        this.fileTypes = fileTypes;
-    }
-
-    public Set<String> getFileTypes() {
-        return fileTypes;
-    }
-
-    @JsonIgnore
-    public boolean isSupportedFileType(String type) {
-        if (fileTypes.isEmpty()) {
-            return true;
-        }
-
-        return fileTypes.contains(type);
-    }
-
-    public IngestBuilder setFileTypes(Set<String> fileTypes) {
-        this.fileTypes = fileTypes;
-        return this;
     }
 
     public String getPath() {
