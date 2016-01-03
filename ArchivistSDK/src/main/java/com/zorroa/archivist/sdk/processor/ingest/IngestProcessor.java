32aabcc00a080266e968ef02be195576e0327870
Matt Chambers <mvchambers@me.com>
2016-01-03 00:36:35 -0500

    IngestProcesor: added the ability for a processor to declare the formats it supports.  This is used for two things:
    a. When an ingest is running, it uses the sum of all supported formats to filer files
    out of the initial search.
    b. When the processors are being executed, processors are skipped over when they don't
    support a given format.

--- ArchivistSDK/src/main/java/com/zorroa/archivist/sdk/processor/ingest/IngestProcessor.java
+++ ArchivistSDK/src/main/java/com/zorroa/archivist/sdk/processor/ingest/IngestProcessor.java
@@ -1,14 +1,47 @@
 package com.zorroa.archivist.sdk.processor.ingest;
 
+import com.google.common.collect.Sets;
 import com.zorroa.archivist.sdk.domain.AssetBuilder;
 import com.zorroa.archivist.sdk.domain.Ingest;
 import com.zorroa.archivist.sdk.processor.Processor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Set;
 
 public abstract class IngestProcessor extends Processor {
+
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
+
+    protected Set<String> supportedFormats = Sets.newHashSet();
 
     public IngestProcessor() { };
 
     public abstract void process(AssetBuilder asset);
+
+    /**
+     * Return a set of all supported formats.  The point is this is to avoid opening or
+     * attempting to open data that is not supported by the ingest pipeline.
+     *
+     * @return
+     */
+    public Set<String> supportedFormats() {
+        return supportedFormats;
+    }
+
+    /**
+     * Return true if the given string is a supported format.  If the supported
+     * format set is empty, then this function also returns true.
+     *
+     * @param format
+     * @return
+     */
+    public boolean isSupportedFormat(String format) {
+        if (supportedFormats.isEmpty()) {
+            return true;
+        }
+        return supportedFormats.contains(format.toLowerCase());
+    }
 
     public void init(Ingest ingest) { };
 }
