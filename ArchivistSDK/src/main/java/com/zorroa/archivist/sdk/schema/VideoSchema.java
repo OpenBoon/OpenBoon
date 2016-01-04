83a4f8039e9ad694ef7afd325ae8b696b4763fb4
Matt Chambers <mvchambers@me.com>
2016-01-03 20:13:38 -0500

    Video: added more metadata

--- ArchivistSDK/src/main/java/com/zorroa/archivist/sdk/schema/VideoSchema.java
+++ ArchivistSDK/src/main/java/com/zorroa/archivist/sdk/schema/VideoSchema.java
@@ -8,7 +8,13 @@ public class VideoSchema implements Schema {
     private int width;
     private int height;
     private int audioSampleRate;
+    private int sampleRate;
     private double duration;
+    private double aspectRatio;
+    private int frames;
+    private int audioChannels;
+    private String format;
+    private double frameRate;
 
     @Override
     public String getNamespace() {
@@ -48,6 +54,61 @@ public class VideoSchema implements Schema {
 
     public VideoSchema setDuration(double duration) {
         this.duration = duration;
+        return this;
+    }
+
+
+    public double getAspectRatio() {
+        return aspectRatio;
+    }
+
+    public VideoSchema setAspectRatio(double aspectRatio) {
+        this.aspectRatio = aspectRatio;
+        return this;
+    }
+
+    public int getFrames() {
+        return frames;
+    }
+
+    public VideoSchema setFrames(int frames) {
+        this.frames = frames;
+        return this;
+    }
+
+    public int getAudioChannels() {
+        return audioChannels;
+    }
+
+    public VideoSchema setAudioChannels(int audioChannels) {
+        this.audioChannels = audioChannels;
+        return this;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public VideoSchema setFormat(String format) {
+        this.format = format;
+        return this;
+    }
+
+    public double getFrameRate() {
+        return frameRate;
+    }
+
+    public VideoSchema setFrameRate(double frameRate) {
+        this.frameRate = frameRate;
+        return this;
+    }
+
+    public int getSampleRate() {
+        return sampleRate;
+    }
+
+    public VideoSchema setSampleRate(int sampleRate) {
+        this.sampleRate = sampleRate;
         return this;
     }
 }
