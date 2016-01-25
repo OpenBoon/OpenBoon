e97ca53759d0c98c24dc15a9cdbd802eb40da73e
Matt Chambers <mvchambers@me.com>
2016-01-25 13:49:09 -0500

    Added name to the asset worker threads so I can tell where a log is being printed from.

--- Archivist/src/main/java/com/zorroa/archivist/AssetExecutor.java
+++ Archivist/src/main/java/com/zorroa/archivist/AssetExecutor.java
@@ -4,6 +4,8 @@ package com.zorroa.archivist;
  * Created by chambers on 7/31/15.
  */
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -12,6 +14,7 @@ public class AssetExecutor extends ThreadPoolExecutor {
 
     public AssetExecutor(int threads) {
         super(threads, threads, 10, TimeUnit.SECONDS, new LinkedBlockingQueue<>());
+        this.setThreadFactory(new ThreadFactoryBuilder().setNameFormat("ASSET WORKER-%d").build());
     }
 
     public void waitForCompletion() {
