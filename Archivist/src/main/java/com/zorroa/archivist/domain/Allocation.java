8af9b7c78c8f3c3eac3c92d075503d5ee8307d06
Matt Chambers <mvchambers@me.com>
2016-02-02 15:17:45 -0500

    @103691874 Local cloud caches
    The ObjectFileSystem class is the interface that the Archivist use to store its data,
    be it proxies or data obtained from any external source.  The UUIDFileSystemImpl is
    an implementation of this interface which uses UUIDs to avoid naming collions and provides
    and easy way to shard files accross servers if we do a distributed version.
    
    This system replaces ImageService, since all it was doing was providing directories to store image.

---
+++ Archivist/src/main/java/com/zorroa/archivist/domain/Allocation.java
@@ -0,0 +1,89 @@
+package com.zorroa.archivist.domain;
+
+import com.google.common.io.Files;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * An allocation is a directory created by an ObjectFileSystem implementation.
+ */
+public class Allocation {
+
+    private static final Logger logger = LoggerFactory.getLogger(Allocation.class);
+
+    private UUID id;
+    private String category;
+    private File path;
+    private AtomicBoolean exists = new AtomicBoolean(false);
+
+    public Allocation(UUID id, String category, File path) {
+        this.id = id;
+        this.category = category;
+        this.path = path;
+        this.exists.set(path.exists());
+    }
+
+    public Allocation create() {
+        this.path.mkdirs();
+        return this;
+    }
+
+    public File getAbsolutePath(String name) {
+        return new File(new StringBuilder(256)
+                .append(path.getAbsolutePath())
+                .append("/")
+                .append(id.toString())
+                .append("_")
+                .append(name)
+                .toString());
+    }
+
+    public File getAbsolutePath(String type, Object ... tokens) {
+        StringBuilder sb = new StringBuilder(256)
+                .append(path.getAbsolutePath())
+                .append("/")
+                .append(id.toString())
+                .append("_");
+        for (Object token: tokens) {
+            sb.append(token.toString().replace('.', '_'));
+            sb.append("_");
+        }
+        sb.deleteCharAt(sb.length()-1);
+        sb.append(".");
+        sb.append(type);
+        return new File(sb.toString());
+    }
+
+    public boolean exists(String type, Object ... tokens) {
+        return getAbsolutePath(type, tokens).exists();
+    }
+
+    public File store(InputStream src, String type, Object ... tokens) throws IOException {
+        if (exists.compareAndSet(false, true)) {
+            create();
+        }
+        File path = getAbsolutePath(type, tokens);
+        try (FileOutputStream fos = new FileOutputStream(path)) {
+            int inByte;
+            while ((inByte = src.read()) != -1)
+                fos.write(inByte);
+        }
+        return path;
+    }
+
+    public File store(File src, String type, Object ... tokens) throws IOException {
+        if (exists.compareAndSet(false, true)) {
+            create();
+        }
+        File dst = getAbsolutePath(type, tokens);
+        Files.copy(src, dst);
+        return dst;
+    }
+}
---
+++ Archivist/src/main/java/com/zorroa/archivist/service/ObjectFileSystem.java
@@ -0,0 +1,40 @@
+package com.zorroa.archivist.service;
+
+import com.zorroa.archivist.domain.Allocation;
+
+import java.io.File;
+
+/**
+ * The object file system interface is for storing files of all types, including proxies,
+ * cloud based assets, database dumps/files, and any other type of file that an ingestor
+ * may need to do its job.
+ */
+public interface ObjectFileSystem {
+
+    /**
+     * Create an allocation for the given category of data.  An allocation
+     * is a directory.
+     *
+     * @param category
+     * @return
+     */
+    Allocation build(String category);
+
+    /**
+     * Create an allocation for the given category of data with the given ID.
+     *
+     * @param category
+     * @return
+     */
+    Allocation build(Object id, String category);
+
+    /**
+     * Find the path to the given file using only the category and the name.
+     *
+     * @param category
+     * @param name
+     * @return
+     */
+    File find(String category, String name);
+
+}
