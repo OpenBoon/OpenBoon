862f17dcca3db0604cdc209181ae491bb94ab178
Matt Chambers <mvchambers@me.com>
2016-02-05 10:29:20 -0500

    Schema: converted ingest schema to a list, and renamed the namespace to 'imports'

--- ArchivistSDK/src/main/java/com/zorroa/archivist/sdk/schema/IngestSchema.java
+++ ArchivistSDK/src/main/java/com/zorroa/archivist/sdk/schema/IngestSchema.java
@@ -1,37 +1,64 @@
 package com.zorroa.archivist.sdk.schema;
 
+import com.google.common.base.Objects;
+import com.zorroa.archivist.sdk.domain.Ingest;
+
 /**
  * The ingest schema contains all the information related to the ingest that brought in the asset.
  */
-public class IngestSchema implements Schema {
-
-    private int id;
-    private int pipeline;
-
-    private long timestamp = System.currentTimeMillis();
+public class IngestSchema extends SetSchema<IngestSchema.IngestProperties> implements Schema {
 
     @Override
     public String getNamespace() {
-        return "ingest";
+        return "imports";
     }
 
-    public int getId() {
-        return id;
+    public void addIngest(Ingest ingest) {
+        add(new IngestProperties(ingest));
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+    public static class IngestProperties {
+        private int id;
+        private int pipeline;
+        private long timestamp;
 
-    public int getPipeline() {
-        return pipeline;
-    }
+        public IngestProperties(Ingest ingest) {
+            this.id = ingest.getId();
+            this.pipeline = ingest.getPipelineId();
+            this.timestamp = System.currentTimeMillis();
+        }
 
-    public void setPipeline(int pipeline) {
-        this.pipeline = pipeline;
-    }
+        public int getId() {
+            return id;
+        }
 
-    public long getTimestamp() {
-        return timestamp;
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public int getPipeline() {
+            return pipeline;
+        }
+
+        public void setPipeline(int pipeline) {
+            this.pipeline = pipeline;
+        }
+
+        public long getTimestamp() {
+            return timestamp;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof IngestProperties)) return false;
+            IngestProperties that = (IngestProperties) o;
+            return getId() == that.getId();
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hashCode(getId());
+        }
     }
 }
