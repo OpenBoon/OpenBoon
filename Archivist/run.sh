3be511115cb24d3a16322238a3f43e1bd34e2df0
Matt Chambers <mvchambers@me.com>
2016-02-05 12:39:28 -0500

    run.sh: don't execute a clean.

--- Archivist/run.sh
+++ Archivist/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mvn clean package spring-boot:run -Dmaven.test.skip=true
+mvn  package spring-boot:run -Dmaven.test.skip=true
 
---
+++ Archivist/src/main/java/com/zorroa/archivist/service/EventLogService.java
@@ -0,0 +1,24 @@
+package com.zorroa.archivist.service;
+
+import com.zorroa.archivist.sdk.domain.Asset;
+import com.zorroa.archivist.sdk.domain.EventLogMessage;
+import com.zorroa.archivist.sdk.domain.Id;
+
+/**
+ * Created by chambers on 12/29/15.
+ */
+public interface EventLogService {
+
+    void log(EventLogMessage message);
+
+    void log(Id object, String message, Object... args);
+
+    void log(String message, Object... args);
+
+    void log(Id object, String message, Throwable ex, Object... args);
+
+    void log(Asset asset, String message, Object... args);
+
+    void log(Asset asset, String message, Throwable ex, Object... args);
+
+}
