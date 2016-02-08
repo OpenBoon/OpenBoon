f334fd7b235e5d54b8b26480c19d568cdee6a8b5
Matt Chambers <mvchambers@me.com>
2016-02-08 12:44:25 -0500

    Filesystems: moved the object filesystem into the SDK.

---
+++ ArchivistSDK/src/main/java/com/zorroa/archivist/sdk/filesystem/ObjectFileSystem.java
@@ -0,0 +1,52 @@
+package com.zorroa.archivist.sdk.filesystem;
+
+import com.zorroa.archivist.sdk.domain.Allocation;
+
+import java.io.File;
+
+/**
+ * The object file system interface is for storing files of all types, including proxies,
+ * cloud based assets, database dumps/files, and any other type of file that an ingestor
+ * may need to do its job.
+ */
+public interface ObjectFileSystem {
+
+    /**
+     * Must be called to initialize the filesystem.
+     */
+    void init();
+
+    /**
+     * Set the URI location of the file system.
+     *
+     * @param location
+     */
+    void setLocation(String location);
+
+    /**
+     * Create an allocation for the given category of data.  An allocation
+     * is a directory.
+     *
+     * @param category
+     * @return
+     */
+    Allocation build(String category);
+
+    /**
+     * Create an allocation for the given category of data with the given ID.
+     *
+     * @param category
+     * @return
+     */
+    Allocation build(Object id, String category);
+
+    /**
+     * Find the path to the given file using only the category and the name.
+     *
+     * @param category
+     * @param name
+     * @return
+     */
+    File find(String category, String name);
+
+}
---
+++ ArchivistSDK/src/main/java/com/zorroa/archivist/sdk/filesystem/UUIDFileSystem.java
@@ -0,0 +1,80 @@
+package com.zorroa.archivist.sdk.filesystem;
+
+import com.fasterxml.uuid.Generators;
+import com.fasterxml.uuid.impl.NameBasedGenerator;
+import com.zorroa.archivist.sdk.domain.Allocation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.util.UUID;
+
+/**
+ * A very simple UUID/Object based file system.
+ */
+
+public class UUIDFileSystem implements ObjectFileSystem {
+
+    protected final Logger logger = LoggerFactory.getLogger(UUIDFileSystem.class);
+
+    private static final int DEEPNESS = 4;
+
+    private static final NameBasedGenerator nameBasedGenerator = Generators.nameBasedGenerator();
+
+    private File storageBaseDir;
+    private String location;
+
+    @Override
+    public void init() {
+        storageBaseDir = new File(location);
+        storageBaseDir.mkdirs();
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public Allocation build(String category) {
+        return build(UUID.randomUUID(), category);
+    }
+
+    @Override
+    public Allocation build(Object id, String category) {
+        UUID uuid = nameBasedGenerator.generate(id.toString());
+        File path = buildPath(category, uuid.toString(), false);
+        return new Allocation(uuid, category, path);
+    }
+
+    @Override
+    public File find(String category, String name) {
+        return buildPath(category, name, true);
+    }
+
+    private File buildPath(String category, String name, boolean includeName) {
+        /*
+         * In UUIDFileSystemImpl, the first part of the name has to be a UUID.
+         */
+        String id = name.substring(0, 35);
+        UUID.fromString(id);
+
+        StringBuilder sb = new StringBuilder(256);
+        sb.append(storageBaseDir.getAbsolutePath());
+        sb.append("/");
+        sb.append(category);
+        sb.append("/");
+
+        String _id = id.toString();
+        for (int i=0; i<=DEEPNESS; i++) {
+            sb.append(_id.charAt(i));
+            sb.append("/");
+        }
+        if (includeName) {
+            sb.append(name);
+        }
+        else {
+            sb.deleteCharAt(sb.length() - 1);
+        }
+        return new File(sb.toString());
+    }
+}
