e793f7104f9da3ad4d6e3d6e0162eb0d8b15549f
Matt Chambers <mvchambers@me.com>
2016-02-11 18:55:42 -0500

    A processor specifically for unit tests.

---
+++ Archivist/src/main/java/com/zorroa/archivist/domain/UnitTestProcessor.java
@@ -0,0 +1,22 @@
+package com.zorroa.archivist.domain;
+
+import com.zorroa.archivist.sdk.domain.AssetBuilder;
+import com.zorroa.archivist.sdk.domain.Ingest;
+import com.zorroa.archivist.sdk.processor.ingest.IngestProcessor;
+
+/**
+ * Created by chambers on 2/11/16.
+ */
+public class UnitTestProcessor extends IngestProcessor {
+
+    @Override
+    public void init(Ingest ingest) {
+
+    }
+
+    @Override
+    public void process(AssetBuilder assetBuilder) {
+
+
+    }
+}
