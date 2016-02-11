caf5858fd749247878fd3b01df02fe6ddc8fd792
Matt Chambers <mvchambers@me.com>
2016-02-11 18:56:07 -0500

    Merged plugins/config controllers.
    Not sure what to do with the plugins controller yet, but I combined the Plugins/Config controller to one class.  Eventually these need to call through to analysts.
    
    -Matt

--- Archivist/src/test/java/com/zorroa/archivist/web/ConfigControllerTests.java
+++ Archivist/src/test/java/com/zorroa/archivist/web/ConfigControllerTests.java
@@ -2,12 +2,16 @@ package com.zorroa.archivist.web;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.zorroa.archivist.sdk.util.Json;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MvcResult;
 
+import java.util.List;
 import java.util.Map;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -17,6 +21,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 public class ConfigControllerTests extends MockMvcTest {
 
+    /**
+     * Not sure this is even necessary.
+     * @throws Exception
+     */
+    @Ignore
     @Test
     public void testSupportedFormats() throws Exception {
 
@@ -35,5 +44,31 @@ public class ConfigControllerTests extends MockMvcTest {
         assertTrue(json.containsKey("gif"));
         assertTrue(json.containsKey("png"));
         assertTrue(json.containsKey("jpg"));
+    }
+
+    /**
+     * Need to rethink this test since the plugins will actually be on the Analyst.
+     * @throws Exception
+     */
+    @Test
+    @Ignore
+    public void testGetAll() throws Exception {
+        MockHttpSession session = admin();
+
+        MvcResult result = mvc.perform(get("/api/v1/plugins/ingest")
+                .session(session)
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        List<String> ingestors = Json.Mapper.readValue(result.getResponse().getContentAsString(),
+                new TypeReference<List<String>>() {});
+        assertEquals(6, ingestors.size());
+        assertTrue(ingestors.contains("com.zorroa.archivist.TestIngestor"));
+        assertTrue(ingestors.contains("com.zorroa.archivist.aggregators.AggregatorIngestor"));
+        assertTrue(ingestors.contains("com.zorroa.archivist.aggregators.DateAggregator"));
+        assertTrue(ingestors.contains("com.zorroa.archivist.aggregators.IngestPathAggregator"));
+        assertTrue(ingestors.contains("com.zorroa.archivist.aggregators.RatingAggregator"));
+        assertTrue(ingestors.contains("com.zorroa.archivist.ingestors.PermissionIngestor"));
     }
 }
