be383c8578eafb4f72efff32e020156e0f0bb679
Matt Chambers <mvchambers@me.com>
2016-02-16 09:36:36 -0500

    Added a ArchivistCliet bean, this class will handle load balancing and error recovery internally, no real need to have more than one of them.  The SSL configuration is done on this end.  There is probably a nicer way to set this up, but for now this gets the 2 systems talking.  The key/trust stores for example should be configuration options.

--- Analyst/src/main/java/com/zorroa/analyst/config/ApplicationConfig.java
+++ Analyst/src/main/java/com/zorroa/analyst/config/ApplicationConfig.java
@@ -2,10 +2,15 @@ package com.zorroa.analyst.config;
 
 import com.zorroa.archivist.sdk.client.archivist.ArchivistClient;
 import com.zorroa.common.config.ApplicationProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import java.io.InputStream;
+import java.security.KeyStore;
 import java.util.concurrent.Executor;
 
 /**
@@ -14,14 +19,28 @@ import java.util.concurrent.Executor;
 @Configuration
 public class ApplicationConfig {
 
+    private static final Logger logger = LoggerFactory.getLogger(ApplicationConfig.class);
+
     @Bean
     public ApplicationProperties properties() {
         return new ApplicationProperties();
     }
 
     @Bean
-    public ArchivistClient archivist() {
-        return new ArchivistClient(properties().getString("analyst.master.host"));
+    public ArchivistClient archivist() throws Exception {
+
+        ArchivistClient client =  new ArchivistClient(properties().getString("analyst.master.host"));
+
+        KeyStore keystore = KeyStore.getInstance("PKCS12");
+        InputStream keystoreInput = new ClassPathResource("keystore.p12").getInputStream();
+        keystore.load(keystoreInput, "zorroa".toCharArray());
+
+        KeyStore trustStore = KeyStore.getInstance("PKCS12");
+        InputStream trustStoreInput = new ClassPathResource("truststore.p12").getInputStream();
+        trustStore.load(trustStoreInput, "zorroa".toCharArray());
+
+        client.init(keystore, "zorroa", trustStore);
+        return client;
     }
 
     @Bean
