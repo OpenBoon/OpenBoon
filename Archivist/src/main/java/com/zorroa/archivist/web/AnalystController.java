04eae78cbd30b795c2389b0e0202acb2814a178a
Matt Chambers <mvchambers@me.com>
2016-02-16 16:13:56 -0500

    Expose controller method to get all analysts.

--- Archivist/src/main/java/com/zorroa/archivist/web/AnalystController.java
+++ Archivist/src/main/java/com/zorroa/archivist/web/AnalystController.java
@@ -1,29 +1,43 @@
 package com.zorroa.archivist.web;
 
+import com.zorroa.archivist.sdk.domain.Analyst;
 import com.zorroa.archivist.sdk.domain.AnalystPing;
 import com.zorroa.archivist.service.AnalystService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by chambers on 2/9/16.
  */
-@Controller
+@RestController
 public class AnalystController {
+
+    private static final Logger logger = LoggerFactory.getLogger(AnalystController.class);
 
     @Autowired
     AnalystService analystService;
 
     @RequestMapping(value="/api/v1/analyst/_register", method=RequestMethod.POST)
     public void register(@RequestBody AnalystPing ping) {
+        logger.info("Received ping: {}", ping);
         analystService.register(ping);
     }
 
     @RequestMapping(value="/api/v1/analyst/_shutdown", method=RequestMethod.POST)
     public void shutdown(@RequestBody AnalystPing ping) {
+        logger.info("Received shutdown ping: {}", ping);
         analystService.shutdown(ping);
+    }
+
+    @RequestMapping(value="/api/v1/analyst", method=RequestMethod.GET)
+    public List<Analyst> getAll() {
+        return analystService.getAll();
     }
 }
