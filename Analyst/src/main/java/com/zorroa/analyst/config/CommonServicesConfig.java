f52019acf0313828da8ac32167574228eb5c235d
Matt Chambers <mvchambers@me.com>
2016-02-16 17:04:42 -0500

    Added @Cofiguration decorator

--- Analyst/src/main/java/com/zorroa/analyst/config/CommonServicesConfig.java
+++ Analyst/src/main/java/com/zorroa/analyst/config/CommonServicesConfig.java
@@ -3,10 +3,12 @@ package com.zorroa.analyst.config;
 import com.zorroa.common.service.EventLogService;
 import com.zorroa.common.service.EventLogServiceImpl;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by chambers on 2/16/16.
  */
+@Configuration
 public class CommonServicesConfig {
 
     @Bean
