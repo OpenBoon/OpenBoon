c9404338cf7ae5c8daecdebbefd646f212f379a7
Matt Chambers <mvchambers@me.com>
2016-02-16 09:43:05 -0500

    Fixed up the SSL init for the ArchivistClient

--- ArchivistSDK/src/main/java/com/zorroa/archivist/sdk/client/archivist/ArchivistClient.java
+++ ArchivistSDK/src/main/java/com/zorroa/archivist/sdk/client/archivist/ArchivistClient.java
@@ -4,10 +4,21 @@ import com.google.common.collect.Lists;
 import com.zorroa.archivist.sdk.client.Http;
 import com.zorroa.archivist.sdk.domain.AnalystPing;
 import org.apache.http.HttpHost;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.ssl.SSLContexts;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.net.ssl.SSLContext;
+import java.security.KeyStore;
 import java.util.List;
 
 /**
@@ -15,17 +26,42 @@ import java.util.List;
  */
 public class ArchivistClient {
 
-    private final CloseableHttpClient client;
+    public static final int DEFAULT_PORT = 8066;
+
+    private static final Logger logger = LoggerFactory.getLogger(ArchivistClient.class);
+
+    private CloseableHttpClient client;
     private final List<HttpHost> hosts;
 
     public ArchivistClient(String host) {
-        this(Lists.newArrayList(new HttpHost(host)));
+        hosts = Lists.newArrayList();
+        if (host.contains(":")) {
+            String[] parts = host.split(":");
+            hosts.add(new HttpHost(parts[0], Integer.valueOf(parts[1]), "https"));
+        }
+        else {
+            hosts.add(new HttpHost(host, DEFAULT_PORT, "https"));
+        }
     }
 
-    public ArchivistClient(List<HttpHost> hosts) {
-        this.hosts = hosts;
+
+    public void init(KeyStore keyStore, String keyPass, KeyStore trustStore) throws Exception {
+
+        SSLContext ctx = SSLContexts.custom()
+                .loadKeyMaterial(keyStore, keyPass.toCharArray())
+                .loadTrustMaterial(trustStore, new TrustSelfSignedStrategy())
+                .build();
+
+        SSLConnectionSocketFactory factory = new SSLConnectionSocketFactory(ctx, new NoopHostnameVerifier());
+
+        Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder
+                .<ConnectionSocketFactory> create().register("https", factory)
+                .build();
+
+        PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager(socketFactoryRegistry);
+
         this.client = HttpClients.custom()
-                .setConnectionManager(new PoolingHttpClientConnectionManager())
+                .setConnectionManager(cm)
                 .build();
     }
 
@@ -40,6 +76,6 @@ public class ArchivistClient {
      * Register the given host/port as a worker node.
      */
     public void shutdownAnalyst(AnalystPing ping) {
-        Http.post(client, hosts.get(0), "/api/v1/_shutdown", ping);
+        Http.post(client, hosts.get(0), "/api/v1/analyst/_shutdown", ping);
     }
 }
