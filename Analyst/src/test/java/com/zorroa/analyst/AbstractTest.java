5cf0eefb4f9f4d6a161a3ba768ffd0c4cfe03f92
Matt Chambers <mvchambers@me.com>
2016-02-17 17:15:25 -0500

    Added convinience method for initializing an IngestProcessor

--- Analyst/src/test/java/com/zorroa/analyst/AbstractTest.java
+++ Analyst/src/test/java/com/zorroa/analyst/AbstractTest.java
@@ -1,8 +1,12 @@
 package com.zorroa.analyst;
 
+import com.zorroa.archivist.sdk.domain.ApplicationProperties;
+import com.zorroa.archivist.sdk.filesystem.ObjectFileSystem;
+import com.zorroa.archivist.sdk.processor.ingest.IngestProcessor;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -19,4 +23,18 @@ import org.springframework.test.context.web.WebAppConfiguration;
 public abstract class AbstractTest {
 
     protected Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    ApplicationProperties applicationProperties;
+
+    @Autowired
+    ObjectFileSystem objectFileSystem;
+
+    public IngestProcessor initIngestProcessor(IngestProcessor p) {
+        p.setObjectFileSystem(objectFileSystem);
+        p.setApplicationProperties(applicationProperties);
+        p.init();
+        return p;
+    }
+
 }
