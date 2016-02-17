77c55a167a0f5242abc1fe0ef792d0eee75d05cd
Matt Chambers <mvchambers@me.com>
2016-02-17 17:16:51 -0500

    Added a test for ProxyProcessor

---
+++ Analyst/src/test/java/com/zorroa/analyst/ingestors/ProxyProcessorTests.java
@@ -0,0 +1,44 @@
+package com.zorroa.analyst.ingestors;
+
+import com.zorroa.analyst.AbstractTest;
+import com.zorroa.archivist.sdk.domain.ApplicationProperties;
+import com.zorroa.archivist.sdk.domain.AssetBuilder;
+import com.zorroa.archivist.sdk.domain.Proxy;
+import com.zorroa.archivist.sdk.filesystem.ObjectFileSystem;
+import com.zorroa.archivist.sdk.processor.ingest.IngestProcessor;
+import com.zorroa.archivist.sdk.schema.ProxySchema;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.File;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by chambers on 2/17/16.
+ */
+public class ProxyProcessorTests extends AbstractTest {
+
+    @Autowired
+    ApplicationProperties applicationProperties;
+
+    @Autowired
+    ObjectFileSystem objectFileSystem;
+
+    @Test
+    public void testProcess() {
+
+        IngestProcessor imageProcessor = initIngestProcessor(new ImageIngestor());
+        IngestProcessor proxyProcessor = initIngestProcessor(new ProxyProcessor());
+        
+        AssetBuilder builder = new AssetBuilder(new File("src/test/resources/images/toucan.jpg"));
+        imageProcessor.process(builder);
+        proxyProcessor.process(builder);
+
+        for (Proxy proxy: builder.getSchema("proxies", ProxySchema.class)) {
+            assertTrue(new File(proxy.getPath()).isFile());
+        }
+    }
+
+
+}
