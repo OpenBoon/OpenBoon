087bad0dac5393940082ff7c99b9fdb15e71bc77
Matt Chambers <mvchambers@me.com>
2016-02-18 12:45:58 -0500

    Added a logs array to the AnalyzeResult so we can push elastic errors into the event log.

--- ArchivistSDK/src/main/java/com/zorroa/archivist/sdk/domain/AnalyzeResult.java
+++ ArchivistSDK/src/main/java/com/zorroa/archivist/sdk/domain/AnalyzeResult.java
@@ -1,6 +1,10 @@
 package com.zorroa.archivist.sdk.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.google.common.base.MoreObjects;
+import com.google.common.collect.Lists;
+
+import java.util.List;
 
 /**
  * No need for getters/setters in this class
@@ -13,12 +17,16 @@ public class AnalyzeResult {
     public int retries = 0;
     public int errors = 0;
 
+    @JsonIgnore
+    public List<String> logs = Lists.newArrayList();
+
     public AnalyzeResult add(AnalyzeResult other) {
         created+=other.created;
         updated+=other.updated;
         warnings+=other.warnings;
         errors+=other.errors;
         retries+=other.retries;
+        logs.addAll(other.logs);
         return this;
     }
 
