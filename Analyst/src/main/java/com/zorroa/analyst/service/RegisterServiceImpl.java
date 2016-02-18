38877ab5052807d54683e77d35fe9330225d8da5
Matt Chambers <mvchambers@me.com>
2016-02-18 16:24:13 -0500

    Send the full ingestor class list to the archivist.

--- Analyst/src/main/java/com/zorroa/analyst/service/RegisterServiceImpl.java
+++ Analyst/src/main/java/com/zorroa/analyst/service/RegisterServiceImpl.java
@@ -1,9 +1,14 @@
 package com.zorroa.analyst.service;
 
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Lists;
+import com.google.common.reflect.ClassPath;
 import com.google.common.util.concurrent.AbstractScheduledService;
 import com.zorroa.archivist.sdk.client.archivist.ArchivistClient;
 import com.zorroa.archivist.sdk.domain.AnalystPing;
 import com.zorroa.archivist.sdk.domain.ApplicationProperties;
+import com.zorroa.archivist.sdk.processor.ProcessorFactory;
+import com.zorroa.archivist.sdk.processor.ingest.IngestProcessor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +16,11 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
 
@@ -61,7 +69,31 @@ public class RegisterServiceImpl extends AbstractScheduledService implements Reg
         }
     }
 
+    private Set<String> EXCLUDE_INGESTORS =
+            ImmutableSet.of("com.zorroa.archivist.sdk.processor.ingest.IngestProcessor");
+
     private AnalystPing getPing() throws UnknownHostException {
+
+        List<String> ingestClasses = Lists.newArrayListWithCapacity(30);
+        try {
+            ClassLoader classLoader = new ProcessorFactory<>().getSiteClassLoader();
+            ClassPath classPath = ClassPath.from(classLoader);
+            for (ClassPath.ClassInfo info: classPath.getTopLevelClassesRecursive("com.zorroa")) {
+                try {
+                    Class<?> clazz = classLoader.loadClass(info.getName());
+                    if (IngestProcessor.class.isAssignableFrom(clazz) && !EXCLUDE_INGESTORS.contains(info.getName())) {
+                        ingestClasses.add(info.getName());
+                    }
+                } catch (NoClassDefFoundError | ClassNotFoundException ignore) {
+                    /*
+                     * This fails for dynamically loaded classes that inherit from an
+                     * external base class.
+                     */
+                }
+            }
+        } catch (IOException e) {
+            logger.warn("Failed to load ingestors, ", e);
+        }
 
         ThreadPoolTaskExecutor e = (ThreadPoolTaskExecutor) ingestThreadPool;
         AnalystPing ping = new AnalystPing();
@@ -70,6 +102,7 @@ public class RegisterServiceImpl extends AbstractScheduledService implements Reg
         ping.setThreadsActive(e.getActiveCount());
         ping.setThreadsTotal(e.getPoolSize());
         ping.setData(properties.getBoolean("analyst.index.data"));
+        ping.setIngestProcessorClasses(ingestClasses);
         return ping;
     }
 
