6d781ad696811ca956f58d695da6d19cbe60780f
Matt Chambers <mvchambers@me.com>
2016-02-18 13:54:21 -0500

    Fixed logging definition in UUIDFileSystem

--- ArchivistSDK/src/main/java/com/zorroa/archivist/sdk/filesystem/UUIDFileSystem.java
+++ ArchivistSDK/src/main/java/com/zorroa/archivist/sdk/filesystem/UUIDFileSystem.java
@@ -16,7 +16,7 @@ import java.util.UUID;
 
 public class UUIDFileSystem implements ObjectFileSystem {
 
-    protected final Logger logger = LoggerFactory.getLogger(UUIDFileSystem.class);
+    private static final Logger logger = LoggerFactory.getLogger(UUIDFileSystem.class);
 
     private static final int DEEPNESS = 4;
 
