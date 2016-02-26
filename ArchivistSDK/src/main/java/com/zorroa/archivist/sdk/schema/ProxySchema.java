7469845772347c7aa78401eb876b14c1cc490cc9
Matt Chambers <mvchambers@me.com>
2016-02-26 18:09:15 -0500

    ProxySchema: some useful functions for the proxy schema.

--- ArchivistSDK/src/main/java/com/zorroa/archivist/sdk/schema/ProxySchema.java
+++ ArchivistSDK/src/main/java/com/zorroa/archivist/sdk/schema/ProxySchema.java
@@ -13,4 +13,58 @@ public class ProxySchema extends ListSchema<Proxy> {
     public String getNamespace() {
         return "proxies";
     }
+
+    /**
+     * Return the largest proxy.
+     *
+     * @return
+     */
+    @JsonIgnore
+    public Proxy getLargest()  {
+        return stream()
+                .sorted((o1, o2) -> Integer.compare(o2.getWidth(), o1.getWidth()))
+                .findFirst().orElse(null);
+    }
+
+    /**
+     * Return the smallest proxy.
+     *
+     * @return
+     */
+    @JsonIgnore
+    public Proxy getSmallest()  {
+        return stream()
+                .sorted((o1, o2) -> Integer.compare(o1.getWidth(), o2.getWidth()))
+                .findFirst().orElse(null);
+    }
+
+    /**
+     * Return the first proxy greater than or equal to the minimum dimension.  If there is problem loading
+     * the proxy IOException is thrown.
+     *
+     * @param minDim
+     * @return
+     */
+    @JsonIgnore
+    public Proxy atLeastThisSize(int minDim)  {
+        return stream()
+                .filter(p->p.getWidth() >= minDim || p.getHeight() >= minDim)
+                .sorted((o1, o2) -> Integer.compare(o1.getWidth(), o2.getWidth()))
+                .findFirst().orElse(null);
+    }
+
+    /**
+     * Return the first proxy less than or equal to the minimum dimension.  If there is problem loading
+     * the proxy IOException is thrown.
+     *
+     * @param minDim
+     * @return
+     */
+    @JsonIgnore
+    public Proxy thisSizeOrBelow(int minDim)  {
+        return stream()
+                .filter(p->p.getWidth() <=minDim || p.getHeight() <= minDim)
+                .sorted((o1, o2) -> Integer.compare(o2.getWidth(), o1.getWidth()))
+                .findFirst().orElse(null);
+    }
 }
