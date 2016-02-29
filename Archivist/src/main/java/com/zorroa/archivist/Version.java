d0aad549cbed74cdaca4d771f398dbd49f8c6a74
Matt Chambers <mvchambers@me.com>
2016-02-29 14:30:30 -0500

    Added a Version class which simple prints the version number.  This is for the init.d script.
    I can be called manually like so:
    java -cp /Users/chambers/src/Archivist/target/archivist.jar com.zorroa.archivist.Version

---
+++ Archivist/src/main/java/com/zorroa/archivist/Version.java
@@ -0,0 +1,20 @@
+package com.zorroa.archivist;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Created by chambers on 2/29/16.
+ */
+public class Version {
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        try {
+            props.load(Version.class.getResourceAsStream("/application.properties"));
+            System.out.println(props.getProperty("info.build.version"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
