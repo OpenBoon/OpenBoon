6611b7c3fa77cef64e6cece064c93353d9d541f3
Matt Chambers <mvchambers@me.com>
2016-03-01 11:45:22 -0500

    Added distribution packager for the analyst.

---
+++ Analyst/dist/package.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+"""
+A simple script for creating a tar that can be installed and
+run on any mac/linux machine.
+
+We could eventually move this to maven or add more options.
+"""
+import os
+import tarfile
+import subprocess
+import shutil
+
+app_name = "analyst"
+
+def main():
+	base_dir = "%s-%s" % (app_name, get_version())
+	cleanup(base_dir)
+	shutil.copytree("resources", base_dir)
+
+	os.mkdir("%s/lib" % base_dir)
+	shutil.copy("../target/%s.jar" % app_name, "%s/lib" % base_dir)
+
+	tar = tarfile.open("%s.tar.gz" % base_dir, "w:gz")
+	tar.add(base_dir)
+	tar.close()
+	cleanup(base_dir)
+
+def cleanup(base_dir):
+	try:
+		shutil.rmtree(base_dir)
+	except OSError, e:
+		# Just ignore this if its not there
+		pass
+
+def get_version():
+	cmd = ["java", "-cp", "../target/%s.jar" % app_name, "com.zorroa.%s.Version" % app_name]
+	output = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0].strip()
+	return output
+
+if __name__ == '__main__':
+	main()
--- Analyst/src/main/java/com/zorroa/analyst/config/ElasticConfig.java
+++ Analyst/src/main/java/com/zorroa/analyst/config/ElasticConfig.java
@@ -1,9 +1,11 @@
 package com.zorroa.analyst.config;
 
 import com.zorroa.analyst.Application;
+import com.zorroa.archivist.sdk.domain.ApplicationProperties;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.common.settings.ImmutableSettings;
 import org.elasticsearch.node.Node;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,6 +21,9 @@ import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
 @Configuration
 public class ElasticConfig {
 
+    @Autowired
+    ApplicationProperties properties;
+
     @Bean
     public Client elastic() throws IOException {
 
@@ -31,6 +36,7 @@ public class ElasticConfig {
         ImmutableSettings.Builder builder =
                 ImmutableSettings.settingsBuilder()
                         .put("cluster.name", "zorroa")
+                        .put("path.data", properties.getString("analyst.path.index"))
                         .put("node.name", nodeName)
                         .put("node.master", true)
                         .put("script.native.archivistDate.type", "com.zorroa.common.elastic.ArchivistDateScriptFactory")
@@ -40,7 +46,6 @@ public class ElasticConfig {
                         .put("script.engine.groovy.indexed.update", true);
 
         if (Application.isUnitTest()) {
-            builder.put("path.data", "unittest/data");
             builder.put("index.refresh_interval", "1s");
             builder.put("index.translog.disable_flush", true);
         }
