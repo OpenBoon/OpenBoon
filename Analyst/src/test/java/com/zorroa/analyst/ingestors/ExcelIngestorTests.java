0879d8f0e405e7555aab021082c1527a7faf97fc
Dan Wexler <wex@zorroa.com>
2016-03-06 20:35:45 -0800

    Add Geotools and multi-column location mapping to ExcelIngestor.

--- Analyst/src/test/java/com/zorroa/analyst/ingestors/ExcelIngestorTests.java
+++ Analyst/src/test/java/com/zorroa/analyst/ingestors/ExcelIngestorTests.java
@@ -6,6 +6,7 @@ import com.zorroa.archivist.sdk.domain.AssetBuilder;
 import org.apache.commons.lang.time.DateUtils;
 import org.junit.Test;
 
+import java.awt.geom.Point2D;
 import java.io.File;
 import java.util.Arrays;
 import java.util.Calendar;
@@ -67,4 +68,31 @@ public class ExcelIngestorTests extends AbstractTest {
         assertTrue(DateUtils.isSameDay(asset.getAttr("Excel", "RIG_RELEASE_DATE"), calendar.getTime()));
     }
 
+    @Test
+    public void testGeoPoints() {
+        ExcelIngestor excelIngestor = new ExcelIngestor();
+        excelIngestor.fileName = "Data_Listing-Caswell.xlsx";
+        ExcelIngestor.RowMapping rowMapping1 = new ExcelIngestor.RowMapping();
+        rowMapping1.assetField = "petrol.wellName";
+        rowMapping1.sheetName = "WCR - Data Listing";
+        rowMapping1.matchFunction = ExcelIngestor.MatchFunction.containsField;
+        rowMapping1.matchFilters = Arrays.asList(ExcelIngestor.MatchFilter.toLower);
+        rowMapping1.titleRow = 5;
+        rowMapping1.matchColumn = "WELL NAME";
+        rowMapping1.outputColumns = Arrays.asList("GA_BASIN", "GA_SUB_BASIN", "OPERATOR", "SPUD_DATE", "RIG_RELEASE_DATE");
+        rowMapping1.keywordColumns = rowMapping1.outputColumns;
+        rowMapping1.geoColumns = ImmutableList.of(new ExcelIngestor
+                .GeoPointColumns("Location", ExcelIngestor.GeoPointType.latitudeLongitude,
+                    ImmutableList.of("WELL_LAT_DEC_DEGREES", "WELL_LONG_DEC_DEGREES")));
+        excelIngestor.rowMappings = ImmutableList.of(rowMapping1);
+        initIngestProcessor(excelIngestor);
+        File file = getResourceFile("/images/toucan.jpg");
+        AssetBuilder asset = new AssetBuilder(file.getAbsolutePath());
+        asset.getSource().setType("image/" + asset.getExtension());
+        asset.setAttr("petrol", "wellName", "Arlo 1");
+        excelIngestor.process(asset);
+        Point2D.Double p = asset.getAttr("Excel", "Location");
+        Point2D.Double q = new Point2D.Double(-12.97785444, 123.78593278);
+        assertEquals(p, q);
+    }
 }
