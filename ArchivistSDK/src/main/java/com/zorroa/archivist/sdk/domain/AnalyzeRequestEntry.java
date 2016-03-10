b06e8c10298c62adad0759c7fb4093786454dc25
Matt Chambers <mvchambers@me.com>
2016-03-10 17:49:20 -0500

    Remove uneeded logging.

--- ArchivistSDK/src/main/java/com/zorroa/archivist/sdk/domain/AnalyzeRequestEntry.java
+++ ArchivistSDK/src/main/java/com/zorroa/archivist/sdk/domain/AnalyzeRequestEntry.java
@@ -1,8 +1,6 @@
 package com.zorroa.archivist.sdk.domain;
 
 import com.google.common.collect.Maps;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.net.URI;
 import java.util.Map;
@@ -11,16 +9,12 @@ import java.util.Map;
  * Created by chambers on 3/9/16.
  */
 public class AnalyzeRequestEntry {
-
-    static final Logger logger = LoggerFactory.getLogger(AnalyzeRequestEntry.class);
-
     private String uri;
     private Map<String, Object> attrs;
 
     public AnalyzeRequestEntry() { }
 
     public AnalyzeRequestEntry(URI uri) {
-        logger.info("URI {}", uri);
         this.uri = uri.toString();
         this.attrs = Maps.newHashMap();
     }
