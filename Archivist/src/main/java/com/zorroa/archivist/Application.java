2ea69463486ac34159ed168ce872da6a69f7cdb3
Matt Chambers <mvchambers@me.com>
2016-03-11 11:43:46 -0500

    Added PID file option to Archivist launch script.

--- Archivist/src/main/java/com/zorroa/archivist/Application.java
+++ Archivist/src/main/java/com/zorroa/archivist/Application.java
@@ -1,6 +1,7 @@
 package com.zorroa.archivist;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.system.ApplicationPidFileWriter;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -13,6 +14,10 @@ import org.springframework.context.annotation.ComponentScan;
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication app = new SpringApplication(Application.class);
+        if (System.getenv("PIDFILE") != null) {
+            app.addListeners(new ApplicationPidFileWriter());
+        }
+        app.run(args);
     }
 }
