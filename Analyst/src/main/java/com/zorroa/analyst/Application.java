d914c4b5674ff263111f386f2462b443c609c687
Matt Chambers <mvchambers@me.com>
2016-03-15 13:18:18 -0400

    Refactored command line arguments available in the analyst startup script.  getopt does not support arguments like -Darg.name=value, but the hack to get around that fails if there are spaces in the path. The solution was to remove the getopt parsing in the shell script.  Some features are lost however.
    To provide additonal JAVA_OPTS, you must use the JAVA_OPTs env var.
    
    For analyst specific options, you pass them in like so: --analyst.path.models=/new/path
    
    Spaces in the value are supported by using quotation marks: --analyst.path.models="/new/path to models"

--- Analyst/src/main/java/com/zorroa/analyst/Application.java
+++ Analyst/src/main/java/com/zorroa/analyst/Application.java
@@ -1,11 +1,15 @@
 package com.zorroa.analyst;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.system.ApplicationPidFileWriter;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.env.SimpleCommandLinePropertySource;
 
 /**
  * Created by chambers on 2/8/16.
@@ -15,12 +19,15 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @EnableConfigurationProperties
 public class Application {
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
+        PropertySource ps = new SimpleCommandLinePropertySource(args);
         SpringApplication app =
                 new SpringApplication(Application.class);
-        if (System.getenv("PIDFILE") != null) {
-            app.addListeners(new ApplicationPidFileWriter());
+
+        if (ps.getProperty("p") != null) {
+            app.addListeners(new ApplicationPidFileWriter((String) ps.getProperty("p")));
         }
         app.run(args);
     }
