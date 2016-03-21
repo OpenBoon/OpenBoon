0cf0ade2df9a8a58fbf40c126145640abc6a2212
Dan Wexler <wex@zorroa.com>
2016-03-20 17:05:47 -0700

    Test aggregator update

---
+++ Archivist/src/test/java/com/zorroa/archivist/TestAggregator.java
@@ -0,0 +1,14 @@
+package com.zorroa.archivist;
+
+import com.zorroa.archivist.sdk.processor.Aggregator;
+
+/**
+ * Created by wex on 3/20/16.
+ */
+public class TestAggregator extends Aggregator {
+
+    @Override
+    public void aggregate() {
+
+    }
+}
