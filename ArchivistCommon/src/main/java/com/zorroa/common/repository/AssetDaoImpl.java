4db7d9aca82a8b58b1f4bcbdfe176a17ec4e6f04
Matt Chambers <mvchambers@me.com>
2016-03-21 11:45:27 -0400

    AssetDao: fixes issue where an elastic error could not be recovered from because of the new asset delimiter.

--- ArchivistCommon/src/main/java/com/zorroa/common/repository/AssetDaoImpl.java
+++ ArchivistCommon/src/main/java/com/zorroa/common/repository/AssetDaoImpl.java
@@ -134,7 +134,7 @@ public class AssetDaoImpl extends AbstractElasticDao implements AssetDao {
         for (Pattern pattern: RECOVERABLE_BULK_ERRORS) {
             Matcher matcher = pattern.matcher(error);
             if (matcher.find()) {
-                return asset.removeAttr(matcher.group(1));
+                return asset.removeAttr(matcher.group(1).replace(".", ":"));
             }
         }
         return false;
