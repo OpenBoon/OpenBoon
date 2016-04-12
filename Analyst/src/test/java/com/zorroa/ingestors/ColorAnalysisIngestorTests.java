1fa4ae9de6d97c0b439a09a5d8c8e8f8f528e24e
Matt Chambers <mvchambers@me.com>
2016-04-12 14:06:27 -0400

    Revert "Revert "Merge pull request #37 from Zorroa/bh-colorAnalysis""
    This reverts commit 5efd411cc72405d22d2311c949a62deaa0e7ea04.

---
+++ Analyst/src/test/java/com/zorroa/ingestors/ColorAnalysisIngestorTests.java
@@ -0,0 +1,39 @@
+package com.zorroa.ingestors;
+
+import com.zorroa.archivist.sdk.domain.AssetBuilder;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+
+import static org.junit.Assert.assertNotEquals;
+
+/**
+ * Created by barbara on 1/10/16.
+ */
+public class ColorAnalysisIngestorTests extends AssetBuilderTests {
+
+    @Test
+    public void testProcess() throws InterruptedException {
+
+        // Set up and args passing/testing
+        ColorAnalysisIngestor testIngestor = new ColorAnalysisIngestor();
+        HashMap<String, Object> testIngestorArgs = new HashMap<String, Object>();
+        /* String colorSpace = new String("HSV");
+        testIngestorArgs.put("ColorSpace", colorSpace);
+        testIngestor.setArgs(testIngestorArgs);*/
+        setup(testIngestor);
+
+        for (AssetBuilder asset : testAssets) {
+
+            // Process the asset to compute histograms
+            testIngestor.process(asset);
+            Map<int[], Float> analysisResults = asset.getAttr("ColorAnalysis", "ColorClusters");
+            Map<int[], String> colorMapping = asset.getAttr("ColorAnalysis", "ColorMapping");
+            assertNotEquals(analysisResults, null);
+            assertNotEquals(colorMapping, null);
+        }
+    }
+
+}
