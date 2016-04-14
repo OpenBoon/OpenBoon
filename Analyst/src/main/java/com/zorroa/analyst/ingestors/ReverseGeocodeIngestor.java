f04ca778b7e2821eb5e769569fdaacb7ddc87692
Matt Chambers <mvchambers@me.com>
2016-04-14 16:53:37 -0400

    ReverseGeocode: added test, fixed an issue with the default attribute location being wrong

--- Analyst/src/main/java/com/zorroa/analyst/ingestors/ReverseGeocodeIngestor.java
+++ Analyst/src/main/java/com/zorroa/analyst/ingestors/ReverseGeocodeIngestor.java
@@ -4,47 +4,51 @@ import com.zorroa.archivist.sdk.domain.AssetBuilder;
 import com.zorroa.archivist.sdk.geocode.GeoName;
 import com.zorroa.archivist.sdk.processor.Argument;
 import com.zorroa.archivist.sdk.processor.ingest.IngestProcessor;
+import com.zorroa.archivist.sdk.schema.LocationSchema;
 import com.zorroa.archivist.sdk.util.GeoUtils;
 
-import java.awt.geom.Point2D;
-
-import static com.zorroa.archivist.sdk.domain.Attr.attr;
-
 /**
- * Created by wex on 3/7/16.
+ * Looks at the a Location schema and attempts to fill in more data
+ * using the GeoUtils.
  */
 public class ReverseGeocodeIngestor extends IngestProcessor {
 
     @Argument
-    String assetField = "source.location";
-
-    @Argument
-    String namespace = "place";
+    String locationAttr = "location";
 
     @Argument
     boolean isKeyword = true;
 
     @Override
     public void process(AssetBuilder asset) {
-        Point2D.Double pt = asset.getAttr(assetField);
-        if (pt == null) {
+        LocationSchema location = asset.getAttr(locationAttr, LocationSchema.class);
+        if (location == null) {
+            logger.debug("The location attr '{}' does not exist", locationAttr);
             return;
         }
-        GeoName place = GeoUtils.nearestPlace(pt.x, pt.y);
+
+        logger.debug("Looking up location for {},{}",location.getPoint()[1], location.getPoint()[0]);
+        GeoName place = GeoUtils.nearestPlace(
+                location.getPoint()[1],
+                location.getPoint()[0]);
+
         if (place == null) {
+            logger.debug("The name of the location {},{} was unable to be found.", location);
             return;
         }
-        logger.info("Nearest town is " + place.name + " in " + place.country);
+
+        logger.debug("Nearest town is " + place.name + " in " + place.country);
+
         if (place.name != null) {
-            asset.setAttr(attr(namespace, "name"), place.name);
+            location.setCity(place.name);
             if (isKeyword) {
-                asset.addSuggestKeywords("gis", place.name);
+                asset.addSuggestKeywords(locationAttr, place.name);
             }
         }
         if (place.country != null) {
-            asset.setAttr(attr(namespace, "country"), place.country);
+            location.setCountry(place.country);
             if (isKeyword) {
-                asset.addSuggestKeywords("gis", place.country);
+                asset.addSuggestKeywords(locationAttr, place.country);
             }
         }
     }
