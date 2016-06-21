44e28053e5688e4a6b27b750c3c5fdb5f3962929
Matt Chambers <mvchambers@me.com>
2016-06-21 19:12:53 -0400

    web: removed uneeded logging.

--- archivist/src/main/java/com/zorroa/archivist/web/gui/IndexController.java
+++ archivist/src/main/java/com/zorroa/archivist/web/gui/IndexController.java
@@ -212,7 +212,6 @@ public class IndexController {
         }
         Collections.reverse(path);
         model.addAttribute("path", path);
-        logger.info("elements: {}", path.size());
         return "folders";
     }
 
@@ -270,7 +269,6 @@ public class IndexController {
             return "ingest";
         }
         else {
-            logger.info("uris: {}", ingestUpdateBuilder.getUris());
             ingestService.updateIngest(ingestService.getIngest(id), ingestUpdateBuilder);
         }
 
