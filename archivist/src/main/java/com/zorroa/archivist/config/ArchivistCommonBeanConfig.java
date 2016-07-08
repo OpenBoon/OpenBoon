df42701b6abf94f92036e8154bb39b4066606313
Matt Chambers <mvchambers@me.com>
2016-07-07 23:18:03 -0400

    archivist: added a standard bean config from common.

---
+++ archivist/src/main/java/com/zorroa/archivist/config/ArchivistCommonBeanConfig.java
@@ -0,0 +1,17 @@
+package com.zorroa.archivist.config;
+
+import com.zorroa.common.config.CommonBeanConfig;
+import com.zorroa.common.repository.*;
+import com.zorroa.common.service.EventLogService;
+import com.zorroa.common.service.EventLogServiceImpl;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by chambers on 2/16/16.
+ */
+@Configuration
+public class ArchivistCommonBeanConfig extends CommonBeanConfig {
+
+}
