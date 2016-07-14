ead9fa4622c73980bec4eecf4be942fd018d639a
Matt Chambers <mvchambers@me.com>
2016-07-14 06:23:05 -0400

    User info cache.
    Cache user Id to email address info (maybe more later), so we can send
    down an actual username and not have to resolve it from the client.

--- archivist/src/main/java/com/zorroa/archivist/repository/AbstractDao.java
+++ archivist/src/main/java/com/zorroa/archivist/repository/AbstractDao.java
@@ -1,11 +1,16 @@
 package com.zorroa.archivist.repository;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 public class AbstractDao {
 
@@ -18,8 +23,22 @@ public class AbstractDao {
         this.jdbc = new JdbcTemplate(dataSource);
     }
 
-    protected String resolveUser(int id) {
-        return jdbc.queryForObject("SELECT str_email FROM user WHERE pk_user=?", String.class, id);
-    }
+    private LoadingCache<Integer, String> cachedUserName = CacheBuilder.newBuilder()
+            .expireAfterWrite(24, TimeUnit.HOURS)
+            .initialCapacity(32)
+            .maximumSize(512)
+            .build(new CacheLoader<Integer, String>() {
+                @Override
+                public String load(Integer key) throws Exception {
+                    return jdbc.queryForObject("SELECT str_email FROM user WHERE pk_user=?", String.class, key);
+                }
+            });
 
+    protected String resolveUser(int id) {
+        try {
+            return cachedUserName.get(id);
+        } catch (Exception e) {
+            return "unknown";
+        }
+    }
 }
