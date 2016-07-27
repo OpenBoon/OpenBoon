61816e9fc7290674b076a9de707f4c02ae023dfb
Matt Chambers <mvchambers@me.com>
2016-07-27 16:07:55 -0400

    archivist: ofs fixed broken test.

--- archivist/src/test/java/com/zorroa/archivist/web/FileSystemControllerTests.java
+++ archivist/src/test/java/com/zorroa/archivist/web/FileSystemControllerTests.java
@@ -7,15 +7,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.util.NestedServletException;
 
 import java.io.IOException;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -39,16 +35,9 @@ public class FileSystemControllerTests extends AbstractTest {
 
     @Test
     public void testBrokenProxy() throws Exception {
-        try {
-            String uri = "a/b/c/1/2/3/bogus.jpg";
-            MvcResult result = mvc.perform(get("/api/v1/fs/proxies/" + uri))
-                    .andExpect(status().is4xxClientError())
-                    .andReturn();
-            fail();
-        } catch (Exception e) {
-//            logger.debug("Successfully caught invalid proxy exception {}", e);
-            assertEquals(NestedServletException.class, e.getClass());
-            assertEquals("java.lang.IllegalArgumentException: Invalid object ID: bogus.jpg", e.getCause().getMessage());
-        }
+        String uri = "a/b/c/1/2/3/bogus.jpg";
+         mvc.perform(get("/api/v1/fs/proxies/" + uri))
+                .andExpect(status().is4xxClientError())
+                .andReturn();
     }
 }
