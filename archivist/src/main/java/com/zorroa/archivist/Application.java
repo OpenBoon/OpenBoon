31326a7dddf29903c5be82dfa9ffaf9771e1b279
Matt Chambers <mvchambers@me.com>
2017-02-08 10:39:20 -0500

    Fixes deprecated class.

--- archivist/src/main/java/com/zorroa/archivist/Application.java
+++ archivist/src/main/java/com/zorroa/archivist/Application.java
@@ -1,10 +1,10 @@
 package com.zorroa.archivist;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.system.ApplicationPidFileWriter;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.system.ApplicationPidFileWriter;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.PropertySource;
 import org.springframework.core.env.SimpleCommandLinePropertySource;
