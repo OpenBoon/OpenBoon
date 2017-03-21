b09cab6734c74e47e43ceca50f2b94dc50c9bbde
Matt Chambers <mvchambers@me.com>
2017-03-21 10:47:17 -0400

    Added term vector check for new path mapping.

--- common/src/test/java/com/zorroa/common/elastic/ArchivistMappingTests.java
+++ common/src/test/java/com/zorroa/common/elastic/ArchivistMappingTests.java
@@ -1,13 +1,24 @@
 package com.zorroa.common.elastic;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.zorroa.common.AbstractTest;
+import org.apache.lucene.index.Fields;
+import org.apache.lucene.index.Terms;
+import org.apache.lucene.index.TermsEnum;
+import org.apache.lucene.util.BytesRef;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.termvectors.TermVectorsResponse;
 import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.cluster.metadata.IndexMetaData;
 import org.elasticsearch.cluster.metadata.MappingMetaData;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import static org.junit.Assert.assertEquals;
 
@@ -35,4 +46,52 @@ public class ArchivistMappingTests extends AbstractTest {
         assertEquals("geo_point", getMappingType(mdd.getSourceAsMap(), "location.point"));
     }
 
+    /**
+     * Test to ensure the mapping handles geo points.
+     *
+     * @throws IOException
+     */
+    @Test
+    public void testPathMapping() throws IOException, ExecutionException, InterruptedException {
+        IndexResponse rsp = client.prepareIndex("archivist", "asset")
+                .setSource(ImmutableMap.of("path", "/foo/bar/bing"))
+                .get();
+        refreshIndex();
+
+        ClusterState cs = client.admin().cluster().prepareState().setIndices("archivist").execute().actionGet().getState();
+        IndexMetaData imd = cs.getMetaData().index("archivist");
+        MappingMetaData mdd = imd.mapping("asset");
+        assertEquals("string", getMappingType(mdd.getSourceAsMap(), "path"));
+
+        TermVectorsResponse tv = client.prepareTermVectors()
+                .setIndex("archivist")
+                .setType("asset")
+                .setId(rsp.getId())
+                .setSelectedFields("path")
+                .setOffsets(true)
+                .setPositions(true)
+                .setTermStatistics(true)
+                .setFieldStatistics(true)
+                .execute().get();
+        refreshIndex();
+        assertEquals(ImmutableList.of("/foo", "/foo/bar", "/foo/bar/bing"),  getTerms(tv));
+    }
+
+    public List<String> getTerms(TermVectorsResponse resp) throws IOException {
+        List<String> termStrings = new ArrayList<>();
+        Fields fields = resp.getFields();
+        Iterator<String> iterator = fields.iterator();
+        while (iterator.hasNext()) {
+            String field = iterator.next();
+            Terms terms = fields.terms(field);
+            TermsEnum termsEnum = terms.iterator();
+            while(termsEnum.next() != null){
+                BytesRef term = termsEnum.term();
+                if (term != null) {
+                    termStrings.add(term.utf8ToString());
+                }
+            }
+        }
+        return termStrings;
+    }
 }
