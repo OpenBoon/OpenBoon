97cbea53e6ef8721a1b3821e2151c6f60805e8f7
Dan Wexler <wex@zorroa.com>
2017-04-09 00:53:27 -0700

    Skip hidden files and secret places.

--- cloudproxy/src/main/java/com/zorroa/cloudproxy/service/FilesystemService.java
+++ cloudproxy/src/main/java/com/zorroa/cloudproxy/service/FilesystemService.java
@@ -8,5 +8,5 @@ import java.util.List;
  * Created by wex on 4/1/17.
  */
 public interface FilesystemService {
-    List<FilesystemEntry> get(String path);
+    List<FilesystemEntry> get(String path, List<String> hidden);
 }
