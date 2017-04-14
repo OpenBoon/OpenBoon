86852003ee2cebb50de6e35da97784c0d47689b0
Matt Chambers <mvchambers@me.com>
2017-04-14 16:19:41 -0400

    More fixes to cors.

--- archivist/src/main/java/com/zorroa/archivist/security/CorsCredentialsFilter.java
+++ archivist/src/main/java/com/zorroa/archivist/security/CorsCredentialsFilter.java
@@ -35,20 +35,21 @@ public class CorsCredentialsFilter extends GenericFilterBean {
         }
         logger.info("-------------------------------");
         */
-
-        if (req.getMethod().equals("OPTIONS")) {
-            rsp.addHeader("Access-Control-Expose-Headers", "Content-Encoding, content-range, content-length, accept-ranges");
-            rsp.addHeader("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE");
-            rsp.addHeader("Access-Control-Allow-Headers", "authorization, content-type, x-requested-with");
-            rsp.addHeader("Access-Control-Allow-Origin", req.getHeader("origin"));
-            rsp.addHeader("Access-Control-Allow-Credentials", "true");
-            rsp.addHeader("Access-Control-Max-Age", "3600");
-        }
-
+        
         String auth = req.getHeader("authorization");
         if (auth != null) {
             rsp.addHeader("Access-Control-Allow-Credentials", "true");
             rsp.addHeader("Access-Control-Allow-Origin", req.getHeader("origin"));
+        }
+        else {
+            rsp.addHeader("Access-Control-Expose-Headers", "Content-Encoding, content-range, content-length, accept-ranges");
+            rsp.addHeader("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE");
+            rsp.addHeader("Access-Control-Allow-Headers", "authorization, content-type, x-requested-with");
+            rsp.addHeader("Access-Control-Allow-Credentials", "true");
+            rsp.addHeader("Access-Control-Allow-Origin", req.getHeader("origin"));
+            if (req.getMethod().equals("OPTIONS")) {
+                rsp.addHeader("Access-Control-Max-Age", "3600");
+            }
         }
 
         filterChain.doFilter(servletRequest, servletResponse);
