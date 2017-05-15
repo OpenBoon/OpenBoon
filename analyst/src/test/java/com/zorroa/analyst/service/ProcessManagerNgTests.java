bd9ea00085133d2adc35a92911ee7041733bbc71
Matt Chambers <mvchambers@me.com>
2017-05-15 11:47:44 -0400

    analyst: added start of unit tests for ProcessManagerNg

---
+++ analyst/src/test/java/com/zorroa/analyst/service/ProcessManagerNgTests.java
@@ -0,0 +1,47 @@
+package com.zorroa.analyst.service;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import com.zorroa.analyst.AbstractTest;
+import com.zorroa.analyst.cluster.ClusterProcess;
+import com.zorroa.common.cluster.thrift.TaskStartT;
+import com.zorroa.sdk.util.Json;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.concurrent.ExecutionException;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by chambers on 5/8/17.
+ */
+public class ProcessManagerNgTests extends AbstractTest {
+
+    @Autowired
+    ProcessManagerNgService processManagerNg;
+
+    int task = 0;
+
+    @Test
+    public void testExecute() throws IOException, ExecutionException, InterruptedException {
+
+        TaskStartT ts = new TaskStartT()
+                .setId(1)
+                .setJobId(1)
+                .setArgMap(Json.serialize(ImmutableMap.of("path", "../unittest/resources/images/set01")))
+                .setName("task")
+                .setEnv(Maps.newHashMap())
+                .setSharedDir("../unittest/shared")
+                .setMasterHost("http://localhost:8066")
+                .setWorkDir(Files.createTempDirectory("zorroa_analyst_test").toString());
+        
+        ts.setScriptPath(resources
+                .resolve("scripts/import.zps").toString());
+        ClusterProcess p = processManagerNg.executeClusterTask(ts);
+        assertEquals(0, p.getExitStatus());
+    }
+
+}
