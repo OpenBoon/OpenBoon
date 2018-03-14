44fde0ef85973b1b79fc34bdb9efca2f6341d113
Matt Chambers <mvchambers@me.com>
2018-03-14 14:50:22 -0400

    archivist: minor fixes to PermissionSpec

--- archivist/src/main/kotlin/com/zorroa/archivist/domain/Permission.kt
+++ archivist/src/main/kotlin/com/zorroa/archivist/domain/Permission.kt
@@ -9,14 +9,15 @@ class PermissionSpec {
 
     var name: String? = null
     var type: String? = null
+    var source: String = "local"
     var description: String? = null
 
     constructor()
 
     constructor(authority: String) {
-        val parts = authority.split(Permission.JOIN.toRegex(), 2).toTypedArray()
+        val parts = authority.split(Permission.JOIN, limit=2).toTypedArray()
         if (parts.size == 1) {
-            throw IllegalArgumentException("Invalid authority name: " + authority)
+            throw IllegalArgumentException("Invalid authority name: $authority")
         }
         this.name = parts[1]
         this.type = parts[0]
@@ -26,5 +27,6 @@ class PermissionSpec {
         this.name = name
         this.type = type
     }
+
 }
 
