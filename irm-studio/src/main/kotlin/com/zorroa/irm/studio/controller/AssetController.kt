83c03289b1026d917b606ac6ffe2089c8634af25
Matt Chambers <mvchambers@me.com>
2018-06-19 13:32:28 -0400

    IRM Studio / Ingest Controller app.
    This is a slightly IRM specific implementation of the studio backend. Studio is an application
    for developing pipelines for integrating your enterprise with Zorroa.

---
+++ irm-studio/src/main/kotlin/com/zorroa/irm/studio/controller/AssetController.kt
@@ -0,0 +1,13 @@
+package com.zorroa.irm.studio.controller
+
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+class AssetController {
+
+
+
+
+
+}
---
+++ irm-studio/src/main/kotlin/com/zorroa/irm/studio/controller/PipelineController.kt
@@ -0,0 +1,2 @@
+package com.zorroa.irm.studio.controller
+
