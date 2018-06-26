c3e28a2b1a31a4c66d9e220a98f5ed5959c2a296
Matt Chambers <mvchambers@me.com>
2018-06-26 11:42:39 -0400

    Added logger for tests

--- irm-studio/src/test/kotlin/com/zorroa/irm/studio/AbstractTest.kt
+++ irm-studio/src/test/kotlin/com/zorroa/irm/studio/AbstractTest.kt
@@ -1,6 +1,7 @@
 package com.zorroa.irm.studio
 
 import org.junit.runner.RunWith
+import org.slf4j.LoggerFactory
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit4.SpringRunner
@@ -12,5 +13,7 @@ import org.springframework.transaction.annotation.Transactional
 @RunWith(SpringRunner::class)
 @SpringBootTest
 open abstract class AbstractTest {
+
+    protected val logger = LoggerFactory.getLogger(javaClass)
 
 }
