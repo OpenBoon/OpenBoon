4db05156f31076105f329cc3dc97d7401cf5e185
Matt Chambers <mvchambers@me.com>
2018-07-20 14:55:00 -0400

    Added a /* to the gsutil command

--- analyst/run_server.sh
+++ analyst/run_server.sh
@@ -3,6 +3,6 @@ if [ -n "${GCS_CONFIG}" ]
 then
   echo "Downloading config files from GCS secret bucket."
   mkdir -p /config
-  /root/google-cloud-sdk/bin/gsutil cp -r "${GCS_CONFIG}" /config
+  /root/google-cloud-sdk/bin/gsutil cp -r "${GCS_CONFIG}/*" /config
 fi
 java -Dloader.path=/config/ext -Djava.security.egd=file:/dev/./urandom -jar /service/app.jar "$@"
