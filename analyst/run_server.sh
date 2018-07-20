a4821ec3c011c2743d39339d9fad7a822e0d6056
Danny Tiesling <danny@zorroa.com>
2018-07-19 17:46:06 -0700

    Merge remote-tracking branch 'remotes/origin/development' into evi-619-analyst-to-use-zbuild
    # Conflicts:
    #	analyst/Dockerfile

--- analyst/run_server.sh
+++ analyst/run_server.sh
@@ -2,6 +2,7 @@
 if [ -n "${GCS_CONFIG}" ]
 then
   echo "Downloading config files from GCS secret bucket."
-  /root/google-cloud-sdk/bin/gsutil cp -r "${GCS_CONFIG}" ./config
+  mkdir -p /config
+  /root/google-cloud-sdk/bin/gsutil cp -r "${GCS_CONFIG}" /config
 fi
-java -Dloader.path=/service/ext -Djava.security.egd=file:/dev/./urandom -jar /service/app.jar "$@"
+java -Dloader.path=/config/ext -Djava.security.egd=file:/dev/./urandom -jar /service/app.jar "$@"
