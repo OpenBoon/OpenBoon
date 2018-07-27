6f5c69e1227103b21601f86b7ce63a12a6eb71de
Matt Chambers <mvchambers@me.com>
2018-07-27 07:40:10 -0400

    Improved jwt signing of http requests.

--- archivist/src/main/kotlin/com/zorroa/archivist/config/ApplicationConfig.kt
+++ archivist/src/main/kotlin/com/zorroa/archivist/config/ApplicationConfig.kt
@@ -135,7 +135,9 @@ class ArchivistConfiguration {
 
     @Bean
     fun analystClient() : AnalystClient {
-        return AnalystClientImpl(getPublicUrl("analyst"))
+        return AnalystClientImpl(getPublicUrl("analyst"),
+                GcpJwtSigner("/config/service-credentials.json"))
+
     }
 
     @Bean
