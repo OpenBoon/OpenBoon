a7703ff5ca482ddaa2f201ad662779c5a77f8928
Danny Tiesling <danny@zorroa.com>
2018-07-27 06:17:51 -0700

    Updates tests so they are passing.

--- analyst/src/test/kotlin/com/zorroa/analyst/repository/PipelineDaoTests.kt
+++ analyst/src/test/kotlin/com/zorroa/analyst/repository/PipelineDaoTests.kt
@@ -22,7 +22,6 @@ class PipelineDaoTests : AbstractTest() {
         val pl1 = PipelineSpec(pipelineName, PipelineType.IMPORT)
         val pl2 = pipelineDao.create(pl1)
         val pl3 = pipelineDao.get(pipelineName)
-
         assertEquals(pl2, pl3)
     }
 
@@ -36,7 +35,7 @@ class PipelineDaoTests : AbstractTest() {
 
     @Test
     fun testUpdate() {
-        val pl1 = pipelineDao.get("import-test")
+        val pl1 = pipelineDao.create(PipelineSpec("import-test", PipelineType.IMPORT))
         pl1.name = "hello"
         pipelineDao.update(pl1)
         val pl2 =  pipelineDao.get("hello")
@@ -44,6 +43,7 @@ class PipelineDaoTests : AbstractTest() {
 
     @Test
     fun testExists() {
+        pipelineDao.create(PipelineSpec("export-test", PipelineType.EXPORT))
         assertTrue(pipelineDao.exists("export-test"))
         assertFalse(pipelineDao.exists("captain kirk"))
     }
