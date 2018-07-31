3a91397e427821fce7ce61066e0921c4d7ba71d0
Matt Chambers <mvchambers@me.com>
2018-07-31 16:49:13 -0400

    Fixes for docker file and run_server

--- analyst/run_server.sh
+++ analyst/run_server.sh
@@ -3,6 +3,6 @@ if [ -n "${GAE_SERVICE}" ]
 then
   echo "Downloading config files from GCS secret bucket."
   mkdir -p /config
-  /root/google-cloud-sdk/bin/gsutil cp -r "${GCLOUD_PROJECT}-zorroa-configuration/{GAE_SERVICE}-config/*" /config
+  /root/google-cloud-sdk/bin/gsutil cp -r "gs://${GCLOUD_PROJECT}-zorroa-configuration/${GAE_SERVICE}-config/*" /config
 fi
-java -Dloader.path=/config/ext -Djava.security.egd=file:/dev/./urandom -jar /service/analyst.jar "$@"
+java -Dloader.path=/config/ext -Djava.security.egd=file:/dev/./urandom -jar /service/app.jar "$@"
