986f1eae509cdc6cb67e1915a8e03733ebccab42
Matt Chambers <mvchambers@me.com>
2018-08-08 11:52:37 -0400

    Fixes some final ignored tests.

--- archivist/src/test/kotlin/com/zorroa/archivist/service/FieldServiceTests.kt
+++ archivist/src/test/kotlin/com/zorroa/archivist/service/FieldServiceTests.kt
@@ -2,7 +2,6 @@ package com.zorroa.archivist.service
 
 import com.zorroa.archivist.AbstractTest
 import org.junit.Before
-import org.junit.Ignore
 import org.junit.Test
 import kotlin.test.assertTrue
 
@@ -14,9 +13,10 @@ class FieldServiceTests : AbstractTest() {
         refreshIndex()
     }
 
-    @Ignore("TODO: Fix this test. It fails on Travis but runs locally.")
     @Test
     fun testGetQueryFields() {
+        // fails in travis, need to find out why.
+        fieldService.invalidateFields()
         val fields = fieldService.getQueryFields()
         assertTrue(fields.containsKey("media.content"))
         assertTrue(fields.containsKey("media.title"))
