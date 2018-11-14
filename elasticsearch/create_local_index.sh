71f1ebb1c8749d9c489af92fe5ef2efd56a9cfd3
Kitt Hodsden <kitt@users.noreply.github.com>
2018-11-14 18:24:12 -0500

    Allow create_local_index.sh to be run from any directory (#176)

--- elasticsearch/create_local_index.sh
+++ elasticsearch/create_local_index.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-curl -XPUT 'http://localhost:9200/zorroa_v10' -H 'Content-Type: application/json' -d @asset_v40.json
+CESDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+curl -XPUT 'http://localhost:9200/zorroa_v10' -H 'Content-Type: application/json' -d @${CESDIR}/asset_v40.json
 
