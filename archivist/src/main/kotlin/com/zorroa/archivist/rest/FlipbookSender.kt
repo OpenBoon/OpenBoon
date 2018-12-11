b047ec4d9679cbd2836cb8b665a57601cc186dda
Matt <mvchambers@me.com>
2018-12-11 14:04:30 -0500

    Moved all the web/api stuff to rest package.

---
+++ archivist/src/main/kotlin/com/zorroa/archivist/rest/FlipbookSender.kt
@@ -0,0 +1,44 @@
+package com.zorroa.archivist.rest
+
+import com.zorroa.archivist.search.AssetSearch
+import com.zorroa.archivist.service.SearchService
+import com.zorroa.common.util.Json
+import javax.servlet.http.HttpServletResponse
+
+data class Frame(
+        val id: String?,
+        val frame: Int?,
+        val filename: String?)
+
+class FlipBook {
+    val frames : MutableList<Frame> = mutableListOf()
+
+    fun getSize() : Int {
+        return frames.size
+    }
+}
+
+class FlipbookSender constructor(
+        val assetId: String,
+        val searchService : SearchService) {
+
+    fun serveResource(response: HttpServletResponse) {
+        response.contentType = "application/json"
+        response.setHeader("Content-Disposition", "inline;filename=\"flipbook-$assetId.json")
+
+        val search = AssetSearch()
+        search.size = 100
+        search.addToFilter().addToTerms("media.clip.parent.raw", assetId)
+        search.addToFilter().addToTerms("media.clip.type.raw", "flipbook")
+        search.fields = arrayOf("_id",  "source", "media")
+
+        val result = FlipBook()
+        for (doc in searchService.scanAndScroll(search, 0)) {
+            result.frames.add(Frame(doc.id,
+                    doc.getAttr("media.clip.start"),
+                    doc.getAttr("source.filename")))
+        }
+        result.frames.sortBy { it.frame }
+        Json.Mapper.writeValue(response.outputStream, result)
+    }
+}
---
+++ archivist/src/main/kotlin/com/zorroa/archivist/rest/RootController.kt
@@ -0,0 +1,15 @@
+package com.zorroa.archivist.rest
+
+import com.zorroa.archivist.config.NetworkEnvironment
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class RootController @Autowired constructor(val networkEnvironment: NetworkEnvironment){
+
+    @GetMapping("/api/v1/commands")
+    fun getCommands() : List<Any> {
+        return listOf()
+    }
+}
