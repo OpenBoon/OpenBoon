a84ed98ae90502c5067d244a943290a6ee782606
Matt <mvchambers@me.com>
2019-03-19 17:21:54 -0400

    EVI-2833 - Implement storing a stack trace on a TaskError.

--- archivist/src/main/kotlin/com/zorroa/archivist/domain/TaskEvent.kt
+++ archivist/src/main/kotlin/com/zorroa/archivist/domain/TaskEvent.kt
@@ -23,13 +23,26 @@ class TaskStoppedEvent(
         val manualKill: Boolean = false
 )
 
+/**
+ * TaskErrorEvents are emitted by the processing system if an an exception is thrown
+ * while processing.
+ *
+ * @property assetId The assetID that was being processed.  This is optional.
+ * @property path The path that was being processed.  This is optional.
+ * @property message The message from the exception that generated the event, or a custom message.
+ * @property processor The processor class the error occurred in.
+ * @property fatal True if the error was fatal.
+ * @property phase The phase at which the error occurred: generate, execute, teardown.
+ * @property stackTrace The full stack trace from the error, if any. This is optional.
+ */
 class TaskErrorEvent(
         val assetId: UUID?,
         val path: String?,
         val message: String,
         val processor: String,
         val fatal: Boolean,
-        val phase: String
+        val phase: String,
+        val stackTrace: List<StackTraceElement>?=null
 )
 
 class TaskMessageEvent(
