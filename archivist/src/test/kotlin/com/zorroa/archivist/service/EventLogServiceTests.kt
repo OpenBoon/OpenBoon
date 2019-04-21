48d883ca720e01a5f7470ede1b27ae496994748b
Matt <mvchambers@me.com>
2019-04-21 07:39:45 -0400

    Fixes broken unittest caused by how Spring does dependency injection.

--- archivist/src/test/kotlin/com/zorroa/archivist/service/EventLogServiceTests.kt
+++ archivist/src/test/kotlin/com/zorroa/archivist/service/EventLogServiceTests.kt
@@ -3,16 +3,15 @@ package com.zorroa.archivist.service
 import com.zorroa.archivist.AbstractTest
 import com.zorroa.archivist.search.AssetFilter
 import com.zorroa.archivist.search.AssetSearch
+import io.micrometer.core.instrument.Counter
 import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.Tag
+import org.junit.Before
 import org.junit.Test
 import org.springframework.beans.factory.annotation.Autowired
 import kotlin.test.assertEquals
 
 class EventLogServiceTests : AbstractTest() {
-
-    @Autowired
-    lateinit var meterRegistry: MeterRegistry
 
     /**
      * Test that we're getting metrics on the searches we're doing. We're not
@@ -20,15 +19,18 @@ class EventLogServiceTests : AbstractTest() {
      */
     @Test
     fun testApplyAssetSearchMetricsFilter() {
-        val counter = meterRegistry.counter("zorroa.asset.search.filter",
-                listOf(Tag.of("type", "exists")))
+        val counterName = "zorroa.asset.search.filter"
+        var counter = MeterRegistryHolder
+                .meterRegisty.counter(counterName, listOf(Tag.of("type", "exists")))
         val currentValue = counter.count()
 
         val search = AssetSearch()
         search.filter = AssetFilter()
         search.filter.addToExists("foo")
-
         applyAssetSearchMetrics(search)
+        counter =  MeterRegistryHolder
+                .meterRegisty.counter(counterName, listOf(Tag.of("type", "exists")))
+
         assertEquals(currentValue + 1, counter.count())
     }
 }
