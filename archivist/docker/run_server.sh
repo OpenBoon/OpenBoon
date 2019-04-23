215bd6412ea9b8c2595aeb1dc10f64f7d4142861
Matt <mvchambers@me.com>
2019-04-23 12:41:30 -0400

    Added system for passing custom JVM args to Archivist.

--- archivist/docker/run_server.sh
+++ archivist/docker/run_server.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #This script the docker file entrypoint.
 
 if [ -n "${ZORROA_ARCHIVIST_EXT}" ]
@@ -14,12 +14,8 @@ fi
 if [ -n "${GAE_SERVICE}" ]
 then
   echo "Downloading config files from GCS secret bucket."
-  mkdir -p /config
   /root/google-cloud-sdk/bin/gsutil cp -r "gs://${GCLOUD_PROJECT}-zorroa-configuration/${GAE_SERVICE}-config/*" /config
 fi
 
-MEM=`cat /proc/meminfo | grep MemTotal | awk '{printf ("%0.fm", $2/2/1280)}'`
-JAVA_OPTS="$JAVA_OPTS -Xms$MEM"
-JAVA_OPTS="$JAVA_OPTS -Xmx$MEM"
-
-java $JAVA_OPTS -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:InitiatingHeapOccupancyPercent=60 -Dloader.path=/extensions/active -Djava.security.egd=file:/dev/./urandom -jar /service/archivist.jar "$@"
+export ARCHIVIST_JAVA_OPTS=`./jvm_options_parser /config/jvm.options`
+java $JAVA_OPTS ${ARCHIVIST_JAVA_OPTS} -Djava.awt.headless=true -Dloader.path=/extensions/active -Djava.security.egd=file:/dev/./urandom -jar /service/archivist.jar "$@"
