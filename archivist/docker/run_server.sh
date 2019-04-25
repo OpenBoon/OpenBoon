d7b7fc9352918a7f4a59dca005f4a8097a44efc7
Matt <mvchambers@me.com>
2019-04-25 19:48:58 -0400

    Added concept of archivist.env for adding extra env vars without using the docker file.

--- archivist/docker/run_server.sh
+++ archivist/docker/run_server.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 #This script the docker file entrypoint.
 
+if [ -f "/config/archivist.env" ]
+then
+    source /config/archivist.env
+fi
+
 if [ -n "${ZORROA_ARCHIVIST_EXT}" ]
 then
    extArray=$(echo ${ZORROA_ARCHIVIST_EXT} | tr ";" "\n")
@@ -16,6 +21,7 @@ then
   echo "Downloading config files from GCS secret bucket."
   gsutil cp -r "gs://${GCLOUD_PROJECT}-zorroa-configuration/${GAE_SERVICE}-config/*" /config
 fi
+
 
 export ZORROA_JAVA_OPTS=`./jvm_options_parser /config/jvm.options`
 java ${ZORROA_JAVA_OPTS} \
