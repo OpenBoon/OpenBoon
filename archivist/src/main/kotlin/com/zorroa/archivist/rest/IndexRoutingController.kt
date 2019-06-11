5a50279d70c4876fc5ff94bc424aa7ef740de4a2
Matt <mvchambers@me.com>
2019-06-11 09:40:23 -0400

    Added IndexRoute controller which can create, get, get mappings, and migrate for now.

---
+++ archivist/src/main/kotlin/com/zorroa/archivist/rest/IndexRoutingController.kt
@@ -0,0 +1,49 @@
+package com.zorroa.archivist.rest
+
+import com.zorroa.archivist.domain.IndexMappingVersion
+import com.zorroa.archivist.domain.IndexMigrationSpec
+import com.zorroa.archivist.domain.IndexRoute
+import com.zorroa.archivist.domain.IndexRouteSpec
+import com.zorroa.archivist.security.getOrgId
+import com.zorroa.archivist.service.IndexMigrationService
+import com.zorroa.archivist.service.IndexRoutingService
+import com.zorroa.archivist.service.OrganizationService
+import io.micrometer.core.annotation.Timed
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
+
+@PreAuthorize("hasAuthority(T(com.zorroa.security.Groups).SUPERADMIN)")
+@RestController
+@Timed
+class IndexRoutingController @Autowired constructor(
+    val indexRoutingService: IndexRoutingService,
+    val indexMigrationService: IndexMigrationService,
+    val organizationService: OrganizationService
+) {
+
+    @PostMapping(value = ["/api/v1/index-route"])
+    fun create(@RequestBody spec: IndexRouteSpec): IndexRoute {
+        return indexRoutingService.createIndexRoute(spec)
+    }
+
+    @GetMapping(value = ["/api/v1/index-route/{id}"])
+    fun get(@PathVariable id: UUID): IndexRoute {
+        return indexRoutingService.getIndexRoute(id)
+    }
+
+    @GetMapping(value = ["/api/v1/index-route/_mappings"])
+    fun getMappings(): List<IndexMappingVersion> {
+        return indexRoutingService.getIndexMappingVersions()
+    }
+
+    @PostMapping(value = ["/api/v1/index-route/_migrate"])
+    fun migrate(@RequestBody mig: IndexMigrationSpec): Any {
+        return indexMigrationService.migrate(organizationService.get(getOrgId()), mig)
+    }
+}
---
+++ archivist/src/test/kotlin/com/zorroa/archivist/rest/IndexRoutingControllerTests.kt
@@ -0,0 +1,143 @@
+package com.zorroa.archivist.rest
+
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.zorroa.archivist.domain.IndexMappingVersion
+import com.zorroa.archivist.domain.IndexMigrationSpec
+import com.zorroa.archivist.domain.IndexRoute
+import com.zorroa.archivist.domain.IndexRouteSpec
+import com.zorroa.archivist.domain.PipelineType
+import com.zorroa.common.domain.Job
+import com.zorroa.common.domain.JobState
+import com.zorroa.common.util.Json
+import org.junit.Before
+import org.junit.Test
+import org.springframework.http.MediaType
+import org.springframework.mock.web.MockHttpSession
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class IndexRoutingControllerTests : MockMvcTest() {
+
+    internal lateinit var session: MockHttpSession
+
+    override fun requiresElasticSearch(): Boolean {
+        return true
+    }
+
+    @Before
+    fun init() {
+        session = admin()
+    }
+
+    @Test
+    fun testCreate() {
+        val spec = IndexRouteSpec(
+            "http://localhost:9200",
+            "testing123",
+            "test",
+            1,
+            false)
+
+        val rsp = mvc.perform(
+            MockMvcRequestBuilders.post("/api/v1/index-route")
+                .session(session)
+                .with(SecurityMockMvcRequestPostProcessors.csrf())
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(Json.serialize(spec)))
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andReturn()
+
+        val result = Json.Mapper.readValue<IndexRoute>(rsp.response.contentAsString)
+
+        assertEquals("http://localhost:9200", result.clusterUrl)
+        assertEquals("testing123", result.indexName)
+        assertEquals("test", result.mappingType)
+        assertEquals(1, result.mappingMajorVer)
+        assertEquals(false, result.defaultPool)
+        assertEquals(false, result.useRouteKey)
+        assertEquals(2, result.replicas)
+        assertEquals(5, result.shards)
+    }
+
+    @Test
+    fun testGet() {
+        val spec = IndexRouteSpec(
+            "http://localhost:9200",
+            "testing123",
+            "test",
+            1,
+            false)
+
+        val route = indexRoutingService.createIndexRoute(spec)
+
+
+        val rsp = mvc.perform(
+            MockMvcRequestBuilders.get("/api/v1/index-route/${route.id}")
+                .session(session)
+                .with(SecurityMockMvcRequestPostProcessors.csrf())
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andReturn()
+
+        val result = Json.Mapper.readValue<IndexRoute>(rsp.response.contentAsString)
+
+        assertEquals("http://localhost:9200", result.clusterUrl)
+        assertEquals("testing123", result.indexName)
+        assertEquals("test", result.mappingType)
+        assertEquals(1, result.mappingMajorVer)
+        assertEquals(false, result.defaultPool)
+        assertEquals(false, result.useRouteKey)
+        assertEquals(2, result.replicas)
+        assertEquals(5, result.shards)
+
+    }
+
+
+    @Test
+    fun testGetMappings() {
+
+        val rsp = mvc.perform(
+            MockMvcRequestBuilders.get("/api/v1/index-route/_mappings")
+                .session(session)
+                .with(SecurityMockMvcRequestPostProcessors.csrf())
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andReturn()
+
+        val result = Json.Mapper.readValue<List<IndexMappingVersion>>(rsp.response.contentAsString)
+        val mapping = result[0]
+        assertEquals("test", mapping.mapping)
+        assertEquals(1, mapping.mappingMajorVer)
+    }
+
+    @Test
+    fun testCreateMigration() {
+
+        val spec = IndexRouteSpec(
+            "http://localhost:9200",
+            "testing123",
+            "test",
+            1,
+            false)
+
+        val route = indexRoutingService.createIndexRoute(spec)
+        val mspec = IndexMigrationSpec(route.id, true)
+
+        val rsp = mvc.perform(
+            MockMvcRequestBuilders.post("/api/v1/index-route/_migrate")
+                .session(session)
+                .with(SecurityMockMvcRequestPostProcessors.csrf())
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(Json.serialize(mspec)))
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andReturn()
+
+        val result = Json.Mapper.readValue<Job>(rsp.response.contentAsString)
+        assertEquals(PipelineType.Batch, result.type)
+        assertTrue(result.name.startsWith("migration"))
+        assertEquals(JobState.Active, result.state)
+    }
+}
