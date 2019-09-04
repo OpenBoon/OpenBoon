e97fa3ec0eafbde924814fea7c01e0e5920d443f
Matt <mvchambers@me.com>
2019-09-04 17:40:37 -0400

    Removed commented code.

--- archivist/src/main/kotlin/com/zorroa/archivist/security/Jwt.kt
+++ archivist/src/main/kotlin/com/zorroa/archivist/security/Jwt.kt
@@ -74,8 +74,6 @@ class JWTAuthorizationFilter(authManager: AuthenticationManager) :
 
             req.setAttribute("authType", HttpServletRequest.CLIENT_CERT_AUTH)
             req.setAttribute("sessionId", authToken.sessionId)
-            // I don't think we need this.
-            //res.addHeader(HEADER_STRING_RSP, token)
             SecurityContextHolder.getContext().authentication = authToken
         } catch (e: JwtValidatorException) {
             if (doSessionValidaton) {
