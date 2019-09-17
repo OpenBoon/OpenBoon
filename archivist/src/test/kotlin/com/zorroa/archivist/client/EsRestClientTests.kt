815dfaab104044c02411e05f6b45ce6a7ceb1a12
Matt <mvchambers@me.com>
2019-09-17 14:18:58 -0400

    Added methods for getting and updating ES mapping.

--- archivist/src/test/kotlin/com/zorroa/archivist/client/EsRestClientTests.kt
+++ archivist/src/test/kotlin/com/zorroa/archivist/client/EsRestClientTests.kt
@@ -1,7 +1,11 @@
 package com.zorroa.archivist.client
 
 import com.zorroa.archivist.AbstractTest
+import com.zorroa.archivist.domain.AttrType
+import com.zorroa.archivist.domain.Document
+import com.zorroa.common.util.Json
 import org.junit.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class EsRestClientTests : AbstractTest() {
@@ -21,5 +25,15 @@ class EsRestClientTests : AbstractTest() {
     fun testIndexExists() {
         val client1 = indexRoutingService.getOrgRestClient()
         assertTrue(client1.indexExists())
+    }
+
+    @Test
+    fun testUpdateAndGetMapping() {
+        val client = indexRoutingService.getOrgRestClient()
+        assertTrue(client.updateMapping(
+            mapOf("properties" to mapOf("name" to mutableMapOf<String, Any>("type" to "keyword")))))
+        val mapping = client.getMapping()
+        val doc = Document(mapping)
+        assertEquals("keyword", doc.getAttr("unittest.mappings.asset.properties.name.type", String::class.java))
     }
 }
