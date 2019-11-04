0b08f09a20d454e7cda31df1b3d7d94de40fd302
Wajid Kagzi <wjkagzi@gmail.com>
2019-11-04 15:13:54 -0500

    WAL-19: Add webpack configurations for production and development environment

---
+++ services/wallet/app/frontend/webpack.common.js
@@ -0,0 +1,71 @@
+const { join, resolve } = require('path')
+const webpack = require('webpack')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const ENV = process.env.NODE_ENV
+const ROOT_DIR = resolve(__dirname)
+const SRC_DIR = join(ROOT_DIR, 'src')
+
+module.exports = {
+  mode: ENV,
+  entry: SRC_DIR,
+  output: {
+    path: join(ROOT_DIR, 'dist'),
+    publicPath: '',
+    filename: 'bundle.[hash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: [/.js$/],
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: [/.css$|.scss$/],
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      }, {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[hash].[ext]',
+              outputPath: './images'
+            }
+          }
+        ]
+      }, {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js']
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: join(SRC_DIR, 'index.html'),
+      filename: join(ROOT_DIR, 'dist/index.html')
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash].css'
+    })
+  ]
+}
