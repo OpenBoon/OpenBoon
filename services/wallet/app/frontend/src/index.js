439233468c612f542a88a6d560323ca24193c14a
octaviaaris <octavia@zorroa.com>
2019-11-04 16:17:20 -0800

    restructures repo

--- services/wallet/app/frontend/src/index.js
+++ services/wallet/app/frontend/src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-class Wallet extends React.Component {
-  render() {
-    return <div>Hello World!</div>;
-  }
-}
+import Wallet from './components/Wallet'
 
 ReactDOM.render(
   <Wallet />,
