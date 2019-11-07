59d35c700c17004c841ea3a1a2304d7c96f6f191
octaviaaris <octavia@zorroa.com>
2019-11-07 13:42:15 -0800

    checkpoint -- sets up axios and auth gateway

--- services/wallet/app/frontend/src/components/Wallet/Wallet.js
+++ services/wallet/app/frontend/src/components/Wallet/Wallet.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 
 class Wallet extends Component {
+  state = {
+    authenticated: false,
+  }
+
   render() {
     return <div className="Wallet">{'Hello World!'}</div>
   }
