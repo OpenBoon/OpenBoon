64a89f46e338031f4a2fa4ad8e986e78b9b4da08
octaviaaris <octavia@zorroa.com>
2019-11-13 10:52:11 -0800

    updates test

--- services/wallet/app/frontend/src/components/__tests__/Wallet.test.js
+++ services/wallet/app/frontend/src/components/__tests__/Wallet.test.js
@@ -5,6 +5,6 @@ import Wallet from '../Wallet'
 describe('<Wallet />', () => {
   it('Should render the app', () => {
     const component = shallow(<Wallet />)
-    expect(component.text()).toBe('Hello World!')
+    expect(component.text()).toBe('<Wallet />')
   })
 })
