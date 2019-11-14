be90f3d0200beeca12f10c05883cd6d60fc388b3
Matt <mvchambers@me.com>
2019-11-14 15:32:33 -0800

    Actually fixes filter registration issue.

--- services/archivist/archivist/src/main/kotlin/com/zorroa/archivist/security/MultipleWebSecurityConfig.kt
+++ services/archivist/archivist/src/main/kotlin/com/zorroa/archivist/security/MultipleWebSecurityConfig.kt
@@ -7,12 +7,11 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest
+import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.Ordered
 import org.springframework.core.annotation.Order
-import org.springframework.security.authentication.AuthenticationManager
-import org.springframework.security.config.BeanIds
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -55,7 +54,7 @@ class MultipleWebSecurityConfig {
         override fun configure(http: HttpSecurity) {
             http
                 .antMatcher("/api/**")
-                .addFilterBefore(
+                .addFilterAfter(
                     apiKeyAuthorizationFilter,
                     UsernamePasswordAuthenticationFilter::class.java
                 )
@@ -82,7 +81,8 @@ class MultipleWebSecurityConfig {
         override fun configure(http: HttpSecurity) {
             http
                 .antMatcher("/cluster/**")
-                .addFilterBefore(analystAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
+                .addFilterBefore(analystAuthenticationFilter,
+                    UsernamePasswordAuthenticationFilter::class.java)
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
@@ -106,8 +106,7 @@ class MultipleWebSecurityConfig {
                 .httpBasic()
                 .and()
                 .csrf().disable()
-                .addFilterBefore(
-                    apiKeyAuthorizationFilter,
+                .addFilterBefore(apiKeyAuthorizationFilter,
                     UsernamePasswordAuthenticationFilter::class.java
                 )
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -123,11 +122,6 @@ class MultipleWebSecurityConfig {
     @Order(Ordered.HIGHEST_PRECEDENCE + 4)
     @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
     class RootSecurityConfig : WebSecurityConfigurerAdapter() {
-
-        @Bean(name = [BeanIds.AUTHENTICATION_MANAGER])
-        fun globalAuthenticationManager(): AuthenticationManager {
-            return super.authenticationManagerBean()
-        }
 
         @Throws(Exception::class)
         override fun configure(http: HttpSecurity) {
@@ -170,10 +164,35 @@ class MultipleWebSecurityConfig {
     }
 
     @Bean
+    fun apiKeyAuthenticationFilter(): ApiKeyAuthorizationFilter {
+        return ApiKeyAuthorizationFilter(authServerClient())
+    }
+
+    @Bean
+    fun analystAuthenticationFilter(): AnalystAuthenticationFilter {
+        return AnalystAuthenticationFilter()
+    }
+
+    @Bean
     fun authServerClient(): AuthServerClient {
         return AuthServerClientImpl(properties.getString("security.auth-server.url"))
     }
 
+    @Bean
+    fun apiKeyFilterRegistration(): FilterRegistrationBean<ApiKeyAuthorizationFilter> {
+        val bean = FilterRegistrationBean<ApiKeyAuthorizationFilter>()
+        bean.filter = apiKeyAuthenticationFilter()
+        bean.isEnabled = false
+        return bean
+    }
+
+    @Bean
+    fun analystFilterRegistration(): FilterRegistrationBean<AnalystAuthenticationFilter> {
+        val bean = FilterRegistrationBean<AnalystAuthenticationFilter>()
+        bean.filter = analystAuthenticationFilter()
+        bean.isEnabled = false
+        return bean
+    }
     companion object {
         private val logger = LoggerFactory.getLogger(MultipleWebSecurityConfig::class.java)
     }
