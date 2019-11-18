d4f8d4aa764158de8595538f2ae8823e862f360b
Matt <mvchambers@me.com>
2019-11-17 20:13:40 -0500

    Fixes issue with AuthServerClient hitting wrong URL.

--- services/archivist/archivist/src/main/kotlin/com/zorroa/archivist/clients/AuthServerClient.kt
+++ services/archivist/archivist/src/main/kotlin/com/zorroa/archivist/clients/AuthServerClient.kt
@@ -116,10 +116,10 @@ class AuthServerClientImpl(val baseUri: String) : AuthServerClient {
             }
         })
 
-    val serviceKey: ApiKey? = detectServiceKey()
+    private val serviceKey: ApiKey? = detectServiceKey()
 
     private fun detectServiceKey(): ApiKey? {
-        val keyPath = Paths.get("/service/config/key.json")
+        val keyPath = Paths.get("config/key.json")
         return if (Files.exists(keyPath)) {
             Json.Mapper.readValue(keyPath.toFile())
         } else {
@@ -143,7 +143,7 @@ class AuthServerClientImpl(val baseUri: String) : AuthServerClient {
             "name" to name,
             "permissions" to perms
         )
-        val req = signRequest(RequestEntity.post(URI("${baseUri}/auth/v1/api-key")))
+        val req = signRequest(RequestEntity.post(URI("${baseUri}/auth/v1/apikey")))
             .body(body)
         return rest.exchange(req, TYPE_APIKEY).body
     }
@@ -154,7 +154,7 @@ class AuthServerClientImpl(val baseUri: String) : AuthServerClient {
             "names" to listOf(name)
         )
         val req = signRequest(
-            RequestEntity.post(URI("${baseUri}/auth/v1/api-key/_findOne"))
+            RequestEntity.post(URI("${baseUri}/auth/v1/apikey/_findOne"))
         ).body(body)
         return rest.exchange(req, TYPE_APIKEY).body
     }
--- services/archivist/archivist/src/test/kotlin/com/zorroa/archivist/client/AuthServerClientTests.kt
+++ services/archivist/archivist/src/test/kotlin/com/zorroa/archivist/client/AuthServerClientTests.kt
@@ -2,6 +2,8 @@ package com.zorroa.archivist.client
 
 import com.zorroa.archivist.AbstractTest
 import com.zorroa.archivist.clients.AuthServerClientImpl
+import com.zorroa.archivist.domain.ProjectFilter
+import com.zorroa.archivist.security.Perm
 import org.junit.Before
 import org.junit.Test
 import org.springframework.http.HttpMethod
@@ -13,6 +15,7 @@ import org.springframework.test.web.client.MockRestServiceServer
 import org.springframework.test.web.client.match.MockRestRequestMatchers
 import org.springframework.test.web.client.response.MockRestResponseCreators
 import java.net.URI
+import java.util.UUID
 import kotlin.test.assertEquals
 
 @TestPropertySource(locations = ["classpath:test.properties", "classpath:jwt.properties"])
@@ -24,6 +27,61 @@ class AuthServerClientTests : AbstractTest() {
     fun init() {
         authServerClient = AuthServerClientImpl("http://localhost:9090")
         mockServer = MockRestServiceServer.createServer(authServerClient.rest)
+    }
+
+    @Test
+    fun testCreateApiKey() {
+        val payload = """
+        {
+            "projectId": "cc7c2e6f-1e36-4731-9154-9598e22408b7",
+            "keyId": "11eaad16-d355-4006-90e4-6bd100c3cd81",
+            "sharedKey": "abc123"
+        }
+        """.trimIndent()
+
+        mockServer.expect(
+            ExpectedCount.once(),
+            MockRestRequestMatchers.requestTo(URI("http://localhost:9090/auth/v1/apikey"))
+        )
+            .andExpect(MockRestRequestMatchers.method(HttpMethod.POST))
+            .andRespond(
+                MockRestResponseCreators.withStatus(HttpStatus.OK)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(payload)
+            )
+
+        val project = projectService.findOne(ProjectFilter())
+        val apiKey = authServerClient.createApiKey(project, "test", listOf(Perm.ASSETS_IMPORT))
+        assertEquals("cc7c2e6f-1e36-4731-9154-9598e22408b7", apiKey.projectId.toString())
+        assertEquals("11eaad16-d355-4006-90e4-6bd100c3cd81", apiKey.keyId.toString())
+        assertEquals("abc123", apiKey.sharedKey)
+    }
+
+    @Test
+    fun testGetApiKey() {
+        val payload = """
+        {
+            "projectId": "cc7c2e6f-1e36-4731-9154-9598e22408b7",
+            "keyId": "11eaad16-d355-4006-90e4-6bd100c3cd81",
+            "sharedKey": "abc123"
+        }
+        """.trimIndent()
+
+        mockServer.expect(
+            ExpectedCount.once(),
+            MockRestRequestMatchers.requestTo(URI("http://localhost:9090/auth/v1/apikey/_findOne"))
+        )
+            .andExpect(MockRestRequestMatchers.method(HttpMethod.POST))
+            .andRespond(
+                MockRestResponseCreators.withStatus(HttpStatus.OK)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(payload)
+            )
+
+        val apiKey = authServerClient.getApiKey(UUID.randomUUID(), "test")
+        assertEquals("cc7c2e6f-1e36-4731-9154-9598e22408b7", apiKey.projectId.toString())
+        assertEquals("11eaad16-d355-4006-90e4-6bd100c3cd81", apiKey.keyId.toString())
+        assertEquals("abc123", apiKey.sharedKey)
     }
 
     @Test
