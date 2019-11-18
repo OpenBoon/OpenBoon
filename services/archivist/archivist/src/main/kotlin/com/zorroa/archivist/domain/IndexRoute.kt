fe94dbdef9cf648a8a70862c252f426098e86417
Iron <iron.araujo@zorroa.com>
2019-11-18 17:24:50 -0300

    Swagger Properties and operations updates

--- services/archivist/archivist/src/main/kotlin/com/zorroa/archivist/domain/IndexRoute.kt
+++ services/archivist/archivist/src/main/kotlin/com/zorroa/archivist/domain/IndexRoute.kt
@@ -56,33 +56,34 @@ enum class IndexRouteState {
  * @property shards Number of shards.
  * @property indexUrl The ES index URL, or the cluster URL and index name combined.
  */
-@ApiModel("IndexRoute", description = "TaskErrorEvents are emitted by the processing system if an an exception is thrown while processing")
+@ApiModel(
+    "IndexRoute",
+    description = "TaskErrorEvents are emitted by the processing system if an an exception is thrown while processing"
+)
 
 class IndexRoute(
-        @ApiModelProperty("The unique ID of the index route.")
-        val id: UUID,
-        @ApiModelProperty("The unique ID of the project.")
-        val projectId: UUID,
-        @ApiModelProperty("The unique ID of the cluster.")
-        val clusterId: UUID,
-        @ApiModelProperty("The URL to the ES cluster.")
-        val clusterUrl: String,
-        @ApiModelProperty("The state of the index.")
-        val state: IndexRouteState,
-        @ApiModelProperty("The name of the ES index.")
-        val indexName: String,
-        @ApiModelProperty("The mapping type. This is extracted from the mapping file name, not the ES type.")
-        val mapping: String,
-        @ApiModelProperty("The major version of the mapping file.")
-        val mappingMajorVer: Int,
-        @ApiModelProperty("The minor version of the mapping file in a date format.")
-        val mappingMinorVer: Int,
-        @ApiModelProperty("True if the index is closed and not in use.")
-        val closed: Boolean,
-        @ApiModelProperty("Number of index replicas.")
-        val replicas: Int,
-        @ApiModelProperty("Number of shards.")
-        val shards: Int
+    @ApiModelProperty("The unique ID of the index route.")
+    val id: UUID,
+    @ApiModelProperty("The unique ID of the project.")
+    val projectId: UUID,
+    @ApiModelProperty("The unique ID of the cluster.")
+    val clusterId: UUID,
+    @ApiModelProperty("The URL to the ES cluster.")
+    val clusterUrl: String,
+    @ApiModelProperty("The state of the index.")
+    val state: IndexRouteState,
+    @ApiModelProperty("The name of the ES index.")
+    val indexName: String,
+    @ApiModelProperty("The mapping type. This is extracted from the mapping file name, not the ES type.")
+    val mapping: String,
+    @ApiModelProperty("The major version of the mapping file.")
+    val mappingMajorVer: Int,
+    @ApiModelProperty("The minor version of the mapping file in a date format.")
+    val mappingMinorVer: Int,
+    @ApiModelProperty("Number of index replicas.")
+    val replicas: Int,
+    @ApiModelProperty("Number of shards.")
+    val shards: Int
 ) {
     @ApiModelProperty("The ES index URL, or the cluster URL and index name combined.")
     val indexUrl = "$clusterUrl/$indexName"
@@ -107,17 +108,17 @@ class IndexRoute(
  */
 @ApiModel("IndexRouteSpec", description = "The IndexRouteSpec defines all the values needed to create an index route.")
 class IndexRouteSpec(
-        @ApiModelProperty("The type of mapping (not ES object type)")
-        var mapping: String,
-        @ApiModelProperty("The major version to use. It will be patched up to highest level.")
-        var mappingMajorVer: Int,
-        var state: IndexRouteState = IndexRouteState.BUILDING,
-        @ApiModelProperty("The number of replicas there should be for each shard. Defaults to 0.")
-        var replicas: Int = 0,
-        @ApiModelProperty(" The number of shards in the index. Defaults to 2.")
-        var shards: Int = 2,
-        @ApiModelProperty("The cluster ID to use for the index.")
-        var clusterId: UUID? = null
+    @ApiModelProperty("The type of mapping (not ES object type)")
+    var mapping: String,
+    @ApiModelProperty("The major version to use. It will be patched up to highest level.")
+    var mappingMajorVer: Int,
+    var state: IndexRouteState = IndexRouteState.BUILDING,
+    @ApiModelProperty("The number of replicas there should be for each shard. Defaults to 0.")
+    var replicas: Int = 0,
+    @ApiModelProperty(" The number of shards in the index. Defaults to 2.")
+    var shards: Int = 2,
+    @ApiModelProperty("The cluster ID to use for the index.")
+    var clusterId: UUID? = null
 )
 
 /**
@@ -127,12 +128,15 @@ class IndexRouteSpec(
  * @property mapping The name of the mapping.
  * @property mappingMajorVer The major version of the mapping.
  */
-@ApiModel("IndexMappingVersion", description = "An IndexMappingVersion is a version of an ES mapping found on disk or packaged with the Archivist that can be used to make an [IndexRoute]")
+@ApiModel(
+    "IndexMappingVersion",
+    description = "An IndexMappingVersion is a version of an ES mapping found on disk or packaged with the Archivist that can be used to make an [IndexRoute]"
+)
 class IndexMappingVersion(
-        @ApiModelProperty("The name of the mapping.")
-        val mapping: String,
-        @ApiModelProperty("The major version of the mapping.")
-        val mappingMajorVer: Int
+    @ApiModelProperty("The name of the mapping.")
+    val mapping: String,
+    @ApiModelProperty("The major version of the mapping.")
+    val mappingMajorVer: Int
 )
 
 /**
@@ -143,12 +147,15 @@ class IndexMappingVersion(
  * @property indexName The name of the index.
  * @property indexUrl The full URL to the index.
  */
-@ApiModel("EsClientCacheKey", description = "The ESClientCacheKey is used to lookup or create cached ElasticSearch client instances.")
+@ApiModel(
+    "EsClientCacheKey",
+    description = "The ESClientCacheKey is used to lookup or create cached ElasticSearch client instances."
+)
 class EsClientCacheKey(
-        @ApiModelProperty(" The url to the cluster")
-        val clusterUrl: String,
-        @ApiModelProperty("The name of the index.")
-        val indexName: String
+    @ApiModelProperty(" The url to the cluster")
+    val clusterUrl: String,
+    @ApiModelProperty("The name of the index.")
+    val indexName: String
 ) {
     @ApiModelProperty("The full URL to the index.")
     val indexUrl = "$clusterUrl/$indexName"
@@ -158,19 +165,19 @@ class EsClientCacheKey(
  * A class for filtering [IndexRoute]s
  */
 class IndexRouteFilter(
-        val ids: List<UUID>? = null,
-        val clusterIds: List<UUID>? = null,
-        val mappings: List<String>? = null
+    val ids: List<UUID>? = null,
+    val clusterIds: List<UUID>? = null,
+    val mappings: List<String>? = null
 ) : KDaoFilter() {
 
     @JsonIgnore
     override val sortMap: Map<String, String> =
-            mapOf(
-                    "id" to "index_route.pk_index_route",
-                    "clusterUrl" to "index_cluster.str_url",
-                    "mapping" to "index_route.str_mapping_type",
-                    "timeCreated" to "index_route.time_created"
-            )
+        mapOf(
+            "id" to "index_route.pk_index_route",
+            "clusterUrl" to "index_cluster.str_url",
+            "mapping" to "index_route.str_mapping_type",
+            "timeCreated" to "index_route.time_created"
+        )
 
     @JsonIgnore
     override fun build() {
--- services/archivist/archivist/src/main/kotlin/com/zorroa/archivist/rest/ProjectController.kt
+++ services/archivist/archivist/src/main/kotlin/com/zorroa/archivist/rest/ProjectController.kt
@@ -5,6 +5,8 @@ import com.zorroa.archivist.domain.ProjectFilter
 import com.zorroa.archivist.domain.ProjectSpec
 import com.zorroa.archivist.repository.KPagedList
 import com.zorroa.archivist.service.ProjectService
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -17,26 +19,31 @@ import java.util.UUID
 
 @PreAuthorize("hasAuthority('SuperAdmin')")
 @RestController
+@Api(tags = ["Project"], description = "Operations for managing Projects.")
 class ProjectController constructor(
     val projectService: ProjectService
 ) {
 
     @PostMapping(value = ["/api/v1/projects"])
+    @ApiOperation("Create Project.")
     fun create(@RequestBody spec: ProjectSpec) : Project {
         return projectService.create(spec)
     }
 
     @GetMapping(value = ["/api/v1/projects/{id}"])
+    @ApiOperation("Retrieve Project by Id.")
     fun get(@PathVariable id: UUID) : Project {
         return projectService.get(id)
     }
 
     @RequestMapping(value = ["/api/v1/projects/_search"], method=[RequestMethod.POST, RequestMethod.GET])
+    @ApiOperation("Get all Projects")
     fun getAll(@RequestBody(required = false) filter: ProjectFilter?) : KPagedList<Project> {
         return projectService.getAll(filter ?: ProjectFilter())
     }
 
     @RequestMapping(value = ["/api/v1/projects/_findOne"], method=[RequestMethod.POST, RequestMethod.GET])
+    @ApiOperation("Search Filter")
     fun findOne(@RequestBody(required = false) filter: ProjectFilter?) : Project {
         return projectService.findOne(filter ?: ProjectFilter())
     }
