f57ab4050e6324087b21d719fc378f8e654eb872
Matt <mvchambers@me.com>
2019-11-19 16:24:30 -0500

    Moved the sentry beans.

--- services/archivist/archivist/src/main/kotlin/com/zorroa/archivist/Application.kt
+++ services/archivist/archivist/src/main/kotlin/com/zorroa/archivist/Application.kt
@@ -3,21 +3,9 @@ package com.zorroa.archivist
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
-import io.sentry.spring.SentryExceptionResolver
-import io.sentry.spring.SentryServletContextInitializer
-import org.springframework.web.servlet.HandlerExceptionResolver
-import org.springframework.boot.web.servlet.ServletContextInitializer
-import org.springframework.context.annotation.Bean
-
 
 @SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
-class Application {
-    @Bean
-    fun sentryExceptionResolver(): HandlerExceptionResolver = SentryExceptionResolver()
-
-    @Bean
-    fun sentryServletContextInitializer(): ServletContextInitializer = SentryServletContextInitializer()
-}
+class Application
 
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
