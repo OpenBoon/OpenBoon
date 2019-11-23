485b0132fffdf9cea5c951f02a5034e185fbd815
Wajid Kagzi <wjkagzi@gmail.com>
2019-11-22 19:28:24 -0500

    Revert media stub and use require for all images now

--- services/wallet/app/frontend/src/components/Logo/Logo.js
+++ services/wallet/app/frontend/src/components/Logo/Logo.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import LogoSrc from '../../assets/images/logo.svg'
-
 function Logo({ width, height }) {
-  return <img width={width} height={height} className="Logo" src={LogoSrc} />
+  return (
+    <img
+      width={width}
+      height={height}
+      className="Logo"
+      src={require('../../assets/images/logo.svg')}
+    />
+  )
 }
 
 Logo.propTypes = {
