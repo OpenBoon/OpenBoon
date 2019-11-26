fc0de0c8e87ceb554b9529a34073e55ddfe1204c
Damien Varron <damien@varron.fr>
2019-11-26 15:18:45 -0800

    Update _document.js

--- services/wallet/app/nextjs/pages/_document.js
+++ services/wallet/app/nextjs/pages/_document.js
@@ -15,6 +15,7 @@ class MyDocument extends Document {
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta charSet="utf-8" />
+          <title>Wallet</title>
         </Head>
         <StylesReset />
         <body>
