cdbc239a5b0c4f64fb67446a2894b23b3df44ff6
Damien Varron <damien@varron.fr>
2019-11-26 16:17:42 -0800

    Update index.spec.js

--- services/wallet/app/nextjs/src/Login/__tests__/index.spec.js
+++ services/wallet/app/nextjs/src/Login/__tests__/index.spec.js
@@ -1,4 +1,4 @@
-import TestRenderer from 'react-test-renderer'
+import TestRenderer, { act } from 'react-test-renderer'
 
 import Login from '../'
 
@@ -9,5 +9,16 @@ describe('<Login />', () => {
     const component = TestRenderer.create(<Login onSubmit={noop} />)
 
     expect(component.toJSON()).toMatchSnapshot()
+
+    const emailInput = component.root.findByProps({ id: 'email' })
+    const passwordInput = component.root.findByProps({ id: 'password' })
+
+    act(() => {
+      emailInput.props.onChange({ target: { value: 'foo@bar.baz' } })
+      passwordInput.props.onChange({ target: { value: 'password' } })
+    })
+
+    expect(emailInput.props.value).toEqual('foo@bar.baz')
+    expect(passwordInput.props.value).toEqual('password')
   })
 })
