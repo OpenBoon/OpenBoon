e24a1fc3225fde681e355c5604dc7582a0f1dc6d
Damien Varron <damien@varron.fr>
2019-12-04 11:29:21 -0800

    WAL-76: add analyze and bundlesize

--- services/wallet/app/nextjs/next.config.js
+++ services/wallet/app/nextjs/next.config.js
@@ -1,5 +1,20 @@
+const { ANALYZE } = process.env
+
 module.exports = {
-  webpack: config => {
+  webpack: (config, { isServer }) => {
+    if (ANALYZE && !isServer) {
+      const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+
+      config.plugins.push(
+        new BundleAnalyzerPlugin({
+          analyzerMode: 'static',
+          analyzerPort: 8888,
+          openAnalyzer: true,
+          defaultSizes: 'gzip',
+        }),
+      )
+    }
+
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
