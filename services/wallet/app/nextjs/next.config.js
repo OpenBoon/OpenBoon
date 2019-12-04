1cf59ffffc8029809cd10bcc0fd63488f313fc99
Wajid Kagzi <wjkagzi@gmail.com>
2019-12-04 17:53:19 -0500

    WAL-44: Add error handling via Sentry from server and client side

--- services/wallet/app/nextjs/next.config.js
+++ services/wallet/app/nextjs/next.config.js
@@ -1,10 +1,18 @@
-module.exports = {
-  webpack: config => {
+const withSourceMaps = require('@zeit/next-source-maps')()
+
+module.exports = withSourceMaps({
+  webpack: (config, options) => {
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     })
 
+    // Ask Webpack to replace @sentry/node imports with @sentry/browser when
+    // building the browser's bundle and not being served by Node.js.
+    if (!options.isServer) {
+      config.resolve.alias['@sentry/node'] = '@sentry/browser'
+    }
+
     return config
   },
-}
+})
--- services/wallet/app/nextjs/pages/_app.js
+++ services/wallet/app/nextjs/pages/_app.js
@@ -1,7 +1,12 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import App from 'next/app'
+import * as Sentry from '@sentry/browser'
 
 import Authentication from '../src/Authentication'
+
+Sentry.init({
+  dsn: 'https://d772538aae2649d38a8931583ed7719b@sentry.io/1504338',
+})
 
 class MyApp extends App {
   render() {
