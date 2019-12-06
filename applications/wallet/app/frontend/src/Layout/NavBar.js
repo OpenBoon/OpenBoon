e76cc8b0ee9999be05ac9a8878375dad199eda8e
Wajid Kagzi <wjkagzi@gmail.com>
2019-12-06 10:21:43 -0500

    WAL-27: Add Navbar

---
+++ applications/wallet/app/frontend/src/Layout/NavBar.js
@@ -0,0 +1,24 @@
+import ProjectSwitcher from '../ProjectSwitcher'
+import { colors } from '../Styles'
+import LogoSvg from './logo.svg'
+
+const HEIGHT = 40
+const LOGO_WIDTH = 110
+
+const NavBar = () => {
+  return (
+    <div
+      css={{
+        display: 'flex',
+        justifyContent: 'left',
+        alignItems: 'center',
+        height: HEIGHT,
+        backgroundColor: colors.grey1,
+      }}>
+      <LogoSvg width={LOGO_WIDTH} />
+      <ProjectSwitcher />
+    </div>
+  )
+}
+
+export default NavBar
