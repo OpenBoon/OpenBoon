44963a8dfe8e4a73a14763b0bb945d70a5bfa818
Wajid Kagzi <wjkagzi@gmail.com>
2019-12-06 10:20:36 -0500

    WAL-27: Add Layout tests and update authentication snapshot

---
+++ applications/wallet/app/frontend/src/Layout/__tests__/index.spec.js
@@ -0,0 +1,13 @@
+import TestRenderer from 'react-test-renderer'
+
+import Layout from '..'
+
+jest.mock('../NavBar', () => 'NavBar')
+
+describe('<Layout />', () => {
+  it('should render properly', () => {
+    const component = TestRenderer.create(<Layout />)
+
+    expect(component.toJSON()).toMatchSnapshot()
+  })
+})
