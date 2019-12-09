8e50d80d984449376cb7198d7101e821c3178af3
Iron <iron.araujo@zorroa.com>
2019-12-09 11:36:33 -0300

    Working on Rest Request Methods

--- sdk/java/src/main/java/domain/PixmlClient.java
+++ sdk/java/src/main/java/domain/PixmlClient.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.IOException;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Base64;
@@ -50,7 +51,7 @@ public class PixmlClient {
     }
 
 
-        private JsonNode loadApiKey(Object apiKey) {
+    private JsonNode loadApiKey(Object apiKey) {
 
         if (apiKey instanceof Map) {
             return mapper.valueToTree(apiKey);
@@ -59,7 +60,7 @@ public class PixmlClient {
             if (apiKey instanceof byte[])
                 apiKeyString = new String(Base64.getDecoder().decode((byte[]) apiKey));
             else
-                apiKeyString = (String)apiKey;
+                apiKeyString = (String) apiKey;
 
             //Check if is a valid JSON
             try {
@@ -71,30 +72,6 @@ public class PixmlClient {
 
         return null;
     }
-
-    /*
-        def headers(self, content_type="application/json"):
-        """
-        Generate the return some request headers.
-
-        Args:
-            content_type(str):  The content-type for the request. Defaults to
-                'application/json'
-
-        Returns:
-            dict: An http header struct.
-
-        """
-        header = {'Authorization': "Bearer {}".format(self.__sign_request())}
-
-        if content_type:
-            header['Content-Type'] = content_type
-
-        if logger.getEffectiveLevel() == logging.DEBUG:
-            logger.debug("headers: %s" % header)
-
-        return header
-     */
 
     public Map<String, String> headers() {
         return this.headers("application/json");
@@ -129,7 +106,6 @@ public class PixmlClient {
 
     }
 
-
     private String signRequest() throws JsonProcessingException {
 
         if (this.apiKey == null) {
@@ -161,23 +137,89 @@ public class PixmlClient {
         return jwtEncoded;
     }
 
+    public Object post(String path, Map body, Boolean isJson) {
+
     /*
-        def __sign_request(self):
-        if not self.apikey:
-            raise RuntimeError("Unable to make request, no ApiKey has been specified.")
-        claims = {
-            'aud': self.server,
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=60),
-            'keyId': self.apikey["keyId"],
-        }
+        """
+        Performs a post request.
 
-        if os.environ.get("PIXML_TASK_ID"):
-            claims['taskId'] = os.environ.get("PIXML_TASK_ID")
-            claims['jobId'] = os.environ.get("PIXML_JOB_ID")
+        Args:
+            path (str): An archivist URI path.
+            body (object): The request body which will be serialized to json.
+            is_json (bool): Set to true to specify a JSON return value
 
-        if self.project_id:
-            claims["projectId"] = self.project_id
-        return jwt.encode(claims, self.apikey['sharedKey'], algorithm='HS512').decode("utf-8")
+        Returns:
+            object: The http response object or an object deserialized from the
+                response json if the ``json`` argument is true.
+
+        Raises:
+            Exception: An error occurred making the request or parsing the
+                JSON response
+        """
+        return self._make_request('post', path, body, is_json)
      */
 
+        isJson = Optional.ofNullable(isJson).orElse(true);
+
+        return this.makeRequest("post", path, body, isJson);
+
+    }
+
+    private Map makeRequest(String httpMethod, String path, Map body, Boolean isJson) {
+        String httpResponseString = "";
+
+        try {
+            httpResponseString = Utils.executeHttpRequest(httpMethod, path, this.headers(), body);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+
+        try {
+            Map<String, String> map = mapper.readValue(httpResponseString, Map.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return null;
+    }
+
+    /*
+    def _make_request(self, method, path, body=None, is_json=True):
+        request_function = getattr(requests, method)
+        if body is not None:
+            data = json.dumps(body, cls=PixmlJsonEncoder)
+        else:
+            data = body
+
+        # Making the request is wrapped in its own try/catch so it's easier
+        # to catch any and all socket and http exceptions that can possibly be
+        # thrown.  Once hat happens, handle_rsp is called which may throw
+        # application level exceptions.
+        rsp = None
+        tries = 0
+        url = self.get_url(path, body)
+        while True:
+            try:
+                rsp = request_function(url, data=data, headers=self.headers(),
+                                       verify=False)
+                break
+            except Exception as e:
+                # Some form of connection error, wait until archivist comes
+                # back.
+                tries += 1
+                if 0 < self.max_retries <= tries:
+                    raise e
+                wait = random.randint(1, random.randint(1, 60))
+                # Switched to stderr in case no logger is setup, still want
+                # to see messages.
+                msg = "Communicating to Pixml (%s) timed out %d times, " \
+                      "waiting ... %d seconds, error=%s\n"
+                sys.stderr.write(msg % (url, tries, wait, e))
+                time.sleep(wait)
+
+        return self.__handle_rsp(rsp, is_json)
+     */
 }
