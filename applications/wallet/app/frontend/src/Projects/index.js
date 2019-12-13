5dd8a85826f33a11e6108d30b59fa1c190c680f2
Damien Varron <damien@varron.fr>
2019-12-13 10:06:50 -0800

    fixup

--- applications/wallet/app/frontend/src/Projects/index.js
+++ applications/wallet/app/frontend/src/Projects/index.js
@@ -1,9 +1,11 @@
 import PropTypes from 'prop-types'
 import useSWR from 'swr'
 
+import userShape from '../User/shape'
+
 import Layout from '../Layout'
 
-const Projects = ({ logout, children }) => {
+const Projects = ({ user, logout, children }) => {
   const { data: { results } = {} } = useSWR('api/v1/projects/')
 
   if (!Array.isArray(results)) return 'Loading...'
@@ -11,13 +13,14 @@ const Projects = ({ logout, children }) => {
   if (results.length === 0) return 'You have 0 projects'
 
   return (
-    <Layout logout={logout} results={results}>
+    <Layout user={user} logout={logout} results={results}>
       {children}
     </Layout>
   )
 }
 
 Projects.propTypes = {
+  user: PropTypes.shape(userShape).isRequired,
   logout: PropTypes.func.isRequired,
   children: PropTypes.func.isRequired,
 }
