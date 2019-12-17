bacf686b582ed900f0eac357ea462b83181ab6a3
Danny Tiesling <danny@zorroa.com>
2019-12-16 16:02:18 -0800

    Adds cd of development branch

---
+++ deployment/bin/k8s-redeploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+TYPE=$1
+PROJECT=$2
+DEPLOYMENT=$3
+VERSION=$4
+NAMESPACE=$5
+
+set -e
+echo 'Patching $DEPLOYMENT in $PROJECT'
+kubectl patch $TYPE $DEPLOYMENT -p "{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"ci-job-id\":\"$CI_JOB_ID\"}}}}}" --namespace=$NAMESPACE
