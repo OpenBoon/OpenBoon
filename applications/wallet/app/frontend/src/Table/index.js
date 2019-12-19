29cf7556717f8ed964009b950fb9ba155abc44ca
Damien Varron <damien@varron.fr>
2019-12-19 17:54:32 +0000

    WAL-12: fix scroll Table instead of page

--- applications/wallet/app/frontend/src/Table/index.js
+++ applications/wallet/app/frontend/src/Table/index.js
@@ -8,159 +8,163 @@ const BORDER_RADIUS = 32
 
 const Table = ({ columns, rows }) => {
   return (
-    <table
-      css={{
-        width: '100%',
-        borderSpacing: 0,
-        boxShadow: constants.boxShadows.table,
-        whiteSpace: 'nowrap',
-        th: {
-          fontSize: typography.size.kilo,
-          lineHeight: typography.height.kilo,
-          fontWeight: typography.weight.medium,
-          color: colors.structure.pebble,
-          backgroundColor: colors.structure.iron,
-          padding: `${spacing.moderate}px ${spacing.normal}px`,
-          borderBottom: constants.borders.default,
-          ':nth-of-type(2)': {
+    <div css={{ position: 'relative' }}>
+      <div css={{ overflow: 'auto' }}>
+        <table
+          css={{
             width: '100%',
-          },
-          '&:not(:last-child)': {
-            borderRight: constants.borders.default,
-          },
-        },
-        tr: {
-          ':hover': {
-            backgroundColor: colors.structure.iron,
-            boxShadow: constants.boxShadows.tableRow,
+            borderSpacing: 0,
+            boxShadow: constants.boxShadows.table,
+            whiteSpace: 'nowrap',
+            th: {
+              fontSize: typography.size.kilo,
+              lineHeight: typography.height.kilo,
+              fontWeight: typography.weight.medium,
+              color: colors.structure.pebble,
+              backgroundColor: colors.structure.iron,
+              padding: `${spacing.moderate}px ${spacing.normal}px`,
+              borderBottom: constants.borders.default,
+              ':nth-of-type(2)': {
+                width: '100%',
+              },
+              '&:not(:last-child)': {
+                borderRight: constants.borders.default,
+              },
+            },
+            tr: {
+              ':hover': {
+                backgroundColor: colors.structure.iron,
+                boxShadow: constants.boxShadows.tableRow,
+                td: {
+                  border: constants.borders.tableRow,
+                  borderLeft: '0',
+                  borderRight: '0',
+                  '&:first-of-type': {
+                    border: constants.borders.tableRow,
+                    borderRight: '0',
+                  },
+                  '&:last-of-type': {
+                    border: constants.borders.tableRow,
+                    borderLeft: '0',
+                  },
+                },
+              },
+            },
             td: {
-              border: constants.borders.tableRow,
+              fontWeight: typography.weight.extraLight,
+              color: colors.structure.pebble,
+              padding: `${spacing.base}px ${spacing.normal}px`,
+              border: constants.borders.transparent,
               borderLeft: '0',
               borderRight: '0',
-              '&:first-of-type': {
-                border: constants.borders.tableRow,
+              ':first-of-type': {
+                border: constants.borders.transparent,
                 borderRight: '0',
               },
-              '&:last-of-type': {
-                border: constants.borders.tableRow,
+              ':last-of-type': {
+                border: constants.borders.transparent,
                 borderLeft: '0',
               },
             },
-          },
-        },
-        td: {
-          fontWeight: typography.weight.extraLight,
-          color: colors.structure.pebble,
-          padding: `${spacing.base}px ${spacing.normal}px`,
-          border: constants.borders.transparent,
-          borderLeft: '0',
-          borderRight: '0',
-          ':first-of-type': {
-            border: constants.borders.transparent,
-            borderRight: '0',
-          },
-          ':last-of-type': {
-            border: constants.borders.transparent,
-            borderLeft: '0',
-          },
-        },
-      }}>
-      <thead>
-        <tr>
-          {columns.map(column => {
-            return (
-              <th key={column}>
-                <div css={{ display: 'flex' }}>{column}</div>
-              </th>
-            )
-          })}
-        </tr>
-      </thead>
-      <tbody>
-        {rows.map(
-          ({
-            id,
-            state,
-            name,
-            username,
-            assetCounts,
-            priority,
-            timeCreated,
-            timeStarted,
-            timeUpdated,
-            taskCounts,
-          }) => {
-            return (
-              <tr
-                key={id}
-                css={{
-                  backgroundColor: colors.structure.lead,
-                  '&:nth-of-type(2n)': {
-                    backgroundColor: colors.structure.mattGrey,
-                  },
-                }}>
-                <td>
-                  <Status jobStatus={state} />
-                </td>
-                <td>{name}</td>
-                <td>{username}</td>
-                <td>{priority}</td>
-                <td>{formatFullDate({ timestamp: timeCreated })}</td>
-                <td>
-                  {taskCounts.tasksFailure > 0 && (
-                    <div
-                      css={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        color: colors.signal.warning.base,
-                        fontWeight: typography.weight.bold,
-                        fontSize: typography.size.kilo,
-                        lineHeight: typography.height.kilo,
-                        padding: spacing.base,
-                        borderRadius: BORDER_RADIUS,
-                        backgroundColor: colors.structure.coal,
-                      }}>
-                      {taskCounts.tasksFailure}
-                    </div>
-                  )}
-                </td>
-                <td>
-                  {assetCounts.assetErrorCount > 0 && (
-                    <div
-                      css={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        color: colors.signal.warning.base,
-                        fontWeight: typography.weight.bold,
-                        fontSize: typography.size.kilo,
-                        lineHeight: typography.height.kilo,
-                        padding: spacing.base,
-                        borderRadius: BORDER_RADIUS,
-                        backgroundColor: colors.structure.coal,
-                      }}>
-                      {assetCounts.assetErrorCount}
-                    </div>
-                  )}
-                </td>
-                <td>
-                  {Object.values(assetCounts).reduce(
-                    (total, count) => total + count,
-                  )}
-                </td>
-                <td>
-                  <ProgressBar
-                    state={state}
-                    taskCounts={taskCounts}
-                    timeStarted={timeStarted}
-                    timeUpdated={timeUpdated}
-                  />
-                </td>
-              </tr>
-            )
-          },
-        )}
-      </tbody>
-    </table>
+          }}>
+          <thead>
+            <tr>
+              {columns.map(column => {
+                return (
+                  <th key={column}>
+                    <div css={{ display: 'flex' }}>{column}</div>
+                  </th>
+                )
+              })}
+            </tr>
+          </thead>
+          <tbody>
+            {rows.map(
+              ({
+                id,
+                state,
+                name,
+                username,
+                assetCounts,
+                priority,
+                timeCreated,
+                timeStarted,
+                timeUpdated,
+                taskCounts,
+              }) => {
+                return (
+                  <tr
+                    key={id}
+                    css={{
+                      backgroundColor: colors.structure.lead,
+                      '&:nth-of-type(2n)': {
+                        backgroundColor: colors.structure.mattGrey,
+                      },
+                    }}>
+                    <td>
+                      <Status jobStatus={state} />
+                    </td>
+                    <td>{name}</td>
+                    <td>{username}</td>
+                    <td>{priority}</td>
+                    <td>{formatFullDate({ timestamp: timeCreated })}</td>
+                    <td>
+                      {taskCounts.tasksFailure > 0 && (
+                        <div
+                          css={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            color: colors.signal.warning.base,
+                            fontWeight: typography.weight.bold,
+                            fontSize: typography.size.kilo,
+                            lineHeight: typography.height.kilo,
+                            padding: spacing.base,
+                            borderRadius: BORDER_RADIUS,
+                            backgroundColor: colors.structure.coal,
+                          }}>
+                          {taskCounts.tasksFailure}
+                        </div>
+                      )}
+                    </td>
+                    <td>
+                      {assetCounts.assetErrorCount > 0 && (
+                        <div
+                          css={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            color: colors.signal.warning.base,
+                            fontWeight: typography.weight.bold,
+                            fontSize: typography.size.kilo,
+                            lineHeight: typography.height.kilo,
+                            padding: spacing.base,
+                            borderRadius: BORDER_RADIUS,
+                            backgroundColor: colors.structure.coal,
+                          }}>
+                          {assetCounts.assetErrorCount}
+                        </div>
+                      )}
+                    </td>
+                    <td>
+                      {Object.values(assetCounts).reduce(
+                        (total, count) => total + count,
+                      )}
+                    </td>
+                    <td>
+                      <ProgressBar
+                        state={state}
+                        taskCounts={taskCounts}
+                        timeStarted={timeStarted}
+                        timeUpdated={timeUpdated}
+                      />
+                    </td>
+                  </tr>
+                )
+              },
+            )}
+          </tbody>
+        </table>
+      </div>
+    </div>
   )
 }
 
