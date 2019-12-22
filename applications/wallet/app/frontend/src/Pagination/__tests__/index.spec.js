f1f140f3418eca1523b038ff2e1e1d7624ffb055
Damien Varron <damien@varron.fr>
2019-12-22 07:16:13 +0000

    WAL-12: update Pagination

--- applications/wallet/app/frontend/src/Pagination/__tests__/index.spec.js
+++ applications/wallet/app/frontend/src/Pagination/__tests__/index.spec.js
@@ -2,30 +2,30 @@ import TestRenderer from 'react-test-renderer'
 
 import Pagination from '..'
 
+const PROJECT_ID = '76917058-b147-4556-987a-0a0f11e46d9b'
+
 describe('<Pagination />', () => {
   it('should render properly on first page', () => {
+    require('next/router').__setUseRouter({
+      pathname: '/[projectId]/jobs',
+      query: { projectId: PROJECT_ID },
+    })
+
     const component = TestRenderer.create(
-      <Pagination
-        legend="Jobs: 1–17 of 415"
-        currentPage={1}
-        totalPages={2}
-        prevLink="/"
-        nextLink="/?page=2"
-      />,
+      <Pagination currentPage={1} totalPages={2} />,
     )
 
     expect(component.toJSON()).toMatchSnapshot()
   })
 
   it('should render properly on last page', () => {
+    require('next/router').__setUseRouter({
+      pathname: '/[projectId]/jobs',
+      query: { projectId: PROJECT_ID },
+    })
+
     const component = TestRenderer.create(
-      <Pagination
-        legend="Jobs: 1–17 of 415"
-        currentPage={2}
-        totalPages={2}
-        prevLink="/"
-        nextLink="/?page=2"
-      />,
+      <Pagination currentPage={2} totalPages={2} />,
     )
 
     expect(component.toJSON()).toMatchSnapshot()
