b28f50b2967c036b98fc4fc29150f43f18653e53
octaviaaris <octavia@zorroa.com>
2019-12-22 17:43:08 -0800

    add options for coming soon job state options

--- applications/wallet/app/frontend/src/Status/index.js
+++ applications/wallet/app/frontend/src/Status/index.js
@@ -2,9 +2,19 @@ import PropTypes from 'prop-types'
 import { colors, spacing, constants } from '../Styles'
 
 const STATUS_COLORS = {
+  // running color
   Active: colors.signal.canary.base,
-  Cancelled: colors.structure.steel,
+  InProgress: colors.signal.canary.base,
+  Paused: colors.signal.canary.base,
+
+  // failure color
+  Cancelled: colors.signal.warning.base,
+  Failure: colors.signal.warning.base,
+
+  // success color
+  Success: colors.signal.grass.base,
   Finished: colors.signal.grass.base,
+  Archived: colors.signal.grass.base,
 }
 
 const Status = ({ jobStatus }) => {
