4d6ac5180a446d5d78fb531469ee7a7f5e6da825
Damien Varron <damien@varron.fr>
2019-12-27 17:25:30 +0100

    add ApiKeys table

--- applications/wallet/app/frontend/src/ApiKeys/index.js
+++ applications/wallet/app/frontend/src/ApiKeys/index.js
@@ -1,9 +1,17 @@
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 
 import PageTitle from '../PageTitle'
 import Tabs from '../Tabs'
+import Table from '../Table'
+
+import ApiKeysRow from './Row'
 
 const ApiKeys = () => {
+  const {
+    query: { projectId },
+  } = useRouter()
+
   return (
     <div>
       <Head>
@@ -17,6 +25,14 @@ const ApiKeys = () => {
           { title: 'View all', href: '/api-keys' },
           { title: 'Create API key', href: '/api-keys/add' },
         ]}
+      />
+
+      <Table
+        url={`/api/v1/projects/${projectId}/apikeys/`}
+        columns={['API Key Name', 'Permissions']}
+        renderRow={({ result }) => (
+          <ApiKeysRow key={result.id} apiKey={result} />
+        )}
       />
     </div>
   )
