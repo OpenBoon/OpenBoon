7c497fb13a1de5b579e2d28df9d353e9f34daeb5
Matt <mvchambers@me.com>
2019-12-28 21:18:39 -0500

    Added comments for permission enum.

--- services/auth-server/auth-client/src/main/kotlin/com/zorroa/auth/client/Permission.kt
+++ services/auth-server/auth-client/src/main/kotlin/com/zorroa/auth/client/Permission.kt
@@ -1,17 +1,35 @@
 package com.zorroa.auth.client
 
+import io.swagger.annotations.ApiModelProperty
+
 enum class Permission {
+    @ApiModelProperty("Permission which allows access to monitoring endpoints")
     PlatformMonitor,
+
+    @ApiModelProperty("Permission which allows access to platform management endpoints")
     PlatformManage,
 
+    @ApiModelProperty("Permission which provides ability to switch projects")
     ProjectOverride,
+
+    @ApiModelProperty("Permission which provides ability to view encrypted project data")
     ProjectDecrypt,
 
+    @ApiModelProperty("Permission which provides ability to manage API keys")
     ApiKeyManage,
 
+    @ApiModelProperty("Permission which provides ability to read assets and associated files")
     AssetsRead,
+
+    @ApiModelProperty("Permission which provides ability to import assets. (created and update)")
     AssetsImport,
+
+    @ApiModelProperty("Permission which provides ability to remove assets")
     AssetsDelete,
 
-    ProjectManage;
+    @ApiModelProperty("Permission which provides ability to manage projects.")
+    ProjectManage,
+
+    @ApiModelProperty("Permission which provides ability to create projects.")
+    ProjectCreate;
 }
