9d08b486c1d7e7c2657ce039bd833c5d8d76cbbc
Damien Varron <damien@varron.fr>
2019-12-31 17:50:26 +0100

    add title

--- applications/wallet/frontend/src/NoProject/index.js
+++ applications/wallet/frontend/src/NoProject/index.js
@@ -1,5 +1,14 @@
+import Head from 'next/head'
+
 const NoProject = () => {
-  return 'You have no project available'
+  return (
+    <div>
+      <Head>
+        <title>Zorroa</title>
+      </Head>
+      You have no project available
+    </div>
+  )
 }
 
 export default NoProject
