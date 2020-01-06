5a11a021e591b1408db7b0b4702212d22225c64f
Matt <mvchambers@me.com>
2020-01-06 16:12:51 -0500

    Added concept of project settings.

--- services/archivist/archivist/src/main/kotlin/com/zorroa/archivist/service/ProjectService.kt
+++ services/archivist/archivist/src/main/kotlin/com/zorroa/archivist/service/ProjectService.kt
@@ -5,26 +5,35 @@ import com.zorroa.archivist.domain.FileCategory
 import com.zorroa.archivist.domain.FileGroup
 import com.zorroa.archivist.domain.FileStorageLocator
 import com.zorroa.archivist.domain.FileStorageSpec
+import com.zorroa.archivist.domain.IndexRoute
 import com.zorroa.archivist.domain.IndexRouteSpec
 import com.zorroa.archivist.domain.IndexRouteState
 import com.zorroa.archivist.domain.LogAction
 import com.zorroa.archivist.domain.LogObject
+import com.zorroa.archivist.domain.Pipeline
 import com.zorroa.archivist.domain.PipelineMode
 import com.zorroa.archivist.domain.PipelineSpec
 import com.zorroa.archivist.domain.Project
 import com.zorroa.archivist.domain.ProjectFilter
+import com.zorroa.archivist.domain.ProjectSettings
 import com.zorroa.archivist.domain.ProjectSpec
 import com.zorroa.archivist.repository.KPagedList
+import com.zorroa.archivist.repository.ProjectCustomDao
 import com.zorroa.archivist.repository.ProjectDao
-import com.zorroa.archivist.repository.ProjectFilterDao
 import com.zorroa.archivist.repository.UUIDGen
+import com.zorroa.archivist.repository.throwWhenNotFound
+import com.zorroa.archivist.security.InternalThreadAuthentication
 import com.zorroa.archivist.security.KnownKeys
+import com.zorroa.archivist.security.getProjectId
 import com.zorroa.archivist.security.getZmlpActor
+import com.zorroa.archivist.security.hasPermission
+import com.zorroa.archivist.security.withAuth
 import com.zorroa.archivist.storage.FileStorageService
 import com.zorroa.auth.client.AuthServerClient
 import com.zorroa.auth.client.Permission
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.dao.DataRetrievalFailureException
 import org.springframework.security.crypto.keygen.KeyGenerators
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -63,19 +72,31 @@ interface ProjectService {
      * old key.
      */
     fun getCredentialsKey(): String
+
+    /**
+     * Get the project settings
+     */
+    fun getSettings(projectId: UUID): ProjectSettings
+
+    /**
+     * Get the project settings blob.
+     */
+    fun updateSettings(projectId: UUID, settings: ProjectSettings): Boolean
 }
 
 @Service
 @Transactional
 class ProjectServiceImpl constructor(
     val projectDao: ProjectDao,
-    val projectFilterDao: ProjectFilterDao,
+    val projectCustomDao: ProjectCustomDao,
     val authServerClient: AuthServerClient,
-    val indexRoutingService: IndexRoutingService,
     val fileStorageService: FileStorageService,
     val properties: ApplicationProperties,
     val txEvent: TransactionEventManager
 ) : ProjectService {
+
+    @Autowired
+    lateinit var indexRoutingService: IndexRoutingService
 
     @Autowired
     lateinit var pipelineService: PipelineService
@@ -93,11 +114,20 @@ class ProjectServiceImpl constructor(
                 actor.name
             )
         )
-        createIndexRoute(project)
+        withAuth(InternalThreadAuthentication(project.id, setOf())) {
+
+            val route = createIndexRoute(project)
+            val pipeline = createDefaultPipeline(project)
+            projectCustomDao.createSettings(
+                project.id, ProjectSettings(
+                    pipeline.id,
+                    route.id
+                )
+            )
+        }
         txEvent.afterCommit(sync = true) {
             createProjectCryptoKey(project)
             createStandardApiKeys(project)
-            createDefaultPipeline(project)
         }
 
         logger.event(
@@ -110,15 +140,54 @@ class ProjectServiceImpl constructor(
         return project
     }
 
-    private fun createIndexRoute(project: Project) {
+    private fun createIndexRoute(project: Project): IndexRoute {
         val mapping = properties.getString("archivist.es.default-mapping-type")
         val ver = properties.getInt("archivist.es.default-mapping-version")
-        indexRoutingService.createIndexRoute(
+        return indexRoutingService.createIndexRoute(
             IndexRouteSpec(
                 mapping, ver, projectId = project.id,
                 state = IndexRouteState.CURRENT
             )
         )
+    }
+
+    override fun getCredentialsKey(): String {
+        val loc = FileStorageLocator(FileGroup.INTERNAL, "project", FileCategory.KEYS, "project.key")
+        return String(fileStorageService.fetch(loc))
+    }
+
+    @Transactional(readOnly = true)
+    override fun get(id: UUID): Project = projectDao.getOne(id)
+
+    @Transactional(readOnly = true)
+    override fun get(name: String): Project = projectDao.getByName(name)
+
+    @Transactional(readOnly = true)
+    override fun getAll(filter: ProjectFilter): KPagedList<Project> = projectCustomDao.getAll(filter)
+
+    @Transactional(readOnly = true)
+    override fun findOne(filter: ProjectFilter): Project = projectCustomDao.findOne(filter)
+
+    @Transactional(readOnly = true)
+    override fun getSettings(projectId: UUID): ProjectSettings = projectCustomDao.getSettings(get(projectId).id)
+
+    override fun updateSettings(projectId: UUID, settings: ProjectSettings): Boolean {
+        val project = get(projectId)
+
+        if (!hasPermission(listOf(Permission.ProjectManage))) {
+            throw DataRetrievalFailureException("Unable to find project: $projectId")
+        }
+
+        /**
+         * Validate these IDs exist and are within the same project.
+         */
+        throwWhenNotFound("Unable to find Pipeline ID ${settings.defaultPipelineId}") {
+            pipelineService.get(settings.defaultPipelineId)
+        }
+        throwWhenNotFound("Unable to find Index ID ${settings.defaultIndexRouteId}") {
+            indexRoutingService.getIndexRoute(settings.defaultIndexRouteId)
+        }
+        return projectCustomDao.updateSettings(project.id, settings)
     }
 
     /**
@@ -135,9 +204,9 @@ class ProjectServiceImpl constructor(
         )
     }
 
-    private fun createDefaultPipeline(project: Project) {
+    private fun createDefaultPipeline(project: Project): Pipeline {
         val spec = PipelineSpec("default", PipelineMode.MODULAR, projectId = project.id)
-        val pipeline = pipelineService.create(spec)
+        return pipelineService.create(spec)
     }
 
     private fun createProjectCryptoKey(project: Project) {
@@ -152,27 +221,6 @@ class ProjectServiceImpl constructor(
 
         val spec = FileStorageSpec(projectKeyLocation, mapOf(), key.toByteArray())
         fileStorageService.store(spec)
-    }
-
-    override fun getCredentialsKey(): String {
-        val loc = FileStorageLocator(FileGroup.INTERNAL, "project", FileCategory.KEYS, "project.key")
-        return String(fileStorageService.fetch(loc))
-    }
-
-    override fun get(id: UUID): Project {
-        return projectDao.getOne(id)
-    }
-
-    override fun get(name: String): Project {
-        return projectDao.getByName(name)
-    }
-
-    override fun getAll(filter: ProjectFilter): KPagedList<Project> {
-        return projectFilterDao.getAll(filter)
-    }
-
-    override fun findOne(filter: ProjectFilter): Project {
-        return projectFilterDao.findOne(filter)
     }
 
     companion object {
