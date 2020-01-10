0ed656a195999543c402a91c9f97d93b5d27989e
Matt <mvchambers@me.com>
2020-01-09 22:36:44 -0500

    Handle SDK renames.

--- containers/zmlp-plugins-core/pylib/zmlp_core/proxy/video.py
+++ containers/zmlp-plugins-core/pylib/zmlp_core/proxy/video.py
@@ -2,7 +2,7 @@ import subprocess
 import tempfile
 
 from zmlp.analysis.base import AssetProcessor
-from zmlp.analysis.proxy import store_proxy_media
+from zmlp.analysis.proxy import store_proxy_file
 from zmlp.analysis.storage import file_storage
 from zmlp_core.util.media import get_video_metadata
 
@@ -108,4 +108,4 @@ def store_video_proxy(asset, path):
     props = get_video_metadata(path)
     attrs = {"frames": props['frames'], 'frameRate': props['frameRate']}
     size = (props['width'], props['height'])
-    return store_proxy_media(asset, path, size, 'video', attrs)
+    return store_proxy_file(asset, path, size, 'video', attrs)
