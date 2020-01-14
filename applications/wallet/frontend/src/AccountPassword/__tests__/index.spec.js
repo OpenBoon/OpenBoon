ae1457ea0707350b39cc8694dcef310d9d587301
Damien Varron <damien@varron.fr>
2020-01-14 21:27:35 +0100

    fixup

--- applications/wallet/frontend/src/AccountPassword/__tests__/index.spec.js
+++ applications/wallet/frontend/src/AccountPassword/__tests__/index.spec.js
@@ -7,7 +7,7 @@ const PROJECT_ID = '76917058-b147-4556-987a-0a0f11e46d9b'
 describe('<AccountPassword />', () => {
   it('should render properly', () => {
     require('next/router').__setUseRouter({
-      pathname: '/[projectId]/api-keys/add',
+      pathname: '/[projectId]/account/password',
       query: { projectId: PROJECT_ID },
     })
 
