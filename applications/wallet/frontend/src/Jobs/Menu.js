bda12d0de5f6dea6a3e406a70eb6f8862d0fc191
Damien Varron <damien@varron.fr>
2020-01-14 13:05:47 +0100

    WAL-145: show gear menu on hover only

--- applications/wallet/frontend/src/Jobs/Menu.js
+++ applications/wallet/frontend/src/Jobs/Menu.js
@@ -38,6 +38,7 @@ const JobsMenu = ({ projectId, jobId, revalidate }) => {
       open="left"
       button={({ onBlur, onClick }) => (
         <Button
+          className="gear"
           aria-label="Toggle Actions Menu"
           variant={VARIANTS.NEUTRAL}
           style={{
