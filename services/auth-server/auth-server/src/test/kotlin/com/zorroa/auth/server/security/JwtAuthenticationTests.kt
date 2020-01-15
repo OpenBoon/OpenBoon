09fb57036c853c4eae40c9e9392b01661a6d01f8
Matt <mvchambers@me.com>
2020-01-15 17:39:37 -0500

    Allow project override via header.

--- services/auth-server/auth-server/src/test/kotlin/com/zorroa/auth/server/security/JwtAuthenticationTests.kt
+++ services/auth-server/auth-server/src/test/kotlin/com/zorroa/auth/server/security/JwtAuthenticationTests.kt
@@ -9,6 +9,7 @@ import com.zorroa.auth.server.domain.ApiKeySpec
 import org.junit.Test
 import org.springframework.beans.factory.annotation.Autowired
 import java.io.File
+import java.util.UUID
 import kotlin.test.assertEquals
 
 class JwtAuthenticationTests : AbstractTest() {
@@ -36,5 +37,16 @@ class JwtAuthenticationTests : AbstractTest() {
 
         val auth = jwtAuthenticationFilter.validateToken(token)
         assertEquals(auth.user.id, apiKey.id)
+    }
+
+    @Test
+    fun testValidateInceptionTokenWithProjectHeader() {
+        val apiKey = Json.mapper.readValue<ApiKey>(File("src/test/resources/key.json"))
+        val token = apiKey.getJwtToken()
+        val pid = UUID.randomUUID()
+
+        val auth = jwtAuthenticationFilter.validateToken(token, pid)
+        assertEquals(auth.user.id, apiKey.id)
+        assertEquals(auth.user.projectId, pid)
     }
 }
