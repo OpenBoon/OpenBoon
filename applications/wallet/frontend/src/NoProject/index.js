d6b0293efa9390a7dc1053270a8cf26602ab00bb
Wajid Kagzi <wjkagzi@gmail.com>
2020-01-15 11:30:47 -0500

    WAL-107: Update no project component and snapshot

--- applications/wallet/frontend/src/NoProject/index.js
+++ applications/wallet/frontend/src/NoProject/index.js
@@ -1,12 +1,26 @@
 import Head from 'next/head'
 
+import NoProjectSvg from '../Icons/noProject.svg'
+
+import { colors } from '../Styles'
+
 const NoProject = () => {
   return (
     <div>
       <Head>
         <title>Zorroa</title>
       </Head>
-      You have no project available
+      <div
+        css={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}>
+        <NoProjectSvg width={250} />
+        <h3 css={{ color: colors.structure.steel }}>
+          You currently have no projects.
+        </h3>
+      </div>
     </div>
   )
 }
