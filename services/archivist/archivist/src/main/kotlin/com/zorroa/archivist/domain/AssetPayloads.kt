bb3189a2bf7e02f15a0723dfca4a123fd931b6f4
Matt <mvchambers@me.com>
2020-01-22 22:24:26 -0500

    Fixes linting issue

--- services/archivist/archivist/src/main/kotlin/com/zorroa/archivist/domain/AssetPayloads.kt
+++ services/archivist/archivist/src/main/kotlin/com/zorroa/archivist/domain/AssetPayloads.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore
 import io.swagger.annotations.ApiModel
 import io.swagger.annotations.ApiModelProperty
 import org.springframework.web.multipart.MultipartFile
+import java.util.UUID
 
 @ApiModel("Update Asset By Query Request", description = "Request structure to update an Asset.")
 class UpdateAssetsByQueryRequest(
@@ -71,4 +72,21 @@ class BatchCreateAssetsRequest(
     @JsonIgnore
     @ApiModelProperty("The taskId that is creating the assets via expand.", hidden = true)
     val task: InternalTask? = null
+)
+
+@ApiModel("Batch Create Assets Response",
+    description = "The response returned after provisioning assets.")
+class BatchCreateAssetsResponse(
+
+    @ApiModelProperty("The ES bulk response.")
+    val bulkResponse: Map<String, Any>,
+
+    @ApiModelProperty("A map of failed asset ids to error message")
+    val failed: List<Map<String, String?>>,
+
+    @ApiModelProperty("A list of asset Ids created.")
+    val created: List<String>,
+
+    @ApiModelProperty("The ID of the analysis job, if analysis was selected")
+    var jobId: UUID? = null
 )
