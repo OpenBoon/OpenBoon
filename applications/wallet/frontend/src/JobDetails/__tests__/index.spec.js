e3272c7f89ba869d17e1b6e2f71bc54ec06fb893
octaviaaris <octavia@zorroa.com>
2020-01-23 10:03:15 -0800

    add job id to test

--- applications/wallet/frontend/src/JobDetails/__tests__/index.spec.js
+++ applications/wallet/frontend/src/JobDetails/__tests__/index.spec.js
@@ -6,12 +6,13 @@ jest.mock('../../Pagination', () => 'Pagination')
 jest.mock('../../UserMenu', () => 'UserMenu')
 
 const PROJECT_ID = '76917058-b147-4556-987a-0a0f11e46d9b'
+const JOB_ID = '82d5308b-67c2-1433-8fef-0a580a000956'
 
 describe('<JobDetails />', () => {
   it('should render properly', () => {
     require('next/router').__setUseRouter({
       pathname: '/[projectId]/jobs/details',
-      query: { projectId: PROJECT_ID },
+      query: { projectId: PROJECT_ID, jobId: JOB_ID },
     })
 
     require('swr').__setMockUseSWRResponse({
