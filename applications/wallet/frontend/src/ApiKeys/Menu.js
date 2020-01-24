a56ee826f53e2f3ee6f51c90420eb210ca5eb2c4
Damien Varron <damien@varron.fr>
2020-01-24 22:32:21 +0000

    WAL-23: update API Keys UI

---
+++ applications/wallet/frontend/src/ApiKeys/Menu.js
@@ -0,0 +1,84 @@
+import { useState } from 'react'
+import PropTypes from 'prop-types'
+
+import { colors, spacing, constants } from '../Styles'
+
+import { fetcher } from '../Fetch/helpers'
+
+import Menu from '../Menu'
+import Button, { VARIANTS } from '../Button'
+import Modal from '../Modal'
+
+import GearSvg from '../Icons/gear.svg'
+
+const ApiKeysMenu = ({ projectId, apiKeyId, revalidate }) => {
+  const [isDeleteModalOpen, setDeleteModalOpen] = useState(false)
+
+  return (
+    <Menu
+      open="left"
+      button={({ onBlur, onClick }) => (
+        <Button
+          className="gear"
+          aria-label="Toggle Actions Menu"
+          variant={VARIANTS.NEUTRAL}
+          style={{
+            color: colors.structure.coal,
+            padding: spacing.moderate / 2,
+            borderRadius: constants.borderRadius.round,
+            ':hover': { backgroundColor: colors.structure.steel },
+          }}
+          onBlur={onBlur}
+          onClick={onClick}
+          isDisabled={false}>
+          <GearSvg width={20} />
+        </Button>
+      )}>
+      {({ onClick }) => (
+        <div>
+          <ul>
+            <li>
+              <>
+                <Button
+                  variant={VARIANTS.MENU_ITEM}
+                  onClick={() => {
+                    setDeleteModalOpen(true)
+                  }}
+                  isDisabled={false}>
+                  Delete
+                </Button>
+                {isDeleteModalOpen && (
+                  <Modal
+                    onCancel={() => {
+                      setDeleteModalOpen(false)
+                      onClick()
+                    }}
+                    onConfirm={async () => {
+                      setDeleteModalOpen(false)
+                      onClick()
+
+                      await fetcher(
+                        `/api/v1/projects/${projectId}/apikeys/${apiKeyId}/delete/`,
+                        { method: 'PUT' },
+                      )
+
+                      revalidate()
+                    }}
+                  />
+                )}
+              </>
+            </li>
+          </ul>
+        </div>
+      )}
+    </Menu>
+  )
+}
+
+ApiKeysMenu.propTypes = {
+  projectId: PropTypes.string.isRequired,
+  apiKeyId: PropTypes.string.isRequired,
+  revalidate: PropTypes.func.isRequired,
+}
+
+export default ApiKeysMenu
--- applications/wallet/frontend/src/Modal/index.js
+++ applications/wallet/frontend/src/Modal/index.js
@@ -1,124 +1,102 @@
-import { useState } from 'react'
+import PropTypes from 'prop-types'
 import AriaModal from 'react-aria-modal'
 
 import { colors, constants, spacing } from '../Styles'
 
 import Button, { VARIANTS } from '../Button'
 
-import WarningSvg from '../Icons/warning.svg'
 import CrossSvg from '../Icons/cross.svg'
 
 const BUTTON_HEIGHT = 40
 const MODAL_HEIGHT = 200
 const MODAL_WIDTH = 480
 
-const Modal = () => {
-  const [showModal, setShowModal] = useState(false)
-
+const Modal = ({ onCancel, onConfirm }) => {
   return (
-    <div>
-      <button
-        type="button"
-        onClick={() => {
-          setShowModal(true)
+    <AriaModal
+      titleId="Delete API Key"
+      getApplicationNode={() => document.getElementById('__next')}
+      underlayColor="rgba(0, 0, 0, 0.75)"
+      onExit={onCancel}
+      verticallyCenter>
+      <div
+        css={{
+          display: 'flex',
+          flexDirection: 'column',
+          width: MODAL_WIDTH,
+          height: MODAL_HEIGHT,
         }}>
-        Open Modal
-      </button>
+        <header
+          css={{
+            backgroundColor: colors.structure.black,
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            padding: spacing.normal,
+            textTransform: 'uppercase',
+          }}>
+          <div css={{ color: colors.structure.zinc }}>Delete</div>
+          <div
+            role="button"
+            aria-label="Close Modal"
+            tabIndex="-1"
+            onClick={onCancel}
+            onKeyDown={onCancel}>
+            <CrossSvg width={20} color={colors.structure.white} />
+          </div>
+        </header>
 
-      {showModal && (
-        <AriaModal
-          titleId="Modal"
-          onExit={() => {
-            setShowModal(false)
-          }}
-          underlayClickExits
-          verticallyCenter>
+        <div
+          css={{
+            height: '100%',
+            backgroundColor: colors.structure.iron,
+            padding: spacing.spacious,
+          }}>
           <div
             css={{
               display: 'flex',
-              flexDirection: 'column',
-              width: MODAL_WIDTH,
-              height: MODAL_HEIGHT,
+              alignItems: 'center',
+              color: colors.marble,
             }}>
-            <header
-              css={{
-                backgroundColor: colors.structure.iron,
-                display: 'flex',
-                justifyContent: 'space-between',
-                alignItems: 'center',
-                padding: spacing.normal,
-                textTransform: 'uppercase',
-              }}>
-              <div css={{ color: colors.structure.zinc }}>Delete</div>
-              <div
-                role="button"
-                aria-label="Close Modal"
-                tabIndex="-1"
-                onClick={() => {
-                  setShowModal(false)
-                }}
-                onKeyDown={() => {
-                  setShowModal(false)
-                }}>
-                <CrossSvg width={20} color={colors.structure.zinc} />
-              </div>
-            </header>
-
-            <div
-              css={{
-                height: '100%',
-                backgroundColor: colors.structure.mattGrey,
-                padding: spacing.spacious,
-              }}>
-              <div
-                css={{
-                  display: 'flex',
-                  alignItems: 'center',
-                  color: colors.marble,
-                }}>
-                <WarningSvg width={20} color={colors.signal.warning.base} />
-                <div css={{ paddingLeft: spacing.base }}>
-                  Deleting this key cannot be undone.
-                </div>
-              </div>
-              <div
-                css={{
-                  display: 'flex',
-                  justifyContent: 'flex-end',
-                  paddingTop: spacing.spacious,
-                  button: {
-                    height: BUTTON_HEIGHT,
-                    padding: `0 ${spacing.spacious}px`,
-                    border: 'none',
-                    borderRadius: constants.borderRadius.small,
-                  },
-                }}>
-                <Button
-                  type="button"
-                  variant={VARIANTS.SECONDARY}
-                  css={{
-                    marginRight: spacing.normal,
-                  }}
-                  onClick={() => {
-                    setShowModal(false)
-                  }}>
-                  Cancel
-                </Button>
-                <Button
-                  type="button"
-                  variant={VARIANTS.WARNING}
-                  onClick={() => {
-                    setShowModal(false)
-                  }}>
-                  Delete Permanently
-                </Button>
-              </div>
-            </div>
+            Deleting this key cannot be undone.
           </div>
-        </AriaModal>
-      )}
-    </div>
+          <div
+            css={{
+              display: 'flex',
+              justifyContent: 'flex-end',
+              paddingTop: spacing.spacious,
+              button: {
+                height: BUTTON_HEIGHT,
+                padding: `0 ${spacing.spacious}px`,
+                border: 'none',
+                borderRadius: constants.borderRadius.small,
+              },
+            }}>
+            <Button
+              type="button"
+              variant={VARIANTS.SECONDARY}
+              css={{
+                marginRight: spacing.normal,
+              }}
+              onClick={onCancel}>
+              Cancel
+            </Button>
+            <Button
+              type="button"
+              variant={VARIANTS.WARNING}
+              onClick={onConfirm}>
+              Delete Permanently
+            </Button>
+          </div>
+        </div>
+      </div>
+    </AriaModal>
   )
+}
+
+Modal.propTypes = {
+  onCancel: PropTypes.func.isRequired,
+  onConfirm: PropTypes.func.isRequired,
 }
 
 export default Modal
