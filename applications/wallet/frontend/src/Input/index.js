94a9eb0f11d61ad8022b9cd3c6afdfa4ffdf40f5
Wajid Kagzi <wjkagzi@gmail.com>
2020-01-27 11:00:34 -0500

    WAL-114: Add new input styles and update tests

--- applications/wallet/frontend/src/Input/index.js
+++ applications/wallet/frontend/src/Input/index.js
@@ -1,16 +1,52 @@
 import PropTypes from 'prop-types'
 
-import { constants, spacing } from '../Styles'
+import { constants, spacing, colors, typography } from '../Styles'
 
 const PADDING = spacing.base
 
+const BASE = {
+  outlineOffset: 0,
+  padding: spacing.moderate,
+  borderRadius: constants.borderRadius.small,
+  boxShadow: constants.boxShadows.input,
+  width: '100%',
+}
+
+const STYLES = {
+  PRIMARY: {
+    color: colors.structure.black,
+    backgroundColor: colors.structure.white,
+  },
+  SECONDARY: {
+    color: colors.structure.white,
+    backgroundColor: colors.structure.mattGrey,
+    '&.focus-visible': {
+      border: constants.borders.input,
+      backgroundColor: colors.structure.white,
+      outline: 'none',
+      color: colors.structure.black,
+    },
+  },
+}
+
+const BORDER_STYLES = ({ hasError }) => ({
+  border: hasError ? constants.borders.error : constants.borders.transparent,
+})
+
+export const VARIANTS = Object.keys(STYLES).reduce(
+  (accumulator, style) => ({ ...accumulator, [style]: style }),
+  {},
+)
+
 const Input = ({
   id,
+  variant,
   type,
   label,
   value,
   onChange,
   hasError,
+  errorMessage,
   after,
   ...props
 }) => (
@@ -25,16 +61,7 @@ const Input = ({
         name={id}
         value={value}
         onChange={onChange}
-        css={{
-          outlineOffset: 0,
-          padding: spacing.moderate,
-          borderRadius: constants.borderRadius.small,
-          boxShadow: constants.boxShadows.input,
-          width: '100%',
-          border: hasError
-            ? constants.borders.error
-            : constants.borders.transparent,
-        }}
+        css={[BASE, STYLES[variant], BORDER_STYLES({ hasError })]}
         // eslint-disable-next-line react/jsx-props-no-spreading
         {...props}
       />
@@ -51,21 +78,35 @@ const Input = ({
           {after}
         </div>
       )}
+      {hasError && errorMessage && (
+        <div css={{ paddingTop: PADDING }}>
+          <span
+            css={{
+              color: colors.signal.warning.base,
+              fontStyle: typography.style.italic,
+            }}>
+            {errorMessage}
+          </span>
+        </div>
+      )}
     </div>
   </div>
 )
 
 Input.defaultProps = {
   after: false,
+  errorMessage: '',
 }
 
 Input.propTypes = {
   id: PropTypes.string.isRequired,
+  variant: PropTypes.oneOf(Object.keys(VARIANTS)).isRequired,
   type: PropTypes.oneOf(['text', 'password']).isRequired,
   label: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
   hasError: PropTypes.bool.isRequired,
+  errorMessage: PropTypes.string,
   after: PropTypes.node,
 }
 
