6e62966373a3fb2221de394505d3ac2dd388aa1d
Danny Tiesling <danny@zorroa.com>
2020-01-31 21:25:37 +0000

    Updates terraform and ci scripts to fix various deployment issues.

--- deployment/terraform/modules/auth-server/variables.tf
+++ deployment/terraform/modules/auth-server/variables.tf
@@ -2,6 +2,7 @@ variable "image-pull-secret" {}
 variable "sql-instance-name" {}
 variable "sql-connection-name" {}
 variable "inception-key-b64" {}
+variable "system-bucket" {}
 
 variable "container-tag" {default = "development"}
 variable "namespace" {default = "default"}
