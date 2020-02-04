9d075fab381b86034754707285cce32b856caffa
Wajid Kagzi <wajid.kagzi@zorroa.com>
2020-02-04 14:10:39 -0500

    WAL-153: Remove permissions

--- applications/wallet/frontend/src/AccountProfile/Info.js
+++ applications/wallet/frontend/src/AccountProfile/Info.js
@@ -1,14 +1,9 @@
-import { typography, colors, spacing } from '../Styles'
-
-import permissions from '../Permissions/__mocks__/permissions'
+import { typography, spacing } from '../Styles'
 
 import { getUser } from '../Authentication/helpers'
 
 const AccountProfileInfo = () => {
   const { email } = getUser()
-  const permissionsList = permissions.results
-    .map(p => p.name.replace(/([A-Z])/g, match => ` ${match}`))
-    .join(', ')
 
   return (
     <>
@@ -20,16 +15,6 @@ const AccountProfileInfo = () => {
           paddingTop: spacing.normal,
         }}>
         {`User ID: ${email}`}
-      </div>
-
-      <div
-        css={{
-          color: colors.structure.steel,
-          fontSize: typography.size.regular,
-          lineHeight: typography.height.regular,
-          fontWeight: typography.weight.regular,
-        }}>
-        {`Permissions: ${permissionsList}`}
       </div>
     </>
   )
