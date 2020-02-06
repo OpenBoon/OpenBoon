9c4a1475a1cec6247df436c0e3c69900c6fec346
Damien Varron <damien@varron.fr>
2020-02-06 11:43:34 -0800

    mock user

--- applications/wallet/frontend/src/AccountProfile/__tests__/Form.spec.js
+++ applications/wallet/frontend/src/AccountProfile/__tests__/Form.spec.js
@@ -4,15 +4,16 @@ import mockUser from '../../User/__mocks__/user'
 
 import AccountProfileForm from '../Form'
 
-const noop = () => () => {}
+jest.mock('../../Authentication/helpers', () => ({
+  getUser: () => mockUser,
+}))
 
-jest.mock('../../Authentication/helpers')
 jest.mock('../helpers')
+
+const noop = () => () => {}
 
 describe('<AccountProfileForm />', () => {
   it('should render properly when a user edits username', () => {
-    require('../../Authentication/helpers').__setMockUser(mockUser)
-
     const component = TestRenderer.create(<AccountProfileForm />)
 
     expect(component.toJSON()).toMatchSnapshot()
@@ -50,8 +51,6 @@ describe('<AccountProfileForm />', () => {
 
   it('should render properly when a user cancels', () => {
     const component = TestRenderer.create(<AccountProfileForm />)
-
-    require('../../Authentication/helpers').__setMockUser(mockUser)
 
     // Click edit username
     act(() => {
