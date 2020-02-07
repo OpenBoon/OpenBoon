310ca27325f88bb040d4af585d83f1aebc39d537
Iron <iron.araujo@zorroa.com>
2020-02-07 17:52:09 -0300

    Removing unused classes. Add docs to SimilaritySearch

--- client/java/src/main/java/com/zorroa/zmlp/client/domain/similarity/SimilaritySearch.java
+++ client/java/src/main/java/com/zorroa/zmlp/client/domain/similarity/SimilaritySearch.java
@@ -6,15 +6,31 @@ import org.elasticsearch.index.query.TermQueryBuilder;
 import java.io.IOException;
 import java.util.*;
 
+/**
+ * Similarity Search Query Builder
+ */
 public class SimilaritySearch extends TermQueryBuilder {
 
-    List<Map> batch;
+    /**
+     * Contains all Hash and Weight tuples
+     */
+    private List<Map> batch;
 
+    /**
+     * Constructor receives a Field Name to a List of Hashes
+     * @param fieldName Name of the Field
+     */
     public SimilaritySearch(String fieldName) {
         super(fieldName, new ArrayList());
         batch = (List)value;
     }
 
+    /**
+     * Add a tuple of hash and weight to the query
+     * @param hash Reference to the image
+     * @param weight Reference weight
+     * @return SimilaritySearch object
+     */
     public SimilaritySearch add(String hash, Double weight){
         Map<String, Object> map = new HashMap();
         map.put("hash", hash);
@@ -23,6 +39,11 @@ public class SimilaritySearch extends TermQueryBuilder {
         return this;
     }
 
+    /**
+     * Add a Hash to the query and use the default value of the weight
+     * @param hash Reference to the image
+     * @return SimilaritySearch object
+     */
     public SimilaritySearch add(String hash){
         Map<String, Object> map = new HashMap();
         map.put("hash", hash);
@@ -31,6 +52,11 @@ public class SimilaritySearch extends TermQueryBuilder {
         return this;
     }
 
+    /**
+     * Add a list of tuples that must contain hash and weight
+     * @param mapList List of Tuples
+     * @return
+     */
     public SimilaritySearch addAll(List<Map<String, Object>> mapList){
         batch.addAll(mapList);
         return this;
