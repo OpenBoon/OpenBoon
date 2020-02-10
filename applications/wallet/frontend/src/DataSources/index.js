ff8040fd1fa23acdc6021494e9189024311312e9
Damien Varron <damien@varron.fr>
2020-02-10 15:40:44 -0800

    WAL-165: list Data Sources and Delete action

---
+++ applications/wallet/frontend/src/DataSources/index.js
@@ -0,0 +1,58 @@
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+
+import PageTitle from '../PageTitle'
+import Tabs from '../Tabs'
+import Table from '../Table'
+
+import DataSourcesRow from './Row'
+
+const DataSources = () => {
+  const {
+    query: { projectId },
+  } = useRouter()
+
+  return (
+    <>
+      <Head>
+        <title>Data Sources</title>
+      </Head>
+
+      <PageTitle>Data Sources</PageTitle>
+
+      <Tabs
+        tabs={[
+          { title: 'View all', href: '/[projectId]/data-sources' },
+          { title: 'Add Data Source', href: '/[projectId]/data-sources/add' },
+        ]}
+      />
+
+      <div>&nbsp;</div>
+
+      <Table
+        url={`/api/v1/projects/${projectId}/datasources/`}
+        columns={[
+          'Name',
+          'Source Type',
+          'Path',
+          'Date Created',
+          'Date Modified',
+          'File Types',
+          '#Actions#',
+        ]}
+        expandColumn={3}
+        renderEmpty="No data sources"
+        renderRow={({ result, revalidate }) => (
+          <DataSourcesRow
+            key={result.id}
+            projectId={projectId}
+            dataSource={result}
+            revalidate={revalidate}
+          />
+        )}
+      />
+    </>
+  )
+}
+
+export default DataSources
