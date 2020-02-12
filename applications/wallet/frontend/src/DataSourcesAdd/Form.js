baae0f788d2f738de83fc58295a280420a0d17fb
Damien Varron <damien@varron.fr>
2020-02-12 11:44:51 -0800

    WAL-162: update DataSourcesAdd form

--- applications/wallet/frontend/src/DataSourcesAdd/Form.js
+++ applications/wallet/frontend/src/DataSourcesAdd/Form.js
@@ -2,13 +2,15 @@ import { useReducer } from 'react'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 
-import Form from '../Form'
+import Form, { MAX_WIDTH } from '../Form'
 import SectionTitle from '../SectionTitle'
 import SectionSubTitle from '../SectionSubTitle'
 import Input, { VARIANTS as INPUT_VARIANTS } from '../Input'
 import Textarea, { VARIANTS as TEXTAREA_VARIANTS } from '../Textarea'
 import Button, { VARIANTS as BUTTON_VARIANTS } from '../Button'
 import ButtonGroup from '../Button/Group'
+
+import DataSourcesAddAutomaticAnalysis from './AutomaticAnalysis'
 
 const INITIAL_STATE = {
   name: '',
@@ -27,43 +29,47 @@ const DataSourcesAddForm = () => {
   } = useRouter()
 
   return (
-    <Form>
-      <SectionTitle>Data Source Name</SectionTitle>
+    <Form style={{ width: 'auto' }}>
+      <div css={{ width: MAX_WIDTH }}>
+        <SectionTitle>Data Source Name</SectionTitle>
 
-      <Input
-        autoFocus
-        id="name"
-        variant={INPUT_VARIANTS.SECONDARY}
-        label="Name"
-        type="text"
-        value={state.name}
-        onChange={({ target: { value } }) => dispatch({ name: value })}
-        hasError={state.errors.name !== undefined}
-        errorMessage={state.errors.name}
-      />
+        <Input
+          autoFocus
+          id="name"
+          variant={INPUT_VARIANTS.SECONDARY}
+          label="Name"
+          type="text"
+          value={state.name}
+          onChange={({ target: { value } }) => dispatch({ name: value })}
+          hasError={state.errors.name !== undefined}
+          errorMessage={state.errors.name}
+        />
 
-      <SectionTitle>Connect to Source: Google Cloud Storage</SectionTitle>
+        <SectionTitle>Connect to Source: Google Cloud Storage</SectionTitle>
 
-      <Input
-        id="url"
-        variant={INPUT_VARIANTS.SECONDARY}
-        label="Bucket Address"
-        type="text"
-        value={state.url}
-        onChange={({ target: { value } }) => dispatch({ url: value })}
-        hasError={state.errors.name !== undefined}
-        errorMessage={state.errors.url}
-      />
+        <Input
+          id="url"
+          variant={INPUT_VARIANTS.SECONDARY}
+          label="Bucket Address"
+          type="text"
+          value={state.url}
+          onChange={({ target: { value } }) => dispatch({ url: value })}
+          hasError={state.errors.name !== undefined}
+          errorMessage={state.errors.url}
+        />
+      </div>
 
-      <Textarea
-        id="key"
-        variant={TEXTAREA_VARIANTS.SECONDARY}
-        label="If this bucket is private, please paste the JSON service account key:"
-        value={state.key}
-        onChange={({ target: { value } }) => dispatch({ key: value })}
-        hasError={state.errors.name !== undefined}
-        errorMessage={state.errors.key}
-      />
+      <div css={{ minWidth: MAX_WIDTH, maxWidth: '50%' }}>
+        <Textarea
+          id="key"
+          variant={TEXTAREA_VARIANTS.SECONDARY}
+          label="If this bucket is private, please paste the JSON service account key:"
+          value={state.key}
+          onChange={({ target: { value } }) => dispatch({ key: value })}
+          hasError={state.errors.name !== undefined}
+          errorMessage={state.errors.key}
+        />
+      </div>
 
       <SectionTitle>Select File Types to Import</SectionTitle>
 
@@ -72,6 +78,8 @@ const DataSourcesAddForm = () => {
       <SectionSubTitle>
         Choose the type of analysis you would like performed on your data set:
       </SectionSubTitle>
+
+      <DataSourcesAddAutomaticAnalysis />
 
       <ButtonGroup>
         <Link
