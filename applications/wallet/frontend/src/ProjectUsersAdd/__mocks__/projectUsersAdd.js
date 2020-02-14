c0272eba754e69d0be897dfafd45957fb028709d
Wajid Kagzi <wajid.kagzi@zorroa.com>
2020-02-14 22:00:37 +0000

    WAL-88: update Add Project User

---
+++ applications/wallet/frontend/src/ProjectUsersAdd/__mocks__/projectUsersAdd.js
@@ -0,0 +1,49 @@
+const projectUsersAdd = {
+  results: {
+    succeeded: [
+      {
+        email: 'tester@fake.com',
+        permissions: ['AssetsRead'],
+        statusCode: 201,
+        body: {
+          id: 6,
+          url:
+            'https://wallet.zmlp.zorroa.com/api/v1/projects/00000000-0000-0000-0000-000000000000/users/6/',
+          username: 'tester@fake.com',
+          firstName: '',
+          lastName: '',
+          email: '',
+          isActive: true,
+          isStaff: false,
+          isSuperuser: false,
+          lastLogin: null,
+          dateJoined: '2020-02-12T18:15:50Z',
+          permissions: ['AssetsRead'],
+        },
+      },
+      {
+        email: 'danny@zorroa.com',
+        permissions: ['AssetsRead'],
+        statusCode: 201,
+        body: {
+          id: 4,
+          url:
+            'https://wallet.zmlp.zorroa.com/api/v1/projects/00000000-0000-0000-0000-000000000000/users/4/',
+          username: 'danny@zorroa.com',
+          firstName: 'Danny',
+          lastName: 'Tiesling',
+          email: 'danny@zorroa.com',
+          isActive: true,
+          isStaff: false,
+          isSuperuser: false,
+          lastLogin: '2020-02-07T02:34:48.138187Z',
+          dateJoined: '2020-01-31T17:19:46.003951Z',
+          permissions: ['AssetsRead'],
+        },
+      },
+    ],
+    failed: [],
+  },
+}
+
+export default projectUsersAdd
---
+++ applications/wallet/frontend/src/ProjectUsersAdd/__tests__/helpers.spec.js
@@ -0,0 +1,91 @@
+import projectUsersAdd from '../__mocks__/projectUsersAdd'
+
+import { onSubmit } from '../helpers'
+
+const PROJECT_ID = '76917058-b147-4556-987a-0a0f11e46d9b'
+
+describe('<ProjectUsersAdd /> helpers', () => {
+  describe('onSubmit()', () => {
+    it('should call the API', async () => {
+      const mockFn = jest.fn()
+
+      fetch.mockResponseOnce(JSON.stringify(projectUsersAdd))
+
+      await onSubmit({
+        dispatch: mockFn,
+        projectId: PROJECT_ID,
+        state: {
+          emails: 'jane@zorroa.com, joe@zorroa.com',
+          permissions: {
+            SuperAdmin: true,
+            ProjectAdmin: false,
+            AssetsRead: true,
+            AssetsImport: false,
+          },
+        },
+      })
+
+      expect(fetch.mock.calls.length).toEqual(1)
+
+      expect(fetch.mock.calls[0][0]).toEqual(
+        '/api/v1/projects/76917058-b147-4556-987a-0a0f11e46d9b/users/',
+      )
+
+      expect(fetch.mock.calls[0][1]).toEqual({
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=UTF-8',
+          'X-CSRFToken': 'CSRF_TOKEN',
+        },
+        body: JSON.stringify({
+          batch: [
+            {
+              email: 'jane@zorroa.com',
+              permissions: ['SuperAdmin', 'AssetsRead'],
+            },
+            {
+              email: 'joe@zorroa.com',
+              permissions: ['SuperAdmin', 'AssetsRead'],
+            },
+          ],
+        }),
+      })
+
+      expect(mockFn).toHaveBeenCalledWith(projectUsersAdd.results)
+    })
+
+    it('should error', async () => {
+      const mockFn = jest.fn()
+
+      fetch.mockRejectOnce({
+        json: () => Promise.resolve({ name: ["This email doesn't work."] }),
+      })
+
+      await onSubmit({
+        dispatch: mockFn,
+        projectId: PROJECT_ID,
+        state: {
+          emails: 'jane@zorroa.com, joe@zorroa.com',
+          permissions: {
+            SuperAdmin: true,
+            ProjectAdmin: true,
+            AssetsRead: true,
+            AssetsImport: false,
+          },
+        },
+      })
+
+      expect(fetch.mock.calls.length).toEqual(1)
+
+      expect(fetch.mock.calls[0][0]).toEqual(
+        '/api/v1/projects/76917058-b147-4556-987a-0a0f11e46d9b/users/',
+      )
+
+      expect(mockFn).toHaveBeenCalledWith({
+        errors: {
+          name: "This email doesn't work.",
+        },
+      })
+    })
+  })
+})
