71c8c1db78d697396060df92776b891ec8020774
octaviaaris <octavia@zorroa.com>
2020-03-02 13:06:29 -0800

    update tests; account for updated api

--- applications/wallet/frontend/src/JobError/StackTrace.js
+++ applications/wallet/frontend/src/JobError/StackTrace.js
@@ -13,40 +13,48 @@ const JobErrorStackTrace = ({ jobError: { message, stackTrace } }) => {
         padding: spacing.normal,
       }}>
       <div>{`"message" : ${message}`}</div>
-      <div
-        css={{
-          paddingTop: spacing.normal,
-          color: colors.structure.zinc,
-          whiteSpace: 'nowrap',
-          overFlow: 'auto',
-        }}
-      />
-      {stackTrace.map(frame => {
-        return (
-          <div key={JSON.stringify(frame)}>
-            <div>{`{`}</div>
-            <div css={{ paddingLeft: spacing.large }}>
-              <div
-                css={{ paddingTop: spacing.base }}>{`"file": ${JSON.stringify(
-                frame.file,
-              )}`}</div>
-              <div
-                css={{
-                  paddingTop: spacing.base,
-                }}>{`"lineNumber": ${JSON.stringify(frame.lineNumber)}`}</div>
-              <div
-                css={{
-                  paddingTop: spacing.base,
-                }}>{`"className": ${JSON.stringify(frame.className)}`}</div>
-              <div
-                css={{
-                  paddingTop: spacing.base,
-                }}>{`"methodName": ${JSON.stringify(frame.methodName)}`}</div>
-            </div>
-            <div>{`}`}</div>
-          </div>
-        )
-      })}
+      {stackTrace.length ? (
+        <>
+          <div
+            css={{
+              paddingTop: spacing.normal,
+              color: colors.structure.zinc,
+              whiteSpace: 'nowrap',
+              overFlow: 'auto',
+            }}
+          />
+          {stackTrace.map((frame, index) => {
+            return (
+              <div key={JSON.stringify(frame)}>
+                <div>{`{`}</div>
+                <div css={{ paddingLeft: spacing.large }}>
+                  <div
+                    css={{
+                      paddingTop: spacing.base,
+                    }}>{`"file": ${JSON.stringify(frame.file)}`}</div>
+                  <div
+                    css={{
+                      paddingTop: spacing.base,
+                    }}>{`"lineNumber": ${JSON.stringify(
+                    frame.lineNumber,
+                  )}`}</div>
+                  <div
+                    css={{
+                      paddingTop: spacing.base,
+                    }}>{`"className": ${JSON.stringify(frame.className)}`}</div>
+                  <div
+                    css={{
+                      paddingTop: spacing.base,
+                    }}>{`"methodName": ${JSON.stringify(
+                    frame.methodName,
+                  )}`}</div>
+                </div>
+                <div>{index === stackTrace.length - 1 ? `}` : `},`}</div>
+              </div>
+            )
+          })}
+        </>
+      ) : null}
     </div>
   )
 }
