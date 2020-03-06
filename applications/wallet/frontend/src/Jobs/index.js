926f98292c76dc2567ce13c17ee2dae714d84d99
timothychin <tim.chin@zorroa.com>
2020-03-06 11:48:05 -0800

    wip

--- applications/wallet/frontend/src/Jobs/index.js
+++ applications/wallet/frontend/src/Jobs/index.js
@@ -1,11 +1,18 @@
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 
+import RefreshSvg from '../Icons/refresh.svg'
+
+import { colors, spacing, typography } from '../Styles'
+
 import PageTitle from '../PageTitle'
 import Table from '../Table'
+import Button, { VARIANTS } from '../Button'
 
 import JobsEmpty from './Empty'
 import JobsRow from './Row'
+
+const REFRESH_HEIGHT = 32
 
 const Jobs = () => {
   const {
@@ -19,6 +26,24 @@ const Jobs = () => {
       </Head>
 
       <PageTitle>Job Queue</PageTitle>
+
+      <div css={{ paddingTop: spacing.comfy, paddingBottom: spacing.normal }}>
+        <Button
+          variant={VARIANTS.PRIMARY}
+          style={{
+            height: REFRESH_HEIGHT,
+          }}
+          onClick={() => {}}>
+          <div
+            css={{
+              display: 'flex',
+              alignItems: 'center',
+            }}>
+            <RefreshSvg width={20} color={colors.structure.white} />
+            <div css={{ paddingLeft: spacing.small }}>Refresh Jobs</div>
+          </div>
+        </Button>
+      </div>
 
       <Table
         url={`/api/v1/projects/${projectId}/jobs/`}
