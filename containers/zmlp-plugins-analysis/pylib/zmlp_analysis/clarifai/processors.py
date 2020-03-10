7d0156822339d149c4129f22f740345b1a872803
Matt <mvchambers@me.com>
2020-03-10 14:23:25 -0400

    Added a clarifai processor.

---
+++ containers/zmlp-plugins-analysis/pylib/zmlp_analysis/clarifai/processors.py
@@ -0,0 +1,40 @@
+from clarifai.rest import ClarifaiApp
+
+import zmlp
+from zmlp.exception import ZmlpException
+from zmlpsdk import AssetProcessor, ZmlpEnv, ZmlpFatalProcessorException
+from zmlpsdk.proxy import get_proxy_level_path
+
+
+class ClarifaiPredictProcessor(AssetProcessor):
+    namespace = "clarifai.predict"
+
+    def __init_(self):
+        super(ClarifaiPredictProcessor, self).__init__()
+        self.clarifai = None
+
+    def init(self):
+        self.clarifai = get_clarifai_app()
+
+    def process(self, frame):
+        asset = frame.asset
+        p_path = get_proxy_level_path(asset, 1)
+
+        model = self.clarifai.public_models.general_model
+        response = model.predict_by_filename(p_path)
+        labels = response["outputs"][0]["data"]["concepts"]
+        model = response["outputs"][0]["model"]["name"]
+
+        result = [{"label": label['name'], "score": label["value"]} for label in labels]
+        asset.add_analysis(self.namespace, {"model": model, "labels": result})
+
+
+def get_clarifai_app():
+    app = zmlp.app_from_env()
+    jobid = ZmlpEnv.get_job_id()
+    try:
+        # creds = app.client.get('/api/v1/jobs/{}/_credentials/CLARIFAI'.format(jobid))
+        key = "17974843be1942c1a41657304eeeea07"
+        return ClarifaiApp(api_key=key)
+    except ZmlpException as e:
+        raise ZmlpFatalProcessorException("Job is missing Clarifai credentials", e)
