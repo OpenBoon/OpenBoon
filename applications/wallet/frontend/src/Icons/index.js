e6339461190b581d0dab049fcda8e00c4b3d3ae3
Damien Varron <damien@varron.fr>
2020-03-10 12:14:16 -0700

    hand made icons storybook macro

---
+++ applications/wallet/frontend/src/Icons/index.js
@@ -0,0 +1,48 @@
+import Head from 'next/head'
+// eslint-disable-next-line import/no-extraneous-dependencies
+import importAll from 'import-all.macro'
+
+import PageTitle from '../PageTitle'
+import { spacing } from '../Styles'
+
+const icons = importAll.sync('./*.svg')
+
+const Icons = () => {
+  return (
+    <div>
+      <Head>
+        <title>Icons</title>
+      </Head>
+
+      <PageTitle>Icons</PageTitle>
+
+      <div css={{ display: 'flex', flexWrap: 'wrap' }}>
+        {Object.entries(icons).map(([key, { default: Icon }]) => (
+          <div
+            key={key}
+            css={{
+              padding: spacing.normal,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              width: 200,
+              height: 200,
+            }}>
+            <div>{key}</div>
+            <div
+              css={{
+                height: '100%',
+                padding: spacing.spacious,
+                display: 'flex',
+                flexDirection: 'column',
+              }}>
+              <Icon width="100%" height="100%" />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Icons
