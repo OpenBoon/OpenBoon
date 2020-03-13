fb0074c141bad505d50937fdb1a509329fe60d3c
octaviaaris <octavia@zorroa.com>
2020-03-12 22:03:48 -0700

    consolidate asset code

--- applications/wallet/frontend/src/JobErrorAsset/index.js
+++ applications/wallet/frontend/src/JobErrorAsset/index.js
@@ -1,13 +1,20 @@
 import PropTypes from 'prop-types'
+import JSONPretty from 'react-json-pretty'
 
 import { useRouter } from 'next/router'
 import useSWR from 'swr'
 
+<<<<<<< HEAD
 import JsonDisplay from '../JsonDisplay'
 
 import { colors, constants, spacing } from '../Styles'
 
 const ASSET_THUMBNAIL_SIZE = 48
+=======
+import { colors, spacing, constants } from '../Styles'
+
+const ASSET_THUMBNAIL_SIZE = 80
+>>>>>>> consolidate asset code
 
 const JobErrorAsset = ({ assetId }) => {
   const {
@@ -24,6 +31,7 @@ const JobErrorAsset = ({ assetId }) => {
   } = useSWR(`/api/v1/projects/${projectId}/assets/${assetId}/`)
 
   return (
+<<<<<<< HEAD
     <div
       css={{
         fontFamily: 'Roboto Mono',
@@ -55,6 +63,54 @@ const JobErrorAsset = ({ assetId }) => {
           backgroundColor: colors.structure.black,
         }}>
         <JsonDisplay json={asset} />
+=======
+    <div>
+      <div
+        css={{
+          backgroundColor: colors.structure.lead,
+          boxShadow: constants.boxShadows.default,
+          padding: spacing.normal,
+          display: 'flex',
+          justifyContent: 'space-between',
+        }}>
+        <div
+          css={{
+            display: 'flex',
+            alignItems: 'center',
+            fontFamily: 'Roboto Mono',
+          }}>
+          <img
+            src={url.replace('https://wallet.zmlp.zorroa.com', '')}
+            alt={filename}
+            css={{
+              width: ASSET_THUMBNAIL_SIZE,
+              height: ASSET_THUMBNAIL_SIZE,
+              objectFit: 'cover',
+            }}
+          />
+          <div css={{ paddingLeft: spacing.comfy }}>{filename}</div>
+        </div>
+      </div>
+      <div
+        css={{
+          paddingBottom: spacing.spacious,
+          height: 'auto',
+        }}>
+        <div css={{ backgroundColor: colors.structure.black }}>
+          <div css={{ padding: spacing.normal }}>
+            <JSONPretty
+              id="json-pretty"
+              data={asset}
+              theme={{
+                main: 'line-height:1.3;overflow:auto;',
+                string: `color:${colors.signal.grass.base};`,
+                value: `color:${colors.signal.sky.base};`,
+                boolean: `color:${colors.signal.canary.base};`,
+              }}
+            />
+          </div>
+        </div>
+>>>>>>> consolidate asset code
       </div>
     </div>
   )
