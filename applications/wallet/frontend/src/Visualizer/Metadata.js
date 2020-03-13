ac967f2ae92cf7bbeed8ee7fc5c3c01dec6e850a
Timothy Chin <tim.chin@zorroa.com>
2020-03-13 00:08:24 +0000

    WAL-210: add metadata

--- applications/wallet/frontend/src/Visualizer/Metadata.js
+++ applications/wallet/frontend/src/Visualizer/Metadata.js
@@ -1,44 +1,84 @@
+import { useRouter } from 'next/router'
+import PropTypes from 'prop-types'
+
 import { colors, constants, spacing, typography } from '../Styles'
+
+import JsonDisplay from '../JsonDisplay'
 
 import InformationSvg from './information.svg'
 
+import assetShape from '../Asset/shape'
+
 export const WIDTH = 400
 
-const VisualizerMetadata = () => (
-  <div
-    css={{
-      backgroundColor: colors.structure.mattGrey,
-      marginTop: spacing.hairline,
-      height: '100%',
-      width: WIDTH,
-      display: 'flex',
-      flexDirection: 'column',
-      boxShadow: constants.boxShadows.metadata,
-    }}>
+const VisualizerMetadata = ({ assets }) => {
+  const {
+    query: { id },
+  } = useRouter()
+
+  const asset = !!id && assets.find(({ id: assetId }) => assetId === id)
+
+  const { metadata: { source: { filename } = {} } = {} } = asset || {}
+
+  return (
     <div
       css={{
+        backgroundColor: colors.structure.mattGrey,
+        marginTop: spacing.hairline,
+        height: '100%',
+        width: WIDTH,
         display: 'flex',
-        height: constants.navbar.height,
-        alignItems: 'center',
-        borderBottom: constants.borders.divider,
-        padding: spacing.normal,
+        flexDirection: 'column',
+        boxShadow: constants.boxShadows.metadata,
       }}>
-      <InformationSvg width={20} color={colors.structure.steel} />
       <div
         css={{
+          display: 'flex',
+          height: constants.navbar.height,
+          alignItems: 'center',
+          borderBottom: constants.borders.divider,
           padding: spacing.normal,
-          color: colors.structure.steel,
-          fontWeight: typography.weight.bold,
         }}>
-        ASSET METADATA
+        <InformationSvg
+          width={20}
+          color={id ? colors.structure.white : colors.structure.steel}
+        />
+        <div
+          css={{
+            padding: spacing.normal,
+            color: id ? colors.structure.white : colors.structure.steel,
+            fontWeight: typography.weight.bold,
+          }}>
+          ASSET METADATA
+        </div>
+      </div>
+      <div css={{ padding: spacing.normal }}>
+        <div
+          css={{
+            color: filename ? colors.signal.sky.base : colors.key.one,
+            fontStyle: filename ? '' : typography.style.italic,
+          }}>
+          {filename || 'Select an asset to view its metadata'}
+        </div>
+      </div>
+      <div
+        css={{
+          height: '100%',
+          overflow: 'auto',
+          backgroundColor: id
+            ? colors.structure.coal
+            : colors.structure.mattGrey,
+          padding: spacing.normal,
+          paddingBottom: 0,
+        }}>
+        <JsonDisplay json={asset} />
       </div>
     </div>
-    <div css={{ padding: spacing.normal }}>
-      <div css={{ color: colors.key.one, fontStyle: typography.style.italic }}>
-        Select an asset to view its metadata
-      </div>
-    </div>
-  </div>
-)
+  )
+}
+
+VisualizerMetadata.propTypes = {
+  assets: PropTypes.arrayOf(assetShape).isRequired,
+}
 
 export default VisualizerMetadata
--- applications/wallet/frontend/src/Visualizer/__tests__/index.spec.js
+++ applications/wallet/frontend/src/Visualizer/__tests__/index.spec.js
@@ -4,7 +4,10 @@ import Visualizer from '..'
 
 import assets from '../../Assets/__mocks__/assets'
 
+jest.mock('../../JsonDisplay', () => 'JsonDisplay')
+
 const PROJECT_ID = '76917058-b147-4556-987a-0a0f11e46d9b'
+const ASSET_ID = assets.results[0].id
 
 describe('<Visualizer />', () => {
   it('should render properly', () => {
@@ -14,6 +17,20 @@ describe('<Visualizer />', () => {
 
     require('next/router').__setUseRouter({
       query: { projectId: PROJECT_ID },
+    })
+
+    const component = TestRenderer.create(<Visualizer />)
+
+    expect(component.toJSON()).toMatchSnapshot()
+  })
+
+  it('should render selected asset', () => {
+    require('swr').__setMockUseSWRResponse({
+      data: assets,
+    })
+
+    require('next/router').__setUseRouter({
+      query: { id: ASSET_ID, projectId: PROJECT_ID },
     })
 
     const component = TestRenderer.create(<Visualizer />)
