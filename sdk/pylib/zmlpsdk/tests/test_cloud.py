82893dce054908c4ec4828bab66f049d9afed1ee
Matt <mvchambers@me.com>
2020-03-22 21:13:30 -0400

    Azure support.

--- sdk/pylib/zmlpsdk/tests/test_cloud.py
+++ sdk/pylib/zmlpsdk/tests/test_cloud.py
@@ -6,7 +6,7 @@ from unittest import TestCase
 from unittest.mock import patch
 
 from zmlpsdk.cloud import get_google_storage_client, get_pipeline_storage_client, \
-    get_aws_client, get_credentials_blob
+    get_aws_client, get_credentials_blob, get_azure_storage_client
 from zmlp.client import ZmlpClient
 
 logging.basicConfig(level=logging.DEBUG)
@@ -69,6 +69,22 @@ class TetCloudUtilFunction(TestCase):
         client = get_aws_client('s3')
         assert getattr(client, 'create_bucket')
         assert getattr(client, 'get_object')
+
+    @patch.object(ZmlpClient, 'get')
+    def test_get_azure_client(self, get_patch):
+        get_patch.return_value = {
+            'connection_string': 'DefaultEndpointsProtocol=https;AccountName=zorroatest;'
+                                 'AccountKey=RANDOMDUDE/TOMBRADY==;EndpointSuffix=core.windows.net'
+        }
+        os.environ['ZMLP_JOB_ID'] = 'abc123'
+        os.environ['ZMLP_CREDENTIALS_TYPES'] = 'AZURE'
+        try:
+            client = get_azure_storage_client()
+            assert getattr(client, 'list_containers')
+            assert getattr(client, 'get_container_client')
+        finally:
+            del os.environ['ZMLP_JOB_ID']
+            del os.environ['ZMLP_CREDENTIALS_TYPES']
 
     @patch.object(ZmlpClient, 'get')
     def test_get_credentials_blob(self, get_patch):
