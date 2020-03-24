381cadb1ea2666be729e790a3a5b0a8ef2a59bfa
Damien Varron <damien@varron.fr>
2020-03-24 11:06:00 -0600

    WAL-307: add Bouncer

---
+++ applications/wallet/frontend/src/Bouncer/index.js
@@ -0,0 +1,28 @@
+import { useContext } from 'react'
+import PropTypes from 'prop-types'
+
+import { UserContext } from '../User'
+
+export const ROLES = {
+  ML_Tools: 'ML_Tools',
+  API_Keys: 'API_Keys',
+  User_Admin: 'User_Admin',
+}
+
+const Bouncer = ({ role, children }) => {
+  const {
+    user: { roles = [] },
+  } = useContext(UserContext)
+
+  // Sorry mate, your name ain't on the list
+  if (!roles.includes(role)) return null
+
+  return children
+}
+
+Bouncer.propTypes = {
+  role: PropTypes.oneOf(Object.keys(ROLES)).isRequired,
+  children: PropTypes.node.isRequired,
+}
+
+export default Bouncer
--- applications/wallet/frontend/src/User/__mocks__/user.js
+++ applications/wallet/frontend/src/User/__mocks__/user.js
@@ -4,6 +4,14 @@ const user = {
   email: 'jane.doe@zorroa.com',
   firstName: 'Jane',
   lastName: 'Doe',
+  groups: [],
+  roles: ['ML_Tools', 'API_Keys', 'User_Admin'],
+  permissions: [],
+  isActive: true,
+  isStaff: true,
+  isSuperuser: false,
+  lastLogin: '2020-03-20T18:17:02.472778Z',
+  dateJoined: '2020-03-02T19:09:16Z',
   projectId: '76917058-b147-4556-987a-0a0f11e46d9b',
 }
 
