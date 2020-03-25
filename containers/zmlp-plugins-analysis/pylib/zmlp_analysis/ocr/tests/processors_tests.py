e4319357054d7ae2eaf1f6b01b4e3fa52740cd6e
Matt <mvchambers@me.com>
2020-03-25 13:32:33 -0400

    Added ocr support.

---
+++ containers/zmlp-plugins-analysis/pylib/zmlp_analysis/ocr/tests/processors_tests.py
@@ -0,0 +1,30 @@
+from unittest.mock import patch
+
+from zmlp import ZmlpClient
+from zmlp_analysis.ocr.processors import OcrProcessor
+from zmlpsdk import Frame
+from zmlpsdk.proxy import store_asset_proxy
+from zmlpsdk.testing import PluginUnitTestCase, TestAsset, zorroa_test_data
+
+
+class OcrProcessorTests(PluginUnitTestCase):
+
+    @patch.object(ZmlpClient, 'upload_file')
+    def test_process(self, upload_patch):
+        upload_patch.return_value = {
+            'name': 'proxy_200x200.jpg',
+            'category': 'proxy',
+            'assetId': '12345',
+            'mimetype': 'image/jpeg',
+            'attrs': {
+                'width': 1023,
+                'height': 1024
+            }
+        }
+        image_path = zorroa_test_data('images/set09/nvidia_manual_page.jpg', uri=False)
+        frame = Frame(TestAsset(image_path))
+        store_asset_proxy(frame.asset, image_path, (512, 512))
+        processor = self.init_processor(OcrProcessor(), {})
+        processor.process(frame)
+
+        assert 'NVIDIA' in frame.asset.get_attr('analysis.zvi.text-detection.content')
