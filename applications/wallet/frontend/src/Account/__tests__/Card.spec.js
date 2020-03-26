4d891e3ef2e5f6fc0d64745815e650d255c083ce
Timothy Chin <tim.chin@zorroa.com>
2020-03-26 19:39:09 +0000

    WAL-357: add Account dashboard

---
+++ applications/wallet/frontend/src/Account/__tests__/Card.spec.js
@@ -0,0 +1,71 @@
+import TestRenderer from 'react-test-renderer'
+
+import project from '../../Project/__mocks__/project'
+import subscriptions from '../../Subscriptions/__mocks__/subscriptions'
+
+import AccountCard from '../Card'
+
+const PROJECT_ID = project.id
+const PROJECT_NAME = project.name
+
+describe('<AccountCard />', () => {
+  it('should render properly', () => {
+    require('swr').__setMockUseSWRResponse({ data: subscriptions })
+
+    const component = TestRenderer.create(
+      <AccountCard id={PROJECT_ID} name={PROJECT_NAME} />,
+    )
+
+    expect(component.toJSON()).toMatchSnapshot()
+  })
+
+  it('should render properly with over usage', () => {
+    require('next/router').__setUseRouter({
+      pathname: '/[projectId]',
+      query: { projectId: project.id },
+    })
+
+    require('swr').__setMockUseSWRResponse({
+      data: {
+        ...subscriptions,
+        results: [
+          {
+            ...subscriptions.results[0],
+            usage: {
+              videoHours: 320,
+              imageCount: 30040,
+            },
+          },
+        ],
+      },
+    })
+
+    const component = TestRenderer.create(
+      <AccountCard id={PROJECT_ID} name={PROJECT_NAME} />,
+    )
+
+    expect(component.toJSON()).toMatchSnapshot()
+  })
+
+  it('should not render without subscriptions', () => {
+    require('next/router').__setUseRouter({
+      pathname: '/[projectId]',
+      query: { projectId: project.id },
+    })
+
+    require('swr').__setMockUseSWRResponse({
+      data: {
+        count: 0,
+        next: null,
+        previous: null,
+        results: [],
+      },
+    })
+
+    const component = TestRenderer.create(
+      <AccountCard id={PROJECT_ID} name={PROJECT_NAME} />,
+    )
+
+    expect(component.toJSON()).toMatchSnapshot()
+  })
+})
