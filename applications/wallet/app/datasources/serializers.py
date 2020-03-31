08543a23889fb074e06b3751953285975a18ddc6
Danny Tiesling <danny@zorroa.com>
2020-03-31 15:42:43 -0700

    Removes unused serializer.

--- applications/wallet/app/datasources/serializers.py
+++ applications/wallet/app/datasources/serializers.py
@@ -14,11 +14,3 @@ class DataSourceSerializer(serializers.Serializer):
     timeCreated = serializers.IntegerField(default=0)
     timeModified = serializers.IntegerField(default=0)
     url = serializers.CharField(required=False)
-
-
-class DataSourceUpdateSerializer(serializers.Serializer):
-    name = serializers.CharField(required=True)
-    uri = serializers.CharField(required=True)
-    credentials = serializers.ListField(child=serializers.CharField(), default=[])
-    fileTypes = serializers.ListField(child=serializers.CharField(), default=[])
-    modules = serializers.ListField(child=serializers.CharField(), default=[])
