83f6a80ea5f78e31520c9af38eb12e0eff99f2ba
Iron <iron.araujo@zorroa.com>
2020-04-01 20:27:30 -0300

    Adding SQLite utils class

--- applications/zvi-cloud-proxy/pylib/zvi-cloud-proxy/crawler.py
+++ applications/zvi-cloud-proxy/pylib/zvi-cloud-proxy/crawler.py
@@ -1,8 +1,11 @@
 import json
 import os
 import time
-
+from zmlp.exception import ZmlpException
+import sqlite3
 import yaml
+import hashlib
+import datetime
 
 from zmlp import ZmlpApp, FileUpload
 
@@ -13,10 +16,14 @@ class Crawler(object):
         try:
             with open("../properties.yaml", 'r') as stream:
                 try:
+
                     self.properties = yaml.safe_load(stream)
                     self.app = ZmlpApp(self.properties["api_key"], self.properties["zmlp_server"])
+                    self.db_utils = SqliteUtils(self.properties["sqlite_db"])
+
                 except yaml.YAMLError as exc:
-                    print("Failed on yaml reading: {0}".format(exc))
+                    raise ZmlpException("Failed on yaml reading: {0}".format(exc))
+
         except FileNotFoundError as err:
             self.properties = {}
             print("yaml not found: {0}".format(str(err)))
@@ -48,6 +55,56 @@ class Crawler(object):
     def upload_batch(self, batch):
         assets = [FileUpload(path) for path in batch]
         print(json.dumps(self.app.assets.batch_upload_files(assets)))
+
+
+class SqliteUtils(object):
+
+    def __init__(self, path):
+        self.path = path
+        self.con = sqlite3.connect(path)
+        self.create_tables()
+
+    def toHash(self, path):
+        return hashlib.sha1(path)
+
+    def create_tables(self):
+
+        sql = """
+        CREATE TABLE IF NOT EXISTS assets (
+            hash text NOT NULL,
+            url text NOT NULL,
+            date text
+            PRIMARY KEY (hash)
+        );
+        """
+        try:
+            cursor = self.con.cursor()
+            a = cursor.execute(sql)
+        except Exception as e:
+            raise ZmlpException(e)
+
+    def exists(self, path):
+
+        sql = """
+        SELECT COUNT(*) FROM assets WHERE hash = ? 
+        """
+
+        try:
+            cursor = self.con.cursor()
+            cursor.execute(sql, self.toHash(path))
+            result = cursor.fetchone()
+            return int(result) == 1
+        except Exception as e:
+            raise ZmlpException(e)
+
+    def insert(self, path):
+        sql = """ INSERT INTO assets  VALUES (?,?,?)"""
+
+        try:
+            cursor = self.con.cursor()
+            cursor.execute(sql, self.toHash(path), path, datetime.datetime.now())
+        except Exception as e:
+            raise ZmlpException(e)
 
 
 if __name__ == "__main__":
