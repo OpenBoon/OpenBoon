1ae41d7a634010e0203ce4a817551ce55838c8e5
Iron <iron.araujo@zorroa.com>
2020-04-02 15:52:28 -0300

    Check  if asset has already been uploaded in sqlite. Check ext.

--- applications/zvi-cloud-proxy/pylib/zvi-cloud-proxy/crawler.py
+++ applications/zvi-cloud-proxy/pylib/zvi-cloud-proxy/crawler.py
@@ -16,11 +16,9 @@ class Crawler(object):
         try:
             with open("../properties.yaml", 'r') as stream:
                 try:
-
                     self.properties = yaml.safe_load(stream)
                     self.app = ZmlpApp(self.properties["api_key"], self.properties["zmlp_server"])
                     self.db_utils = SqliteUtils(self.properties["sqlite_db"])
-
                 except yaml.YAMLError as exc:
                     raise ZmlpException("Failed on yaml reading: {0}".format(exc))
 
@@ -31,9 +29,7 @@ class Crawler(object):
         self.crawl()
 
     def crawl(self):
-
         batch = []
-        uploaded = []
 
         while True:
             time.sleep(1)
@@ -41,40 +37,50 @@ class Crawler(object):
                 for path, dirnames, names in os.walk(mount):
                     for name in names:
                         filename = "{0}/{1}".format(path, name)
-                        if filename not in batch and filename not in uploaded:
-                            print(filename)
+
+                        if filename not in batch and not self.db_utils.exists(filename) and self.check_ext(filename):
+                            print("enqueued ", filename)
                             batch.append(filename)
-                        if len(batch) >= int(self.properties["batch_size"]):
-                            try:
-                                self.upload_batch(batch)
-                                uploaded.extend(batch)
-                                batch = []
-                            except ValueError as error:
-                                print(error)
+
+                            if len(batch) >= int(self.properties["batch_size"]):
+                                try:
+                                    print("upload batch ", batch)
+                                    self.upload_batch(batch)
+                                    self.db_utils.insert_batch(batch)
+                                    batch = []
+                                except ValueError as error:
+                                    print(error)
 
     def upload_batch(self, batch):
         assets = [FileUpload(path) for path in batch]
         print(json.dumps(self.app.assets.batch_upload_files(assets)))
 
+    def check_ext(self, filename):
+        name, ext = os.path.splitext(filename)
+        if not ext:
+            return False
+        if ext[1:] in self.properties["supported_types"]:
+            return True
+        return False
+
 
 class SqliteUtils(object):
 
-    def __init__(self, path):
-        self.path = path
-        self.con = sqlite3.connect(path)
+    def __init__(self, db_path):
+        self.db_path = db_path
+        self.con = sqlite3.connect(self.db_path)
         self.create_tables()
 
     def toHash(self, path):
-        return hashlib.sha1(path)
+        return hashlib.sha1(path.encode('utf-8')).hexdigest()
 
     def create_tables(self):
 
         sql = """
         CREATE TABLE IF NOT EXISTS assets (
-            hash text NOT NULL,
+            hash text PRIMARY_KEY,
             url text NOT NULL,
             date text
-            PRIMARY KEY (hash)
         );
         """
         try:
@@ -86,25 +92,30 @@ class SqliteUtils(object):
     def exists(self, path):
 
         sql = """
-        SELECT COUNT(*) FROM assets WHERE hash = ? 
-        """
+        SELECT COUNT(*) FROM assets WHERE hash = '%s' 
+        """ % self.toHash(path)
 
         try:
             cursor = self.con.cursor()
-            cursor.execute(sql, self.toHash(path))
-            result = cursor.fetchone()
+            cursor.execute(sql)
+            result, = cursor.fetchone()
             return int(result) == 1
         except Exception as e:
             raise ZmlpException(e)
 
     def insert(self, path):
-        sql = """ INSERT INTO assets  VALUES (?,?,?)"""
+        sql = """ INSERT INTO assets  VALUES ('%s','%s','%s')""" % (self.toHash(path), path, datetime.datetime.now())
 
         try:
             cursor = self.con.cursor()
-            cursor.execute(sql, self.toHash(path), path, datetime.datetime.now())
+            cursor.execute(sql)
+            self.con.commit()
         except Exception as e:
             raise ZmlpException(e)
+
+    def insert_batch(self, batch):
+        for file in batch:
+            self.insert(file)
 
 
 if __name__ == "__main__":
