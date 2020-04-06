09e9c47573d967c5ae72da7c87c7b06a61e65bb3
Iron <iron.araujo@zorroa.com>
2020-04-06 14:23:56 -0300

    Unit Testing Extension

--- applications/zvi-cloud-proxy/pylib/proxy/tests/test_crawler.py
+++ applications/zvi-cloud-proxy/pylib/proxy/tests/test_crawler.py
@@ -1,14 +1,18 @@
-from unittest import TestCase
+import unittest
+
 from proxy.app.crawler import *
 
 
-class TestCrawler(TestCase):
+class TestCrawler(unittest.TestCase):
     def setUp(self):
-        print("Setup")
-        self.crawler = Crawler()
-
-    def tearDown(self):
-        print("Finish")
+        self.crawler = Crawler("./properties-test.yaml")
 
     def test_check_ext(self):
         assert self.crawler.check_ext("file.jpg") is True
+        assert self.crawler.check_ext("file.png") is True
+        assert self.crawler.check_ext("file.mov") is False
+        assert self.crawler.check_ext("file.py") is False
+        assert self.crawler.check_ext("file") is False
+
+    def tearDown(self):
+        print("Finish")
