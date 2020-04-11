e0b7f3b6769eb78b7723fb5b51468008d1739b7c
octaviaaris <octavia@zorroa.com>
2020-04-10 18:09:23 -0700

    upload new policies correctly

--- applications/wallet/frontend/src/Policies/__tests__/index.spec.js
+++ applications/wallet/frontend/src/Policies/__tests__/index.spec.js
@@ -45,12 +45,12 @@ describe('<Policies />', () => {
         'X-CSRFToken': 'CSRF_TOKEN',
         'Content-Type': 'application/json;charset=UTF-8',
       },
-      body: '{"policiesDate":"20200327"}',
+      body: '{"policiesDate":"20200410"}',
       method: 'POST',
     })
 
     expect(mockMutate).toHaveBeenCalledWith({
-      agreedToPoliciesDate: '20200327',
+      agreedToPoliciesDate: '20200410',
     })
   })
 
