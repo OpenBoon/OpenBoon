e693c551a1914c622a8e011336e8b06185a8d644
Matt <mvchambers@me.com>
2020-04-14 11:20:45 -0400

    Updated other entities to use base class.

--- client/python/pylib/zmlp/project.py
+++ client/python/pylib/zmlp/project.py
@@ -1,13 +1,12 @@
-from datetime import datetime
+from .entity import BaseEntity
 
-
-class Project(object):
+class Project(BaseEntity):
     """
     Represents a ZMLP Project.
 
     """
     def __init__(self, data):
-        self._data = data
+        super(Project, self).__init__(data)
 
     @property
     def name(self):
@@ -18,23 +17,3 @@ class Project(object):
     def id(self):
         """The project's unique id."""
         return self._data['id']
-
-    @property
-    def time_created(self):
-        """The date/time the project was created."""
-        return datetime.fromtimestamp(self._data['timeCreated'] / 1000.0)
-
-    @property
-    def time_modified(self):
-        """The date/time the project was modified."""
-        return datetime.fromtimestamp(self._data['timeModified'] / 1000.0)
-
-    @property
-    def actor_created(self):
-        """The UUID of the actor that created the project."""
-        return self._data['actorCreated']
-
-    @property
-    def actor_modified(self):
-        """The UUID of the actor that modified the project."""
-        return self._data['actorModified']
