74a120164a579ce5edb2f12960ccc7a680b4a7ce
Damien Varron <damien@varron.fr>
2020-04-16 20:33:53 +0000

    WAL-285: refactor Metadata Panel

---
+++ applications/wallet/frontend/src/Panel/index.js
@@ -0,0 +1,98 @@
+import PropTypes from 'prop-types'
+
+import useLocalStorage from '../LocalStorage'
+
+import { colors, spacing, constants } from '../Styles'
+
+import Button, { VARIANTS } from '../Button'
+
+import PanelContent from './Content'
+
+const Panel = ({ openToThe, children }) => {
+  const [openPanel, setOpenPanel] = useLocalStorage({
+    key: `${openToThe}OpeningPanel`,
+    initialValue: '',
+  })
+
+  const panel = children[openPanel]
+
+  return (
+    <div
+      css={{
+        display: 'flex',
+        marginTop: spacing.hairline,
+        boxShadow: `${openToThe === 'left' ? '-' : ''}${
+          constants.boxShadows.panel
+        }`,
+      }}
+    >
+      {!!openPanel && openToThe === 'left' && (
+        <PanelContent
+          openToThe={openToThe}
+          panel={panel}
+          setOpenPanel={setOpenPanel}
+        />
+      )}
+      <div
+        css={{
+          display: 'flex',
+          flexDirection: 'column',
+          marginRight: spacing.hairline,
+        }}
+      >
+        {Object.entries(children).map(([key, { title, icon }]) => (
+          <Button
+            key={title}
+            aria-label={title}
+            variant={VARIANTS.NEUTRAL}
+            onClick={() =>
+              setOpenPanel({ value: key === openPanel ? '' : key })
+            }
+            isDisabled={false}
+            style={{
+              padding: spacing.base,
+              paddingTop: spacing.normal,
+              paddingBottom: spacing.normal,
+              backgroundColor: colors.structure.lead,
+              marginBottom: spacing.hairline,
+              color:
+                key === openPanel ? colors.key.one : colors.structure.steel,
+              ':hover': {
+                color: colors.structure.white,
+                backgroundColor: colors.structure.mattGrey,
+              },
+            }}
+          >
+            {icon}
+          </Button>
+        ))}
+        <div
+          css={{
+            flex: 1,
+            backgroundColor: colors.structure.lead,
+          }}
+        />
+      </div>
+      {!!openPanel && openToThe === 'right' && (
+        <PanelContent
+          openToThe={openToThe}
+          panel={panel}
+          setOpenPanel={setOpenPanel}
+        />
+      )}
+    </div>
+  )
+}
+
+Panel.propTypes = {
+  openToThe: PropTypes.oneOf(['left', 'right']).isRequired,
+  children: PropTypes.objectOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      icon: PropTypes.node.isRequired,
+      content: PropTypes.node.isRequired,
+    }).isRequired,
+  ).isRequired,
+}
+
+export default Panel
