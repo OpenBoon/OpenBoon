6eb8f3657352e0b1e2971066dfd876d55412da2d
Timothy Chin <tim.chin@zorroa.com>
2020-04-20 16:25:49 +0000

    WAL-296: fix mapping on Edit load

--- applications/wallet/frontend/src/DataSourcesEdit/Content.js
+++ applications/wallet/frontend/src/DataSourcesEdit/Content.js
@@ -1,8 +1,6 @@
 import useSWR from 'swr'
 import { useRouter } from 'next/router'
 
-import { FILE_TYPES } from '../DataSourcesAdd/helpers'
-
 import DataSourcesEditForm from './Form'
 
 const DataSourcesEditContent = () => {
@@ -14,23 +12,12 @@ const DataSourcesEditContent = () => {
     data: { name, uri, fileTypes, modules },
   } = useSWR(`/api/v1/projects/${projectId}/data_sources/${dataSourceId}/`)
 
-  const groupedFileTypes = FILE_TYPES.reduce(
-    (accumulator, { value, identifier }) => {
-      const matchFileType = fileTypes.includes(identifier)
-
-      if (matchFileType) {
-        accumulator[value] = true
-      }
-
-      return accumulator
-    },
-    {},
-  )
-
   const initialState = {
     name,
     uri,
-    fileTypes: groupedFileTypes,
+    fileTypes: fileTypes.reduce((accumulator, value) => {
+      return { ...accumulator, [value]: true }
+    }, {}),
     modules,
     errors: { global: '' },
   }
