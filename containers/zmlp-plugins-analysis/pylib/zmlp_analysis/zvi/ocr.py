c1a659d3e590d541cc8649e614a674380aa46f89
Matt <mvchambers@me.com>
2020-04-23 09:33:36 -0400

    Fixes some issues with empty analysis being added.

--- containers/zmlp-plugins-analysis/pylib/zmlp_analysis/zvi/ocr.py
+++ containers/zmlp-plugins-analysis/pylib/zmlp_analysis/zvi/ocr.py
@@ -2,7 +2,7 @@ import pytesseract
 
 from zmlpsdk.base import AssetProcessor, FileTypes
 from zmlpsdk.proxy import get_proxy_level_path
-
+from zmlpsdk.analysis import ContentDetectionAnalysis
 
 class ZviOcrProcessor(AssetProcessor):
     """
@@ -19,9 +19,10 @@ class ZviOcrProcessor(AssetProcessor):
     def process(self, frame):
         p_path = get_proxy_level_path(frame.asset, 3)
         data = pytesseract.image_to_string(p_path)
-        data = data.replace('\r', '').replace('\n', '')
-        frame.asset.add_analysis(self.namespace, {
-            'type': 'content',
-            'content': data,
-            'count': len(data.split())
-        })
+        data = data.replace('\r', ' ').replace('\n', ' ')
+
+        analysis = ContentDetectionAnalysis()
+        analysis.add_content(data)
+
+        if analysis:
+            frame.asset.add_analysis(self.namespace, analysis)
