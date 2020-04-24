c60a5064c22f6f21d1b575a0b18204357431e4f4
Damien Varron <damien@varron.fr>
2020-04-24 14:54:11 -0600

    cover clear all filters

--- applications/wallet/frontend/src/Filters/__tests__/index.spec.js
+++ applications/wallet/frontend/src/Filters/__tests__/index.spec.js
@@ -187,6 +187,42 @@ describe('<Filters />', () => {
     )
   })
 
+  it('should clear all filters', () => {
+    const mockRouterPush = jest.fn()
+
+    require('next/router').__setMockPushFunction(mockRouterPush)
+
+    require('next/router').__setUseRouter({
+      pathname: '/[projectId]/visualizer',
+      query: {
+        projectId: PROJECT_ID,
+        filters: JSON.stringify([{ type: 'search', value: 'Cat' }]),
+      },
+    })
+
+    require('swr').__setMockUseSWRResponse({ data: fields })
+
+    const component = TestRenderer.create(<Filters />)
+
+    // Clear All Filters
+    act(() => {
+      component.root
+        .findByProps({ children: 'Clear All Filters' })
+        .props.onClick({ preventDefault: noop })
+    })
+
+    expect(mockRouterPush).toHaveBeenCalledWith(
+      {
+        pathname: '/[projectId]/visualizer',
+        query: {
+          projectId: '76917058-b147-4556-987a-0a0f11e46d9b',
+          id: '',
+        },
+      },
+      '/76917058-b147-4556-987a-0a0f11e46d9b/visualizer',
+    )
+  })
+
   it('should open the menu', () => {
     require('next/router').__setUseRouter({
       pathname: '/[projectId]/visualizer',
@@ -206,6 +242,7 @@ describe('<Filters />', () => {
         .props.onClick({ preventDefault: noop })
     })
 
+    // Expand Analysis Section
     act(() => {
       component.root
         .findAllByProps({ 'aria-label': 'Expand Section' })[0]
