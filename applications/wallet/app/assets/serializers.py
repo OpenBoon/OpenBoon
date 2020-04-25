8b034753f07f59d0a31671889e1c3fffe1ca41c9
Zach Schipono <zach@zorroa.com>
2020-04-24 17:19:29 -0700

    Removes URLs from the Assets and their files.

--- applications/wallet/app/assets/serializers.py
+++ applications/wallet/app/assets/serializers.py
@@ -6,7 +6,6 @@ from wallet.serializers import PageSerializer
 class AssetSerializer(serializers.Serializer):
     id = serializers.CharField(required=False, allow_null=True)
     metadata = serializers.DictField(required=True)
-    url = serializers.CharField(required=True)
 
 
 class AssetsSerializer(serializers.Serializer):
--- applications/wallet/app/assets/views.py
+++ applications/wallet/app/assets/views.py
@@ -12,27 +12,14 @@ from wallet.paginators import ZMLPFromSizePagination
 
 
 def asset_modifier(request, item):
-    current_url = request.build_absolute_uri(request.path)
     if '_source' in item:
         item['id'] = item['_id']
         item['metadata'] = item['_source']
     else:
-        # Normalize the current URL
-        current_url = current_url.replace(f'{item["id"]}/', '')
         item['metadata'] = item['document']
 
-    # Now add the asset id
-    current_url = f'{current_url}{item["id"]}/'
-    item['url'] = current_url
-
-    # Add urls to the proxy files
     if 'files' not in item['metadata']:
         item['metadata']['files'] = []
-    for entry in item['metadata']['files']:
-        entry['url'] = f'{current_url}files/category/{entry["category"]}/name/{entry["name"]}'
-
-    # Add url for the source file
-    item['metadata']['source']['url'] = f'{current_url}files/source/{item["metadata"]["source"]["filename"]}'  # noqa
 
 
 def stream(request, path):
@@ -106,17 +93,6 @@ Querystring:
         width = int(request.query_params.get('width', 255))
         response_data = {attr.split('.')[-1]: imager.get_attr_with_box_images(attr, width=width)}
         return Response(response_data)
-
-
-class SourceFileViewSet(BaseProjectViewSet):
-    zmlp_only = True
-    zmlp_root_api_path = 'api/v3/assets'
-    lookup_value_regex = '[^/]+'
-
-    def retrieve(self, request, project_pk, asset_pk, pk):
-        path = f'{self.zmlp_root_api_path}/{asset_pk}/_stream'
-        content_type, encoding = mimetypes.guess_type(pk)
-        return StreamingHttpResponse(stream(request, path), content_type=content_type)
 
 
 class FileCategoryViewSet(BaseProjectViewSet):
