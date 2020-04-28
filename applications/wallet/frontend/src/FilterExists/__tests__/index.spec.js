8f333d86728acf0d03ecdf854da0c096f4c84dc4
Damien Varron <damien@varron.fr>
2020-04-27 18:46:24 -0600

    coverage

---
+++ applications/wallet/frontend/src/FilterExists/__tests__/index.spec.js
@@ -0,0 +1,31 @@
+import TestRenderer from 'react-test-renderer'
+
+import FilterExists from '..'
+
+const PROJECT_ID = '76917058-b147-4556-987a-0a0f11e46d9b'
+
+describe('<FilterExists />', () => {
+  it('should render properly', () => {
+    const component = TestRenderer.create(
+      <FilterExists
+        projectId={PROJECT_ID}
+        assetId=""
+        filters={[
+          {
+            type: 'exists',
+            attribute: 'location.point',
+            values: { exists: false },
+          },
+        ]}
+        filter={{
+          type: 'exists',
+          attribute: 'location.point',
+          values: { exists: false },
+        }}
+        filterIndex={0}
+      />,
+    )
+
+    expect(component.toJSON()).toMatchSnapshot()
+  })
+})
