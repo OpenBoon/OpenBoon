31da1c11de32cfd8c9b3de1c75f248d4dad48ecb
Matt <mvchambers@me.com>
2020-04-28 11:29:34 -0400

    Added SDK to python path in examples env. Increase batch size in filer crawler qx

--- client/python/examples/env.sh
+++ client/python/examples/env.sh
@@ -1,3 +1,3 @@
 export ZMLP_SERVER="http://localhost:8080"
 export ZMLP_APIKEY_FILE="${PWD}/../../../dev/config/keys/signing-key.json"
-export PYTHONPATH="${PWD}/../pylib"
+export PYTHONPATH="${PWD}/../pylib:${PWD}/../../../sdk/pylib"
