ca068aa77d0b10943c2c5212212f067736c33245
Zach Schipono <zach@zorroa.com>
2020-04-29 17:14:11 -0700

    Removes unnecessary view changes

--- applications/wallet/app/subscriptions/views.py
+++ applications/wallet/app/subscriptions/views.py
@@ -1,7 +1,6 @@
 from django.http import Http404
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
 from rest_framework.viewsets import GenericViewSet
-from rest_framework.response import Response
 
 from projects.views import BaseProjectViewSet
 from subscriptions.models import Subscription
@@ -29,19 +28,3 @@ class SubscriptionViewSet(ConvertCamelToSnakeViewSetMixin,
 
     def get_queryset(self):
         return Subscription.objects.filter(project=self.kwargs['project_pk'])
-
-    def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-
-        # self._update_usage(request, queryset)
-
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
-
-    # def _update_usage(self, request, queryset):
-    #     for plan in queryset:
