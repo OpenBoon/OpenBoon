dc9af349fee9bff07dcbd9f36bdc0da9aec4634a
octaviaaris <octavia@zorroa.com>
2020-04-29 17:49:47 -0700

    add ButtonCopy file

---
+++ applications/wallet/frontend/src/Button/Copy.js
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types'
+import useClipboard from 'react-use-clipboard'
+
+import { colors } from '../Styles'
+
+import CopySvg from '../Icons/copy.svg'
+
+import Button, { VARIANTS as BUTTON_VARIANTS } from '.'
+
+export const COPY_SIZE = 20
+
+const ButtonCopy = ({ value }) => {
+  const [isCopied, setCopied] = useClipboard(value, { successDuration: 1000 })
+
+  return (
+    <Button
+      title="Copy to Clipboard"
+      variant={BUTTON_VARIANTS.NEUTRAL}
+      onClick={setCopied}
+      isDisabled={isCopied}
+    >
+      <CopySvg
+        width={COPY_SIZE}
+        color={colors.structure.steel}
+        css={{
+          display: 'none',
+          ':hover': { color: colors.structure.white },
+        }}
+      />
+    </Button>
+  )
+}
+
+ButtonCopy.propTypes = {
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+}
+
+export default ButtonCopy
