e39259683e084587a813c507521fd86201c5543c
Timothy Chin <tim.chin@zorroa.com>
2020-05-01 21:46:06 +0000

    WAL-285: add Facet selection

--- applications/wallet/frontend/src/FilterFacet/__tests__/Content.spec.js
+++ applications/wallet/frontend/src/FilterFacet/__tests__/Content.spec.js
@@ -7,8 +7,12 @@ import FilterFacetContent from '../Content'
 const PROJECT_ID = '76917058-b147-4556-987a-0a0f11e46d9b'
 
 describe('<FilterFacetContent />', () => {
-  it('should render with no docCount', () => {
-    const filters = { attribute: 'location.city', type: 'facet' }
+  it('should render selected', () => {
+    const filter = {
+      attribute: 'location.city',
+      type: 'facet',
+      values: { Tyngsboro: true },
+    }
 
     require('swr').__setMockUseSWRResponse({
       data: {
@@ -25,7 +29,43 @@ describe('<FilterFacetContent />', () => {
     })
 
     const component = TestRenderer.create(
-      <FilterFacetContent projectId={PROJECT_ID} filter={filters} />,
+      <FilterFacetContent
+        projectId={PROJECT_ID}
+        assetId=""
+        filters={[filter]}
+        filter={filter}
+        filterIndex={0}
+      />,
+    )
+
+    expect(component.toJSON()).toMatchSnapshot()
+  })
+
+  it('should render with no docCount', () => {
+    const filter = { attribute: 'location.city', type: 'facet', values: {} }
+
+    require('swr').__setMockUseSWRResponse({
+      data: {
+        ...facetAggregate,
+        results: {
+          ...facetAggregate.results,
+          buckets: [
+            { key: 'Tyngsboro' },
+            { key: 'Brooklyn' },
+            { key: 'Cát Bà' },
+          ],
+        },
+      },
+    })
+
+    const component = TestRenderer.create(
+      <FilterFacetContent
+        projectId={PROJECT_ID}
+        assetId=""
+        filters={[filter]}
+        filter={filter}
+        filterIndex={0}
+      />,
     )
 
     expect(component.toJSON()).toMatchSnapshot()
