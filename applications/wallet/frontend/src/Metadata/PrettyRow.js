f87188be648d116c08acae6705f37177d707aab4
octaviaaris <octavia@zorroa.com>
2020-05-04 13:38:27 -0700

    rpelace if-eles blocks with switch

--- applications/wallet/frontend/src/Metadata/PrettyRow.js
+++ applications/wallet/frontend/src/Metadata/PrettyRow.js
@@ -12,67 +12,74 @@ import { formatDisplayName, formatDisplayValue } from './helpers'
 
 const MetadataPrettyRow = ({ name, value, path }) => {
   if (typeof value === 'object') {
-    if (value.type === 'labels') {
-      if (Object.keys(value.predictions[0]).includes('bbox')) {
-        return <MetadataBbox name={name} />
-      }
-      return (
-        <MetadataLabelDetection name={name} predictions={value.predictions} />
-      )
-    }
-
-    if (value.type === 'content') {
-      return <MetadataContentDetection name={name} content={value.content} />
-    }
-
-    if (value.type === 'similarity') {
-      return <MetadataSimilarityDetection name={name} simhash={value.simhash} />
-    }
-
-    return (
-      <>
-        <div
-          css={{
-            borderTop: constants.borders.divider,
-            ':hover': {
-              div: {
-                svg: {
-                  display: 'inline-block',
+    switch (value.type) {
+      case 'labels':
+        return Object.keys(value.predictions[0]).includes('bbox') ? (
+          <MetadataBbox name={name} />
+        ) : (
+          <MetadataLabelDetection name={name} predictions={value.predictions} />
+        )
+      // if (Object.keys(value.predictions[0]).includes('bbox')) {
+      //   return <MetadataBbox name={name} />
+      // }
+      // return (
+      //   <MetadataLabelDetection name={name} predictions={value.predictions} />
+      // )
+      case 'content':
+        return <MetadataContentDetection name={name} content={value.content} />
+      case 'similarity':
+        return (
+          <MetadataSimilarityDetection name={name} simhash={value.simhash} />
+        )
+      default:
+        return (
+          <>
+            <div
+              css={{
+                borderTop: constants.borders.divider,
+                ':hover': {
+                  div: {
+                    svg: {
+                      display: 'inline-block',
+                    },
+                  },
                 },
-              },
-            },
-          }}
-        >
-          <div
-            css={{
-              fontFamily: 'Roboto Condensed',
-              color: colors.structure.steel,
-              padding: spacing.normal,
-              paddingBottom: 0,
-              flex: 1,
-            }}
-          >
-            <span title={`${path.toLowerCase()}.${name}`}>
-              {formatDisplayName({ name })}
-            </span>
-          </div>
-          <div />
-        </div>
+              }}
+            >
+              <div
+                css={{
+                  fontFamily: 'Roboto Condensed',
+                  color: colors.structure.steel,
+                  padding: spacing.normal,
+                  paddingBottom: 0,
+                  flex: 1,
+                }}
+              >
+                <span title={`${path.toLowerCase()}.${name}`}>
+                  {formatDisplayName({ name })}
+                </span>
+              </div>
+              <div />
+            </div>
 
-        <div
-          css={{ paddingLeft: spacing.normal, paddingRight: spacing.normal }}
-        >
-          {Object.entries(value).map(([k, v]) => (
-            <MetadataPrettyRow
-              key={k}
-              name={k}
-              value={v}
-              path={`${path.toLowerCase()}.${name}`}
-            />
-          ))}
-        </div>
-      </>
-    )
+            <div
+              css={{
+                paddingLeft: spacing.normal,
+                paddingRight: spacing.normal,
+              }}
+            >
+              {Object.entries(value).map(([k, v]) => (
+                <MetadataPrettyRow
+                  key={k}
+                  name={k}
+                  value={v}
+                  path={`${path.toLowerCase()}.${name}`}
+                />
+              ))}
+            </div>
+          </>
+        )
+    }
   }
 
   return (
