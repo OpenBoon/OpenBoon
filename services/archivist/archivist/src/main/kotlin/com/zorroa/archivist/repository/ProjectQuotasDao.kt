0a1cef9825f81ca64900a844e045a550fe77d1f2
Matt <mvchambers@me.com>
2020-05-05 15:37:43 -0400

    Changed into logging to warn

--- services/archivist/archivist/src/main/kotlin/com/zorroa/archivist/repository/ProjectQuotasDao.kt
+++ services/archivist/archivist/src/main/kotlin/com/zorroa/archivist/repository/ProjectQuotasDao.kt
@@ -82,7 +82,7 @@ class ProjectQuotasDaoImpl : AbstractDao(), ProjectQuotasDao {
         cal.set(Calendar.MILLISECOND, 0)
 
         val entry = ((cal.get(Calendar.DAY_OF_YEAR) - 1) * 24) + cal.get(Calendar.HOUR_OF_DAY)
-        logger.info("Updating TimeSeriesCounters $entry ${cal.timeInMillis}")
+        logger.warn("Updating TimeSeriesCounters $entry ${cal.timeInMillis}")
         jdbc.update(
             UPDATE_TIMESCALE_COUNTERS,
             cal.timeInMillis,
