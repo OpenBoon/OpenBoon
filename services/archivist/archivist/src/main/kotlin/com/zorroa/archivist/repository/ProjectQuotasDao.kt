2837007fb02e8cc4a2861b7318357512646f0418
Matt <mvchambers@me.com>
2020-05-05 14:35:46 -0400

    linting

--- services/archivist/archivist/src/main/kotlin/com/zorroa/archivist/repository/ProjectQuotasDao.kt
+++ services/archivist/archivist/src/main/kotlin/com/zorroa/archivist/repository/ProjectQuotasDao.kt
@@ -81,7 +81,6 @@ class ProjectQuotasDaoImpl : AbstractDao(), ProjectQuotasDao {
         cal.set(Calendar.SECOND, 0)
         cal.set(Calendar.MILLISECOND, 0)
 
-
         val entry = ((cal.get(Calendar.DAY_OF_YEAR) - 1) * 24) + cal.get(Calendar.HOUR_OF_DAY)
         logger.info("Updating TimeSeriesCounters $entry ${cal.timeInMillis}")
         jdbc.update(
