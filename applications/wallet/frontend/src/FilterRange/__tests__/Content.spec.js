31e051fd5ec1321b4e870b90198903073f76d48e
Damien Varron <damien@varron.fr>
2020-05-07 12:31:07 -0600

    refactor tests

---
+++ applications/wallet/frontend/src/FilterRange/__tests__/Content.spec.js
@@ -0,0 +1,64 @@
+import TestRenderer, { act } from 'react-test-renderer'
+
+import FilterRangeContent from '../Content'
+
+const PROJECT_ID = '76917058-b147-4556-987a-0a0f11e46d9b'
+
+jest.mock('../../Filters/Reset', () => 'FiltersReset')
+
+describe('<FilterRangeContent />', () => {
+  it('should render with file sizes and reset', () => {
+    const filter = {
+      type: 'range',
+      attribute: 'source.filesize',
+      values: { min: 200, max: 8000 },
+    }
+
+    const mockRouterPush = jest.fn()
+
+    require('next/router').__setMockPushFunction(mockRouterPush)
+
+    require('swr').__setMockUseSWRResponse({
+      data: {
+        count: 584,
+        results: {
+          count: 580,
+          min: 180,
+          max: 8525.0,
+          avg: 899.5689655172414,
+          sum: 521750.0,
+        },
+      },
+    })
+
+    const component = TestRenderer.create(
+      <FilterRangeContent
+        projectId={PROJECT_ID}
+        assetId=""
+        filters={[filter]}
+        filter={filter}
+        filterIndex={0}
+      />,
+    )
+
+    expect(component.toJSON()).toMatchSnapshot()
+
+    act(() => {
+      component.root.findByProps({ mode: 2 }).props.onUpdate([200, 8000])
+    })
+
+    expect(component.toJSON()).toMatchSnapshot()
+
+    act(() => {
+      component.root.findByProps({ mode: 2 }).props.onChange([300, 7000])
+    })
+
+    expect(component.toJSON()).toMatchSnapshot()
+
+    act(() => {
+      component.root.findByType('FiltersReset').props.onReset()
+    })
+
+    expect(component.toJSON()).toMatchSnapshot()
+  })
+})
--- applications/wallet/frontend/src/Filters/__tests__/Content.spec.js
+++ applications/wallet/frontend/src/Filters/__tests__/Content.spec.js
@@ -1,4 +1,4 @@
-import TestRenderer, { act } from 'react-test-renderer'
+import TestRenderer from 'react-test-renderer'
 
 import facetAggregate from '../../FilterFacet/__mocks__/aggregate'
 import rangeAggregate from '../../FilterRange/__mocks__/aggregate'
@@ -14,11 +14,7 @@ jest.mock('../../Filters/Reset', () => 'FiltersReset')
 
 describe('<FiltersContent />', () => {
   it('should render the "Exists" filter', () => {
-    const filters = [{ attribute: 'location.point', type: 'exists' }]
-
-    const mockRouterPush = jest.fn()
-
-    require('next/router').__setMockPushFunction(mockRouterPush)
+    const filters = [{ type: 'exists', attribute: 'location.point' }]
 
     const component = TestRenderer.create(
       <FiltersContent
@@ -30,37 +26,10 @@ describe('<FiltersContent />', () => {
     )
 
     expect(component.toJSON()).toMatchSnapshot()
-
-    // click "Missing"
-    act(() => {
-      component.root
-        .findByProps({ children: 'Missing' })
-        .props.onClick({ preventDefault: noop })
-    })
-
-    expect(mockRouterPush).toHaveBeenCalledWith(
-      {
-        pathname: '/[projectId]/visualizer',
-        query: {
-          projectId: '76917058-b147-4556-987a-0a0f11e46d9b',
-          id: '',
-          query: btoa(
-            JSON.stringify([
-              {
-                attribute: 'location.point',
-                type: 'exists',
-                values: { exists: false },
-              },
-            ]),
-          ),
-        },
-      },
-      '/76917058-b147-4556-987a-0a0f11e46d9b/visualizer?query=W3siYXR0cmlidXRlIjoibG9jYXRpb24ucG9pbnQiLCJ0eXBlIjoiZXhpc3RzIiwidmFsdWVzIjp7ImV4aXN0cyI6ZmFsc2V9fV0=',
-    )
   })
 
   it('should render the "Facet" filter', () => {
-    const filters = [{ attribute: 'location.city', type: 'facet', values: {} }]
+    const filters = [{ type: 'facet', attribute: 'location.city', values: {} }]
 
     require('swr').__setMockUseSWRResponse({ data: facetAggregate })
 
@@ -77,19 +46,10 @@ describe('<FiltersContent />', () => {
   })
 
   it('should render the "Range" filter', () => {
-    const {
-      results: { max },
-    } = rangeAggregate
-
-    const filters = [
-      { attribute: 'clip.length', type: 'range', values: { min: 0.1, max } },
-    ]
+    const filters = [{ type: 'range', attribute: 'clip.length', values: {} }]
 
     require('swr').__setMockUseSWRResponse({
-      data: {
-        ...rangeAggregate,
-        results: { ...rangeAggregate.results, min: 0.1 },
-      },
+      data: rangeAggregate,
     })
 
     const component = TestRenderer.create(
@@ -100,17 +60,6 @@ describe('<FiltersContent />', () => {
         setIsMenuOpen={noop}
       />,
     )
-
-    act(() => {
-      component.root.findByProps({ mode: 2 }).props.onUpdate([0.255, max])
-      component.root.findByProps({ mode: 2 }).props.onChange([0.255, max])
-    })
-
-    expect(component.toJSON()).toMatchSnapshot()
-
-    act(() => {
-      component.root.findByType('FiltersReset').props.onReset()
-    })
 
     expect(component.toJSON()).toMatchSnapshot()
   })
@@ -118,32 +67,13 @@ describe('<FiltersContent />', () => {
   it('should render the "Label Confidence" filter', () => {
     const filters = [
       {
-        attribute: 'analysis.zvi-label-detection',
         type: 'labelConfidence',
+        attribute: 'analysis.zvi-label-detection',
         values: {},
       },
     ]
 
     require('swr').__setMockUseSWRResponse({ data: labelConfidenceAggregate })
-
-    const component = TestRenderer.create(
-      <FiltersContent
-        projectId={PROJECT_ID}
-        assetId=""
-        filters={filters}
-        setIsMenuOpen={noop}
-      />,
-    )
-
-    expect(component.toJSON()).toMatchSnapshot()
-  })
-
-  it('should render the "Range" filter with file sizes', () => {
-    const filters = [
-      { attribute: 'source.filesize', type: 'range', values: {} },
-    ]
-
-    require('swr').__setMockUseSWRResponse({ data: rangeAggregate })
 
     const component = TestRenderer.create(
       <FiltersContent
