00d6ffea129d4994b6e20096ff6fb6642e11db7c
Matt <mvchambers@me.com>
2020-05-19 17:09:23 -0400

    Don't validate disabled projects.

--- services/archivist/archivist/src/main/kotlin/com/zorroa/archivist/security/ApiKeyAuthorizationFilter.kt
+++ services/archivist/archivist/src/main/kotlin/com/zorroa/archivist/security/ApiKeyAuthorizationFilter.kt
@@ -1,5 +1,6 @@
 package com.zorroa.archivist.security
 
+import com.zorroa.archivist.service.ProjectService
 import com.zorroa.zmlp.apikey.AuthServerClient
 import com.zorroa.zmlp.apikey.ZmlpActor
 import org.slf4j.LoggerFactory
@@ -19,7 +20,8 @@ val HEADER = "Authorization"
 val PREFIX = "Bearer "
 
 class ApiKeyAuthorizationFilter constructor(
-    val authServerClient: AuthServerClient
+    val authServerClient: AuthServerClient,
+    val projectService: ProjectService
 ) : OncePerRequestFilter() {
 
     @Throws(IOException::class, ServletException::class)
@@ -51,6 +53,11 @@ class ApiKeyAuthorizationFilter constructor(
 
     fun validate(token: String, projectId: UUID? = null) {
         val actor = authServerClient.authenticate(token, projectId)
+
+        if (!projectService.isEnabled(actor.projectId)) {
+            throw RuntimeException("Project does not exist")
+        }
+
         SecurityContextHolder.getContext().authentication = ApiTokenAuthentication(actor)
     }
 
