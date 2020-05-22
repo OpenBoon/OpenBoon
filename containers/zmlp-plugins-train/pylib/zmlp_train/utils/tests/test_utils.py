202d8cdce212740b70c0b110a9c7a05f063b5e0c
Ryan Gaspar <ryangaspar@rg-zorroa.hub>
2020-05-23 04:51:12 +1200

    skip utils tests

--- containers/zmlp-plugins-train/pylib/zmlp_train/utils/tests/test_utils.py
+++ containers/zmlp-plugins-train/pylib/zmlp_train/utils/tests/test_utils.py
@@ -1,5 +1,5 @@
 import os.path as path
-import unittest
+import pytest
 
 from zmlp_train.utils.utils import (
     get_labels,
@@ -11,13 +11,13 @@ cur_dir = path.abspath(path.dirname(__file__))
 test_dir = path.abspath(path.join(cur_dir, "../../", "tests/"))
 
 
-@unittest.skip
+@pytest.mark.skip(reason="dont run automatically")
 def test_get_labels():
     labels = get_labels(test_dir, "ClassifierTest_labels.txt")
     assert len(labels) == 6
 
 
-@unittest.skip
+@pytest.mark.skip(reason="dont run automatically")
 def test_extract_model():
     model_zip = path.join(test_dir, "model.zip")
     loc = extract_model(model_zip)
@@ -26,7 +26,7 @@ def test_extract_model():
     assert path.exists(path.join(loc, "model/"))
 
 
-@unittest.skip
+@pytest.mark.skip(reason="dont run automatically")
 def test_load_image():
     img_path = path.join(test_dir, "test_dsy.jpg")
     img = load_image(img_path)
