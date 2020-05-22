9e19c480796a6ade0c6d4e0dd4de1e87c448569e
Ryan Gaspar <ryangaspar@rg-zorroa.hub>
2020-05-22 20:25:56 +1200

    trainClassifier + tests

---
+++ containers/zmlp-plugins-train/pylib/zmlp_train/tests/test_trainClassifier.py
@@ -0,0 +1,92 @@
+import os
+import string
+import random
+import logging
+from unittest.mock import patch
+
+from zmlp import ZmlpClient
+from zmlp.app import ModelApp
+from zmlp.entity import Model, StoredFile, PipelineMod
+from zmlpsdk.storage import file_storage
+from zmlpsdk.testing import PluginUnitTestCase
+from zmlp_train.trainClassifier import run
+
+logging.basicConfig()
+
+
+class TrainClassifierTests(PluginUnitTestCase):
+    ds_id = "ds-id-12345"
+    model_id = "model-id-12345"
+    base_dir = os.path.dirname(__file__)
+    class_names = ["dog", "cat"]
+
+    def setUp(self):
+        # A mock search result used for asset search tests
+        self.mock_search_result = {
+            "took": 4,
+            "timed_out": False,
+            "hits": {
+                "total": {"value": 2},
+                "max_score": 0.2876821,
+                "hits": [
+                    {
+                        "_id": "dd0KZtqyec48n1q1ffogVMV5yzthRRGx2WKzKLjDphg",
+                        "_source": {
+                            "shash": id_generator(size=2048),
+                            "label": self.class_names[random.randint(0, 1)],
+                        },
+                    }
+                    for _ in range(100)
+                ],
+            },
+        }
+
+    @patch.object(ModelApp, "publish_model")
+    @patch.object(ModelApp, "get_model")
+    @patch.object(file_storage.projects, "localize_file")
+    @patch.object(ZmlpClient, "post")
+    @patch("zmlp_train.trainClassifier.upload_model_directory")
+    def test_main(
+        self, upload_patch, post_patch, file_patch, model_patch, pub_patch
+    ):
+        name = "custom-flowers-label-detection-tf2-xfer-mobilenet2"
+        file_patch.return_value = "{}/{}.zip".format(self.base_dir, name)
+        pub_patch.return_value = PipelineMod({"id": "12345", "name": name})
+        post_patch.return_value = self.mock_search_result
+        model_patch.return_value = Model(
+            {
+                "id": self.model_id,
+                "dataSetId": self.ds_id,
+                "type": "LABEL_DETECTION_MOBILENET2",
+                "fileId": "models/{}/foo/bar".format(self.model_id),
+                "name": name,
+            }
+        )
+        upload_patch.return_value = StoredFile({"id": "12345"})
+
+        main_args = [
+            "--model_id",
+            self.model_id,
+            "--folder",
+            "pets",
+            "--attr",
+            "shash",
+        ]
+        model_dir = run(main_args)
+
+        assert os.path.exists(model_dir)
+        assert os.path.exists(os.path.join(model_dir, "assets"))
+        assert os.path.exists(os.path.join(model_dir, "_labels.txt"))
+
+
+def id_generator(size=6, chars=string.ascii_uppercase):
+    """Generate a random simhash
+
+    Args:
+        size: (int) size of hash
+        chars: (str) values to use for the hash
+
+    Returns:
+        (str) generated similarity hash
+    """
+    return "".join(random.choice(chars) for _ in range(size))
--- containers/zmlp-plugins-train/pylib/zmlp_train/trainClassifier.py
+++ containers/zmlp-plugins-train/pylib/zmlp_train/trainClassifier.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
+"""Train a classifier and save the model"""
 
+import os
+import sys
 import logging
 import argparse
 import numpy as np
 import random
+import tempfile
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation
@@ -15,36 +19,47 @@ from zmlpsdk import file_storage
 from zmlp_train.utils.utils import get_labels, extract_model
 from zmlp_train.utils.models import upload_model_directory
 
-logging.basicConfig()
+logging.basicConfig(level=logging.INFO)
+
+PTEST = 0.3
+SEED = 42
+ORD_VAL = 65
+NORMALIZED_VAL = 16.0
 
 
-def load_data(app, app_model, simhash):
-    train_x = []
-    train_y = []
-    test_x = []
-    test_y = []
+def load_data(app, app_model_name, simhash):
+    """Load data into train and test formats that Keras likes
 
-    PTEST = 0.3
-    random.seed(42)
+    Args:
+        app: (ZmlpClient) ZMLP client
+        app_model_name: (str) model name based off Model Id
+        simhash: (str) similarity hash attribute
 
-    app_model_name = app_model.name
-    model_zip = file_storage.projects.localize_file(app_model.file_id)
-    tmpfile_dir = extract_model(model_zip)
-    class_names = get_labels(tmpfile_dir, app_model_name, "labels.txt")
+    Returns:
+        (tuple) of (np.ndarray, np.ndarray, np.ndarray, np.ndarray, List[str])
+    """
+    train_x, train_y = [], []
+    test_x, test_y = [], []
 
-    for a in app.assets.search():
-        num_hash = []
-        charhash = a.get_attr(simhash)
+    random.seed(SEED)
 
-        for char in charhash:
-            num_hash.append((ord(char) - 65) / 16.0)
+    search = app.assets.search()
+    for a in search:
+        try:
+            charhash = a.get_attr(simhash)
+            label = a.get_attr('label')
+        except TypeError:
+            continue
+        num_hash = [
+            (ord(char) - ORD_VAL) / NORMALIZED_VAL for char in charhash
+        ]
 
         if random.random() > PTEST:
             train_x.append(num_hash)
-            train_y.append(app_model_name)
+            train_y.append(label)
         else:
             test_x.append(num_hash)
-            test_y.append(app_model_name)
+            test_y.append(label)
 
     # Convert labels to an array of indices
     cats = list(set(train_y))
@@ -53,14 +68,91 @@ def load_data(app, app_model, simhash):
 
     # Make everything into numpy arrays
     train_x = np.array(train_x)
-    train_y = to_categorical(train_y, len(class_names))
+    train_y = to_categorical(train_y, len(cats))
     test_x = np.array(test_x)
-    test_y = to_categorical(test_y, len(class_names))
+    test_y = to_categorical(test_y, len(cats))
 
     return (train_x, train_y), (test_x, test_y), cats
 
 
-def main():
+def build_model(n_neurons, train_x, train_y, test_x, test_y, class_names):
+    """Build and fit the model
+
+    Args:
+        n_neurons: (int) number of neurons in the hidden layer
+        train_x: (np.ndarray) training values
+        train_y: (np.ndarray) training labels
+        test_x: (np.ndarray) test values
+        test_y: (np.ndarray) test labels
+        class_names: (List[str]) list of class names
+
+    Returns:
+        tf.keras model
+    """
+    # add layers
+    model = Sequential()
+    model.add(Dense(n_neurons, input_dim=train_x.shape[1], activation="relu"))
+    model.add(Dense(len(class_names)))
+    model.add(Activation("softmax"))
+
+    # train
+    sgd = SGD(lr=0.01)
+    model.compile(
+        loss="categorical_crossentropy", optimizer=sgd, metrics=["accuracy"]
+    )
+    model.fit(train_x, train_y, epochs=50, batch_size=128, verbose=1)
+
+    # get scores
+    scores = model.evaluate(test_x, test_y)
+    logging.info(
+        "Results on the test set are %f accuracy and %f loss."
+        % (scores[1], scores[0])
+    )
+
+    return model
+
+
+def run(main_args):
+    args = parse_args(main_args)
+
+    # get model info
+    app = zmlp.app_from_env()
+    app_model = app.models.get_model(args.model_id)
+    model_dir = tempfile.mkdtemp() + '/' + args.folder
+
+    app_model_name = app_model.name
+    model_zip = file_storage.projects.localize_file(app_model.file_id)
+    tmpfile_dir = extract_model(model_zip)
+    class_names = get_labels(tmpfile_dir, app_model_name, "labels.txt")
+
+    # load data
+    logging.info("Looking for categories:")
+    (train_x, train_y), (test_x, test_y), class_names = load_data(
+        app, app_model_name, args.attr
+    )
+
+    model = build_model(
+        args.n_neurons, train_x, train_y, test_x, test_y, class_names
+    )
+
+    # save model and labels
+    model.save(model_dir)
+    with open(os.path.join(model_dir, "_labels.txt"), "w") as fp:
+        for label in class_names:
+            fp.write("{}\n".format(label))
+
+    # zip and publish
+    upload_model_directory(model_dir, app_model.file_id)
+    app.models.publish_model(app_model)
+
+    logging.info("Model saved in " + model_dir)
+    logging.info("Labels are in " + model_dir + "/_labels.txt")
+    logging.info("Use these two files with the KerasClassifier processor")
+
+    return model_dir
+
+
+def parse_args(main_args):
     parser = argparse.ArgumentParser(prog="trainClassifier")
     parser.add_argument("-m", "--model_id", help="The model ID")
     parser.add_argument(
@@ -82,56 +174,9 @@ def main():
         default="100",
         help="Number of neurons in the hidden layer.",
     )
-    args = parser.parse_args()
-
-    logging.info("Looking for categories:")
-
-    # get model info
-    app = zmlp.app_from_env()
-    app_model = app.models.get_model(args.model_id)
-    model_dir = args.folder
-
-    # load data
-    (train_x, train_y), (test_x, test_y), class_names = load_data(
-        app, app_model, args.attr
-    )
-
-    # add layers
-    model = Sequential()
-    model.add(
-        Dense(args.n_neurons, input_dim=train_x.shape[1], activation="relu")
-    )
-    model.add(Dense(len(class_names)))
-    model.add(Activation("softmax"))
-
-    # train
-    sgd = SGD(lr=0.01)
-    model.compile(
-        loss="binary_crossentropy", optimizer=sgd, metrics=["accuracy"]
-    )
-    model.fit(train_x, train_y, epochs=50, batch_size=128, verbose=1)
-
-    # get scores
-    scores = model.evaluate(test_x, test_y)
-    logging.info(
-        "Results on the test set are %f accuracy and %f loss."
-        % (scores[1], scores[0])
-    )
-
-    # save model and labels
-    model.save(model_dir + ".hd5")
-    with open(model_dir + "_labels.txt", "w") as classes_file:
-        for label in class_names:
-            print(label, file=classes_file)
-
-    # zip and publish
-    upload_model_directory(model_dir, app_model.file_id)
-    app.models.publish_model(app_model)
-
-    logging.info("Model saved as " + model_dir + ".hd5")
-    logging.info("Labels are in " + model_dir + "_labels.txt")
-    logging.info("Use these two files with the KerasClassifier processor")
+    args = parser.parse_args(main_args)
+    return args
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(run(sys.argv[1:]))
