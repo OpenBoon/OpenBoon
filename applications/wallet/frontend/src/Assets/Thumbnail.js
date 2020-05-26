a9e8d3ab18b9deaadf6873131f6ca54256fcd283
Damien Varron <damien@varron.fr>
2020-05-26 15:14:22 -0700

    WAL-553: fix visible thumbnail link

--- applications/wallet/frontend/src/Assets/Thumbnail.js
+++ applications/wallet/frontend/src/Assets/Thumbnail.js
@@ -56,7 +56,7 @@ const AssetsThumbnail = ({
   return (
     <div
       css={{
-        display: 'relative',
+        position: 'relative',
         border: isSelected
           ? constants.borders.assetSelected
           : constants.borders.assetInactive,
@@ -67,9 +67,7 @@ const AssetsThumbnail = ({
             ? constants.borders.assetSelected
             : constants.borders.assetHover,
           a: {
-            svg: {
-              display: 'inline-block',
-            },
+            display: 'flex',
           },
         },
       }}
@@ -125,9 +123,10 @@ const AssetsThumbnail = ({
         <Button
           variant={VARIANTS.NEUTRAL}
           style={{
+            display: 'none',
             position: 'absolute',
-            bottom: spacing.base,
-            right: spacing.base,
+            bottom: spacing.small,
+            right: spacing.small,
             padding: spacing.small,
             backgroundColor: colors.structure.smoke,
             opacity: constants.opacity.half,
@@ -136,21 +135,15 @@ const AssetsThumbnail = ({
             },
           }}
         >
-          <ExpandSvg
-            width={20}
-            color={colors.structure.white}
-            css={{
-              display: 'none',
-            }}
-          />
+          <ExpandSvg width={20} color={colors.structure.white} />
         </Button>
       </Link>
       {assetStyle === 'video' && (
         <div
           css={{
             position: 'absolute',
-            bottom: spacing.base,
-            left: spacing.base,
+            bottom: spacing.small,
+            left: spacing.small,
             padding: spacing.mini,
             color: colors.structure.black,
             // Append 80 for half opacity without affecting text
