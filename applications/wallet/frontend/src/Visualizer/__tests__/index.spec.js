21099ed3e2ec4f572367c06925fbf78562a16f15
timothychin <tim.chin@zorroa.com>
2020-06-01 16:28:09 -0700

    add changes to Visualizer

--- applications/wallet/frontend/src/Visualizer/__tests__/index.spec.js
+++ applications/wallet/frontend/src/Visualizer/__tests__/index.spec.js
@@ -15,6 +15,7 @@ const ASSET_ID = assets.results[0].id
 describe('<Visualizer />', () => {
   it('should render properly', () => {
     require('swr').__setMockUseSWRResponse({ data: assets })
+    require('swr').__setPageSWRs([{ data: assets }])
 
     require('next/router').__setUseRouter({
       query: { projectId: PROJECT_ID, action: 'delete-asset-success' },
@@ -31,6 +32,7 @@ describe('<Visualizer />', () => {
 
   it('should render selected asset', () => {
     require('swr').__setMockUseSWRResponse({ data: assets })
+    require('swr').__setPageSWRs([{ data: assets }])
 
     require('next/router').__setUseRouter({
       query: { id: ASSET_ID, projectId: PROJECT_ID },
