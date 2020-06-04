dffa708a749071bca0842e8303c285c2d907d21a
Iron <iron.araujo@zorroa.com>
2020-06-04 13:57:19 -0300

    Project Tier Migration

--- services/archivist/archivist/src/main/resources/db/migration/postgresql/V1__init.sql
+++ services/archivist/archivist/src/main/resources/db/migration/postgresql/V1__init.sql
@@ -378,8 +378,7 @@ CREATE TABLE project (
     time_modified bigint NOT NULL,
     actor_created text NOT NULL,
     actor_modified text NOT NULL,
-    enabled boolean DEFAULT true NOT NULL,
-    int_tier int2 NOT NULL DEFAULT 0
+    enabled boolean DEFAULT true NOT NULL
 );
 
 --
---
+++ services/archivist/archivist/src/main/resources/db/migration/postgresql/V2__project_tier.sql
@@ -0,0 +1,4 @@
+--
+-- Name: project int_tier; Type: COLUMN; Schema: zorroa; Owner: zorroa
+--
+ALTER TABLE zorroa.project ADD int_tier int2 NOT NULL DEFAULT 0;
