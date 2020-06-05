da1dc76260f958dc42941fe6213b28c5102079c9
Timothy Chin <tim.chin@zorroa.com>
2020-06-05 05:33:16 +0000

    WAL-490: add Visualizer empty state

---
+++ applications/wallet/frontend/src/Assets/Empty.js
@@ -0,0 +1,78 @@
+import PropTypes from 'prop-types'
+
+import { colors, spacing, typography } from '../Styles'
+
+import NoAssetsSvg from '../Icons/noAssets.svg'
+
+import Button, { VARIANTS } from '../Button'
+
+import { ACTIONS, dispatch, decode } from '../Filters/helpers'
+
+const AssetsEmpty = ({ projectId, query, assetId }) => {
+  const filters = query ? decode({ query }) : []
+  const hasFilters = filters.length > 0
+
+  return (
+    <div
+      css={{
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: spacing.normal,
+      }}
+    >
+      <NoAssetsSvg width={168} color={colors.structure.steel} />
+      <h2
+        css={{
+          paddingTop: spacing.normal,
+          fontSize: typography.size.giant,
+          lineHeight: typography.height.giant,
+        }}
+      >
+        There are currently no assets to show.
+      </h2>
+      <h3
+        css={{
+          fontSize: typography.size.large,
+          lineHeight: typography.height.large,
+          color: colors.structure.zinc,
+        }}
+      >
+        Either all have been filtered out or there aren’t any in the system yet.
+      </h3>
+      {hasFilters && (
+        <>
+          <div css={{ height: spacing.comfy }} />
+          <Button
+            aria-label="Clear All Filters"
+            variant={VARIANTS.PRIMARY}
+            isDisabled={filters.length === 0}
+            onClick={() => {
+              dispatch({
+                action: ACTIONS.CLEAR_FILTERS,
+                payload: { projectId, assetId },
+              })
+            }}
+          >
+            Clear All Filters
+          </Button>
+        </>
+      )}
+    </div>
+  )
+}
+
+AssetsEmpty.defaultProps = {
+  query: '',
+  assetId: '',
+}
+
+AssetsEmpty.propTypes = {
+  projectId: PropTypes.string.isRequired,
+  query: PropTypes.string,
+  assetId: PropTypes.string,
+}
+
+export default AssetsEmpty
