e3f9aa6f0004de8f7a82756e12b49a42874dc8cc
Matt Chambers <mrc@zorroa.com>
2020-06-09 20:58:58 +0000

    Update V2__project_tier.sql

--- services/archivist/archivist/src/main/resources/db/migration/postgresql/V2__project_tier.sql
+++ services/archivist/archivist/src/main/resources/db/migration/postgresql/V2__project_tier.sql
@@ -1,4 +1,4 @@
 --
 -- Name: project int_tier; Type: COLUMN; Schema: zorroa; Owner: zorroa
 --
-ALTER TABLE project ADD int_tier int2 NOT NULL DEFAULT 0;
+ALTER TABLE project ADD int_tier SMALLINT NOT NULL DEFAULT 0;
