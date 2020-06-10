33bfc0e732774dc11ca502fd9e03b8f00c023c4b
Damien Varron <damien@varron.fr>
2020-06-10 15:33:48 -0700

    WAL-593: remove getInitialProps

--- applications/wallet/frontend/next.config.js
+++ applications/wallet/frontend/next.config.js
@@ -12,10 +12,8 @@ const {
 
 module.exports = withSourceMaps({
   reactStrictMode: true,
-  env: {
-    CI_COMMIT_SHA,
-  },
   publicRuntimeConfig: {
+    CI_COMMIT_SHA,
     GOOGLE_OAUTH_CLIENT_ID,
     ENVIRONMENT,
     ENABLE_SENTRY,
