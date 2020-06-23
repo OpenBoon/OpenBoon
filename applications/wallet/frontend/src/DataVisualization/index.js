74422fb81976b0ddff274881369ab4d59e41e424
Damien Varron <damien@varron.fr>
2020-06-23 10:11:27 -0700

    add role boundary

--- applications/wallet/frontend/src/DataVisualization/index.js
+++ applications/wallet/frontend/src/DataVisualization/index.js
@@ -19,35 +19,37 @@ const DataVisualization = () => {
         <title>Data Visualization</title>
       </Head>
 
-      <div
-        css={{
-          height: '100%',
-          backgroundColor: colors.structure.coal,
-          marginLeft: -spacing.spacious,
-          marginRight: -spacing.spacious,
-          marginBottom: -spacing.spacious,
-          paddingTop: spacing.hairline,
-          display: 'flex',
-          flex: 1,
-          flexDirection: 'column',
-        }}
-      >
-        <div css={{ display: 'flex', height: '100%', overflowY: 'hidden' }}>
-          <Panel openToThe="right">
-            {{
-              filters: {
-                title: 'Filters',
-                icon: <FilterSvg width={ICON_SIZE} aria-hidden />,
-                content: <Filters />,
-              },
-            }}
-          </Panel>
+      <SuspenseBoundary role={ROLES.ML_Tools}>
+        <div
+          css={{
+            height: '100%',
+            backgroundColor: colors.structure.coal,
+            marginLeft: -spacing.spacious,
+            marginRight: -spacing.spacious,
+            marginBottom: -spacing.spacious,
+            paddingTop: spacing.hairline,
+            display: 'flex',
+            flex: 1,
+            flexDirection: 'column',
+          }}
+        >
+          <div css={{ display: 'flex', height: '100%', overflowY: 'hidden' }}>
+            <Panel openToThe="right">
+              {{
+                filters: {
+                  title: 'Filters',
+                  icon: <FilterSvg width={ICON_SIZE} aria-hidden />,
+                  content: <Filters />,
+                },
+              }}
+            </Panel>
 
-          <SuspenseBoundary role={ROLES.ML_Tools}>
-            <DataVisualizationContent />
-          </SuspenseBoundary>
+            <SuspenseBoundary>
+              <DataVisualizationContent />
+            </SuspenseBoundary>
+          </div>
         </div>
-      </div>
+      </SuspenseBoundary>
     </>
   )
 }
