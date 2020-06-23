ccdf7e2c621a8e8e33a097bf26a97a95b9f7db46
Damien Varron <damien@varron.fr>
2020-06-23 10:09:01 -0700

    do the shufflin

--- applications/wallet/frontend/src/DataVisualization/index.js
+++ applications/wallet/frontend/src/DataVisualization/index.js
@@ -1,8 +1,16 @@
 import Head from 'next/head'
 
+import { colors, spacing } from '../Styles'
+
+import Panel from '../Panel'
+import Filters from '../Filters'
 import SuspenseBoundary, { ROLES } from '../SuspenseBoundary'
 
+import FilterSvg from '../Icons/filter.svg'
+
 import DataVisualizationContent from './Content'
+
+const ICON_SIZE = 20
 
 const DataVisualization = () => {
   return (
@@ -11,9 +19,35 @@ const DataVisualization = () => {
         <title>Data Visualization</title>
       </Head>
 
-      <SuspenseBoundary role={ROLES.ML_Tools}>
-        <DataVisualizationContent />
-      </SuspenseBoundary>
+      <div
+        css={{
+          height: '100%',
+          backgroundColor: colors.structure.coal,
+          marginLeft: -spacing.spacious,
+          marginRight: -spacing.spacious,
+          marginBottom: -spacing.spacious,
+          paddingTop: spacing.hairline,
+          display: 'flex',
+          flex: 1,
+          flexDirection: 'column',
+        }}
+      >
+        <div css={{ display: 'flex', height: '100%', overflowY: 'hidden' }}>
+          <Panel openToThe="right">
+            {{
+              filters: {
+                title: 'Filters',
+                icon: <FilterSvg width={ICON_SIZE} aria-hidden />,
+                content: <Filters />,
+              },
+            }}
+          </Panel>
+
+          <SuspenseBoundary role={ROLES.ML_Tools}>
+            <DataVisualizationContent />
+          </SuspenseBoundary>
+        </div>
+      </div>
     </>
   )
 }
