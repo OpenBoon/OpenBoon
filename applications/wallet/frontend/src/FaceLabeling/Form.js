fc50d3b9e4702477326d92939c0a2889662779eb
octaviaaris <octavia@zorroa.com>
2020-06-24 19:22:49 -0700

    remove unnecessary options

--- applications/wallet/frontend/src/FaceLabeling/Form.js
+++ applications/wallet/frontend/src/FaceLabeling/Form.js
@@ -32,11 +32,7 @@ const FaceLabelingForm = ({ projectId, assetId, predictions }) => {
 
   const [state, dispatch] = useReducer(reducer, initializedState)
 
-  const { data } = useSWR(`/api/v1/projects/${projectId}/faces/labels/`, {
-    revalidateOnFocus: false,
-    revalidateOnReconnect: false,
-    shouldRetryOnError: false,
-  })
+  const { data } = useSWR(`/api/v1/projects/${projectId}/faces/labels/`)
 
   const { possibleLabels = [] } = data || {}
 
