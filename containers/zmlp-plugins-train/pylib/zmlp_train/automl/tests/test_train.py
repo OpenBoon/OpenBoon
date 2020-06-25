f469fb47cd73373dfdce9e9cdf692a2f24693e9b
Ryan Gaspar <ryangaspar@rg-zorroa.hub>
2020-06-25 21:40:21 +1200

    atuoml tests

--- containers/zmlp-plugins-train/pylib/zmlp_train/automl/tests/test_train.py
+++ containers/zmlp-plugins-train/pylib/zmlp_train/automl/tests/test_train.py
@@ -1,11 +1,14 @@
 import os
 import logging
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
+
+from google.cloud import automl_v1beta1 as automl
+from google.api_core.operation import Operation
 
 from zmlp.app import ModelApp
-from zmlp.entity import Model, PipelineMod
+from zmlp.entity import Model, PipelineMod, StoredFile
 from zmlp_train.automl import AutoMLModelTrainer
-from zmlpsdk import Frame
+from zmlpsdk import Frame, file_storage
 from zmlpsdk.testing import PluginUnitTestCase, TestAsset, zorroa_test_data
 
 logging.basicConfig()
@@ -16,9 +19,15 @@ ds_id = "ds-id-12345"
 
 class AutoMLModelProcessorTests(PluginUnitTestCase):
 
+    @patch.object(AutoMLModelTrainer, 'import_dataset')
+    @patch.object(AutoMLModelTrainer, 'create_model')
+    @patch.object(AutoMLModelTrainer, '_get_id')
     @patch.object(ModelApp, 'publish_model')
     @patch.object(ModelApp, 'get_model')
-    def do_not_test_model_eval(self, model_patch, pub_patch):
+    @patch.object(automl.AutoMlClient, 'deploy_model')
+    @patch.object(file_storage.projects, "store_file_by_id")
+    def test_process(self, upload_patch, deploy_patch, model_patch, pub_patch, dataset_id_patch,
+                     create_model_patch, import_patch):
         # Prep the frame, asset, and proxy
         toucan_fname = zorroa_test_data('training/test_dsy.jpg').split("file://")[-1]
         asset = TestAsset(toucan_fname)
@@ -36,6 +45,11 @@ class AutoMLModelProcessorTests(PluginUnitTestCase):
             'fileId': 'models/{}/foo/bar'.format(model_id),
             'name': name
         })
+        import_patch.return_value = None
+        dataset_id_patch.return_value = "ICN443750798342488064"
+        create_model_patch.return_value = None
+        deploy_patch.return_value = None
+        upload_patch.return_value = StoredFile({"id": "12345"})
 
         # Prep the processor
         self.processor = AutoMLModelTrainer()
@@ -48,7 +62,8 @@ class AutoMLModelProcessorTests(PluginUnitTestCase):
             'model_id': model_id,
             'gcp_credentials_path': creds_path,
             'display_name': name,
-            'project_path': 'gs://{}-vcm/csv/all_data.csv'.format(project_id)
+            'project_path': 'gs://{}-vcm/csv/csv_some_data.csv'.format(project_id),
+            'model_path': "ICN655064838573129728"
         }
         self.init_processor(self.processor, args)
 
--- containers/zmlp-plugins-train/pylib/zmlp_train/automl/train.py
+++ containers/zmlp-plugins-train/pylib/zmlp_train/automl/train.py
@@ -1,4 +1,5 @@
 import os
+import time
 import tempfile
 import logging
 
@@ -38,6 +39,8 @@ class AutoMLModelTrainer(AssetProcessor):
                               toolTip=AutoMLModelTrainer.tool_tips['region']))
         self.add_arg(Argument("project_path", "string",
                               toolTip=AutoMLModelTrainer.tool_tips['project_path']))
+        self.add_arg(Argument("model_path", "string",
+                              toolTip=AutoMLModelTrainer.tool_tips['project_path']))
 
         self.model_id = None
         self.model_path = None
@@ -58,6 +61,7 @@ class AutoMLModelTrainer(AssetProcessor):
         self.model_id = self.arg_value('model_id')
         self.display_name = self.arg_value('display_name')
         self.project_path = self.arg_value('project_path')
+        self.model_path = self.arg_value('model_path')
 
         self.gcp_credentials_path = self.arg_value('gcp_credentials_path')
         os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = self.gcp_credentials_path
@@ -68,11 +72,11 @@ class AutoMLModelTrainer(AssetProcessor):
         self.dataset = self.create_dataset(self.project_id, self.display_name, self.region)
         dataset_id = self._get_id(self.dataset)
 
-        self.import_dataset(self.project_id, dataset_id, self.project_path)
+        self.import_dataset(self.project_id, dataset_id, self.project_path, self.region)
 
         self.model = self.create_model(self.project_id, dataset_id, self.display_name, self.region)
-        self.model_path = self.model.name
-        # model_id = self._get_id(self.model)
+        # self.model_path = self.model.operation.name
+        # model_id = self.model.operation.name.split("/")[-1]  # self._get_id(self.model.operation)
 
         self.publish_model(self.model_path)
 
@@ -120,13 +124,13 @@ class AutoMLModelTrainer(AssetProcessor):
         created_dataset = self.client.create_dataset(project_location, dataset)
 
         # Display the dataset information
-        logging.info("Dataset name: {}".format(created_dataset.name))
-        logging.info("Dataset id: {}".format(created_dataset.name.split("/")[-1]))
+        print("Dataset name: {}".format(created_dataset.name))
+        print("Dataset id: {}".format(created_dataset.name.split("/")[-1]))
 
         return created_dataset
 
     @backoff.on_exception(backoff.expo, ResourceExhausted, max_time=10 * 60)
-    def import_dataset(self, project_id, dataset_id, path):
+    def import_dataset(self, project_id, dataset_id, path, region="us-central1"):
         """Takes as input a .csv file that lists the locations of all training images and the
         proper label for each one
 
@@ -134,14 +138,15 @@ class AutoMLModelTrainer(AssetProcessor):
             project_id: GC Project ID
             dataset_id: Dataset ID
             path: Path to CSV
+            region: GC Region ID (default: us-central1)
 
         Returns:
             None
         """
-
+        start_time = time.time()
         # Get the full path of the dataset.
         dataset_full_id = self.client.dataset_path(
-            project_id, "us-central1", dataset_id
+            project_id, region, dataset_id
         )
         # Get the multiple Google Cloud Storage URIs
         input_uris = path.split(",")
@@ -150,8 +155,9 @@ class AutoMLModelTrainer(AssetProcessor):
         # Import data from the input URI
         response = self.client.import_data(dataset_full_id, input_config)
 
-        logging.info("Processing import...")
-        logging.info("Data imported. {}".format(response.result()))
+        self.reactor.emit_status("Processing import...")
+        self.reactor.emit_status("Data imported. {}".format(response.result()))
+        self.reactor.emit_status("Total import time: {}".format(time.time()-start_time))
 
     def create_model(self, project_id, dataset_id, display_name, region="us-central1"):
         """Create and train a model
@@ -170,12 +176,11 @@ class AutoMLModelTrainer(AssetProcessor):
         Returns:
             (google.cloud.automl_v1.types.Model¶) Model metadata
         """
+        start_time = time.time()
 
         # A resource that represents Google Cloud Platform location.
         project_location = self.client.location_path(project_id, region)
-        metadata = automl.types.ImageClassificationModelMetadata(
-            train_budget_milli_node_hours=24000
-        )
+        metadata = automl.types.ImageClassificationModelMetadata(train_budget=100)
         model = automl.types.Model(
             display_name=display_name,
             dataset_id=dataset_id,
@@ -185,8 +190,11 @@ class AutoMLModelTrainer(AssetProcessor):
         # Create a model with the model metadata in the region.
         response = self.client.create_model(project_location, model)
 
-        logging.info("Training operation name: {}".format(response.operation.name))
-        logging.info("Training started...")
+        self.reactor.emit_status("Training operation name: {}".format(response.operation.name))
+        self.reactor.emit_status("Training started...")
+
+        self.reactor.emit_status("Training complete. {}".format(response.result()))
+        self.reactor.emit_status("Total training time: {}".format(time.time() - start_time))
 
         return response
 
@@ -204,7 +212,8 @@ class AutoMLModelTrainer(AssetProcessor):
         os.makedirs(model_dir)
 
         # deploy model
-        self.client.deploy_model(model)
+        model_full_id = self.client.model_path(self.project_id, self.region, model)
+        self.client.deploy_model(model_full_id)
 
         # publish
         pmod = file_storage.models.save_model(model_dir, self.app_model)
