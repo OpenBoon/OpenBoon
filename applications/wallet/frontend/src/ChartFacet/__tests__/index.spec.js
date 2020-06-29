caef78268aa585275165096e219018c6fde3f4ff
Timothy Chin <tim.chin@zorroa.com>
2020-06-29 17:12:56 +0000

    WAL-519: add Header and delete function

--- applications/wallet/frontend/src/ChartFacet/__tests__/index.spec.js
+++ applications/wallet/frontend/src/ChartFacet/__tests__/index.spec.js
@@ -241,4 +241,36 @@ describe('<ChartFacet />', () => {
 
     expect(component.toJSON()).toMatchSnapshot()
   })
+
+  it('should delete', () => {
+    const mockDispatch = jest.fn()
+
+    require('next/router').__setUseRouter({
+      pathname: '/[projectId]/visualizer/data-visualization',
+      query: { projectId: PROJECT_ID },
+    })
+
+    const chart = {
+      id: CHART_ID,
+      type: 'facet',
+      attribute: 'system.type',
+    }
+
+    const component = TestRenderer.create(
+      <ChartFacet chart={chart} chartIndex={0} dispatch={mockDispatch} />,
+    )
+
+    act(() => {
+      component.root
+        .findByProps({ 'aria-label': 'Delete Chart' })
+        .props.onClick()
+    })
+
+    expect(mockDispatch).toHaveBeenCalledWith({
+      type: 'DELETE',
+      payload: {
+        chartIndex: 0,
+      },
+    })
+  })
 })
