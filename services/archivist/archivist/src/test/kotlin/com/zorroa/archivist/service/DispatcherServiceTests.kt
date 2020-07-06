697cd800ca1eede34911540fe8aa3a9f6a6fd246
Iron <iron.araujo@zorroa.com>
2020-07-06 17:29:18 -0300

    Avoiding missing type exception

--- services/archivist/archivist/src/test/kotlin/com/zorroa/archivist/service/DispatcherServiceTests.kt
+++ services/archivist/archivist/src/test/kotlin/com/zorroa/archivist/service/DispatcherServiceTests.kt
@@ -117,7 +117,8 @@ class DispatcherServiceTests : AbstractTest() {
         val rsp = dispatcherService.handleIndexEvent(
             task,
             BatchIndexAssetsEvent(
-                mapOf(createRsp.created[0] to mutableMapOf<String, Any>("foo" to "bar")), null
+                mapOf(createRsp.created[0] to mutableMapOf<String, Any>("foo" to "bar",
+                    "media" to mapOf("type" to "image"))), null
             )
         )
         assertTrue(rsp?.hasFailures() ?: false)
