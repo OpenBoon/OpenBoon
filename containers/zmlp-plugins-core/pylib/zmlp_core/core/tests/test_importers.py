e8e2598435f771e277d02f0d6e5ad9809a5fb8ff
Matt <mrc@zorroa.com>
2020-07-06 20:07:04 -0400

    Fixes file importer tests that needed new mocks

--- containers/zmlp-plugins-core/pylib/zmlp_core/core/tests/test_importers.py
+++ containers/zmlp-plugins-core/pylib/zmlp_core/core/tests/test_importers.py
@@ -6,21 +6,25 @@ from zmlp_core.core.importers import FileImportProcessor
 from zmlp_core.office.importers import OfficerClient, OfficeImporter
 from zmlpsdk import Frame, ZmlpFatalProcessorException
 from zmlpsdk.testing import PluginUnitTestCase, TestAsset, zorroa_test_data
+from zmlp.app import ProjectApp
 
 
 class FileImportProcessorTests(PluginUnitTestCase):
 
-    def test_process_image(self):
+    @patch.object(ProjectApp, 'get_project')
+    def test_process_image(self, _):
         frame = Frame(TestAsset(zorroa_test_data("images/set01/toucan.jpg")))
         ih = self.init_processor(FileImportProcessor())
         ih.process(frame)
 
-    def test_process_video(self):
+    @patch.object(ProjectApp, 'get_project')
+    def test_process_video(self, _):
         frame = Frame(TestAsset(zorroa_test_data('video/sample_ipad.m4v')))
         ih = self.init_processor(FileImportProcessor())
         ih.process(frame)
 
-    def test_process_gps_point(self):
+    @patch.object(ProjectApp, 'get_project')
+    def test_process_gps_point(self, _):
         frame = Frame(TestAsset(zorroa_test_data('video/sample_ipad.m4v')))
         frame.asset.set_attr("location.point.lat", -37.81)
         frame.asset.set_attr("location.point.lon", 144.96)
@@ -33,16 +37,18 @@ class FileImportProcessorTests(PluginUnitTestCase):
         assert 'AU' == frame.asset.get_attr('location.code')
         assert 'Australia' == frame.asset.get_attr('location.country')
 
+    @patch.object(ProjectApp, 'get_project')
     @patch.object(OfficeImporter, 'get_metadata',
                   return_value={'author': 'Zach', 'content': 'temp'})
     @patch.object(OfficerClient, 'render', return_value='/fake')
     @patch.object(OfficerClient, 'get_cache_location', return_value=None)
-    def test_process_document(self, _, __, ___):
+    def test_process_document(self, _, __, ___, ____):
         frame = Frame(TestAsset(zorroa_test_data("office/minimal.pdf")))
         ih = self.init_processor(FileImportProcessor())
         ih.process(frame)
 
-    def test_process_unsupported(self):
+    @patch.object(ProjectApp, 'get_project')
+    def test_process_unsupported(self, _):
         frame = Frame(TestAsset(zorroa_test_data("offie/minimal.rar")))
         ih = self.init_processor(FileImportProcessor())
         with pytest.raises(ZmlpFatalProcessorException):
