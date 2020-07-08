970aee9aab4afb48133ebca1a635f137bb5825be
Damien Varron <damien@varron.fr>
2020-07-08 22:26:49 +0000

    WAL-661: add Feature flag component

---
+++ applications/wallet/frontend/src/Feature/__tests__/index.spec.js
@@ -0,0 +1,63 @@
+import TestRenderer from 'react-test-renderer'
+
+import Feature, { ENVS } from '..'
+
+describe('<Feature />', () => {
+  it('should not render a disabled feature', () => {
+    localStorage.setItem('FeatureFlags', JSON.stringify({ hello: false }))
+
+    const component = TestRenderer.create(
+      <Feature flag="hello" envs={[ENVS.LOCAL]}>
+        Hello
+      </Feature>,
+    )
+
+    expect(component.toJSON()).toEqual(null)
+  })
+
+  it('should render an enabled feature', () => {
+    localStorage.setItem('FeatureFlags', JSON.stringify({ hello: true }))
+
+    const component = TestRenderer.create(
+      <Feature flag="hello" envs={[ENVS.LOCAL]}>
+        Hello
+      </Feature>,
+    )
+
+    expect(component.toJSON()).toEqual('Hello')
+  })
+
+  it('should render a feature with no env in localdev', () => {
+    const component = TestRenderer.create(
+      <Feature flag="hello" envs={[]}>
+        Hello
+      </Feature>,
+    )
+
+    expect(component.toJSON()).toEqual('Hello')
+  })
+
+  it('should not render a feature with no env in prod', () => {
+    require('next/config').__setPublicRuntimeConfig({ ENVIRONMENT: 'zvi-prod' })
+
+    const component = TestRenderer.create(
+      <Feature flag="hello" envs={[]}>
+        Hello
+      </Feature>,
+    )
+
+    expect(component.toJSON()).toEqual(null)
+  })
+
+  it('should render a prod feature in prod', () => {
+    require('next/config').__setPublicRuntimeConfig({ ENVIRONMENT: 'zvi-prod' })
+
+    const component = TestRenderer.create(
+      <Feature flag="hello" envs={[ENVS.PROD]}>
+        Hello
+      </Feature>,
+    )
+
+    expect(component.toJSON()).toEqual('Hello')
+  })
+})
