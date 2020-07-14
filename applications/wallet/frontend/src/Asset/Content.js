c752e6deacd0cb5579b0a8917c884ddb83e1489a
octaviaaris <octavia@zorroa.com>
2020-07-14 11:35:01 -0700

    abstract asset navigation

--- applications/wallet/frontend/src/Asset/Content.js
+++ applications/wallet/frontend/src/Asset/Content.js
@@ -1,16 +1,14 @@
-import Link from 'next/link'
 import { useRouter } from 'next/router'
 
-import { constants, colors, spacing } from '../Styles'
+import { colors, spacing } from '../Styles'
 
-import Button, { VARIANTS } from '../Button'
 import SuspenseBoundary from '../SuspenseBoundary'
 import Panel from '../Panel'
 import Metadata from '../Metadata'
 import AssetDelete from '../AssetDelete'
+import AssetNavigation from './Navigation'
 
 import InformationSvg from '../Icons/information.svg'
-import BackSvg from '../Icons/back.svg'
 import TrashSvg from '../Icons/trash.svg'
 
 import AssetAsset from './Asset'
@@ -21,9 +19,6 @@ const AssetContent = () => {
   const {
     query: { projectId, id: assetId, query },
   } = useRouter()
-
-  const idString = `?id=${assetId}`
-  const queryString = query ? `&query=${query}` : ''
 
   return (
     <div
@@ -51,43 +46,14 @@ const AssetContent = () => {
             display: 'flex',
             flexDirection: 'column',
             width: '100%',
-            overflowY: 'hidden',
           }}
         >
-          <div
-            css={{
-              paddingLeft: spacing.base,
-              paddingRight: spacing.base,
-              backgroundColor: colors.structure.lead,
-              color: colors.structure.steel,
-              boxShadow: constants.boxShadows.navBar,
-              marginBottom: spacing.hairline,
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              width: '100%',
-            }}
-          >
-            <Link
-              href={`/[projectId]/visualizer${idString}${queryString}`}
-              as={`/${projectId}/visualizer${idString}${queryString}`}
-              passHref
-            >
-              <Button
-                variant={VARIANTS.NEUTRAL}
-                css={{
-                  padding: spacing.base,
-                  color: colors.structure.steel,
-                  borderRadius: constants.borderRadius.small,
-                  ':hover': {
-                    color: colors.structure.white,
-                  },
-                }}
-              >
-                <BackSvg height={ICON_SIZE} />
-              </Button>
-            </Link>
-          </div>
+          <AssetNavigation
+            projectId={projectId}
+            assetId={assetId}
+            query={query}
+          />
+
           <SuspenseBoundary>
             <AssetAsset projectId={projectId} assetId={assetId} />
           </SuspenseBoundary>
