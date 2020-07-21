e293be5ca0a6e183ab907a1a581a85051aabe79d
Damien Varron <damien@varron.fr>
2020-07-21 11:05:10 -0700

    WAL-708: update VisualizerNavigation spacings

--- applications/wallet/frontend/src/Visualizer/Navigation.js
+++ applications/wallet/frontend/src/Visualizer/Navigation.js
@@ -20,8 +20,6 @@ const VisualizerNavigation = ({ itemCount }) => {
   return (
     <div
       css={{
-        paddingLeft: spacing.base,
-        paddingRight: spacing.base,
         backgroundColor: colors.structure.lead,
         color: colors.structure.steel,
         margin: spacing.hairline,
@@ -38,6 +36,8 @@ const VisualizerNavigation = ({ itemCount }) => {
             textTransform: 'uppercase',
             fontWeight: typography.weight.medium,
             padding: spacing.base,
+            paddingLeft: spacing.moderate,
+            paddingRight: spacing.moderate,
             display: 'flex',
             justifyContent: 'space-between',
             alignItems: 'center',
@@ -75,7 +75,7 @@ const VisualizerNavigation = ({ itemCount }) => {
           css={{
             marginTop: spacing.base,
             marginBottom: spacing.base,
-            borderLeft: constants.borders.regular.coal,
+            borderLeft: constants.borders.medium.coal,
           }}
         />
 
@@ -107,6 +107,8 @@ const VisualizerNavigation = ({ itemCount }) => {
         css={{
           whiteSpace: 'nowrap',
           padding: spacing.base,
+          paddingLeft: spacing.moderate,
+          paddingRight: spacing.moderate,
           fontFamily: typography.family.condensed,
         }}
       >
