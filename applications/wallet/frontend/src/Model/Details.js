958833d2fbe19715e85763cad3557482ba449ab7
Damien Varron <damien@varron.fr>
2020-07-22 13:09:16 -0700

    add stuff

--- applications/wallet/frontend/src/Model/Details.js
+++ applications/wallet/frontend/src/Model/Details.js
@@ -1,7 +1,16 @@
+import { useState } from 'react'
 import { useRouter } from 'next/router'
 import useSWR from 'swr'
+import Link from 'next/link'
 
-import { typography } from '../Styles'
+import { typography, spacing } from '../Styles'
+
+import FlashMessage, { VARIANTS as FLASH_VARIANTS } from '../FlashMessage'
+import Button, { VARIANTS as BUTTON_VARIANTS } from '../Button'
+import ButtonGroup from '../Button/Group'
+import Modal from '../Modal'
+
+import { onTrain, onDelete } from './helpers'
 
 const LINE_HEIGHT = '23px'
 
@@ -10,14 +19,42 @@ const ModelDetails = () => {
     query: { projectId, modelId },
   } = useRouter()
 
-  const { data: job } = useSWR(
+  const [error, setError] = useState('')
+
+  const [isDeleteModalOpen, setDeleteModalOpen] = useState(false)
+
+  const { data: model } = useSWR(
     `/api/v1/projects/${projectId}/models/${modelId}/`,
+    {
+      refreshInterval: 3000,
+    },
   )
 
-  const { name, type, moduleName } = job
+  const { name, type, moduleName, ready, runningJobId } = model
 
   return (
     <div>
+      {runningJobId && (
+        <div css={{ display: 'flex', paddingBottom: spacing.normal }}>
+          <FlashMessage variant={FLASH_VARIANTS.PROCESSING}>
+            &quot;{name}&quot; training in progress.{' '}
+            <Link
+              href="/[projectId]/jobs/[jobId]"
+              as={`/${projectId}/jobs/${runningJobId}`}
+              passHref
+            >
+              <a>Check Status</a>
+            </Link>
+          </FlashMessage>
+        </div>
+      )}
+
+      {error && (
+        <div css={{ display: 'flex', paddingBottom: spacing.normal }}>
+          <FlashMessage variant={FLASH_VARIANTS.ERROR}>{error}</FlashMessage>
+        </div>
+      )}
+
       <ul
         css={{
           margin: 0,
@@ -37,6 +74,50 @@ const ModelDetails = () => {
           <strong>Module Name:</strong> {moduleName}
         </li>
       </ul>
+
+      <div>
+        <ButtonGroup>
+          <Button
+            variant={BUTTON_VARIANTS.SECONDARY}
+            onClick={() => onTrain({ apply: false, projectId, setError })}
+            isDisabled={ready}
+          >
+            Train
+          </Button>
+
+          <Button
+            type="submit"
+            variant={BUTTON_VARIANTS.SECONDARY}
+            onClick={() => onTrain({ apply: true, projectId, setError })}
+            isDisabled={ready}
+          >
+            Train &amp; Apply
+          </Button>
+
+          <Button
+            type="submit"
+            variant={BUTTON_VARIANTS.SECONDARY}
+            onClick={() => {
+              setDeleteModalOpen(true)
+            }}
+            isDisabled={false}
+          >
+            Delete
+          </Button>
+
+          {isDeleteModalOpen && (
+            <Modal
+              title="Delete Model"
+              message="Deleting this model cannot be undone."
+              action="Delete Permanently"
+              onCancel={() => {
+                setDeleteModalOpen(false)
+              }}
+              onConfirm={onDelete({ setDeleteModalOpen, projectId, modelId })}
+            />
+          )}
+        </ButtonGroup>
+      </div>
     </div>
   )
 }
---
+++ applications/wallet/frontend/src/Model/helpers.js
@@ -0,0 +1,48 @@
+import { mutate } from 'swr'
+import { Router } from 'next/router'
+
+import { fetcher } from '../Fetch/helpers'
+
+export const onTrain = async ({ apply, projectId, setError }) => {
+  try {
+    setError('')
+
+    const { jobId } = await fetcher(
+      `/api/v1/projects/${projectId}/faces/train/`,
+      {
+        body: JSON.stringify({ apply }),
+        method: 'POST',
+      },
+    )
+
+    mutate(
+      `/api/v1/projects/${projectId}/faces/status/`,
+      {
+        unappliedChanges: false,
+        jobId,
+      },
+      false,
+    )
+  } catch (error) {
+    setError('Something went wrong. Please try again.')
+  }
+}
+
+export const onDelete = ({
+  setDeleteModalOpen,
+  projectId,
+  modelId,
+}) => async () => {
+  setDeleteModalOpen(false)
+
+  await fetcher(`/api/v1/projects/${projectId}/models/${modelId}/`, {
+    method: 'DELETE',
+  })
+
+  await mutate(`/api/v1/projects/${projectId}/models/`)
+
+  Router.push(
+    '/[projectId]/models?action=delete-model-success',
+    `/${projectId}/models?action=delete-model-success`,
+  )
+}
