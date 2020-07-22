bb48f0a792f721d175689085d862d40e872e4638
Danny Tiesling <danny@zorroa.com>
2020-07-22 16:06:58 +0000

    Removes the users and groups endpoints from Wallet. Moves agreements to hang off the me endpoint.

--- applications/wallet/app/agreements/serializers.py
+++ applications/wallet/app/agreements/serializers.py
@@ -6,9 +6,4 @@ from agreements.models import Agreement
 class AgreementSerializer(serializers.ModelSerializer):
     class Meta:
         model = Agreement
-        fields = ('id', 'user', 'policies_date', 'ip_address', 'created_date', 'modified_date')
-
-    def to_representation(self, instance):
-        self.fields['user'] = serializers.HyperlinkedRelatedField(view_name='user-detail',
-                                                                  read_only=True)
-        return super(AgreementSerializer, self).to_representation(instance)
+        fields = '__all__'
--- applications/wallet/app/projects/tests/test_unit.py
+++ applications/wallet/app/projects/tests/test_unit.py
@@ -1,9 +1,11 @@
 from uuid import uuid4
 
 import pytest
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 from projects.models import Project, Membership
+
+User = get_user_model()
 
 
 @pytest.fixture
