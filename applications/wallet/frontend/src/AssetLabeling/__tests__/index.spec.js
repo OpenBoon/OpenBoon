2472656576af77b74d623d43049b6ae8ecce16ff
timothychin <tim.chin@zorroa.com>
2020-08-11 12:19:42 -0700

    Break up AssetLabeling tests

--- applications/wallet/frontend/src/AssetLabeling/__tests__/index.spec.js
+++ applications/wallet/frontend/src/AssetLabeling/__tests__/index.spec.js
@@ -2,17 +2,14 @@ import TestRenderer, { act } from 'react-test-renderer'
 
 import asset from '../../Asset/__mocks__/asset'
 import models from '../../Models/__mocks__/models'
-import project from '../../Project/__mocks__/project'
 
 import AssetLabeling from '..'
 
-const PROJECT_ID = project.id
-const ASSET_ID = asset.id
-const MODEL_ID = models.results[0].id
-const ALT_MODEL_ID = models.results[2].id
-const EXISTING_ASSET_LABEL = asset.metadata.labels[0].label
+const PROJECT_ID = '76917058-b147-4556-987a-0a0f11e46d9b'
+const ASSET_ID = 'vZgbkqPftuRJ_-Of7mHWDNnJjUpFQs0C'
+const MODEL_ID = '621bf774-89d9-1244-9596-d6df43f1ede5'
 
-jest.mock('../../Combobox', () => 'Combobox')
+jest.mock('../Add', () => 'AssetLabelingAdd')
 
 const noop = () => () => {}
 
@@ -34,10 +31,6 @@ describe('<AssetLabeling />', () => {
 
     expect(component.toJSON()).toMatchSnapshot()
 
-    act(() => {
-      component.root.findByType('Combobox').props.options()
-    })
-
     // Click Accordion header link
     act(() => {
       component.root
@@ -45,123 +38,88 @@ describe('<AssetLabeling />', () => {
         .props.onClick({ stopPropagation: noop })
     })
 
-    // Select Model
+    // Open Asset Label list Accordion
     act(() => {
       component.root
-        .findByProps({ label: 'Model' })
-        .props.onChange({ value: MODEL_ID })
+        .findByType('details')
+        .props.onToggle({ target: { open: true } })
     })
 
     expect(component.toJSON()).toMatchSnapshot()
+  })
 
-    // Input Label
+  it('should render properly with no labels', () => {
+    require('swr').__setMockUseSWRResponse({
+      data: { ...models, metadata: { source: asset.metadata.source } },
+    })
+
+    require('next/router').__setUseRouter({
+      query: { projectId: PROJECT_ID, assetId: ASSET_ID },
+    })
+
+    const component = TestRenderer.create(<AssetLabeling />)
+
     act(() => {
       component.root
-        .findByType('Combobox')
-        .props.onChange({ value: 'Flimflarm' })
+        .findByProps({ 'aria-label': 'Expand Section' })
+        .props.onClick()
     })
 
     expect(component.toJSON()).toMatchSnapshot()
+  })
 
-    // Mock Failure
-    fetch.mockResponseOnce(
-      JSON.stringify({ label: ['I guess you cannot use this label'] }),
-      {
-        status: 400,
-      },
+  it('should edit labels', () => {
+    require('next/router').__setUseRouter({
+      pathname: '/[projectId]/visualizer',
+      query: { projectId: PROJECT_ID, assetId: ASSET_ID },
+    })
+
+    require('swr').__setMockUseSWRResponse({ data: { ...models, ...asset } })
+
+    const component = TestRenderer.create(<AssetLabeling />)
+
+    act(() => {
+      component.root
+        .findByProps({ 'aria-label': 'Expand Section' })
+        .props.onClick()
+    })
+
+    // eslint-disable-next-line no-proto
+    const spy = jest.spyOn(localStorage.__proto__, 'setItem')
+
+    // Open first label's kebab menu
+    act(() => {
+      component.root
+        .findAllByProps({ 'aria-label': 'Toggle Actions Menu' })[0]
+        .props.onClick()
+    })
+
+    // Click Edit Label
+    act(() => {
+      component.root.findByProps({ children: 'Edit Label' }).props.onClick()
+    })
+
+    expect(spy).toHaveBeenCalledWith(
+      `AssetLabelingAdd.${PROJECT_ID}.modelId`,
+      `"${MODEL_ID}"`,
+    )
+    expect(spy).toHaveBeenCalledWith(
+      `AssetLabelingAdd.${PROJECT_ID}.label`,
+      '"Space"',
     )
 
-    // Click Submit
-    await act(async () => {
-      component.root
-        .findByProps({ type: 'submit' })
-        .props.onClick({ preventDefault: noop })
+    spy.mockClear()
+  })
+
+  it('should delete labels', async () => {
+    require('next/router').__setUseRouter({
+      pathname: '/[projectId]/visualizer',
+      query: { projectId: PROJECT_ID, assetId: ASSET_ID },
     })
 
-    // Mock Unknown Failure
-    fetch.mockRejectOnce(null, { status: 500 })
+    require('swr').__setMockUseSWRResponse({ data: { ...models, ...asset } })
 
-    // Click Submit
-    await act(async () => {
-      component.root
-        .findByProps({ type: 'submit' })
-        .props.onClick({ preventDefault: noop })
-    })
-
-    // Mock Success
-    fetch.mockResponseOnce(JSON.stringify({ detail: 'Label Saved' }))
-
-    // Click Submit (updating existing model/label)
-    await act(async () => {
-      component.root
-        .findByProps({ type: 'submit' })
-        .props.onClick({ preventDefault: noop })
-    })
-
-    // Select Model
-    act(() => {
-      component.root
-        .findByProps({ label: 'Model' })
-        .props.onChange({ value: ALT_MODEL_ID })
-    })
-
-    // Input Label
-    act(() => {
-      component.root
-        .findByType('Combobox')
-        .props.onChange({ value: 'Other Flimflarm' })
-    })
-
-    // Mock Success
-    fetch.mockResponseOnce(JSON.stringify({ detail: 'Label Saved' }))
-
-    // Click Submit (adding to new model)
-    await act(async () => {
-      component.root
-        .findByProps({ type: 'submit' })
-        .props.onClick({ preventDefault: noop })
-    })
-
-    // Select Model
-    act(() => {
-      component.root
-        .findByProps({ label: 'Model' })
-        .props.onChange({ value: MODEL_ID })
-    })
-
-    // Input Label
-    act(() => {
-      component.root.findByType('Combobox').props.onChange({ value: '' })
-    })
-
-    // Mock Success
-    fetch.mockResponseOnce(JSON.stringify({ detail: 'Label Deleted' }))
-
-    // Click Submit (with empty label value to delete label)
-    await act(async () => {
-      component.root
-        .findByProps({ type: 'submit' })
-        .props.onClick({ preventDefault: noop })
-    })
-
-    // Edit Model/Label fields
-    act(() => {
-      component.root
-        .findByProps({ label: 'Model' })
-        .props.onChange({ value: ALT_MODEL_ID })
-      component.root
-        .findByType('Combobox')
-        .props.onChange({ value: 'FlamFlirm' })
-    })
-
-    expect(component.toJSON()).toMatchSnapshot()
-
-    // Clear Model/Label fields
-    act(() => {
-      component.root.findByProps({ children: 'Cancel' }).props.onClick()
-    })
-
-    expect(component.toJSON()).toMatchSnapshot()
+    const component = TestRenderer.create(<AssetLabeling />)
 
     // Open first label's kebab menu
     act(() => {
@@ -232,85 +190,14 @@ describe('<AssetLabeling />', () => {
       component.root.findByProps({ title: 'Delete Label' }).props.onConfirm()
     })
 
-    expect(component.toJSON()).toMatchSnapshot()
-
-    expect(fetch.mock.calls.length).toEqual(7)
-
-    // Call from submitting label for used model
-    expect(fetch.mock.calls[0][0]).toEqual(
-      `/api/v1/projects/${PROJECT_ID}/models/${MODEL_ID}/update_labels/`,
-    )
-
-    expect(fetch.mock.calls[0][1]).toEqual({
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json;charset=UTF-8',
-        'X-CSRFToken': 'CSRF_TOKEN',
-      },
-      body: JSON.stringify({
-        removeLabels: [
-          {
-            assetId: ASSET_ID,
-            label: EXISTING_ASSET_LABEL,
-          },
-        ],
-        addLabels: [
-          {
-            assetId: ASSET_ID,
-            label: 'Flimflarm',
-          },
-        ],
-      }),
-    })
-
-    // Call from submitting label for un-used model
-    expect(fetch.mock.calls[3][0]).toEqual(
-      `/api/v1/projects/${PROJECT_ID}/models/${ALT_MODEL_ID}/add_labels/`,
-    )
-
-    expect(fetch.mock.calls[3][1]).toEqual({
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json;charset=UTF-8',
-        'X-CSRFToken': 'CSRF_TOKEN',
-      },
-      body: JSON.stringify({
-        addLabels: [
-          {
-            assetId: ASSET_ID,
-            label: 'Other Flimflarm',
-          },
-        ],
-      }),
-    })
-
-    // Call from submitting empty label value
-    expect(fetch.mock.calls[5][0]).toEqual(
-      `/api/v1/projects/${PROJECT_ID}/models/${MODEL_ID}/delete_labels/`,
-    )
-
-    expect(fetch.mock.calls[5][1]).toEqual({
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json;charset=UTF-8',
-        'X-CSRFToken': 'CSRF_TOKEN',
-      },
-      body: JSON.stringify({
-        removeLabels: [
-          {
-            assetId: ASSET_ID,
-            label: 'Space',
-          },
-        ],
-      }),
-    })
+    expect(fetch.mock.calls.length).toEqual(2)
 
     // Call from Delete Modal
-    expect(fetch.mock.calls[6][0]).toEqual(
+    expect(fetch.mock.calls[1][0]).toEqual(
       `/api/v1/projects/${PROJECT_ID}/models/${MODEL_ID}/delete_labels/`,
     )
 
-    expect(fetch.mock.calls[6][1]).toEqual({
+    expect(fetch.mock.calls[1][1]).toEqual({
       method: 'DELETE',
       headers: {
         'Content-Type': 'application/json;charset=UTF-8',
@@ -325,19 +212,5 @@ describe('<AssetLabeling />', () => {
         ],
       }),
     })
-  })
-
-  it('should render properly with no labels', () => {
-    require('swr').__setMockUseSWRResponse({
-      data: { ...models, metadata: { source: asset.metadata.source } },
-    })
-
-    require('next/router').__setUseRouter({
-      query: { projectId: PROJECT_ID, assetId: ASSET_ID },
-    })
-
-    const component = TestRenderer.create(<AssetLabeling />)
-
-    expect(component.toJSON()).toMatchSnapshot()
   })
 })
