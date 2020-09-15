0fb104d7c2c5dd899c67463ee2b61d652ee211a7
Damien Varron <damien@varron.fr>
2020-09-15 23:33:27 +0000

    WAL-831: make Playhead draggable

--- applications/wallet/frontend/src/Timeline/Playhead.js
+++ applications/wallet/frontend/src/Timeline/Playhead.js
@@ -1,9 +1,15 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 import { useRef, useEffect } from 'react'
 import PropTypes from 'prop-types'
 
 import { colors, constants } from '../Styles'
 
 import { updatePlayheadPosition, GUIDE_WIDTH } from './helpers'
+
+const DRAG_WIDTH = 10
+
+let originX
+let originLeft
 
 const TimelinePlayhead = ({ videoRef }) => {
   const playheadRef = useRef()
@@ -12,8 +18,8 @@ const TimelinePlayhead = ({ videoRef }) => {
   const video = videoRef.current
   const playhead = playheadRef.current
 
+  /* istanbul ignore next */
   useEffect(() => {
-    /* istanbul ignore next */
     const animate = () => {
       updatePlayheadPosition({ video, playhead })
 
@@ -25,38 +31,70 @@ const TimelinePlayhead = ({ videoRef }) => {
     return () => cancelAnimationFrame(frameRef.current)
   }, [video, playhead])
 
+  /* istanbul ignore next */
+  const handleMouseMove = ({ clientX }) => {
+    const maxPosition = playhead.parentNode.offsetWidth - GUIDE_WIDTH / 2
+
+    const newPosition = Math.min(
+      Math.max(0, originLeft + clientX - originX),
+      maxPosition,
+    )
+
+    const newCurrentTime = (newPosition / maxPosition) * video.duration
+
+    video.currentTime = newCurrentTime
+  }
+
+  /* istanbul ignore next */
+  const handleMouseUp = () => {
+    window.removeEventListener('mousemove', handleMouseMove)
+    window.removeEventListener('mouseup', handleMouseUp)
+  }
+
+  /* istanbul ignore next */
+  const handleMouseDown = ({ clientX }) => {
+    originX = clientX
+    originLeft = playhead.offsetLeft
+
+    window.addEventListener('mousemove', handleMouseMove)
+    window.addEventListener('mouseup', handleMouseUp)
+  }
+
   return (
-    <div>
-      <div css={{ width: constants.timeline.modulesWidth }} />
+    <div
+      ref={playheadRef}
+      onMouseDown={handleMouseDown}
+      css={{
+        userSelect: 'none',
+        cursor: 'col-resize',
+        position: 'absolute',
+        marginTop: 0,
+        top:
+          constants.timeline.rulerRowHeight - constants.timeline.playheadHeight,
+        bottom: 0,
+        marginLeft: -DRAG_WIDTH / 2,
+        width: DRAG_WIDTH,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
       <div
-        ref={playheadRef}
         css={{
-          position: 'absolute',
-          marginTop: 0,
-          top:
-            constants.timeline.rulerRowHeight -
-            constants.timeline.playheadHeight,
-          bottom: 0,
-          left: 0,
+          borderStyle: 'solid',
+          borderWidth: `${constants.timeline.playheadHeight}px ${
+            constants.timeline.playheadWidth / 2
+          }px 0 ${constants.timeline.playheadWidth / 2}px`,
+          borderColor: `${colors.signal.sky.base} transparent transparent transparent`,
+        }}
+      />
+      <div
+        css={{
+          flex: 1,
           width: GUIDE_WIDTH,
           backgroundColor: colors.signal.sky.base,
         }}
-      >
-        <div
-          css={{
-            position: 'absolute',
-            left: -((constants.timeline.playheadWidth - GUIDE_WIDTH) / 2),
-            top: 0,
-            width: '0',
-            height: '0',
-            borderStyle: 'solid',
-            borderWidth: `${constants.timeline.playheadHeight}px ${
-              constants.timeline.playheadWidth / 2
-            }px 0 ${constants.timeline.playheadWidth / 2}px`,
-            borderColor: `${colors.signal.sky.base} transparent transparent transparent`,
-          }}
-        />
-      </div>
+      />
     </div>
   )
 }
