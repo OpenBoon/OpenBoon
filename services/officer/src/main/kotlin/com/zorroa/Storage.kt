62f5fb0b1e50edc1a1ba1e0dd848b90a03a637cb
Iron Araújo <iron.araujo@zorroa.com>
2020-09-22 15:10:46 -0300

    Creating an IOHandler own file

--- services/officer/src/main/kotlin/com/zorroa/Storage.kt
+++ services/officer/src/main/kotlin/com/zorroa/Storage.kt
@@ -4,9 +4,6 @@ import io.minio.MinioClient
 import io.minio.errors.ErrorResponseException
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import java.io.BufferedInputStream
-import java.io.ByteArrayInputStream
-import java.io.ByteArrayOutputStream
 import java.io.InputStream
 
 object StorageManager {
@@ -101,81 +98,3 @@ class MinioStorageClient : StorageClient {
     }
 }
 
-/**
- * A ByteArrayOutputStream which provides an efficient way to
- * obtain a BufferedInputStream without copying the internal byte buffer.
- */
-class ReversibleByteArrayOutputStream(size: Int = 2048) : ByteArrayOutputStream(size) {
-
-    fun toInputStream(): InputStream {
-        return BufferedInputStream(ByteArrayInputStream(buf, 0, count))
-    }
-}
-
-/**
- * Manages render inputs and outputs.
- */
-class IOHandler(val options: RenderRequest) {
-
-    fun writeImage(page: Int, outputStream: ReversibleByteArrayOutputStream) {
-        StorageManager.storageClient().store(
-            getImagePath(page),
-            outputStream.toInputStream(),
-            outputStream.size().toLong(),
-            "image/jpeg"
-        )
-    }
-
-    fun writeMetadata(page: Int, outputStream: ReversibleByteArrayOutputStream) {
-        StorageManager.storageClient().store(
-            getMetadataPath(page),
-            outputStream.toInputStream(),
-            outputStream.size().toLong(),
-            "application/json"
-        )
-    }
-
-    fun getImagePath(page: Int): String {
-        return "$PREFIX/${options.outputDir}/proxy.$page.jpg"
-    }
-
-    fun getMetadataPath(page: Int): String {
-        return "$PREFIX/${options.outputDir}/metadata.$page.json"
-    }
-
-    fun getOutputUri(): String {
-        return "zmlp://${Config.minioBucket.name}/$PREFIX/${options.outputDir}"
-    }
-
-    fun getMetadata(page: Int = 1): InputStream {
-        return StorageManager.storageClient().fetch(getMetadataPath(page))
-    }
-
-    fun getImage(page: Int = 1): InputStream {
-        return StorageManager.storageClient().fetch(getImagePath(page))
-    }
-
-    fun exists(page: Int = 1): Boolean {
-        val path = getMetadataPath(page)
-        logger.info("Checking path: {}", path)
-        return StorageManager.storageClient().exists(path)
-    }
-
-    fun removeImage(page: Int = 1) {
-        StorageManager.storageClient().delete(getImagePath(page))
-    }
-
-    fun removeMetadata(page: Int = 1) {
-        StorageManager.storageClient().delete(getMetadataPath(page))
-    }
-
-    companion object {
-        val logger: Logger = LoggerFactory.getLogger(IOHandler::class.java)
-
-        // The size of the pre-allocated by array for images.
-        val IMG_BUFFER_SIZE = 65536
-
-        // The object path prefix
-        val PREFIX = "tmp-files/officer"
-    }
-}
