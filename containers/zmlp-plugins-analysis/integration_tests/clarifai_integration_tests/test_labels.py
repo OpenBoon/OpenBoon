2a6151654eb45550add76526bfb46d26540c66f2
Ryan Gaspar <ryan@zorroa.com>
2020-10-07 14:51:19 +1300

    color detection, integration+unit tests

--- containers/zmlp-plugins-analysis/integration_tests/clarifai_integration_tests/test_labels.py
+++ containers/zmlp-plugins-analysis/integration_tests/clarifai_integration_tests/test_labels.py
@@ -1,7 +1,6 @@
 # flake8: noqa
 import os
 from unittest.mock import patch
-
 import pytest
 
 from zmlp_analysis.clarifai.labels import *
--- containers/zmlp-plugins-analysis/pylib/zmlp_analysis/clarifai/colors.py
+++ containers/zmlp-plugins-analysis/pylib/zmlp_analysis/clarifai/colors.py
@@ -1,22 +1,67 @@
-from zmlpsdk import AssetProcessor, Argument
+from zmlpsdk import AssetProcessor, Argument, FileTypes
 from zmlpsdk.analysis import LabelDetectionAnalysis
 from zmlpsdk.proxy import get_proxy_level_path
 
 from .util import get_clarifai_app
+
+__all__ = [
+    'ClarifaiColorDetectionProcessor',
+]
 
 models = [
     'color-model'
 ]
 
 
-class ClarifaiColorDetectionProcessor(AssetProcessor):
-    namespace = 'clarifai-color'
+# class ClarifaiColorDetectionProcessor(AssetProcessor):
+#     namespace = 'clarifai-color'
+#
+#     def __init_(self):
+#         super(ClarifaiColorDetectionProcessor, self).__init__()
+#         for model in models:
+#             self.add_arg(Argument(model, "boolean", required=False,
+#                                   toolTip="Enable the {} model".format(model)))
+#         self.clarifai = None
+#
+#     def init(self):
+#         self.clarifai = get_clarifai_app()
+#
+#     def process(self, frame):
+#         asset = frame.asset
+#         p_path = get_proxy_level_path(asset, 1)
+#
+#         for model_name in models:
+#             if self.arg_value(model_name):
+#                 model = getattr(self.clarifai.public_models, model_name.replace("-", "_"))
+#                 response = model.predict_by_filename(p_path)
+#                 labels = response['outputs'][0]['data'].get('colors')
+#                 if not labels:
+#                     continue
+#
+#                 analysis = LabelDetectionAnalysis()
+#                 for label in labels:
+#                     analysis.add_label_and_score(label['w3c']['name'], label['value'],
+#                                                  hex=label['w3c']['hex'])
+#
+#                 asset.add_analysis("-".join([self.namespace, model_name]), analysis)
 
-    def __init_(self):
-        super(ClarifaiColorDetectionProcessor, self).__init__()
-        for model in models:
-            self.add_arg(Argument(model, "boolean", required=False,
-                                  toolTip="Enable the {} model".format(model)))
+
+class AbstractClarifaiProcessor(AssetProcessor):
+    """
+        This base class is used for all Microsoft Computer Vision features.  Subclasses
+        only have to implement the "predict(asset, image) method.
+        """
+
+    file_types = FileTypes.images | FileTypes.documents
+
+    namespace = 'clarifai'
+    model_name = 'general-model'
+
+    def __init__(self, reactor=None):
+        super(AbstractClarifaiProcessor, self).__init__()
+        self.add_arg(Argument('debug', 'bool', default=False))
+        self.reactor = reactor
+
         self.clarifai = None
 
     def init(self):
@@ -26,17 +71,37 @@ class ClarifaiColorDetectionProcessor(AssetProcessor):
         asset = frame.asset
         p_path = get_proxy_level_path(asset, 1)
 
-        for model_name in models:
-            if self.arg_value(model_name):
-                model = getattr(self.clarifai.public_models, model_name.replace("-", "_"))
-                response = model.predict_by_filename(p_path)
-                labels = response['outputs'][0]['data'].get('colors')
-                if not labels:
-                    continue
+        model = getattr(self.clarifai.public_models, self.model_name.replace("-", "_"))
+        response = model.predict_by_filename(p_path)
+        labels = response['outputs'][0]['data'].get('colors')
+        if not labels:
+            return
 
-                analysis = LabelDetectionAnalysis()
-                for label in labels:
-                    analysis.add_label_and_score(label['w3c']['name'], label['value'],
-                                                 hex=label['w3c']['hex'])
+        analysis = LabelDetectionAnalysis()
+        for label in labels:
+            analysis.add_label_and_score(
+                label['w3c']['name'],
+                label['value'],
+                hex=label['w3c']['hex']
+            )
+        asset.add_analysis("-".join([self.namespace, self.model_name]), analysis)
 
-                asset.add_analysis("-".join([self.namespace, model_name]), analysis)
+    def emit_status(self, msg):
+        """
+        Emit a status back to the Archivist.
+
+        Args:
+            msg (str): The message to emit.
+
+        """
+        if not self.reactor:
+            return
+        self.reactor.emit_status(msg)
+
+
+class ClarifaiColorDetectionProcessor(AbstractClarifaiProcessor):
+    """ Clarifai label detection"""
+
+    def __init__(self):
+        super(ClarifaiColorDetectionProcessor, self).__init__()
+        self.model_name = 'color-model'
