bf70a073922b8b962132b1be30e19204d4640728
Matt <mrc@zorroa.com>
2020-10-08 09:45:45 -0400

    Doc fix

--- containers/zmlp-plugins-analysis/pylib/zmlp_analysis/azure/util.py
+++ containers/zmlp-plugins-analysis/pylib/zmlp_analysis/azure/util.py
@@ -8,7 +8,7 @@ def get_zvi_azure_cv_client():
     Return an Azure Computer Vision client configured for rekognition with ZVI credentials.
 
     Returns:
-        boto3.client: A boto3 client for recognition
+       ComputerVisionClient: an Azure ComputerVisionClient
     """
     key = os.environ.get('ZORROA_AZURE_KEY')
     if not key:
