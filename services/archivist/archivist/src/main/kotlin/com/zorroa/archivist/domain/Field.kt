7326bf372627645eb9f3b8ad80c1e8d3a35b455c
Matt <mrc@zorroa.com>
2020-10-22 15:04:51 -0400

    Added ability to add custom fields to mapping.  Fields carry over in migrations.

---
+++ services/archivist/archivist/src/main/kotlin/com/zorroa/archivist/domain/Field.kt
@@ -0,0 +1,71 @@
+package com.zorroa.archivist.domain
+
+import io.swagger.annotations.ApiModel
+import io.swagger.annotations.ApiModelProperty
+import java.util.UUID
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
+
+class FieldSpec(
+
+    @ApiModelProperty("The ES field name")
+    val name: String,
+
+    @ApiModelProperty("The ES data type.")
+    val type: String,
+)
+
+@Entity
+@Table(name = "field")
+@ApiModel("Field", description = "A ZMLP Field")
+class Field(
+
+    @Id
+    @Column(name = "pk_field")
+    @ApiModelProperty("The Unique ID of the field.")
+    val id: UUID,
+
+    @Column(name = "pk_project")
+    @ApiModelProperty("The Unique ID of the project.")
+    val projectId: UUID,
+
+    @ApiModelProperty("The ES field name")
+    @Column(name = "str_name")
+    val name: String,
+
+    @ApiModelProperty("The ES data type.")
+    @Column(name = "str_type")
+    val type: String,
+
+    @Column(name = "time_created")
+    @ApiModelProperty("The time the Field was created..")
+    val timeCreated: Long,
+
+    @Column(name = "time_modified")
+    @ApiModelProperty("The last time the Field was modified.")
+    val timeModified: Long,
+
+    @Column(name = "actor_created")
+    @ApiModelProperty("The actor which created this Field")
+    val actorCreated: String,
+
+    @Column(name = "actor_modified")
+    @ApiModelProperty("The actor that last made the last modification the Field.")
+    val actorModified: String,
+) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Field) return false
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+}
