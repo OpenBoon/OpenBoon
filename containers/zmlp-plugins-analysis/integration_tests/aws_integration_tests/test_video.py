1a72ffad482d1d915e4ba4852ee55eca2021c6b2
Ryan Gaspar <ryan@zorroa.com>
2020-12-04 09:30:31 +1300

    fixed integration tests

--- containers/zmlp-plugins-analysis/integration_tests/aws_integration_tests/test_video.py
+++ containers/zmlp-plugins-analysis/integration_tests/aws_integration_tests/test_video.py
@@ -5,7 +5,7 @@ from unittest.mock import patch
 
 import pytest
 
-from zmlp_analysis.aws.videos import video
+from zmlp_analysis.aws import videos
 from zmlpsdk import Frame, file_storage
 from zmlpsdk.testing import PluginUnitTestCase, TestAsset, zorroa_test_path, get_mock_stored_file
 
@@ -39,9 +39,10 @@ class AmazonVideoProcessorTestCase(PluginUnitTestCase):
 
         os.environ['ZORROA_AWS_KEY'] = access_key_id
         os.environ['ZORROA_AWS_SECRET'] = secret_access_key
-        os.environ['ZORROA_AWS_BUCKET'] = 'zorroa-integration-tests'
+        os.environ['ZORROA_AWS_BUCKET'] = 'rgz-test'  # 'zorroa-integration-tests'
         os.environ['ZORROA_AWS_REGION'] = 'us-east-2'
         os.environ['ZMLP_PROJECT_ID'] = '00000000-0000-0000-0000-000000000001'
+        os.environ['PATH'] += ':/usr/local/bin'
 
     def tearDown(self):
         del os.environ['ZORROA_AWS_KEY']
@@ -50,129 +51,129 @@ class AmazonVideoProcessorTestCase(PluginUnitTestCase):
         del os.environ['ZORROA_AWS_REGION']
         del os.environ['ZMLP_PROJECT_ID']
 
-    @patch("zmlp_analysis.aws.videos.video.video.save_timeline", return_value={})
+    @patch("zmlp_analysis.aws.videos.video.save_timeline", return_value={})
     @patch.object(file_storage.assets, 'store_blob')
     @patch.object(file_storage.assets, 'store_file')
-    @patch('zmlp_analysis.aws.videos.video.proxy.get_video_proxy')
+    @patch('zmlp_analysis.aws.videos.proxy.get_video_proxy')
     def test_process_label_detection(self, get_prx_patch, store_patch, store_blob_patch, _):
         video_path = zorroa_test_path(MUSTANG)
         get_prx_patch.return_value = zorroa_test_path(MUSTANG)
         store_patch.return_value = get_mock_stored_file()
         store_blob_patch.return_value = get_mock_stored_file()
 
-        processor = self.init_processor(video.LabelVideoDetectProcessor())
+        processor = self.init_processor(videos.LabelVideoDetectProcessor())
         asset = TestAsset(video_path)
         asset.set_attr('media.length', MEDIA_LENGTH)
         frame = Frame(self.asset)
         processor.process(frame)
 
-    @patch("zmlp_analysis.aws.videos.video.video.save_timeline", return_value={})
+    @patch("zmlp_analysis.aws.videos.video.save_timeline", return_value={})
     @patch.object(file_storage.assets, 'store_blob')
     @patch.object(file_storage.assets, 'store_file')
-    @patch('zmlp_analysis.aws.videos.video.proxy.get_video_proxy')
+    @patch('zmlp_analysis.aws.videos.proxy.get_video_proxy')
     def test_process_text_detection(self, get_prx_patch, store_patch, store_blob_patch, _):
         video_path = zorroa_test_path(TED_TALK)
         get_prx_patch.return_value = zorroa_test_path(TED_TALK)
         store_patch.return_value = get_mock_stored_file()
         store_blob_patch.return_value = get_mock_stored_file()
 
-        processor = self.init_processor(video.TextVideoDetectProcessor())
+        processor = self.init_processor(videos.TextVideoDetectProcessor())
         asset = TestAsset(video_path)
         asset.set_attr('media.length', MEDIA_LENGTH)
         frame = Frame(self.asset)
         processor.process(frame)
 
-    @patch("zmlp_analysis.aws.videos.video.video.save_timeline", return_value={})
+    @patch("zmlp_analysis.aws.videos.video.save_timeline", return_value={})
     @patch.object(file_storage.assets, 'store_blob')
     @patch.object(file_storage.assets, 'store_file')
-    @patch('zmlp_analysis.aws.videos.video.proxy.get_video_proxy')
+    @patch('zmlp_analysis.aws.videos.proxy.get_video_proxy')
     def test_process_face_detection(self, get_prx_patch, store_patch, store_blob_patch, _):
         video_path = zorroa_test_path(TED_TALK)
         get_prx_patch.return_value = zorroa_test_path(TED_TALK)
         store_patch.return_value = get_mock_stored_file()
         store_blob_patch.return_value = get_mock_stored_file()
 
-        processor = self.init_processor(video.FaceVideoDetectProcessor())
+        processor = self.init_processor(videos.FaceVideoDetectProcessor())
         asset = TestAsset(video_path)
         asset.set_attr('media.length', MEDIA_LENGTH)
         frame = Frame(self.asset)
         processor.process(frame)
 
-    @patch("zmlp_analysis.aws.videos.video.video.save_timeline", return_value={})
+    @patch("zmlp_analysis.aws.videos.video.save_timeline", return_value={})
     @patch.object(file_storage.assets, 'store_blob')
     @patch.object(file_storage.assets, 'store_file')
-    @patch('zmlp_analysis.aws.videos.video.proxy.get_video_proxy')
+    @patch('zmlp_analysis.aws.videos.proxy.get_video_proxy')
     def test_process_unsafe_detection(self, get_prx_patch, store_patch, store_blob_patch, _):
         video_path = zorroa_test_path(MODEL)
         get_prx_patch.return_value = zorroa_test_path(MODEL)
         store_patch.return_value = get_mock_stored_file()
         store_blob_patch.return_value = get_mock_stored_file()
 
-        processor = self.init_processor(video.UnsafeVideoDetectProcessor())
+        processor = self.init_processor(videos.UnsafeVideoDetectProcessor())
         asset = TestAsset(video_path)
         asset.set_attr('media.length', MEDIA_LENGTH)
         frame = Frame(self.asset)
         processor.process(frame)
 
-    @patch("zmlp_analysis.aws.videos.video.video.save_timeline", return_value={})
+    @patch("zmlp_analysis.aws.videos.video.save_timeline", return_value={})
     @patch.object(file_storage.assets, 'store_blob')
     @patch.object(file_storage.assets, 'store_file')
-    @patch('zmlp_analysis.aws.videos.video.proxy.get_video_proxy')
+    @patch('zmlp_analysis.aws.videos.proxy.get_video_proxy')
     def test_process_celebrity_detection(self, get_prx_patch, store_patch, store_blob_patch, _):
         video_path = zorroa_test_path(BORIS_JOHNSON)
         get_prx_patch.return_value = zorroa_test_path(BORIS_JOHNSON)
         store_patch.return_value = get_mock_stored_file()
         store_blob_patch.return_value = get_mock_stored_file()
 
-        processor = self.init_processor(video.CelebrityVideoDetectProcessor())
+        processor = self.init_processor(videos.CelebrityVideoDetectProcessor())
         asset = TestAsset(video_path)
         asset.set_attr('media.length', MEDIA_LENGTH)
         frame = Frame(self.asset)
         processor.process(frame)
 
-    @patch("zmlp_analysis.aws.videos.video.video.save_timeline", return_value={})
+    @patch("zmlp_analysis.aws.videos.video.save_timeline", return_value={})
     @patch.object(file_storage.assets, 'store_blob')
     @patch.object(file_storage.assets, 'store_file')
-    @patch('zmlp_analysis.aws.videos.video.proxy.get_video_proxy')
+    @patch('zmlp_analysis.aws.videos.proxy.get_video_proxy')
     def test_process_person_tracking(self, get_prx_patch, store_patch, store_blob_patch, _):
         video_path = zorroa_test_path(TED_TALK)
         get_prx_patch.return_value = zorroa_test_path(TED_TALK)
         store_patch.return_value = get_mock_stored_file()
         store_blob_patch.return_value = get_mock_stored_file()
 
-        processor = self.init_processor(video.PeoplePathingVideoDetectProcessor())
+        processor = self.init_processor(videos.PeoplePathingVideoDetectProcessor())
         asset = TestAsset(video_path)
         asset.set_attr('media.length', MEDIA_LENGTH)
         frame = Frame(self.asset)
         processor.process(frame)
 
-    @patch("zmlp_analysis.aws.videos.video.video.save_timeline", return_value={})
+    @patch("zmlp_analysis.aws.videos.video.save_timeline", return_value={})
     @patch.object(file_storage.assets, 'store_blob')
     @patch.object(file_storage.assets, 'store_file')
-    @patch('zmlp_analysis.aws.videos.video.proxy.get_video_proxy')
+    @patch('zmlp_analysis.aws.videos.proxy.get_video_proxy')
     def test_process_black_frame_detection(self, get_prx_patch, store_patch, store_blob_patch, _):
         video_path = zorroa_test_path(MUSTANG)
         get_prx_patch.return_value = zorroa_test_path(MUSTANG)
         store_patch.return_value = get_mock_stored_file()
         store_blob_patch.return_value = get_mock_stored_file()
 
-        processor = self.init_processor(video.BlackFramesVideoDetectProcessor())
+        processor = self.init_processor(videos.BlackFramesVideoDetectProcessor())
         asset = TestAsset(video_path)
         asset.set_attr('media.length', MEDIA_LENGTH)
         frame = Frame(self.asset)
         processor.process(frame)
 
-    @patch("zmlp_analysis.aws.videos.video.video.save_timeline", return_value={})
+    @patch("zmlp_analysis.aws.videos.video.save_timeline", return_value={})
     @patch.object(file_storage.assets, 'store_blob')
     @patch.object(file_storage.assets, 'store_file')
-    @patch('zmlp_analysis.aws.videos.video.proxy.get_video_proxy')
+    @patch('zmlp_analysis.aws.videos.proxy.get_video_proxy')
     def test_process_end_credits_detection(self, get_prx_patch, store_patch, store_blob_patch, _):
         video_path = zorroa_test_path(VID_MP4)
         get_prx_patch.return_value = zorroa_test_path(VID_MP4)
         store_patch.return_value = get_mock_stored_file()
         store_blob_patch.return_value = get_mock_stored_file()
 
-        processor = self.init_processor(video.EndCreditsVideoDetectProcessor())
+        processor = self.init_processor(videos.EndCreditsVideoDetectProcessor())
         asset = TestAsset(video_path)
         asset.set_attr('media.length', MEDIA_LENGTH)
         frame = Frame(self.asset)
