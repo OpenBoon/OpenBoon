6cfab375ad8dcb6051889b1ccdde75c4cc0a87a1
Matt <mrc@zorroa.com>
2020-12-09 15:24:57 -0500

    Really fix te DS job launch test.

--- services/archivist/archivist/src/test/kotlin/com/zorroa/archivist/service/JobLauncherServiceTests.kt
+++ services/archivist/archivist/src/test/kotlin/com/zorroa/archivist/service/JobLauncherServiceTests.kt
@@ -214,7 +214,7 @@ class JobLauncherServiceTests : AbstractTest() {
         val tasks = jobService.getTasks(launchJob.id)
         val script = jobService.getZpsScript(tasks.first().id)
 
-        assertEquals("zmlp_core.core.generators.DeleteBySearchGenerator", script.generate!![0]!!.className)
+        assertEquals("zmlp_core.core.processors.DeleteBySearchProcessor", script.execute!![0]!!.className)
         assertEquals(ds.id.toString(), script.execute?.get(0)?.args?.get("dataSourceId"))
     }
 }
