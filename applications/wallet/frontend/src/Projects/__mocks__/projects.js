5d8db47122396328ca6729d3404ce48107c72b78
Damien Varron <damien@varron.fr>
2021-01-12 11:54:07 -0800

    WAL-918: add Account Projects sorting

--- applications/wallet/frontend/src/Projects/__mocks__/projects.js
+++ applications/wallet/frontend/src/Projects/__mocks__/projects.js
@@ -6,18 +6,26 @@ const projects = {
     {
       id: '76917058-b147-4556-987a-0a0f11e46d9b',
       name: 'qwerty',
+      createdDate: '2021-01-12T18:18:53.675411Z',
+      modifiedDate: '2021-01-12T18:18:53.675411Z',
     },
     {
       id: 'e93cbadb-e5ae-4598-8395-4cf5b30c0e94',
       name: 'azerty',
+      createdDate: '2021-01-11T18:18:53.675411Z',
+      modifiedDate: '2021-01-11T18:18:53.675411Z',
     },
     {
       id: 'e8372289-f6d3-4248-91e7-f3f958867a45',
       name: 'dvorak',
+      createdDate: '2021-01-13T18:18:53.675411Z',
+      modifiedDate: '2021-01-13T18:18:53.675411Z',
     },
     {
       id: 'a36195cc-19d7-4358-b0f8-0685b6d04ce5',
       name: 'dvorak',
+      createdDate: '2021-01-12T18:18:53.675411Z',
+      modifiedDate: '2021-01-12T18:18:53.675411Z',
     },
   ],
 }
