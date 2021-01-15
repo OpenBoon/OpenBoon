6ab133d6c6f7711a92ed428a05599612960a2ee9
Juan Buhler <jbuhler@zorroa.com>
2021-01-14 16:43:55 -0800

    first push, pytorch training and evaluation

---
+++ containers/zmlp-plugins-analysis/pylib/zmlp_analysis/custom/tests/test_pytorch.py
@@ -0,0 +1,61 @@
+import os
+import shutil
+from unittest.mock import patch
+
+from zmlp.app import ModelApp
+from zmlp.entity import Model
+from zmlp_analysis.custom.pytorch import PytorchImageClassifier
+from zmlpsdk.base import Frame
+from zmlpsdk.storage import file_storage
+from zmlpsdk.testing import PluginUnitTestCase, TestAsset, zorroa_test_path, get_prediction_labels
+
+
+class PytorchModelImageClassifierTests(PluginUnitTestCase):
+    model_id = "model-id-34567"
+    base_dir = os.path.dirname(__file__)
+
+    def setUp(self):
+        try:
+            shutil.rmtree("/tmp/model-cache/models_model-id-34567_foo_bar")
+        except FileNotFoundError:
+            print("Didn't clear out model cache, this is ok.")
+
+    @patch.object(ModelApp, "get_model")
+    @patch.object(file_storage.projects, "localize_file")
+    @patch("zmlp_analysis.custom.pytorch.get_proxy_level_path")
+    def test_predict(self, proxy_patch, file_patch, model_patch):
+        name = "pytorch_ants_and_bees"
+
+        model_file = zorroa_test_path("training/{}.zip".format(name))
+        file_patch.return_value = model_file
+        model_patch.return_value = Model(
+            {
+                "id": self.model_id,
+                "type": "PYTORCH_IMAGE_CLASSIFIER",
+                "fileId": "models/{}/foo/bar".format(self.model_id),
+                "name": name,
+                "moduleName": name
+            }
+        )
+
+        args = {
+            "model_id": self.model_id,
+            "input_size": (321, 321)
+        }
+
+        flower_paths = [
+            zorroa_test_path("training/test_dsy.jpg"),
+            zorroa_test_path("training/test_rose.png")
+        ]
+        for paths in flower_paths:
+            proxy_patch.return_value = paths
+            frame = Frame(TestAsset(paths))
+
+            processor = self.init_processor(
+                PytorchImageClassifier(), args
+            )
+            processor.process(frame)
+            analysis = frame.asset.get_analysis(name)
+            assert 'bees' in get_prediction_labels(analysis)
+            assert analysis['count'] == 2
+            assert 'labels' == analysis['type']
