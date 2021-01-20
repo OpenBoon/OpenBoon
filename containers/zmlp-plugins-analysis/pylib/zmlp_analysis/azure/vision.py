4fa43188706ad2215c119d353e4f42eefffdf00a
Matt <mrc@zorroa.com>
2021-01-20 11:14:06 -0500

    Use different loggers for logging quota backoffs.

--- containers/zmlp-plugins-analysis/pylib/zmlp_analysis/azure/vision.py
+++ containers/zmlp-plugins-analysis/pylib/zmlp_analysis/azure/vision.py
@@ -6,12 +6,11 @@ from PIL import Image
 from azure.cognitiveservices.vision.computervision.models import \
     VisualFeatureTypes, OperationStatusCodes, ComputerVisionErrorException
 
-from zmlp_analysis.utils.logs import log_backoff_exception
 from zmlpsdk import Argument, AssetProcessor, FileTypes
 from zmlpsdk import file_storage
 from zmlpsdk.analysis import LabelDetectionAnalysis, ContentDetectionAnalysis
 from zmlpsdk.proxy import get_proxy_level_path, get_proxy_level, calculate_normalized_bbox
-from .util import get_zvi_azure_cv_client
+from .util import get_zvi_azure_cv_client, log_backoff_exception
 
 logger = logging.getLogger(__name__)
 
--- containers/zmlp-plugins-analysis/pylib/zmlp_analysis/clarifai/video/labels.py
+++ containers/zmlp-plugins-analysis/pylib/zmlp_analysis/clarifai/video/labels.py
@@ -3,10 +3,9 @@ from clarifai.errors import ApiError
 
 from zmlpsdk import AssetProcessor, Argument, FileTypes, file_storage, proxy, clips, video
 from zmlpsdk.analysis import LabelDetectionAnalysis
-from zmlp_analysis.utils.logs import log_backoff_exception
 from zmlp_analysis.utils.prechecks import Prechecks
 from zmlp_analysis.clarifai.images import labels as labels_images
-from zmlp_analysis.clarifai.util import not_a_quota_exception, model_map
+from zmlp_analysis.clarifai.util import not_a_quota_exception, model_map, log_backoff_exception
 
 __all__ = [
     'ClarifaiVideoLabelDetectionProcessor',
