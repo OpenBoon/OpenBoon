fb67bffb624481d02d8f2406b63935c3ea7f542e
Zach Schipono <zach@zorroa.com>
2021-01-21 11:56:35 -0800

    Minor tweaks to settings file, testing ci process.

--- services/metrics/app/metrics/settings/__init__.py
+++ services/metrics/app/metrics/settings/__init__.py
@@ -93,25 +93,18 @@ WSGI_APPLICATION = 'metrics.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+# Postgres is required for the type of aggregations that we run
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'metrics',
+        'USER': os.environ.get('PG_DB_USER', 'metrics'),
+        'PASSWORD': os.environ.get('PG_DB_PASSWORD', 'admin'),
+        'HOST': os.environ.get('PG_DB_HOST', '127.0.0.1'),
+        'PORT': os.environ.get('PG_DB_PORT', '5432'),
+    }
+}
 
-if os.environ.get('DB_BACKEND') == 'postgres':
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'metrics',
-            'USER': 'metrics',
-            'PASSWORD': os.environ['PG_DB_PASSWORD'],
-            'HOST': os.environ.get('PG_DB_HOST', '127.0.0.1'),
-            'PORT': os.environ.get('PG_DB_PORT', '5432'),
-        }
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
 
 
 # Password validation
