a8fc9dae0eeb7d4b61c930affed8761a79d2e07b
Ryan Gaspar <ryan@zorroa.com>
2021-01-27 10:55:44 +1300

    aws integration tests

--- containers/zmlp-plugins-analysis/integration_tests/aws_integration_tests/test_video.py
+++ containers/zmlp-plugins-analysis/integration_tests/aws_integration_tests/test_video.py
@@ -7,7 +7,8 @@ import pytest
 
 from zmlp_analysis.aws import video
 from zmlpsdk import Frame, file_storage
-from zmlpsdk.testing import PluginUnitTestCase, TestAsset, zorroa_test_path, get_mock_stored_file
+from zmlpsdk.testing import PluginUnitTestCase, TestAsset, zorroa_test_path, \
+    get_mock_stored_file, get_prediction_labels
 
 VID_MP4 = "video/credits.mov"
 MUSTANG = "video/mustang.mp4"
@@ -37,11 +38,18 @@ class AmazonVideoProcessorTestCase(PluginUnitTestCase):
         with open('rekog_role_arn', 'r') as f:
             os.environ['ZORROA_AWS_ML_USER_ROLE_ARN'] = f.read()
 
+        with open('rekog_topic_sub_queue', 'r') as f:
+            topic_arn, sqs_arn, sqs_url = f.readlines()
+
         os.environ['ZORROA_AWS_KEY'] = access_key_id
         os.environ['ZORROA_AWS_SECRET'] = secret_access_key
-        os.environ['ZORROA_AWS_BUCKET'] = 'zorroa-integration-tests'
+        os.environ['ZORROA_AWS_BUCKET'] = 'rgz-test'  # 'zorroa-integration-tests'
         os.environ['ZORROA_AWS_REGION'] = 'us-east-2'
         os.environ['ZMLP_PROJECT_ID'] = '00000000-0000-0000-0000-000000000001'
+        os.environ['ZORROA_AWS_ML_USER_SQS_ARN'] = sqs_arn.strip()
+        os.environ['ZORROA_AWS_ML_USER_SQS_URL'] = sqs_url.strip()
+        os.environ['ZORROA_AWS_ML_USER_SNS_TOPIC_ARN'] = topic_arn.strip()
+        os.environ['PATH'] += ':/usr/local/bin'
 
     def tearDown(self):
         del os.environ['ZORROA_AWS_KEY']
@@ -50,13 +58,13 @@ class AmazonVideoProcessorTestCase(PluginUnitTestCase):
         del os.environ['ZORROA_AWS_REGION']
         del os.environ['ZMLP_PROJECT_ID']
 
-    @patch("zmlp_analysis.aws.video.all.save_timeline", return_value={})
+    @patch("zmlp_analysis.aws.video.all.video.save_timeline", return_value={})
     @patch.object(file_storage.assets, 'store_blob')
     @patch.object(file_storage.assets, 'store_file')
     @patch('zmlp_analysis.aws.video.all.proxy.get_video_proxy')
     def test_process_label_detection(self, get_prx_patch, store_patch, store_blob_patch, _):
-        video_path = zorroa_test_path(MUSTANG)
-        get_prx_patch.return_value = zorroa_test_path(MUSTANG)
+        video_path = zorroa_test_path(VID_MP4)
+        get_prx_patch.return_value = zorroa_test_path(VID_MP4)
         store_patch.return_value = get_mock_stored_file()
         store_blob_patch.return_value = get_mock_stored_file()
 
@@ -66,7 +74,12 @@ class AmazonVideoProcessorTestCase(PluginUnitTestCase):
         frame = Frame(self.asset)
         processor.process(frame)
 
-    @patch("zmlp_analysis.aws.video.all.save_timeline", return_value={})
+        analysis = frame.asset.get_analysis('aws-label-detection')
+        preds = get_prediction_labels(analysis)
+        assert 'Word' in preds
+        assert analysis['count'] > 10
+
+    @patch("zmlp_analysis.aws.video.all.video.save_timeline", return_value={})
     @patch.object(file_storage.assets, 'store_blob')
     @patch.object(file_storage.assets, 'store_file')
     @patch('zmlp_analysis.aws.video.all.proxy.get_video_proxy')
@@ -82,7 +95,12 @@ class AmazonVideoProcessorTestCase(PluginUnitTestCase):
         frame = Frame(self.asset)
         processor.process(frame)
 
-    @patch("zmlp_analysis.aws.video.all.save_timeline", return_value={})
+        analysis = frame.asset.get_analysis('aws-text-detection')
+        preds = get_prediction_labels(analysis)
+        assert 'have' in preds
+        assert analysis['count'] >= 20
+
+    @patch("zmlp_analysis.aws.video.all.video.save_timeline", return_value={})
     @patch.object(file_storage.assets, 'store_blob')
     @patch.object(file_storage.assets, 'store_file')
     @patch('zmlp_analysis.aws.video.all.proxy.get_video_proxy')
@@ -98,7 +116,12 @@ class AmazonVideoProcessorTestCase(PluginUnitTestCase):
         frame = Frame(self.asset)
         processor.process(frame)
 
-    @patch("zmlp_analysis.aws.video.all.save_timeline", return_value={})
+        analysis = frame.asset.get_analysis('aws-face-detection')
+        preds = get_prediction_labels(analysis)
+        assert 'face70' in preds
+        assert analysis['count'] >= 30
+
+    @patch("zmlp_analysis.aws.video.all.video.save_timeline", return_value={})
     @patch.object(file_storage.assets, 'store_blob')
     @patch.object(file_storage.assets, 'store_file')
     @patch('zmlp_analysis.aws.video.all.proxy.get_video_proxy')
@@ -114,7 +137,12 @@ class AmazonVideoProcessorTestCase(PluginUnitTestCase):
         frame = Frame(self.asset)
         processor.process(frame)
 
-    @patch("zmlp_analysis.aws.video.all.save_timeline", return_value={})
+        analysis = frame.asset.get_analysis('aws-unsafe-detection')
+        preds = get_prediction_labels(analysis)
+        assert 'Suggestive' in preds
+        assert analysis['count'] == 3
+
+    @patch("zmlp_analysis.aws.video.all.video.save_timeline", return_value={})
     @patch.object(file_storage.assets, 'store_blob')
     @patch.object(file_storage.assets, 'store_file')
     @patch('zmlp_analysis.aws.video.all.proxy.get_video_proxy')
@@ -130,13 +158,18 @@ class AmazonVideoProcessorTestCase(PluginUnitTestCase):
         frame = Frame(self.asset)
         processor.process(frame)
 
-    @patch("zmlp_analysis.aws.video.all.save_timeline", return_value={})
+        analysis = frame.asset.get_analysis('aws-celebrity-detection')
+        preds = get_prediction_labels(analysis)
+        assert 'Boris Johnson' in preds
+        assert analysis['count'] == 2
+
+    @patch("zmlp_analysis.aws.video.all.video.save_timeline", return_value={})
     @patch.object(file_storage.assets, 'store_blob')
     @patch.object(file_storage.assets, 'store_file')
     @patch('zmlp_analysis.aws.video.all.proxy.get_video_proxy')
     def test_process_person_tracking(self, get_prx_patch, store_patch, store_blob_patch, _):
-        video_path = zorroa_test_path(TED_TALK)
-        get_prx_patch.return_value = zorroa_test_path(TED_TALK)
+        video_path = zorroa_test_path(BORIS_JOHNSON)
+        get_prx_patch.return_value = zorroa_test_path(BORIS_JOHNSON)
         store_patch.return_value = get_mock_stored_file()
         store_blob_patch.return_value = get_mock_stored_file()
 
@@ -145,6 +178,11 @@ class AmazonVideoProcessorTestCase(PluginUnitTestCase):
         asset.set_attr('media.length', MEDIA_LENGTH)
         frame = Frame(self.asset)
         processor.process(frame)
+
+        analysis = frame.asset.get_analysis('aws-person-tracking-detection')
+        preds = get_prediction_labels(analysis)
+        assert 'person0' in preds
+        assert analysis['count'] >= 30
 
     @patch("zmlp_analysis.aws.video.all.video.save_timeline", return_value={})
     @patch.object(file_storage.assets, 'store_blob')
@@ -162,7 +200,12 @@ class AmazonVideoProcessorTestCase(PluginUnitTestCase):
         frame = Frame(self.asset)
         processor.process(frame)
 
-    @patch("zmlp_analysis.aws.video.all.save_timeline", return_value={})
+        analysis = frame.asset.get_analysis('aws-black-frames-detection')
+        preds = get_prediction_labels(analysis)
+        assert 'BlackFrames' in preds
+        assert analysis['count'] == 1
+
+    @patch("zmlp_analysis.aws.video.all.video.save_timeline", return_value={})
     @patch.object(file_storage.assets, 'store_blob')
     @patch.object(file_storage.assets, 'store_file')
     @patch('zmlp_analysis.aws.video.all.proxy.get_video_proxy')
@@ -177,3 +220,8 @@ class AmazonVideoProcessorTestCase(PluginUnitTestCase):
         asset.set_attr('media.length', MEDIA_LENGTH)
         frame = Frame(self.asset)
         processor.process(frame)
+
+        analysis = frame.asset.get_analysis('aws-credits-detection')
+        preds = get_prediction_labels(analysis)
+        assert 'EndCredits' in preds
+        assert analysis['count'] == 1
