e55c32ff604c89c57079c0458804893e47a9332a
Iron Araújo <iron.araujo@zorroa.com>
2021-02-05 15:09:01 -0300

    officer client with websockets

--- containers/zmlp-plugins-core/pylib/zmlp_core/office/oclient.py
+++ containers/zmlp-plugins-core/pylib/zmlp_core/office/oclient.py
@@ -3,13 +3,15 @@ import logging
 import os
 import time
 
-import backoff
 import requests
+import websocket
+import base64
 
 from zmlpsdk import ZmlpFatalProcessorException
 from zmlpsdk.storage import file_storage, ZmlpStorageException
 from zmlp.client import ZmlpJsonEncoder
 from zmlp.util import as_id
+from enum import Enum
 
 logger = logging.getLogger(__name__)
 
@@ -29,9 +31,9 @@ class OfficerClient(object):
 
         Args:
             url (str): An optional URL. Will look for the OFFICER_URL environment
-            variable and finally default to 'http://officer:7078'
+            variable and finally default to 'ws://officer:7078'
         """
-        self.url = url or os.environ.get('OFFICER_URL', 'http://officer:7078')
+        self.url = url or os.environ.get('OFFICER_URL', 'ws://officer:7078')
         self.dpi = dpi
 
     @property
@@ -44,7 +46,6 @@ class OfficerClient(object):
         """The full exists URL"""
         return '{}/exists'.format(self.url)
 
-    @backoff.on_exception(backoff.expo, requests.exceptions.HTTPError, max_time=5 * 60)
     def render(self, asset, page, disable_images=False):
         """
         Render thumbnails and metadata for the given Asset.
@@ -57,11 +58,12 @@ class OfficerClient(object):
             (str): An internal ZMLP URL where the thumbnails and metadata are located.
         """
         try:
+            conn = self._prepare_connection(self.exists_url)
             post_files = self._get_render_request_body(asset, page, disable_images)
-            rsp = requests.post(self.render_url,
-                                files=post_files)
-            rsp.raise_for_status()
-            json_response = rsp.json()
+
+            conn.send(json.dumps(post_files))
+            json_response = json.loads(conn.recv())
+            self._raise_conn_error(json_response)
 
             asset.set_attr('media.length', asset.get_attr('media.length')
                            or json_response['page-count'])
@@ -79,7 +81,6 @@ class OfficerClient(object):
                 'Storage failure {}, unable to localize asset id={} uri={}'.format(
                     ex, asset.id, asset.uri))
 
-    @backoff.on_exception(backoff.expo, requests.exceptions.HTTPError, max_time=5 * 60)
     def get_cache_location(self, asset, page):
         """
         Return the location of the cached page or None if one is not cached.
@@ -89,28 +90,31 @@ class OfficerClient(object):
             page (int): The page number.
 
         Returns:
-            bool: True if the both the metadata and proxy file exist for the page.
+            bool: Return cache location or None if it doesn't exist.
         """
+
+        conn = self._prepare_connection(self.exists_url)
         body = {
             'outputPath': self.get_full_storage_prefix(asset),
             'page': page
         }
-        rsp = requests.post(self.exists_url, json=body,
-                            headers={'Content-Type': 'application/json'})
-        if rsp.status_code == 200:
-            return rsp.json().get('location')
-        elif rsp.status_code == 404 or rsp.status_code == 410:
-            return None
-        else:
-            rsp.raise_for_status()
 
-    def get_render_status(self, asset, page):
+        conn.send(json.dumps(body))
+        rsp = json.loads(conn.recv())
+        self._raise_conn_error(rsp)
+
+        if rsp["status"] == "EXISTS":
+            return rsp['location']
+        return None
+
+    def get_render_status(self, asset, page, conn):
         """
         Return the render status of the cached page or None if one is not cached.
 
         Args:
             asset (Asset): The asset to check
             page (int): The page number.
+            conn (WebSocket): Connection already established to officer
 
         Returns:
             bool: True if the both the metadata and proxy file exist for the page.
@@ -121,11 +125,36 @@ class OfficerClient(object):
             'page': page,
             'requestId': asset.get_attr('tmp.request.id')
         }
-        rsp = requests.post(self.exists_url, json=body,
-                            headers={'Content-Type': 'application/json'})
+
+        conn.send(json.dumps(body))
+        rsp = json.loads(conn.recv())
+        self._raise_conn_error(rsp)
+
         return rsp
 
-    @backoff.on_exception(backoff.expo, requests.exceptions.HTTPError, max_time=5 * 60)
+    def _prepare_connection(self, url):
+        """
+        Prepare websocket connection
+        :param url:
+        :return:
+        """
+        return websocket.create_connection(url)
+
+    def _raise_conn_error(self, rsp):
+        """
+        Verify if websocket response contains an error
+        :param rsp:
+        :return:
+        """
+        if isinstance(rsp, str):
+            rsp = json.loads(rsp)
+        if rsp["status"] == ResponseStatus.FAIL:
+            raise ZmlpFatalProcessorException(rsp["message"])
+        if rsp["status"] == ResponseStatus.TOO_MANY_REQUESTS:
+            raise ZmlpFatalProcessorException(
+                "Officer server is overloaded: Load: {} - Max {}".format(rsp["load"], rsp["max"])
+            )
+
     def wait_for_rendering(self, asset, page):
         """
         Loop and wait until the asset page rendering accomplish
@@ -136,18 +165,18 @@ class OfficerClient(object):
         """
         number_of_retries = 20
         retry_number = 0
+        conn = self._prepare_connection(self.exists_url)
         while True:
-            rsp = self.get_render_status(asset, page)
-            if rsp.status_code == 200:
+            rsp = self.get_render_status(asset, page, conn)
+            if rsp["status"] == ResponseStatus.EXISTS:
                 logger.info("Page: {} is ready".format(page))
                 break
-            if rsp.status_code == 410:
+            if rsp["status"] == ResponseStatus.NOT_EXISTS:
                 logger.error("An error occurred. Page {} will not be rendered by officer"
                              .format(page))
                 raise ZmlpFatalProcessorException(
                     'Rendering failure, Asset: {} Page: {} will not render'.format(asset.id, page))
 
-            rsp.raise_for_status()
             logger.info('Waiting page : {} of asset: {} try number: {}'
                         .format(page, asset.id, retry_number))
             time.sleep(1)
@@ -166,7 +195,8 @@ class OfficerClient(object):
             page (int): The page number or None to render all pages.
             disable_images (bool): Disable image rendering.
         Returns:
-            list : An array which can be used for a multi-part upload.
+            dict : An dictionary containing a Base64 encoded file and a body
+            containing a rendering request
 
         """
         # localizes the asset's source file if it's not
@@ -185,11 +215,12 @@ class OfficerClient(object):
             'dpi': self.dpi
         }
 
-        # combine the file and json body into a multi-part request
-        post_files = [
-            ('file', (body['fileName'], open(file_path, 'rb'))),
-            ('body', (None, json.dumps(body, cls=ZmlpJsonEncoder), 'application/json'))
-        ]
+        # combine the file and json body into a websocket request
+        post_files = {
+            'file': base64.b64encode(open(file_path, 'rb').read()).decode("utf-8"),
+            'body': json.dumps(body, cls=ZmlpJsonEncoder)
+        }
+
         return post_files
 
     @staticmethod
@@ -255,3 +286,13 @@ class OfficerClient(object):
         """
         return OfficerClient.get_file_storage_uri(
             as_id(asset), "proxy.{}.jpg".format(max(page, 0)))
+
+
+class ResponseStatus(Enum):
+    EXISTS = 'EXISTS'
+    RENDERING = 'RENDERING'
+    NOT_EXISTS = 'NOT_EXISTS'
+    TOO_MANY_REQUESTS = 'TOO_MANY_REQUESTS'
+    FAIL = 'FAIL'
+    RENDER_QUEUE = 'RENDER_QUEUE'
+    BAD_REQUEST = 'BAD_REQUEST'
