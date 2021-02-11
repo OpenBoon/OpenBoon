fd2898273187fe74caf0349c5030a486d56b4de8
Zach Schipono <zach@zorroa.com>
2021-02-11 11:23:03 -0800

    Removes leftover commented out block, updates Url routing name

--- applications/wallet/app/wallet/urls.py
+++ applications/wallet/app/wallet/urls.py
@@ -103,7 +103,7 @@ support_admin_site.enable_nav_sidebar = False
 
 urlpatterns = [
     path('admin/', support_admin_site.urls),
-    path('admin/generate_usage_report', UsageReportView.as_view(), name='generate_usage_report'),
+    path('admin/generate_usage_report', UsageReportView.as_view(), name='generate-usage-report'),
     path('api/v1/login/', LoginView.as_view(), name='api-login'),
     path('api/v1/', include(router.urls)),
     path('api/v1/', include(projects_router.urls)),
