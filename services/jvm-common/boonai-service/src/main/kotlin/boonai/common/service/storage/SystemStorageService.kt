744db5d239d18ebb98626174b265d527382c1fff
Matt <mrc@zorroa.com>
2021-02-17 10:14:38 -0500

    Fixes JVM storage config.

--- services/jvm-common/boonai-service/src/main/kotlin/boonai/common/service/storage/SystemStorageService.kt
+++ services/jvm-common/boonai-service/src/main/kotlin/boonai/common/service/storage/SystemStorageService.kt
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@ConfigurationProperties("zmlp.storage.system")
+@ConfigurationProperties("boonai.storage.system")
 class SystemStorageProperties : StorageProperties()
 
 /**
