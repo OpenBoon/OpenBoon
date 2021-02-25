34722a8ca5c57afc7ed5973d8d4e91e17c60928e
Matt <mrc@zorroa.com>
2021-02-25 15:48:51 -0500

    Fixes aws video tests

--- containers/plugins-analysis/pylib/boonai_analysis/aws/tests/test_video.py
+++ containers/plugins-analysis/pylib/boonai_analysis/aws/tests/test_video.py
@@ -58,7 +58,7 @@ class RekognitionVideoDetectionProcessorTests(PluginUnitTestCase):
         timeline = tl_patch.call_args_list[0][0][1]
         jtl = timeline.for_json()
         assert jtl['tracks'][0]['name'] == 'Word'
-        assert jtl['tracks'][0]['clips'][0]['score'] == 0.606594352722168
+        assert jtl['tracks'][0]['clips'][0]['score'] == 0.607
 
     @patch(general_patch_path, side_effect=mock_clients)
     @patch(s3_patch_path, side_effect=MockS3Client)
@@ -91,7 +91,7 @@ class RekognitionVideoDetectionProcessorTests(PluginUnitTestCase):
         timeline = tl_patch.call_args_list[0][0][1]
         jtl = timeline.for_json()
         assert jtl['tracks'][0]['name'] == 'Revealing Clothes'
-        assert jtl['tracks'][0]['clips'][0]['score'] == 0.8383500671386719
+        assert jtl['tracks'][0]['clips'][0]['score'] == 0.838
 
     @patch(general_patch_path, side_effect=mock_clients)
     @patch(s3_patch_path, side_effect=MockS3Client)
