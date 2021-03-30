6229368adc9f53c55996a7c67e17f6d0cf749730
Zach Schipono <zach@zorroa.com>
2021-03-30 21:59:12 +0000

    Converts jobs endpoint and frontend UI to use DRF compatible "ordering" and "search" params

--- applications/wallet/frontend/src/Table/Content.js
+++ applications/wallet/frontend/src/Table/Content.js
@@ -26,12 +26,17 @@ const TableContent = ({
   refreshButton,
 }) => {
   const {
-    query: { page = 1, sort, filter = '' },
+    query: { page = 1, sort, search = '' },
   } = useRouter()
 
   const parsedPage = parseInt(page, 10)
   const from = parsedPage * SIZE - SIZE
-  const queryParam = getQueryString({ from, size: SIZE, sort, filter })
+  const queryParam = getQueryString({
+    from,
+    size: SIZE,
+    ordering: sort,
+    search,
+  })
 
   const {
     data: { count = 0, results, previous, next },
