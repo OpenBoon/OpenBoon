e117e45758cea2cf072b3edf931b2572d6cde431
Damien Varron <damien@varron.fr>
2021-03-31 13:19:19 -0700

    fixup

--- applications/wallet/frontend/src/OrganizationUserProjects/__mocks__/organizationUserProjects.js
+++ applications/wallet/frontend/src/OrganizationUserProjects/__mocks__/organizationUserProjects.js
@@ -1,16 +1,16 @@
 const organizationUserProjects = {
-  count: 3,
+  count: 2,
   next: null,
   previous: null,
   results: [
     {
       id: 'fe7eac04-a52b-46cc-8b31-40d5edeb4c62',
-      name: 'MarvelFaces',
+      name: 'Alpha',
       roles: ['ML_Tools', 'API_Keys', 'User_Admin'],
     },
     {
       id: 'a92479e5-5c68-4730-9eb7-a08901fe310b',
-      name: 'soccer',
+      name: 'Beta',
       roles: ['ML_Tools', 'API_Keys', 'User_Admin'],
     },
   ],
