dd04b87be7ec6fe4f1c4d7c21929a5e592e1d8dc
Matt <mrc@zorroa.com>
2021-04-07 16:59:34 -0400

    Added swivel server and boonhook example.

---
+++ services/swivel/tests.py
@@ -0,0 +1,55 @@
+import os
+import queue
+import threading
+import unittest
+import time
+import unittest.mock as mock
+
+import jwt
+
+import server
+
+mock_msg = {
+    'url': 'http://foobar:5000',
+    'asset_id': '12345',
+    'project_id': 'abcdefg',
+    'secret_key': 'terminator2000',
+    'trigger': 'ASSET_ANALYZED',
+    'data': b'{}'
+}
+
+
+class ServerTests(unittest.TestCase):
+    def setUp(self):
+        os.environ['GCLOUD_PROJECT'] = 'localdev'
+
+    def tearDown(self):
+        del os.environ['GCLOUD_PROJECT']
+
+    @mock.patch('server.call_webhook')
+    def test_webhook_worker(self, call_patch):
+        q = queue.Queue()
+        t = threading.Thread(target=server.webhook_worker, args=(q,))
+        t.daemon = True
+        t.start()
+
+        q.put(mock_msg)
+        while q.qsize() != 0:
+            time.sleep(1)
+
+        call_args = call_patch.call_args_list[0][0][0]
+        assert mock_msg['url'] == call_args['url']
+
+    @mock.patch('server.requests.post')
+    def test_call_webhook(self, post_patch):
+        class Response:
+            status_code = 999
+
+        post_patch.return_value = Response()
+        rsp = server.call_webhook(mock_msg)
+        assert rsp.status_code == 999
+
+    def test_genertate_token(self):
+        token = server.generate_token(mock_msg)
+        claims = jwt.decode(token, 'terminator2000', algorithms=['HS256'])
+        assert claims['asset_id'] == '12345'
