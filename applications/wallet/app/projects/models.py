66f6c4875f74d0595c45dabdfcc051ea4110d1a4
Danny Tiesling <danny@zorroa.com>
2021-04-12 18:49:36 -0700

    Fixes linging errors.

--- applications/wallet/app/projects/models.py
+++ applications/wallet/app/projects/models.py
@@ -4,14 +4,13 @@ import os
 from datetime import datetime
 
 import requests
-from boonsdk.client import BoonClient, BoonSdkNotFoundException, BoonSdkConnectionException
+from boonsdk.client import BoonClient, BoonSdkNotFoundException, \
+    BoonSdkConnectionException
 from django.conf import settings
 from django.db import models
 from django_cryptography.fields import encrypt
 from multiselectfield import MultiSelectField
 
-from apikeys.utils import create_zmlp_api_key
-from roles.utils import get_permissions_for_roles
 from wallet.mixins import TimeStampMixin, UUIDMixin, ActiveMixin
 from wallet.utils import get_zmlp_superuser_client, convert_base64_to_json, \
     sync_project_with_zmlp, sync_membership_with_zmlp
