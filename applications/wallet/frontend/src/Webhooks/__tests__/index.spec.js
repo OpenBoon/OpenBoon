1148a6d75ce94236472f4a7af2ceb75b2117f748
Damien Varron <damien@varron.fr>
2021-04-13 14:25:47 -0700

    fixup

--- applications/wallet/frontend/src/Webhooks/__tests__/index.spec.js
+++ applications/wallet/frontend/src/Webhooks/__tests__/index.spec.js
@@ -12,7 +12,7 @@ const PROJECT_ID = '76917058-b147-4556-987a-0a0f11e46d9b'
 describe('<Webhooks />', () => {
   it('should render properly with no webhook', () => {
     require('next/router').__setUseRouter({
-      pathname: '/[projectId]/api-keys',
+      pathname: '/[projectId]/webhooks',
       query: { projectId: PROJECT_ID, action: 'delete-webhook-success' },
     })
 
