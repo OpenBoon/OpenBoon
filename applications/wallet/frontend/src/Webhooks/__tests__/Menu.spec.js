fac7e7ddd281b3563d93ff1a9c2db30ae7c63cef
Damien Varron <damien@varron.fr>
2021-04-14 15:16:40 -0700

    MAIN-52: add Webhooks active toggle

--- applications/wallet/frontend/src/Webhooks/__tests__/Menu.spec.js
+++ applications/wallet/frontend/src/Webhooks/__tests__/Menu.spec.js
@@ -1,5 +1,7 @@
 import TestRenderer, { act } from 'react-test-renderer'
 
+import webhooks from '../__mocks__/webhooks'
+
 import WebhooksMenu from '../Menu'
 
 const PROJECT_ID = '76917058-b147-4556-987a-0a0f11e46d9b'
@@ -14,7 +16,7 @@ describe('<WebhooksMenu />', () => {
     const component = TestRenderer.create(
       <WebhooksMenu
         projectId={PROJECT_ID}
-        webhookId={WEBHOOK_ID}
+        webhook={webhooks.results[0]}
         revalidate={mockFn}
       />,
     )
@@ -69,6 +71,51 @@ describe('<WebhooksMenu />', () => {
         'Content-Type': 'application/json;charset=UTF-8',
       },
       method: 'DELETE',
+    })
+
+    expect(mockFn).toHaveBeenCalledWith()
+  })
+
+  it('should toggle active state', async () => {
+    const mockFn = jest.fn()
+
+    fetch.mockResponseOnce('{}')
+
+    const component = TestRenderer.create(
+      <WebhooksMenu
+        projectId={PROJECT_ID}
+        webhook={{ ...webhooks.results[0], active: false }}
+        revalidate={mockFn}
+      />,
+    )
+
+    // Open Menu
+    act(() => {
+      component.root
+        .findByProps({ 'aria-label': 'Toggle Actions Menu' })
+        .props.onClick()
+    })
+
+    expect(component.toJSON()).toMatchSnapshot()
+
+    // Select Delete
+    await act(async () => {
+      component.root.findByProps({ children: 'Activate' }).props.onClick()
+    })
+
+    expect(fetch.mock.calls.length).toEqual(1)
+
+    expect(fetch.mock.calls[0][0]).toEqual(
+      `/api/v1/projects/${PROJECT_ID}/webhooks/${WEBHOOK_ID}/`,
+    )
+
+    expect(fetch.mock.calls[0][1]).toEqual({
+      headers: {
+        'X-CSRFToken': 'CSRF_TOKEN',
+        'Content-Type': 'application/json;charset=UTF-8',
+      },
+      method: 'PUT',
+      body: JSON.stringify({ ...webhooks.results[0], active: true }),
     })
 
     expect(mockFn).toHaveBeenCalledWith()
