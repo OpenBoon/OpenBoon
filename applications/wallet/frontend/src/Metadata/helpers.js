b37c0908434eff6aef06de9171e3afa357339604
Damien Varron <damien@varron.fr>
2021-05-12 12:01:53 -0700

    WAL-1083: fix Metadata regex filter

--- applications/wallet/frontend/src/Metadata/helpers.js
+++ applications/wallet/frontend/src/Metadata/helpers.js
@@ -1,3 +1,5 @@
+import deepfilter from 'deep-filter'
+
 import { bytesToSize } from '../Bytes/helpers'
 import { formatPrettyDate } from '../Date/helpers'
 import { capitalizeFirstLetter } from '../Text/helpers'
@@ -32,7 +34,8 @@ export const formatDisplayValue = ({ name, value }) => {
 
   // https://regex101.com/r/JrKX84/1
   // Will match "2020-04-30T01:48:20" and more but no less
-  const ISO_8601 = /^\d{4}(-\d\d(-\d\d(T\d\d:\d\d(:\d\d)(\.\d+)?(([+-]\d\d:\d\d)|Z)?)))$/i
+  const ISO_8601 =
+    /^\d{4}(-\d\d(-\d\d(T\d\d:\d\d(:\d\d)(\.\d+)?(([+-]\d\d:\d\d)|Z)?)))$/i
 
   if (value.toString().match(ISO_8601)) {
     return formatPrettyDate({ timestamp: value })
@@ -43,4 +46,44 @@ export const formatDisplayValue = ({ name, value }) => {
   }
 
   return value
+}
+
+export const filter = ({ metadata, searchString }) => {
+  const filteredMetadata = deepfilter(metadata, (value, prop) => {
+    if (!searchString) return true
+
+    // Ignore first level section (analysis, files, media, etc.)
+    if (Object.keys(metadata).includes(prop)) return true
+
+    let regex
+
+    try {
+      regex = new RegExp(searchString, 'img')
+    } catch (error) {
+      regex = new RegExp('')
+    }
+
+    // Special case to filter processors
+    if (
+      typeof prop === 'number' &&
+      typeof value === 'object' &&
+      typeof value.processor === 'string'
+    ) {
+      return regex.test(value.processor)
+    }
+
+    // Filter entries that are an object because it means they
+    // are a section a.k.a. a module
+    if (
+      typeof prop === 'string' &&
+      typeof value === 'object' &&
+      !Array.isArray(value)
+    ) {
+      return regex.test(prop)
+    }
+
+    return true
+  })
+
+  return filteredMetadata
 }
