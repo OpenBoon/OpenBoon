9f2bc6e24e9dc3a5f2b866d6e4484a42dca0e92e
Matt <mrc@zorroa.com>
2021-08-04 12:49:32 -0400

    Added support for error propagation to boon functions.

--- containers/boonfunction/tests.py
+++ containers/boonfunction/tests.py
@@ -1,4 +1,6 @@
 import logging
+import sys
+import json
 
 import pytest
 
@@ -27,6 +29,24 @@ def test_function(client):
 
 
 def test_custom_error(client):
-    rsp = server.custom_error('failure', 404)
+    rsp = server.custom_error('failure')
     assert rsp.content_type == 'application/json'
-    assert rsp.status_code == 404
+    assert rsp.status_code == 511
+
+
+def test_custom_error(client):
+
+    try:
+        raise RuntimeError("test")
+    except RuntimeError:
+        rsp = server.custom_error('failure', sys.exc_info()[2])
+        assert rsp.content_type == 'application/json'
+        assert rsp.status_code == 511
+        msg = json.loads(rsp.data)
+
+        assert 'errorId' in msg
+        assert 'stackTrace' in msg
+        assert msg['code'] == 551
+        assert msg['message'] == 'failure'
+        assert msg['path'] == '/'
+        assert msg['exception'] == 'BoonFunctionException'
