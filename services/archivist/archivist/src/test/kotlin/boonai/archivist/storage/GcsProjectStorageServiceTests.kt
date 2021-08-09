61586fdcc192752febbcd6e2d195eb1ebafef0f7
Iron Araújo <iron.araujo@zorroa.com>
2021-08-09 15:58:43 -0300

    unit tests for gcs stream logs

--- services/archivist/archivist/src/test/kotlin/boonai/archivist/storage/GcsProjectStorageServiceTests.kt
+++ services/archivist/archivist/src/test/kotlin/boonai/archivist/storage/GcsProjectStorageServiceTests.kt
@@ -3,14 +3,21 @@ package boonai.archivist.storage
 import boonai.archivist.AbstractTest
 import boonai.archivist.domain.FileStorage
 import boonai.archivist.domain.ProjectFileLocator
-import boonai.archivist.domain.ProjectStorageEntity
 import boonai.archivist.domain.ProjectStorageCategory
+import boonai.archivist.domain.ProjectStorageEntity
 import boonai.archivist.domain.ProjectStorageSpec
 import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.doReturn
 import com.nhaarman.mockito_kotlin.whenever
 import org.junit.Test
 import org.mockito.ArgumentMatchers.anyString
 import org.mockito.Mock
+import org.springframework.core.io.InputStreamResource
+import org.springframework.http.CacheControl
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import java.io.BufferedReader
+import java.util.concurrent.TimeUnit
 import java.util.UUID
 import kotlin.test.assertEquals
 
@@ -57,5 +64,27 @@ class GcsProjectStorageServiceTests : AbstractTest() {
         val listOfItens = googleStorageService.listFiles("bucket-name/project-id")
 
         assert(listOfItens.isEmpty())
+    }
+
+    @Test
+    fun streamLogsTest() {
+
+        val allLogs = "Success"
+        val response = ResponseEntity.ok()
+            .contentType(MediaType.TEXT_PLAIN)
+            .contentLength(allLogs.length.toLong())
+            .cacheControl(CacheControl.maxAge(7, TimeUnit.DAYS).cachePrivate())
+            .body(InputStreamResource(allLogs.byteInputStream()))
+        doReturn(response).`when`(googleStorageService).streamLogs(any())
+
+        val loc = ProjectFileLocator(
+            ProjectStorageEntity.ASSETS,
+            "1234", ProjectStorageCategory.SOURCE, "bob.jpg", projectId = UUID.randomUUID()
+        )
+
+        val res = googleStorageService.streamLogs(loc)
+
+        assertEquals(200, res.statusCode.value())
+        assertEquals(allLogs, BufferedReader(res.body.inputStream.reader()).readLine())
     }
 }
