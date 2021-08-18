9d92e96aecd10facf6341d87215930bd4d102b21
Iron Araújo <iron.araujo@zorroa.com>
2021-08-18 18:21:34 -0300

    splitting up Exception handler

--- services/archivist/archivist/src/main/kotlin/boonai/archivist/rest/ErrorController.kt
+++ services/archivist/archivist/src/main/kotlin/boonai/archivist/rest/ErrorController.kt
@@ -16,7 +16,6 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.web.servlet.error.AbstractErrorController
 import org.springframework.boot.web.servlet.error.ErrorAttributes
 import org.springframework.boot.web.servlet.error.ErrorController
-import org.springframework.core.annotation.AnnotationUtils
 import org.springframework.dao.DataAccessException
 import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.dao.DataRetrievalFailureException
@@ -30,10 +29,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
-import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.context.request.WebRequest
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException
 import springfox.documentation.annotations.ApiIgnore
 import java.util.UUID
 import javax.servlet.http.HttpServletRequest
@@ -60,84 +57,200 @@ class RestApiExceptionHandler {
             HttpStatus.INTERNAL_SERVER_ERROR
         )
 
-    @ExceptionHandler(Exception::class)
-    fun defaultErrorHandler(wb: WebRequest, req: HttpServletRequest, e: Exception): ResponseEntity<Any> {
+    val exceptionMessages = mapOf(
+        ElasticsearchStatusException::class to "Elastic Service Server is Unavailable.",
+        EntityNotFoundException::class to "Entity was not found or is Inaccessible.",
+        ResponseException::class to "An error Occurred in the Response.",
+        DataIntegrityViolationException::class to "An conflict error occurred, check if the registry already exists.",
+        ArchivistSecurityException::class to "The resource cannot be reached with the used credentials.",
+        HttpRequestMethodNotSupportedException::class to "HTTP method not supported",
+        // known generic error
+        ArchivistException::class to "An internal error occurred. We are working on it as soon as possible.",
+        // unknown generic error
+        Exception::class to "An internal error occurred. We are working on it as soon as possible."
+    )
 
-        val annotation = AnnotationUtils.findAnnotation(e::class.java, ResponseStatus::class.java)
+    @ExceptionHandler(ElasticsearchStatusException::class)
+    fun elasticSearchExceptionHandler(
+        wb: WebRequest,
+        req: HttpServletRequest,
+        e: ElasticsearchStatusException
+    ): ResponseEntity<ErrorResponse> {
 
-        val status = if (annotation != null) {
-            annotation.value
-        } else if (e is ElasticsearchStatusException) {
-            val msg = e.message ?: ""
-            if (msg.contains("circuit_breaking_exception")) {
+        val status = e.message?.let {
+            if (it.contains("circuit_breaking_exception"))
                 HttpStatus.TOO_MANY_REQUESTS
-            } else {
-                HttpStatus.INTERNAL_SERVER_ERROR
-            }
-        } else if (e is DataRetrievalFailureException || e is EntityNotFoundException) {
-            HttpStatus.NOT_FOUND
-        } else if (e is ResponseException) {
-            HttpStatus.valueOf(e.response.statusLine.statusCode)
-        } else if (e is IncorrectResultSizeDataAccessException) {
-            // We're borrowing this http status
-            HttpStatus.METHOD_FAILURE
-        } else if (e is DataIntegrityViolationException || e is DuplicateEntityException) {
-            HttpStatus.CONFLICT
-        } else if (e is ArchivistSecurityException || e is AccessDeniedException) {
-            HttpStatus.FORBIDDEN
-        } else if (e is HttpRequestMethodNotSupportedException ||
-            e is MethodArgumentTypeMismatchException
-        ) {
-            HttpStatus.METHOD_NOT_ALLOWED
-        } else if (e is ArchivistException ||
-            e is ElasticsearchException ||
-            e is InvalidRequestException ||
-            e is DataAccessException ||
-            e is NullPointerException ||
-            e is IllegalArgumentException ||
-            e is IllegalStateException ||
-            e is NumberFormatException ||
-            e is ArrayIndexOutOfBoundsException
-        ) {
-            HttpStatus.BAD_REQUEST
-        } else {
-            HttpStatus.INTERNAL_SERVER_ERROR
-        }
+            else null
+        } ?: HttpStatus.INTERNAL_SERVER_ERROR
 
-        /**
-         * Each error gets its own random UUID for each searching in logs.
-         */
-        val errorId = UUID.randomUUID().toString()
+        val errorResponse = runInternalLoggerAndBuildResponse(status, req, e)
+        errorResponse.message = exceptionMessages[ElasticsearchStatusException::class]
+
+        return buildResponseEntity(status, errorResponse)
+    }
+
+    @ExceptionHandler(EntityNotFoundException::class, DataRetrievalFailureException::class)
+    fun dataRetrievalFailureExceptionHandler(
+        wb: WebRequest,
+        req: HttpServletRequest,
+        e: Exception
+    ): ResponseEntity<ErrorResponse> {
+        val status = HttpStatus.NOT_FOUND
+        val errorResponse = runInternalLoggerAndBuildResponse(status, req, e)
+        errorResponse.message = exceptionMessages[EntityNotFoundException::class]
+
+        return buildResponseEntity(status, errorResponse)
+    }
+
+    @ExceptionHandler(ResponseException::class)
+    fun responseExceptionHandler(
+        wb: WebRequest,
+        req: HttpServletRequest,
+        e: ResponseException
+    ): ResponseEntity<ErrorResponse> {
+        val status = HttpStatus.valueOf(e.response.statusLine.statusCode)
+
+        val errorResponse = runInternalLoggerAndBuildResponse(status, req, e)
+        errorResponse.message = exceptionMessages[ResponseException::class]
+
+        return buildResponseEntity(status, errorResponse)
+    }
+
+    @ExceptionHandler(IncorrectResultSizeDataAccessException::class)
+    fun incorrectResultSizeDataAccessExceptionHandler(
+        wb: WebRequest,
+        req: HttpServletRequest,
+        e: IncorrectResultSizeDataAccessException
+    ): ResponseEntity<ErrorResponse> {
+        val status = HttpStatus.METHOD_FAILURE
+
+        val errorResponse = runInternalLoggerAndBuildResponse(status, req, e)
+        errorResponse.message = exceptionMessages[IncorrectResultSizeDataAccessException::class]
+
+        return buildResponseEntity(status, errorResponse)
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException::class, DuplicateEntityException::class)
+    fun dataIntegrityViolationExceptionHandler(
+        wb: WebRequest,
+        req: HttpServletRequest,
+        e: Exception
+    ): ResponseEntity<ErrorResponse> {
+        val status = HttpStatus.FORBIDDEN
+
+        val errorResponse = runInternalLoggerAndBuildResponse(status, req, e)
+        errorResponse.message = exceptionMessages[DataIntegrityViolationException::class]
+
+        return buildResponseEntity(status, errorResponse)
+    }
+
+    @ExceptionHandler(ArchivistSecurityException::class, AccessDeniedException::class)
+    fun archivistSecurityExceptionHandler(
+        wb: WebRequest,
+        req: HttpServletRequest,
+        e: Exception
+    ): ResponseEntity<ErrorResponse> {
+        val status = HttpStatus.METHOD_FAILURE
+
+        val errorResponse = runInternalLoggerAndBuildResponse(status, req, e)
+        errorResponse.message = exceptionMessages[ArchivistSecurityException::class]
+
+        return buildResponseEntity(status, errorResponse)
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException::class)
+    fun httpRequestMethodNotSupportedExceptionHandler(
+        wb: WebRequest,
+        req: HttpServletRequest,
+        e: HttpRequestMethodNotSupportedException
+    ): ResponseEntity<ErrorResponse> {
+        val status = HttpStatus.METHOD_NOT_ALLOWED
+
+        val errorResponse = runInternalLoggerAndBuildResponse(status, req, e)
+        errorResponse.message = exceptionMessages[HttpRequestMethodNotSupportedException::class]
+
+        return buildResponseEntity(status, errorResponse)
+    }
+
+    @ExceptionHandler(
+        ArchivistException::class,
+        ElasticsearchException::class,
+        InvalidRequestException::class,
+        DataAccessException::class,
+        NullPointerException::class,
+        IllegalArgumentException::class,
+        IllegalStateException::class,
+        NumberFormatException::class,
+        ArrayIndexOutOfBoundsException::class
+    )
+    fun genericKnownErrorExceptionHandler(
+        wb: WebRequest,
+        req: HttpServletRequest,
+        e: Exception
+    ): ResponseEntity<ErrorResponse> {
+        val status = HttpStatus.BAD_REQUEST
+
+        val errorResponse = runInternalLoggerAndBuildResponse(status, req, e)
+        errorResponse.message = exceptionMessages[ArchivistException::class]
+
+        return buildResponseEntity(status, errorResponse)
+    }
+
+    @ExceptionHandler(Exception::class)
+    fun genericUnknownExceptionHandler(
+        wb: WebRequest,
+        req: HttpServletRequest,
+        e: HttpRequestMethodNotSupportedException
+    ): ResponseEntity<ErrorResponse> {
+        val status = HttpStatus.INTERNAL_SERVER_ERROR
+
+        val errorResponse = runInternalLoggerAndBuildResponse(status, req, e)
+        errorResponse.message = exceptionMessages[Exception::class]
+
+        return buildResponseEntity(status, errorResponse)
+    }
+
+    private fun buildResponseEntity(
+        status: HttpStatus,
+        errorResponse: ErrorResponse
+    ) = ResponseEntity.status(status)
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(errorResponse)
+
+    private fun runInternalLoggerAndBuildResponse(status: HttpStatus, req: HttpServletRequest, e: Exception): ErrorResponse {
+
+        val errorResponse = ErrorResponse(UUID.randomUUID().toString(), status.value())
 
         if (doExtraLogging.contains(status) || debug) {
             logger.error(
                 "endpoint='{}' project='{}', errorId='{}', status='{}'",
-                req.servletPath, getZmlpActorOrNull()?.projectId, errorId, status.value(), e
+                req.servletPath, getZmlpActorOrNull()?.projectId, errorResponse.errorId, status.value(), e
             )
         } else {
             logger.error(
                 "endpoint='{}' project='{}', errorId='{}', status='{}'",
-                req.servletPath, getZmlpActorOrNull()?.projectId, status.value(), errorId
+                req.servletPath, getZmlpActorOrNull()?.projectId, status.value(), errorResponse.errorId
             )
         }
 
-        val errAttrs = errorAttributes.getErrorAttributes(wb, debug)
-        errAttrs["errorId"] = errorId
-        errAttrs["status"] = status.value()
-
         if (!debug && req.getAttribute("authType") != HttpServletRequest.CLIENT_CERT_AUTH) {
-            errAttrs["message"] = "Please refer to errorId='$errorId' for actual message"
+            errorResponse.genericMessage = "Please refer to errorId='${errorResponse.errorId}' for actual message"
         }
 
-        return ResponseEntity.status(status)
-            .contentType(MediaType.APPLICATION_JSON)
-            .body(errAttrs)
+        return errorResponse
     }
 
     companion object {
         private val logger = LoggerFactory.getLogger(RestApiExceptionHandler::class.java)
     }
 }
+
+data class ErrorResponse(
+    val errorId: String,
+    val status: Int,
+    var message: String? = null,
+    var genericMessage: String? = null
+)
 
 @RestController
 @RequestMapping("/error")
