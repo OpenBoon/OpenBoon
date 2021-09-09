38c08b8b4defbf343723147c0582540d17c97cb5
Matt <mrc@zorroa.com>
2021-09-09 15:24:38 -0400

    Fixes for broken gitlab tests

--- client/boonsdk/pylib/boonsdk/app/tests/test_asset_app.py
+++ client/boonsdk/pylib/boonsdk/app/tests/test_asset_app.py
@@ -239,7 +239,7 @@ class AssetAppTests(unittest.TestCase):
         rsp = self.app.assets.search(search=search, filters=filt)
         path = rsp.raw_response['hits']['hits'][0]['_source']['source']['path']
         assert path == 'https://i.imgur.com/SSN26nN.jpg'
-        assert 'training_set' in post_patch.call_args[0][1]
+        assert 'dataset' in post_patch.call_args[0][1]
 
     @patch.object(BoonClient, 'post')
     def test_search_iter(self, post_patch):
