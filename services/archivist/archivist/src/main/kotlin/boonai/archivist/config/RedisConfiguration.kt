8cd611867886942b746d7af1cd73034844218306
Iron Araújo <iron.araujo@zorroa.com>
2021-09-17 18:32:04 -0300

    ktlint

--- services/archivist/archivist/src/main/kotlin/boonai/archivist/config/RedisConfiguration.kt
+++ services/archivist/archivist/src/main/kotlin/boonai/archivist/config/RedisConfiguration.kt
@@ -1,7 +1,6 @@
 package boonai.archivist.config
 
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration
